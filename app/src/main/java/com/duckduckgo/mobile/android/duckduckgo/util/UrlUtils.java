6c7dcd27554e81d6704179ee8d5699745da51e5e
fgei <gei.fff@gmail.com>
2017-06-07 10:50:37 +0200

    Improve url handling:
    - Use same regex as iOS so we can have the same result.
    - Fix in AppUrls.getQuery to parse  the getRawQuery rather then getQuery so we can parse encoded characters and decode only the parameter  we need

--- app/src/main/java/com/duckduckgo/mobile/android/duckduckgo/util/UrlUtils.java
+++ app/src/main/java/com/duckduckgo/mobile/android/duckduckgo/util/UrlUtils.java
@@ -1,7 +1,10 @@
 package com.duckduckgo.mobile.android.duckduckgo.util;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import java.util.regex.Pattern;
 
 /**
  * Created by fgei on 5/23/17.
@@ -9,9 +12,11 @@ import android.support.annotation.Nullable;
 
 public class UrlUtils {
 
+    private static final String WEB_URL_REGEX = "^(https?:\\/\\/)?([\\da-z\\.-]+\\.[a-z\\.]{2,6}|(([\\d]+[.]){3}[\\d]+))([\\/:?=&#]{1}[\\da-z\\.-]+)*[\\/\\?]?$";
+
     public static boolean isUrl(@Nullable String url) {
         if (url == null) return false;
-        return url.contains(".") && !url.contains(" ");
+        return Pattern.compile(WEB_URL_REGEX).matcher(url).matches();
     }
 
     public static String getUrlWithScheme(@NonNull String url) {
