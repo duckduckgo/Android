cce18c6e6fddba5e755d4c6904523d78c8bf0726
fgei <gei.fff@gmail.com>
2017-06-26 23:19:44 +0200

    Switch from file template to copyright.

--- app/src/main/java/com/duckduckgo/mobile/android/duckduckgo/ui/omnibar/Omnibar.java
+++ app/src/main/java/com/duckduckgo/mobile/android/duckduckgo/ui/omnibar/Omnibar.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.duckduckgo.mobile.android.duckduckgo.ui.omnibar;
 
 import android.content.Context;
@@ -25,21 +41,6 @@ import com.duckduckgo.mobile.android.duckduckgo.util.KeyboardUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-/**
- *    Copyright 2017 DuckDuckGo
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
 
 public class Omnibar extends AppBarLayout implements OmnibarView {
 
