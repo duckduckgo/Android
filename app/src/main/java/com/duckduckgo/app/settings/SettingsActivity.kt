31ec4014dd40f89d375dc0bce459b94e73b27123
Craig Russell <craig.russell.1986@gmail.com>
2018-01-09 09:19:34 +0000

    Simplify `onActivityResult` logic

--- app/src/main/java/com/duckduckgo/app/settings/SettingsActivity.kt
+++ app/src/main/java/com/duckduckgo/app/settings/SettingsActivity.kt
@@ -22,6 +22,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.duckduckgo.app.about.AboutDuckDuckGoActivity
+import com.duckduckgo.app.about.AboutDuckDuckGoActivity.Companion.RESULT_CODE_LOAD_ABOUT_DDG_WEB_PAGE
 import com.duckduckgo.app.browser.R
 import com.duckduckgo.app.global.DuckDuckGoActivity
 import com.duckduckgo.app.global.ViewModelFactory
@@ -66,13 +67,9 @@ class SettingsActivity : DuckDuckGoActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when(requestCode) {
-            REQUEST_CODE_ABOUT_DDG -> {
-                if(resultCode == AboutDuckDuckGoActivity.RESULT_CODE_LOAD_ABOUT_DDG_WEB_PAGE ) {
-                    setResult(resultCode)
-                    finish()
-                }
-            }
+        if(requestCode == REQUEST_CODE_ABOUT_DDG && resultCode == RESULT_CODE_LOAD_ABOUT_DDG_WEB_PAGE) {
+            setResult(resultCode)
+            finish()
         }
     }
 
