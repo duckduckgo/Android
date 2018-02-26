aa05e11dc18a7a17e4780a9d7c72f8b530a7a54a
Craig Russell <craig.russell.1986@gmail.com>
2018-02-26 12:41:47 +0000

    Remove unused function

--- app/src/main/java/com/duckduckgo/app/browser/WebViewRequestInterceptor.kt
+++ app/src/main/java/com/duckduckgo/app/browser/WebViewRequestInterceptor.kt
@@ -89,12 +89,6 @@ class WebViewRequestInterceptor @Inject constructor(
         return null
     }
 
-    private fun shouldStripRequestedWithHeader(request: WebResourceRequest): Boolean {
-        if(!request.isForMainFrame) return false
-        val requestedWithHeader = request.requestHeaders["X-Requested-With"] ?: request.requestHeaders["x-requested-with"]
-        return requestedWithHeader != ""
-    }
-
     private fun shouldUpgrade(request: WebResourceRequest) =
             request.isForMainFrame && request.url != null && httpsUpgrader.shouldUpgrade(request.url)
 
