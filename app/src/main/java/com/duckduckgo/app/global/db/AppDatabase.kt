6ee7d61045c72844064657477360bd31428df52e
Chris Brind <brindy@duckduckgo.com>
2018-08-23 14:36:20 +0100

    default existing tabs to viewed

--- app/src/main/java/com/duckduckgo/app/global/db/AppDatabase.kt
+++ app/src/main/java/com/duckduckgo/app/global/db/AppDatabase.kt
@@ -84,7 +84,7 @@ abstract class AppDatabase : RoomDatabase() {
 
         val MIGRATION_4_TO_5: Migration = object : Migration(4, 5) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE `tabs` ADD COLUMN `viewed` INTEGER NOT NULL DEFAULT 0")
+                database.execSQL("ALTER TABLE `tabs` ADD COLUMN `viewed` INTEGER NOT NULL DEFAULT 1")
                 database.execSQL("ALTER TABLE `tabs` ADD COLUMN `position` INTEGER NOT NULL DEFAULT 0")
 
                 database.query("SELECT `tabId` from `tabs`").use {
