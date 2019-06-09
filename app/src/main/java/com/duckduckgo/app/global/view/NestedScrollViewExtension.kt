b37abc970c22a7758395dec54729bc76245d867d
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2019-06-09 19:33:53 +0100

    duckduckgo#494 remove no longer needed max

--- app/src/main/java/com/duckduckgo/app/global/view/NestedScrollViewExtension.kt
+++ app/src/main/java/com/duckduckgo/app/global/view/NestedScrollViewExtension.kt
@@ -19,7 +19,6 @@ package com.duckduckgo.app.global.view
 import androidx.core.view.get
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.RecyclerView
-import kotlin.math.max
 import kotlin.math.min
 
 /**
@@ -30,7 +29,7 @@ import kotlin.math.min
 fun NestedScrollView.smoothScrollTo(position: Int, recyclerView: RecyclerView) {
     post {
         if (recyclerView.childCount > 0) {
-            val currentItemY = recyclerView[min(position, max(recyclerView.childCount - 1, 0))].y.toInt()
+            val currentItemY = recyclerView[min(position, recyclerView.childCount - 1)].y.toInt()
             smoothScrollTo(0, currentItemY)
         }
     }
