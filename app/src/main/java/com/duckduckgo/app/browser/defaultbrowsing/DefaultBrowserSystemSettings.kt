0947e66df5ff0bfd4258ccea0c21e72536fd3486
Craig Russell <CDRussell@users.noreply.github.com>
2019-06-12 00:58:09 +0100

    Add intent extras so that default browser system setting is highligted (#500)

--- app/src/main/java/com/duckduckgo/app/browser/defaultbrowsing/DefaultBrowserSystemSettings.kt
+++ app/src/main/java/com/duckduckgo/app/browser/defaultbrowsing/DefaultBrowserSystemSettings.kt
@@ -20,12 +20,22 @@ import android.content.Intent
 import android.os.Build
 import android.provider.Settings
 import androidx.annotation.RequiresApi
+import androidx.core.os.bundleOf
 
 
 class DefaultBrowserSystemSettings {
 
     companion object {
         @RequiresApi(Build.VERSION_CODES.N)
-        fun intent() = Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
+        fun intent(): Intent {
+            val intent = Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS)
+            intent.putExtra(SETTINGS_SELECT_OPTION_KEY, DEFAULT_BROWSER_APP_OPTION)
+            intent.putExtra(SETTINGS_SHOW_FRAGMENT_ARGS, bundleOf(SETTINGS_SELECT_OPTION_KEY to DEFAULT_BROWSER_APP_OPTION))
+            return intent
+        }
+
+        private const val SETTINGS_SELECT_OPTION_KEY = ":settings:fragment_args_key"
+        private const val SETTINGS_SHOW_FRAGMENT_ARGS = ":settings:show_fragment_args"
+        private const val DEFAULT_BROWSER_APP_OPTION = "default_browser"
     }
 }
