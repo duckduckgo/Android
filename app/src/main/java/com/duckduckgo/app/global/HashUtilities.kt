803b883a89499205f61dab0bd96bd8a4dee62534
Mia Alexiou <mia@duckduckgo.com>
2019-08-30 22:11:45 +0100

    Remove commons codec from project

--- app/src/main/java/com/duckduckgo/app/global/HashUtilities.kt
+++ app/src/main/java/com/duckduckgo/app/global/HashUtilities.kt
@@ -16,18 +16,25 @@
 
 package com.duckduckgo.app.global
 
-import org.apache.commons.codec.binary.Hex
-import org.apache.commons.codec.digest.DigestUtils
+import java.math.BigInteger
+import java.security.MessageDigest
 
 val ByteArray.sha256: String
-    get() = String(Hex.encodeHex(DigestUtils.sha256(this)))
+    get() = sha("SHA-256", this)
+
 
 fun ByteArray.verifySha256(sha256: String): Boolean {
     return this.sha256 == sha256
 }
 
 val String.sha1: String
-    get() = String(Hex.encodeHex(DigestUtils.sha1(this)))
+    get() = sha("SHA-1", this.toByteArray())
+
+private fun sha(algorithm: String, bytes: ByteArray): String {
+    val md = MessageDigest.getInstance(algorithm)
+    val digest = md.digest(bytes)
+    return String.format("%0" + digest.size * 2 + "x", BigInteger(1, digest))
+}
 
 fun String.verifySha1(sha1: String): Boolean {
     return this.sha1 == sha1
