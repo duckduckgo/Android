b24de69e478bbabc1b24f224ec97af68f1005ece
Craig Russell <CDRussell@users.noreply.github.com>
2019-09-02 16:42:39 +0100

    Change order of if statement for better performance

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -502,7 +502,7 @@ class BrowserTabViewModel(
         val progress = currentLoadingViewState()
         loadingViewState.value = progress.copy(isLoading = isLoading, progress = newProgress)
 
-        if (variantManager.getVariant().hasFeature(TabSwitcherGrid) && !isLoading) {
+        if (!isLoading && variantManager.getVariant().hasFeature(TabSwitcherGrid)) {
             updateOrDeleteWebViewPreview(forceImmediate = false)
         }
     }
