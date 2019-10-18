049bc3e6c11e1194df66c85de8e1a09e3fcb0dc6
Jeff G <21976019+JeffAment@users.noreply.github.com>
2019-10-18 06:02:32 -0400

    Add long click opens new tab functionality to tab switcher button (#608)

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -244,6 +244,7 @@ class BrowserTabFragment : Fragment(), FindListener, CoroutineScope {
         configureAutoComplete()
         configureKeyboardAwareLogoAnimation()
         configureShowTabSwitcherListener()
+        configureLongClickOpensNewTabListener()
 
         if (savedInstanceState == null) {
             viewModel.onViewReady()
@@ -278,6 +279,13 @@ class BrowserTabFragment : Fragment(), FindListener, CoroutineScope {
     private fun configureShowTabSwitcherListener() {
         tabsButton?.actionView?.setOnClickListener {
             launch { viewModel.userLaunchingTabSwitcher() }
+        }
+    }
+
+    private fun configureLongClickOpensNewTabListener() {
+        tabsButton?.actionView?.setOnLongClickListener {
+            launch { viewModel.userRequestedOpeningNewTab() }
+            return@setOnLongClickListener true
         }
     }
 
