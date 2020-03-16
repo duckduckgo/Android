8b040055e352d823955f46645a8d7f255d3d74fb
David González <malmstein@gmail.com>
2020-03-16 09:45:04 +0100

    these values are not used and should not be hardcoded

--- app/src/main/java/com/duckduckgo/app/notification/model/StickySearchNotification.kt
+++ app/src/main/java/com/duckduckgo/app/notification/model/StickySearchNotification.kt
@@ -65,8 +65,8 @@ class StickySearchNotificationSpecification(context: Context) : NotificationSpec
     override val icon = R.drawable.notification_logo
     override val launchButton: String? = null
     override val closeButton: String? = null
-    override val title: String = "Start a Search"
-    override val description: String = "You can now search from anywhere!"
+    override val title: String = ""
+    override val description: String = ""
     override val pixelSuffix: String = ""
     override val autoCancel = false
 }
