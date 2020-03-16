cf078fa42a22b01806c3759854bde29900874e9f
David González <malmstein@gmail.com>
2020-03-16 10:48:14 +0100

    added proper icons to add / remove

--- app/src/main/java/com/duckduckgo/app/notification/NotificationFactory.kt
+++ app/src/main/java/com/duckduckgo/app/notification/NotificationFactory.kt
@@ -76,8 +76,8 @@ class NotificationFactory @Inject constructor(val context: Context, val manager:
             .setVisibility(NotificationCompat.VISIBILITY_SECRET)
             .setAutoCancel(false)
             .setOnlyAlertOnce(true)
-            .addAction(specification.icon, specification.launchButton, launchIntent)
-            .addAction(specification.icon, specification.closeButton, cancelIntent)
+            .addAction(R.drawable.ic_add_24dp, specification.launchButton, launchIntent)
+            .addAction(R.drawable.ic_close_24dp, specification.closeButton, cancelIntent)
         return builder.build()
     }
 
