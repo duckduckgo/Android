52a88e3e161c99b2ebf9d3ca1ba7996580d345ff
David González <malmstein@gmail.com>
2020-03-20 08:48:59 +0100

    added simple bottom navigation bar

---
+++ app/src/main/java/com/duckduckgo/app/browser/BottomNavigationBar.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.browser
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.view_tab_switcher_button.view.*
+
+class BottomNavigationBar(context: Context) : BottomNavigationView(context) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.layout_bottom_navigation_bar, this, true)
+    }
+
+}
