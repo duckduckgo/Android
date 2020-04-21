ab4b723c8a74a44e31afb91f087cf9f8e8e125c6
Craig Russell <CDRussell@users.noreply.github.com>
2020-04-21 15:18:45 +0100

    Ensure NDK version installed

--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,7 @@
 mkdir "$ANDROID_HOME/licenses" || true;
 echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license";
 echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license";
+sdkmanager 'ndk;21.0.6113669'
 sdkmanager tools;
 yes | sdkmanager --licenses;
 echo "Fetching ndk-bundle. Suppressing output to avoid travis 4MG size limit";
