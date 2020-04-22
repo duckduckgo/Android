ababb6cfd9a4a0ca6dd2f8484eea982b44549f9d
David González <malmstein@gmail.com>
2020-04-22 15:06:29 +0200

    deactivate serp experiments

--- app/src/androidTest/java/com/duckduckgo/app/statistics/VariantManagerTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/statistics/VariantManagerTest.kt
@@ -32,14 +32,14 @@ class VariantManagerTest {
     @Test
     fun serpControlVariantIsInactiveAndHasNoFeatures() {
         val variant = variants.first { it.key == "sc" }
-        assertEqualsDouble(1.0, variant.weight)
+        assertEqualsDouble(0.0, variant.weight)
         assertEquals(0, variant.features.size)
     }
 
     @Test
     fun serpExperimentalVariantIsInactiveAndHasNoFeatures() {
         val variant = variants.first { it.key == "se" }
-        assertEqualsDouble(1.0, variant.weight)
+        assertEqualsDouble(0.0, variant.weight)
         assertEquals(0, variant.features.size)
     }
 
