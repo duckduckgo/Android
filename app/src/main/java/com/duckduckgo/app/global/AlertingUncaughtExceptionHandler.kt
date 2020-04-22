171df35c41f4f0ad488c6e2fe4ee75ce64c259cb
Craig Russell <CDRussell@users.noreply.github.com>
2020-04-22 17:30:36 +0100

    InterruptedException and IOException now excluded exceptions to alert on

--- app/src/main/java/com/duckduckgo/app/global/AlertingUncaughtExceptionHandler.kt
+++ app/src/main/java/com/duckduckgo/app/global/AlertingUncaughtExceptionHandler.kt
@@ -19,10 +19,13 @@ package com.duckduckgo.app.global
 import com.duckduckgo.app.global.exception.UncaughtExceptionRepository
 import com.duckduckgo.app.global.exception.UncaughtExceptionSource
 import com.duckduckgo.app.statistics.store.OfflinePixelCountDataStore
+import io.reactivex.exceptions.UndeliverableException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.io.IOException
 
 class AlertingUncaughtExceptionHandler(
     private val originalHandler: Thread.UncaughtExceptionHandler,
@@ -32,12 +35,30 @@ class AlertingUncaughtExceptionHandler(
 
     override fun uncaughtException(t: Thread?, originalException: Throwable?) {
 
+        if (shouldIgnore(originalException)) {
+            Timber.w(originalException, "An exception occurred but we don't need to handle it")
+            return
+        }
+
         GlobalScope.launch(Dispatchers.IO + NonCancellable) {
             uncaughtExceptionRepository.recordUncaughtException(originalException, UncaughtExceptionSource.GLOBAL)
             offlinePixelCountDataStore.applicationCrashCount += 1
 
             // wait until the exception has been fully processed before propagating exception
             originalHandler.uncaughtException(t, originalException)
+        }
+    }
+
+    /**
+     * Some exceptions happen due to lifecycle issues, such as our sync service being forced to stop.
+     * In such cases, we don't need to alert about the exception as they are exceptions essentially signalling that work was interrupted.
+     * Examples of this would be if the internet was lost during the sync,
+     * or when two or more sync operations are scheduled to run at the same time; one would run and the rest would be interrupted.
+     */
+    private fun shouldIgnore(exception: Throwable?): Boolean {
+        return when (exception) {
+            is UndeliverableException, is InterruptedException, is IOException -> true
+            else -> false
         }
     }
 }
