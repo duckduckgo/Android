bed3a0ae8100380602ef027248e8291e12f58b84
David Gonzalez <malmstein@gmail.com>
2020-04-27 21:44:31 +0200

    we want to separate the responsibilities of clearing old jobs and scheduling new ones

---
+++ app/src/androidTest/java/com/duckduckgo/app/job/JobCleanerTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.job
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.duckduckgo.app.job.JobCleaner.Companion.allDeprecatedNotificationWorkTags
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class JobCleanerTest {
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private var workManager = WorkManager.getInstance(context)
+    private lateinit var testee: JobCleaner
+
+    @Before
+    fun before() {
+        testee = AndroidJobCleaner(workManager)
+    }
+
+    @Test
+    fun allDeprecatedWorkIsCancelledUponStart() {
+        testee.cleanDeprecatedJobs()
+
+        allDeprecatedNotificationWorkTags().forEach {
+            assertTrue(getScheduledWorkers(it).isEmpty())
+        }
+    }
+
+    private fun getScheduledWorkers(tag: String): List<WorkInfo> {
+        return workManager
+            .getWorkInfosByTag(tag)
+            .get()
+            .filter { it.state == WorkInfo.State.ENQUEUED }
+    }
+}
---
+++ app/src/main/java/com/duckduckgo/app/job/JobCleaner.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.job
+
+import androidx.work.WorkManager
+import com.duckduckgo.app.job.JobCleaner.Companion.allDeprecatedNotificationWorkTags
+
+interface JobCleaner {
+    fun cleanDeprecatedJobs()
+
+    companion object {
+        // below there is a list of TAGs that were used at some point but that are no longer active
+        // we want to make sure that this TAGs are cancelled to avoid inconsistencies
+        private const val CONTINUOUS_APP_USE_REQUEST_TAG = "com.duckduckgo.notification.schedule.continuous" // Sticky Search Experiment
+
+        fun allDeprecatedNotificationWorkTags() = listOf(CONTINUOUS_APP_USE_REQUEST_TAG)
+    }
+}
+
+class AndroidJobCleaner(private val workManager: WorkManager) : JobCleaner {
+
+    override fun cleanDeprecatedJobs() {
+        allDeprecatedNotificationWorkTags().forEach { tag ->
+            workManager.cancelAllWorkByTag(tag)
+        }
+    }
+}
