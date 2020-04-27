2976bca02a3a944e184ec22afc90482c8da7d929
David Gonzalez <malmstein@gmail.com>
2020-04-27 15:03:50 +0200

    fix merge issue

--- app/src/main/java/com/duckduckgo/app/statistics/VariantManager.kt
+++ app/src/main/java/com/duckduckgo/app/statistics/VariantManager.kt
@@ -41,7 +41,7 @@ interface VariantManager {
             // SERP variants. "sc" may also be used as a shared control for mobile experiments in
             // the future if we can filter by app version
             Variant(key = "sc", weight = 0.0, features = emptyList(), filterBy = { noFilter() }),
-            Variant(key = "se", weight = 0.0, features = emptyList(), filterBy = { noFilter() }),
+            Variant(key = "se", weight = 0.0, features = emptyList(), filterBy = { noFilter() })
 
             // All groups in an experiment (control and variants) MUST use the same filters
         )
