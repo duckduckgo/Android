5768900cddb548c16b5a35910696cdf031a77b91
Cristian Monforte <cristian@duckduckgo.com>
2020-06-07 01:18:13 +0200

    test LoginDetectionInterface

--- app/src/androidTest/java/com/duckduckgo/app/browser/logindetection/LoginDetectionInterfaceTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/browser/logindetection/LoginDetectionInterfaceTest.kt
@@ -16,22 +16,19 @@
 
 package com.duckduckgo.app.browser.logindetection
 
-import android.webkit.JavascriptInterface
-import timber.log.Timber
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
 
-// Interface name used inside login_form_detection.js
-const val LOGIN_DETECTION_INTERFACE_NAME = "LoginDetection"
+class LoginDetectionInterfaceTest {
 
-@Suppress("unused")
-class LoginDetectionInterface(private val onLoginDetected: () -> Unit) {
+    @Test
+    fun whenLoginDetectedThenCallback() {
+        val loginDetected = mock<() -> Unit>()
+        val loginDetectionInterface = LoginDetectionInterface(loginDetected)
 
-    @JavascriptInterface
-    fun log(message: String) {
-        Timber.i("LoginDetectionInterface $message")
-    }
+        loginDetectionInterface.loginDetected()
 
-    @JavascriptInterface
-    fun loginDetected() {
-        onLoginDetected()
+        verify(loginDetected).invoke()
     }
 }
