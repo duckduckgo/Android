e31e110b47835365253a2a00193a9f15c3548f7c
David Gonzalez <malmstein@gmail.com>
2021-05-27 13:45:21 +0200

    missing spotless check

--- vpn/src/main/java/com/duckduckgo/mobile/android/vpn/ui/onboarding/DeviceShieldOnboardingActivity.kt
+++ vpn/src/main/java/com/duckduckgo/mobile/android/vpn/ui/onboarding/DeviceShieldOnboardingActivity.kt
@@ -22,7 +22,6 @@ import android.net.VpnService
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.CompoundButton
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
