cc46390c16689e0162e9baffa9c4043a157571b2
David Gonzalez <malmstein@gmail.com>
2021-09-01 15:34:38 +0200

    added first journey test

---
+++ app/src/androidTest/java/com/duckduckgo/espresso/BasicJourneyTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2021 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.espresso
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.filters.LargeTest
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.duckduckgo.app.browser.BrowserActivity
+import com.duckduckgo.app.browser.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class BasicJourneyTest {
+
+    /**
+     * Use [ActivityScenarioRule] to create and launch the activity under test before each test,
+     * and close it after each test. This is a replacement for
+     * [androidx.test.rule.ActivityTestRule].
+     */
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<BrowserActivity>()
+
+    @Test
+    fun browser_openPopUp() {
+        // since we use a fake toolbar, we want to wait until the real one is visible
+        onView(isRoot()).perform(waitForView(withId(R.id.browserMenu)))
+
+        // tap on PopupMenu
+        onView(withId(R.id.browserMenu)).perform(click())
+
+        // check that the forward arrow is visible
+        onView(withId(R.id.forwardPopupMenuItem)).check(matches(isDisplayed()))
+    }
+
+}
