fc25820bb90033de737f0f9013011a00b8615398
David Gonzalez <malmstein@gmail.com>
2021-09-02 14:31:54 +0200

    added @FlakyTest annotation for DaxDialogs

--- app/src/androidTest/java/com/duckduckgo/espresso/DaxDialogsJourneyTest.kt
+++ app/src/androidTest/java/com/duckduckgo/espresso/DaxDialogsJourneyTest.kt
@@ -22,6 +22,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.FlakyTest
 import com.duckduckgo.app.browser.R
 import com.duckduckgo.app.onboarding.ui.OnboardingActivity
 import org.junit.Rule
@@ -29,6 +30,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@FlakyTest
 class DaxDialogsJourneyTest {
 
     /**
