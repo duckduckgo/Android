bcebba319d9eb7232c7f0c72296da6e2d1061fe8
David Gonzalez <malmstein@gmail.com>
2021-09-02 14:02:54 +0200

    cleaning up the tests

--- app/src/androidTest/java/com/duckduckgo/espresso/BasicJourneyTest.kt
+++ app/src/androidTest/java/com/duckduckgo/espresso/BasicJourneyTest.kt
@@ -30,7 +30,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 class BasicJourneyTest {
 
     /**
--- app/src/androidTest/java/com/duckduckgo/espresso/DaxDialogsJourneyTest.kt
+++ app/src/androidTest/java/com/duckduckgo/espresso/DaxDialogsJourneyTest.kt
@@ -31,7 +31,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 class DaxDialogsJourneyTest {
 
     /**
