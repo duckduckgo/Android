3f4f4921a437b6b079851ef1d8d45329af83859b
Cristian Monforte <cristian@duckduckgo.com>
2021-09-28 12:40:45 +0200

    accessibility screen - configure slider

--- app/src/main/java/com/duckduckgo/app/accessibility/AccessibilityActivity.kt
+++ app/src/main/java/com/duckduckgo/app/accessibility/AccessibilityActivity.kt
@@ -22,6 +22,8 @@ import android.os.Bundle
 import com.duckduckgo.app.browser.databinding.ActivityAccessibilitySettingsBinding
 import com.duckduckgo.app.global.DuckDuckGoActivity
 import com.duckduckgo.mobile.android.ui.viewbinding.viewBinding
+import timber.log.Timber
+import java.text.NumberFormat
 
 class AccessibilityActivity : DuckDuckGoActivity() {
 
@@ -34,6 +36,18 @@ class AccessibilityActivity : DuckDuckGoActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setupToolbar(toolbar)
+        configureListener()
+    }
+
+    private fun configureListener() {
+        binding.accessibilitySlider.value = 100f
+        binding.accessibilitySlider.addOnChangeListener { _, value, _ ->
+            val newValue = value / 100
+            val formatter = NumberFormat.getPercentInstance()
+            Timber.i("accessibilitySlider slider $value")
+            binding.accessibilitySliderValue.text = formatter.format(newValue)
+            binding.accessibilityHint.textSize = 16 * newValue
+        }
     }
 
     companion object {
