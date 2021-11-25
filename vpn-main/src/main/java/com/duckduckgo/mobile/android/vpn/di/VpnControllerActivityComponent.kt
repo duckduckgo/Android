89c2440f7c115dbea953fdade651113f82db23f4
Aitor Viana <aitorvs@gmail.com>
2021-11-25 10:04:22 +0000

    Break down VpnComponent

---
+++ vpn-main/src/main/java/com/duckduckgo/mobile/android/vpn/di/VpnControllerActivityComponent.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.mobile.android.vpn.di
+
+import com.duckduckgo.di.scopes.ActivityObjectGraph
+import com.duckduckgo.di.scopes.AppObjectGraph
+import com.squareup.anvil.annotations.ContributesTo
+import com.squareup.anvil.annotations.MergeSubcomponent
+import dagger.Binds
+import dagger.Module
+import dagger.SingleIn
+import dagger.Subcomponent
+import dagger.android.AndroidInjector
+import dagger.multibindings.ClassKey
+import dagger.multibindings.IntoMap
+import dummy.ui.VpnControllerActivity
+
+@SingleIn(ActivityObjectGraph::class)
+@MergeSubcomponent(
+    scope = ActivityObjectGraph::class
+)
+interface VpnControllerActivityComponent : AndroidInjector<VpnControllerActivity> {
+    @Subcomponent.Factory
+    interface Factory : AndroidInjector.Factory<VpnControllerActivity>
+}
+
+@ContributesTo(AppObjectGraph::class)
+interface VpnControllerActivityComponentProvider {
+    fun vpnControllerActivityComponentFactory(): VpnControllerActivityComponent.Factory
+}
+
+@Module
+@ContributesTo(AppObjectGraph::class)
+abstract class VpnControllerActivityComponentBindingModule {
+    @Binds
+    @IntoMap
+    @ClassKey(VpnControllerActivity::class)
+    abstract fun bindVpnControllerActivityComponentFactory(factory: VpnControllerActivityComponent.Factory): AndroidInjector.Factory<*>
+}
