4cc6f6846123154bc5d43e003e1c52470c1869e1
David <malmstein@gmail.com>
2021-12-03 14:45:16 +0100

    done for hack days

--- vpn-store/src/main/java/com/duckduckgo/mobile/android/vpn/trackers/AppTrackerRepository.kt
+++ vpn-store/src/main/java/com/duckduckgo/mobile/android/vpn/trackers/AppTrackerRepository.kt
@@ -19,6 +19,7 @@ package com.duckduckgo.mobile.android.vpn.trackers
 import androidx.annotation.WorkerThread
 import com.duckduckgo.mobile.android.vpn.dao.VpnAppTrackerBlockingDao
 import kotlinx.coroutines.flow.Flow
+import timber.log.Timber
 
 @WorkerThread
 interface AppTrackerRepository {
@@ -39,16 +40,65 @@ interface AppTrackerRepository {
     fun restoreDefaultProtectedList()
 }
 
-class RealAppTrackerRepository(private val vpnAppTrackerBlockingDao: VpnAppTrackerBlockingDao) : AppTrackerRepository {
+class RealAppTrackerRepository(private val vpnAppTrackerBlockingDao: VpnAppTrackerBlockingDao) :
+    AppTrackerRepository {
+
+    private val smartTVBlockingList =
+        listOf<String>(
+            "api-mf1.meta.ndmdhs.com",
+            "b02.black.ndmdhs.com",
+            "bravia.dl.playstation.net",
+            "call.me.sel.sony.com",
+            "flingo.tv",
+            "sonybivstatic-a.akamaihd.net",
+            "facemap.foldlife.net",
+            "bdcore-apr-lb.bda.ndmdhs.com",
+            "tvsideviewandroidv2-cfgdst-ore-pro.bda.ndmdhs.com",
+            "api.cid.samba.tv",
+            "#platform.cid.samba.tv #See Toshiba",
+            "preferences.cid.samba.tv",
+            "2mdn.net",
+            "ad.71i.de",
+            "adv.ettoday.net",
+            "advertising.com",
+            "cdn.smartclip.net",
+            "cert-test.sandbox.google.com",
+            "database01p.anixe.net",
+            "de.ioam.de",
+            "googleads.g.doubleclick.net",
+            "nbc-jite.nbcuni.com",
+            "redbutton-adproxy-lb-prod.redbutton.de",
+            "redbutton-lb-prod.redbutton.de",
+            "redbutton.sim-technik.de",
+            "script.ioam.de",
+            "start.digitaltext.rtl.de",
+            "stats-irl.sxp.smartclip.net",
+            "trvdp.com",
+            "tv-static.scdn.co",
+            "#tv.deezer.com # Breaks Deezer's smart-TV apps",
+            "xml.opera.com")
 
     override fun findTracker(hostname: String, packageName: String): AppTrackerType {
-        val tracker = vpnAppTrackerBlockingDao.getTrackerBySubdomain(hostname) ?: return AppTrackerType.NotTracker
+        val tracker =
+            vpnAppTrackerBlockingDao.getTrackerBySubdomain(hostname)
+                ?: return AppTrackerType.NotTracker
         val entityName = vpnAppTrackerBlockingDao.getEntityByAppPackageId(packageName)
         if (firstPartyTracker(tracker, entityName)) {
             return AppTrackerType.FirstParty(tracker)
         }
 
         return AppTrackerType.ThirdParty(tracker)
+    }
+
+    private fun findSmartTVTracker(hostname: String): AppTrackerType {
+        return if (smartTVBlockingList.contains(hostname)) {
+            Timber.d("Found specific Smart TV Tracker")
+            AppTrackerType.ThirdParty(
+                AppTracker(
+                    hostname, 1279484251, TrackerOwner("Smart TV", "Smart TV"), TrackerApp(0, 100.0), false))
+        } else {
+            AppTrackerType.NotTracker
+        }
     }
 
     private fun firstPartyTracker(tracker: AppTracker, entityName: AppTrackerPackage?): Boolean {
@@ -73,15 +123,16 @@ class RealAppTrackerRepository(private val vpnAppTrackerBlockingDao: VpnAppTrack
     }
 
     override fun manuallyExcludedApp(packageName: String) {
-        vpnAppTrackerBlockingDao.insertIntoManualAppExclusionList(AppTrackerManualExcludedApp(packageName, false))
+        vpnAppTrackerBlockingDao.insertIntoManualAppExclusionList(
+            AppTrackerManualExcludedApp(packageName, false))
     }
 
     override fun manuallyEnabledApp(packageName: String) {
-        vpnAppTrackerBlockingDao.insertIntoManualAppExclusionList(AppTrackerManualExcludedApp(packageName, true))
+        vpnAppTrackerBlockingDao.insertIntoManualAppExclusionList(
+            AppTrackerManualExcludedApp(packageName, true))
     }
 
     override fun restoreDefaultProtectedList() {
         vpnAppTrackerBlockingDao.deleteManualAppExclusionList()
     }
-
 }
