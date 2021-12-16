37901066f7464542a0d6ed6f39be604b081ac03e
Marcos Holgado <marcosh@duckduckgo.com>
2021-12-16 09:29:14 +0000

    Add JS reference tests for gpc

--- privacy-config/privacy-config-impl/src/test/java/com/duckduckgo/privacy/config/impl/referencetests/GpcJavascriptReferenceTest.kt
+++ privacy-config/privacy-config-impl/src/test/java/com/duckduckgo/privacy/config/impl/referencetests/GpcJavascriptReferenceTest.kt
@@ -44,80 +44,77 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.ParameterizedRobolectricTestRunner
 
-//@RunWith(ParameterizedRobolectricTestRunner::class)
-//class GpcJavascriptReferenceTest(private val testCase: TestCase) {
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class GpcJavascriptReferenceTest(private val testCase: TestCase) {
 
-//    private val mockUnprotectedTemporaryRepository: UnprotectedTemporaryRepository = mock()
-//    private val mockGpcRepository: GpcRepository = mock()
-//    private val mockFeatureToggle: FeatureToggle = mock()
-//    lateinit var gpc: Gpc
-//
-//    companion object {
-//        private val moshi = Moshi.Builder().add(JSONObjectAdapter()).build()
-//        val adapter: JsonAdapter<ReferenceTest> = moshi.adapter(ReferenceTest::class.java)
-//
-//        @JvmStatic
-//        @ParameterizedRobolectricTestRunner.Parameters(name = "Test case: {index} - {0}")
-//        fun testData(): List<TestCase> {
-//            val referenceTest = adapter.fromJson(FileUtilities.loadText("reference_tests/gpc/tests.json"))
-//            return referenceTest?.gpcHeader?.tests?.filterNot { it.exceptPlatforms.contains("android-browser") } ?: emptyList()
-//        }
-//    }
-//
-//    @Before
-//    fun setup() {
-//        val context: Context = mock()
-//        val resources: Resources = mock()
-//        whenever(context.resources).thenReturn(resources)
-//        whenever(resources.openRawResource(any())).thenReturn("".byteInputStream())
-//        whenever(mockGpcRepository.isGpcEnabled()).thenReturn(testCase.gpcUserSettingOn)
-//        mockGpcPrivacyConfig()
-//        gpc = RealGpc(context, mockFeatureToggle, mockGpcRepository, RealUnprotectedTemporary(mockUnprotectedTemporaryRepository))
-//    }
-//
-//    @Test
-//    fun whenReferenceTestRunsItReturnsTheExpectedResult() {
-//        val hasGpcHeader = gpc.getHeaders(testCase.requestURL).containsKey(RealGpc.GPC_HEADER)
-//        assertEquals(testCase.expectGPCHeader, hasGpcHeader)
-//    }
-//
-//    private fun mockGpcPrivacyConfig() {
-//        val gpcExceptions = mutableListOf<GpcException>()
-//        val jsonAdapter: JsonAdapter<JsonPrivacyConfig> = moshi.adapter(JsonPrivacyConfig::class.java)
-//        val config: JsonPrivacyConfig? = jsonAdapter.fromJson(FileUtilities.loadText("reference_tests/gpc/config_reference.json"))
-//        val gpcAdapter: JsonAdapter<GpcFeature> = moshi.adapter(GpcFeature::class.java)
-//        val gpcFeature: GpcFeature? = gpcAdapter.fromJson(config?.features?.get("gpc").toString())
-//
-//        gpcFeature?.exceptions?.map {
-//            gpcExceptions.add(GpcExceptionEntity(it.domain).toGpcException())
-//        }
-//
-//        val isEnabled = gpcFeature?.state == "enabled"
-//        val exceptionsUnprotectedTemporary = CopyOnWriteArrayList(config?.unprotectedTemporary ?: emptyList())
-//
-//        whenever(mockFeatureToggle.isFeatureEnabled(PrivacyFeatureName.GpcFeatureName(), isEnabled)).thenReturn(isEnabled)
-//        whenever(mockGpcRepository.exceptions).thenReturn(CopyOnWriteArrayList(gpcExceptions))
-//        whenever(mockUnprotectedTemporaryRepository.exceptions).thenReturn(exceptionsUnprotectedTemporary)
-//    }
-//
-//    data class TestCase(
-//        val name: String,
-//        val siteURL: String,
-//        val requestURL: String,
-//        val requestType: String,
-//        val gpcUserSettingOn: Boolean,
-//        val expectGPCAPI: Boolean,
-//        val expectGPCAPIValue: String,
-//        val exceptPlatforms: List<String>
-//    )
-//
-//    data class GpcJavascriptTest(
-//        val name: String,
-//        val desc: String,
-//        val tests: List<TestCase>
-//    )
-//
-//    data class ReferenceTest(
-//        val gpcJavaScriptAPI: GpcJavascriptTest
-//    )
-//}
+    private val mockUnprotectedTemporaryRepository: UnprotectedTemporaryRepository = mock()
+    private val mockGpcRepository: GpcRepository = mock()
+    private val mockFeatureToggle: FeatureToggle = mock()
+    lateinit var gpc: Gpc
+
+    companion object {
+        private val moshi = Moshi.Builder().add(JSONObjectAdapter()).build()
+        val adapter: JsonAdapter<ReferenceTest> = moshi.adapter(ReferenceTest::class.java)
+
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "Test case: {index} - {0}")
+        fun testData(): List<TestCase> {
+            val referenceTest = adapter.fromJson(FileUtilities.loadText("reference_tests/gpc/tests.json"))
+            return referenceTest?.gpcJavaScriptAPI?.tests?.filterNot { it.exceptPlatforms.contains("android-browser") } ?: emptyList()
+        }
+    }
+
+    @Before
+    fun setup() {
+        val context: Context = mock()
+        val resources: Resources = mock()
+        whenever(context.resources).thenReturn(resources)
+        whenever(resources.openRawResource(any())).thenReturn("".byteInputStream())
+        whenever(mockGpcRepository.isGpcEnabled()).thenReturn(testCase.gpcUserSettingOn)
+        mockGpcPrivacyConfig()
+        gpc = RealGpc(context, mockFeatureToggle, mockGpcRepository, RealUnprotectedTemporary(mockUnprotectedTemporaryRepository))
+    }
+
+    @Test
+    fun whenReferenceTestRunsItReturnsTheExpectedResult() {
+        assertEquals(testCase.expectGPCAPI, gpc.canGpcBeUsedByUrl(testCase.siteURL))
+    }
+
+    private fun mockGpcPrivacyConfig() {
+        val gpcExceptions = mutableListOf<GpcException>()
+        val jsonAdapter: JsonAdapter<JsonPrivacyConfig> = moshi.adapter(JsonPrivacyConfig::class.java)
+        val config: JsonPrivacyConfig? = jsonAdapter.fromJson(FileUtilities.loadText("reference_tests/gpc/config_reference.json"))
+        val gpcAdapter: JsonAdapter<GpcFeature> = moshi.adapter(GpcFeature::class.java)
+        val gpcFeature: GpcFeature? = gpcAdapter.fromJson(config?.features?.get("gpc").toString())
+
+        gpcFeature?.exceptions?.map {
+            gpcExceptions.add(GpcExceptionEntity(it.domain).toGpcException())
+        }
+
+        val isEnabled = gpcFeature?.state == "enabled"
+        val exceptionsUnprotectedTemporary = CopyOnWriteArrayList(config?.unprotectedTemporary ?: emptyList())
+
+        whenever(mockFeatureToggle.isFeatureEnabled(PrivacyFeatureName.GpcFeatureName(), isEnabled)).thenReturn(isEnabled)
+        whenever(mockGpcRepository.exceptions).thenReturn(CopyOnWriteArrayList(gpcExceptions))
+        whenever(mockUnprotectedTemporaryRepository.exceptions).thenReturn(exceptionsUnprotectedTemporary)
+    }
+
+    data class TestCase(
+        val name: String,
+        val siteURL: String,
+        val gpcUserSettingOn: Boolean,
+        val expectGPCAPI: Boolean,
+        val expectGPCAPIValue: String,
+        val exceptPlatforms: List<String>
+    )
+
+    data class GpcJavascriptTest(
+        val name: String,
+        val desc: String,
+        val tests: List<TestCase>
+    )
+
+    data class ReferenceTest(
+        val gpcJavaScriptAPI: GpcJavascriptTest
+    )
+}
