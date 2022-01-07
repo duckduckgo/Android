99d0af776a2a7d333c5c3c602899c28dd86ef873
Josh Leibstein <joshliebe@gmail.com>
2022-01-07 12:12:36 +0200

    Fix UrlExtractionJavascriptInterfaceTest

--- app/src/test/java/com/duckduckgo/app/browser/urlextraction/UrlExtractionJavascriptInterfaceTest.kt
+++ app/src/test/java/com/duckduckgo/app/browser/urlextraction/UrlExtractionJavascriptInterfaceTest.kt
@@ -16,9 +16,9 @@
 
 package com.duckduckgo.app.browser.urlextraction
 
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
 class UrlExtractionJavascriptInterfaceTest {
 
