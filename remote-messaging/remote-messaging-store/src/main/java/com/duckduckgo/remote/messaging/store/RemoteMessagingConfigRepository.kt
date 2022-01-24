b7fc39c074a657183674ec08a461369a0044903b
Cristian Monforte <cristian@duckduckgo.com>
2022-01-24 19:53:00 +0100

    add logic to check if config expired

--- remote-messaging/remote-messaging-store/src/main/java/com/duckduckgo/remote/messaging/store/RemoteMessagingConfigRepository.kt
+++ remote-messaging/remote-messaging-store/src/main/java/com/duckduckgo/remote/messaging/store/RemoteMessagingConfigRepository.kt
@@ -16,10 +16,15 @@
 
 package com.duckduckgo.remote.messaging.store
 
+import org.threeten.bp.LocalDateTime
+import java.time.temporal.TemporalField
+
 interface RemoteMessagingConfigRepository {
     fun insert(remoteMessagingConfig: RemoteMessagingConfig)
     fun get(): RemoteMessagingConfig
     fun delete()
+    fun expired(): Boolean
+    fun invalidated(): Boolean
 }
 
 class LocalRemoteMessagingConfigRepository(database: RemoteMessagingDatabase) : RemoteMessagingConfigRepository {
@@ -36,5 +41,14 @@ class LocalRemoteMessagingConfigRepository(database: RemoteMessagingDatabase) : 
 
     override fun delete() {
         dao.delete()
+    }
+
+    override fun expired(): Boolean {
+        val yesterday = databaseTimestampFormatter().format(LocalDateTime.now().minusDays(1L))
+        return get().evaluationTimestamp < yesterday
+    }
+
+    override fun invalidated(): Boolean {
+        return get().invalidate
     }
 }
