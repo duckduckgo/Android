893b4e4560e4cd4273eb76166e88317cc7a952e4
Karl Dimla <klmbdimla@gmail.com>
2022-04-06 14:50:39 +0200

    Update scope of ViewModelFactoryPlugin for fragments to use FragmentScope

--- app/src/main/java/com/duckduckgo/app/feedback/ui/positive/initial/PositiveFeedbackLandingViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/feedback/ui/positive/initial/PositiveFeedbackLandingViewModel.kt
@@ -19,10 +19,10 @@ package com.duckduckgo.app.feedback.ui.positive.initial
 import androidx.lifecycle.ViewModel
 import com.duckduckgo.anvil.annotations.ContributesViewModel
 import com.duckduckgo.app.global.SingleLiveEvent
-import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.di.scopes.FragmentScope
 import javax.inject.Inject
 
-@ContributesViewModel(AppScope::class)
+@ContributesViewModel(FragmentScope::class)
 class PositiveFeedbackLandingViewModel @Inject constructor() : ViewModel() {
 
     val command: SingleLiveEvent<Command> = SingleLiveEvent()
