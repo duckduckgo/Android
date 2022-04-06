ebb8ee30f35260d3817253e5815373f1201a19d3
Josh Leibstein <joshliebe@gmail.com>
2022-04-06 21:28:40 +0100

    Undo renaming of activity-alias

--- app/src/main/java/com/duckduckgo/app/icon/api/AppIconModifier.kt
+++ app/src/main/java/com/duckduckgo/app/icon/api/AppIconModifier.kt
@@ -68,7 +68,7 @@ enum class AppIcon(
         icon = R.drawable.ic_app_icon_black_round
     ),
     SILHOUETTE(
-        componentName = "$QUALIFIER.LauncherSilhouette",
+        componentName = "$QUALIFIER.LauncherSilhoutte",
         icon = R.drawable.ic_app_icon_silhouette_round
     );
 
