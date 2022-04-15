d7d1bf17b538a602b779cf8a3b524516417cc0b5
Josh Leibstein <joshliebe@gmail.com>
2022-04-15 21:24:52 +0100

    Add bandwidth module

---
+++ bandwidth/bandwidth-impl/src/main/java/com/duckduckgo/bandwidth/di/BandwidthModule.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.bandwidth.di
+
+import android.content.Context
+import androidx.room.Room
+import com.duckduckgo.bandwidth.store.BandwidthDatabase
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesTo
+import dagger.Module
+import dagger.Provides
+import dagger.SingleInstanceIn
+
+@Module
+@ContributesTo(AppScope::class)
+object BandwidthModule {
+    @Provides
+    @SingleInstanceIn(AppScope::class)
+    fun provideBandwidthDatabase(context: Context): BandwidthDatabase {
+        return Room.databaseBuilder(context, BandwidthDatabase::class.java, "bandwidth_database.db")
+            .enableMultiInstanceInvalidation()
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
---
+++ bandwidth/bandwidth-store/src/main/java/com/duckduckgo/bandwidth/store/BandwidthEntity.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.bandwidth.store
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bandwidth")
+data class BandwidthEntity(
+    @PrimaryKey val id: Int,
+    val timestamp: Long = System.currentTimeMillis(),
+    val appBytes: Long,
+    val totalBytes: Long
+)
