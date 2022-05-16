dc41f1ba5d8028db2d92e5c47c2ffe0f78e84254
Beep1990 <56551439+Beep1990@users.noreply.github.com>
2022-05-16 13:03:11 +0200

    Aut 14 add idling resource (#2)
    * Environment setup. Puste pliki/klasy utworzone są celowo aby dało się wysłać commita (puste foldert się nie wyślą)
    
    * Implementation of IdlingResource
    
    * object name adjustment
    Implementation of IdlingResource

--- app/src/androidTest/java/com/duckduckgo/utils/ViewIdlingResource.kt
+++ app/src/androidTest/java/com/duckduckgo/utils/ViewIdlingResource.kt
@@ -16,5 +16,62 @@
 
 package com.duckduckgo.utils
 
-class ViewIdlingResource {
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.ViewFinder
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.Matcher
+import java.lang.Exception
+import java.lang.reflect.Field
+
+class ViewIdlingResource(
+    private val viewMatcher: Matcher<View?>?,
+    private val idleMatcher: Matcher<View?>?
+) : IdlingResource {
+
+    private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+    override fun isIdleNow(): Boolean {
+        val view: View? = getView(viewMatcher)
+        val isIdle: Boolean = idleMatcher?.matches(view) ?: false
+        if (isIdle) {
+            resourceCallback?.onTransitionToIdle()
+        }
+        return isIdle
+    }
+
+    override fun registerIdleTransitionCallback(resourceCallback: IdlingResource.ResourceCallback?) {
+        this.resourceCallback = resourceCallback
+    }
+
+    override fun getName(): String? {
+        return "$this ${viewMatcher.toString()}"
+    }
+
+    private fun getView(viewMatcher: Matcher<View?>?): View? {
+        return try {
+            val viewInteraction = onView(viewMatcher)
+            val finderField: Field? = viewInteraction.javaClass.getDeclaredField("viewFinder")
+            finderField?.isAccessible = true
+            val finder = finderField?.get(viewInteraction) as ViewFinder
+            finder.view
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
+
+fun waitUntilViewIsDisplayed(matcher: Matcher<View?>) {
+    val idlingResource: IdlingResource = ViewIdlingResource(matcher, isDisplayed())
+    try {
+        IdlingRegistry.getInstance().register(idlingResource)
+        onView(withId(0)).check(doesNotExist())
+    } finally {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+    }
+}
+
