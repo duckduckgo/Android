2f903a296a14de5d859d1b28014e459fefe504d7
lukasikdpp <67908575+lukasikdpp@users.noreply.github.com>
2022-06-09 17:02:33 +0200

    dodanie częsci typingActions (#7)
    zrezygnowałem z odawania zaznaczania textu (trzeba tworzyć totalnie customowe rzeczy do tego), a w sumie można użyć longClicka z akcji do clickania
    
    Co-authored-by: Dawid Łukasik <dawidlukasikpp@gmail.com>

---
+++ app/src/androidTest/java/com/duckduckgo/actions/TypingActions.kt
@@ -0,0 +1,24 @@
+package com.duckduckgo.actions
+
+import androidx.annotation.IdRes
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.matcher.ViewMatchers.withId
+
+fun enterTextFunction(@IdRes viewId: Int, text: String) {
+    onView(withId(viewId))
+        .perform(typeText(text), closeSoftKeyboard())
+}
+
+fun replaceTextFunction(@IdRes viewId: Int, text: String) {
+    onView(withId(viewId))
+        .perform(replaceText(text), closeSoftKeyboard())
+}
+
+fun scrollAndEnterTextFunction(@IdRes viewId: Int, text: String) {
+    onView(withId(viewId))
+        .perform(scrollTo(), typeText(text), closeSoftKeyboard())
+}
