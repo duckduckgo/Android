7c501b4b7dc2d2c0326f0857dfd9cf299b4abe5a
Craig Russell <CDRussell@users.noreply.github.com>
2022-06-16 17:37:33 +0100

    Email injection fully working again

--- app/src/main/java/com/duckduckgo/app/browser/BrowserWebViewClient.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserWebViewClient.kt
@@ -250,7 +250,6 @@ class BrowserWebViewClient(
             }
             lastPageStarted = url
             browserAutofill.configureAutofillForCurrentPage(webView, url)
-            // emailInjector.injectEmailAutofillJs(webView, url) // Needs to be injected onPageStarted
             injectGpcToDom(webView, url)
             loginDetector.onEvent(WebNavigationEvent.OnPageStarted(webView))
         } catch (e: Throwable) {
