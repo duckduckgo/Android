ca193dfc91969a48845ecdce86ceb2ec7959ef91
Marcos Holgado <marcosh@duckduckgo.com>
2022-07-19 07:29:05 +0100

    test

--- lint-rules/src/test/java/com/duckduckgo/lint/NoSingletonDetectorTest.kt
+++ lint-rules/src/test/java/com/duckduckgo/lint/NoSingletonDetectorTest.kt
@@ -39,7 +39,7 @@ class NoSingletonDetectorTest {
             .issues(NoSingletonDetector.NO_SINGLETON_ISSUE)
             .run()
             .expect("""
-               src/com/duckduckgo/lint/Singleton.kt: : Error: The Singleton annotation must not be used [NoSingletonAnnotation]
+               src/com/duckduckgo/lint/Singleton.kt:[5]: Error: The Singleton annotation must not be used [NoSingletonAnnotation]
                  @Singleton
                  ~~~~~~~~~~
                1 errors, 0 warnings
