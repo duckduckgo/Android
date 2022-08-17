7fa16772ed7d3ce272f7df48a8980271bb921729
Marcos <marcosh@duckduckgo.com>
2022-08-17 16:06:18 +0100

    Ignore top cmps (#2181)
    * Add autoconsent settings (#2188)

--- autoconsent/autoconsent-impl/src/main/java/com/duckduckgo/autoconsent/impl/handlers/PopUpFoundMessageHandlerPlugin.kt
+++ autoconsent/autoconsent-impl/src/main/java/com/duckduckgo/autoconsent/impl/handlers/PopUpFoundMessageHandlerPlugin.kt
@@ -19,22 +19,44 @@ package com.duckduckgo.autoconsent.impl.handlers
 import android.webkit.WebView
 import com.duckduckgo.autoconsent.api.AutoconsentCallback
 import com.duckduckgo.autoconsent.impl.MessageHandlerPlugin
+import com.duckduckgo.autoconsent.impl.adapters.JSONObjectAdapter
 import com.duckduckgo.autoconsent.store.AutoconsentSettingsRepository
 import com.duckduckgo.di.scopes.AppScope
 import com.squareup.anvil.annotations.ContributesMultibinding
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import timber.log.Timber
 import javax.inject.Inject
 
 @ContributesMultibinding(AppScope::class)
 class PopUpFoundMessageHandlerPlugin @Inject constructor(private val repository: AutoconsentSettingsRepository) : MessageHandlerPlugin {
 
+    private val moshi = Moshi.Builder().add(JSONObjectAdapter()).build()
+
     override fun process(messageType: String, jsonString: String, webView: WebView, autoconsentCallback: AutoconsentCallback) {
-        if (supportedTypes.contains(messageType)) {
-            if (repository.userSetting) {
-                return
+        try {
+            if (supportedTypes.contains(messageType)) {
+                val message: PopUpFoundMessage = parseMessage(jsonString) ?: return
+                if (repository.userSetting) return
+                if (message.cmp.endsWith(IGNORE_CMP_SUFFIX, ignoreCase = true)) return
+
+                autoconsentCallback.onFirstPopUpHandled()
             }
-            autoconsentCallback.onFirstPopUpHandled()
+        } catch (e: Exception) {
+            Timber.d(e.localizedMessage)
         }
     }
 
     override val supportedTypes: List<String> = listOf("popupFound")
+
+    private fun parseMessage(jsonString: String): PopUpFoundMessage? {
+        val jsonAdapter: JsonAdapter<PopUpFoundMessage> = moshi.adapter(PopUpFoundMessage::class.java)
+        return jsonAdapter.fromJson(jsonString)
+    }
+
+    data class PopUpFoundMessage(val type: String, val cmp: String, val url: String)
+
+    companion object {
+        private const val IGNORE_CMP_SUFFIX = "-top"
+    }
 }
