4f34bd4ca44e02aa8b1b7e76f0c355d425d68b2e
David <malmstein@gmail.com>
2022-09-09 15:16:49 +0200

    better dialog naming

---
+++ lint-rules/src/main/java/com/duckduckgo/lint/ui/DuckDuckGoDesignSystemRegistry.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.lint
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+import com.duckduckgo.lint.NoFragmentDetector.Companion.NO_FRAGMENT_ISSUE
+import com.duckduckgo.lint.NoLifecycleObserverDetector.Companion.NO_LIFECYCLE_OBSERVER_ISSUE
+import com.duckduckgo.lint.NoSingletonDetector.Companion.NO_SINGLETON_ISSUE
+import com.duckduckgo.lint.NoSystemLoadLibraryDetector.Companion.NO_SYSTEM_LOAD_LIBRARY
+import com.duckduckgo.lint.ui.DeprecatedAndroidButtonUsedInXmlDetector.Companion.DEPRECATED_BUTTON_IN_XML
+import com.duckduckgo.lint.ui.NoStyleAppliedToDesignSystemComponentDetector.Companion.STYLE_IN_DESIGN_SYSTEM_COMPONENT
+
+@Suppress("UnstableApiUsage")
+class DuckDuckGoDesignSystemRegistry : IssueRegistry() {
+    override val issues: List<Issue>
+        get() = listOf(
+            DEPRECATED_BUTTON_IN_XML,
+            STYLE_IN_DESIGN_SYSTEM_COMPONENT,
+        )
+
+    override val api: Int
+        get() = CURRENT_API
+
+    override val vendor = Vendor(
+        vendorName = "DuckDuckGo Design System",
+        identifier = "com.duckduckgo:lint",
+        feedbackUrl = "https://app.asana.com/0/1202857801505092/1202858029631618",
+        contact = "https://github.com/duckduckgo/android"
+    )
+}
+
