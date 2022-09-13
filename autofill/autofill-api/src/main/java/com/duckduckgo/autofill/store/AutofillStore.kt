d90853ccc4655dd23b152fc53842e5f0fd3cdd74
Craig Russell <CDRussell@users.noreply.github.com>
2022-09-13 17:26:45 +0100

    Offer non-interested autofill users a way to disable

--- autofill/autofill-api/src/main/java/com/duckduckgo/autofill/store/AutofillStore.kt
+++ autofill/autofill-api/src/main/java/com/duckduckgo/autofill/store/AutofillStore.kt
@@ -42,6 +42,18 @@ interface AutofillStore {
     var showOnboardingWhenOfferingToSaveLogin: Boolean
 
     /**
+     * Whether to monitor autofill decline counts or not
+     * Used to determine whether we should actively detect when a user new to autofill doesn't appear to want it enabled
+     */
+    var monitorDeclineCounts: Boolean
+
+    /**
+     * A count of the number of autofill declines the user has made, persisted across all sessions.
+     * Used to determine whether we should prompt a user new to autofill to disable it if they don't appear to want it enabled
+     */
+    var autofillDeclineCount: Int
+
+    /**
      * Find saved credentials for the given URL, returning an empty list where no matches are found
      * @param rawUrl Can be a full, unmodified URL taken from the URL bar (containing subdomains, query params etc...)
      */
