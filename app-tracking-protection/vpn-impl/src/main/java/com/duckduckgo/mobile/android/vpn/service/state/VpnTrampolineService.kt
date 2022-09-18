897913350e0da49401c155c06fab73096e0d4ee8
Aitor Viana <aitorvs@gmail.com>
2022-09-18 11:22:59 +0200

    VPN start trampoline

---
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/service/state/VpnTrampolineService.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.mobile.android.vpn.service.state
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.duckduckgo.mobile.android.vpn.service.TrackerBlockingVpnService
+import timber.log.Timber
+
+/**
+ * This is a trampoline service that is used to start the VPN service.
+ * The reason why we go through this is because we want to make sure the :vpn process is already created when we call
+ * startForegroundService() method. That should reduce the time between startForegroundService() and startForeground()
+ */
+internal class VpnTrampolineService : Service() {
+
+    override fun onCreate() {
+        Timber.d("onCreate")
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Timber.d("onStartCommand")
+        return super.onStartCommand(intent, flags, startId).also {
+            TrackerBlockingVpnService.startVpnService(this)
+            stopSelf()
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Timber.d("Bound to VPN")
+        return null
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Timber.d("Unbound from VPN")
+        return super.onUnbind(intent)
+    }
+
+    override fun onDestroy() {
+        Timber.d("onDestroy")
+        super.onDestroy()
+    }
+}
