0aeac25070d846ce70c4228b080988631c12f8d4
Anastasia Shuba <ashuba22@gmail.com>
2022-09-30 15:03:26 -0700

    add comment about VPN routes

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/service/TrackerBlockingVpnService.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/service/TrackerBlockingVpnService.kt
@@ -217,7 +217,10 @@ class TrackerBlockingVpnService : VpnService(), CoroutineScope by MainScope() {
             // See https://developer.android.com/reference/android/net/VpnService.Builder#allowFamily(int) for more info as to why
             allowFamily(AF_INET6)
 
+            // TODO: eventually routes will be set by remote config
             if (appBuildConfig.isPerformanceTest) {
+                // Currently allowing host PC address 10.0.2.2 when running performance test on an emulator (normally we don't route local traffic)
+                // The address is also isolated to minimize network interference during performance tests
                 VpnRoutes.includedTestRoutes.forEach { addRoute(it.address, it.maskWidth) }
             } else {
                 VpnRoutes.includedRoutes.forEach { addRoute(it.address, it.maskWidth) }
