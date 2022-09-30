fa8ce5b19a36247d10859090f33e5bab277b0382
David <malmstein@gmail.com>
2022-09-30 10:54:59 +0200

    spotless

--- common-ui/src/main/java/com/duckduckgo/mobile/android/ui/view/listitem/OneLineListItem.kt
+++ common-ui/src/main/java/com/duckduckgo/mobile/android/ui/view/listitem/OneLineListItem.kt
@@ -158,14 +158,14 @@ class OneLineListItem @JvmOverloads constructor(
     }
 
     /** Sets the Switch Visible */
-    fun showSwitch(){
+    fun showSwitch() {
         binding.trailingContainer.show()
         binding.trailingSwitch.show()
         binding.trailingIcon.gone()
     }
 
     /** Sets the Trailing Icon Visible */
-    fun showTrailingIcon(){
+    fun showTrailingIcon() {
         binding.trailingContainer.show()
         binding.trailingIcon.show()
         binding.trailingSwitch.gone()
