dcb44f5bb9d4011d4fc9e8fe1e3a550638df4d94
Noelia Alcala <nalcalag@gmail.com>
2022-10-04 18:09:37 +0100

    Migrated TextViews to SectionHeaderTextView and removed unused style SitePermissionsSectionTitle

--- site-permissions/site-permissions-impl/src/main/java/com/duckduckgo/site/permissions/impl/SitePermissionsDialogActivityLauncher.kt
+++ site-permissions/site-permissions-impl/src/main/java/com/duckduckgo/site/permissions/impl/SitePermissionsDialogActivityLauncher.kt
@@ -18,7 +18,6 @@ package com.duckduckgo.site.permissions.impl
 
 import android.Manifest
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
