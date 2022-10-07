f558e2753e71f5fa2425abae0c56782f45f0b62b
Cristian Monforte <cristian@duckduckgo.com>
2022-10-07 13:19:41 +0200

    Fixes missing di binding

--- statistics/statistics-impl/src/main/java/com/duckduckgo/app/statistics/pixels/ExceptionPixelImpl.kt
+++ statistics/statistics-impl/src/main/java/com/duckduckgo/app/statistics/pixels/ExceptionPixelImpl.kt
@@ -18,6 +18,9 @@ package com.duckduckgo.app.statistics.pixels
 
 import com.duckduckgo.app.global.exception.RootExceptionFinder
 import com.duckduckgo.app.global.exception.extractExceptionCause
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesBinding
+import com.squareup.anvil.annotations.ContributesTo
 import javax.inject.Inject
 
 /**
@@ -25,9 +28,8 @@ import javax.inject.Inject
  * or illegal states to be stored and send as offline pixels
  */
 @Suppress("MemberVisibilityCanBePrivate", "unused")
-class ExceptionPixelImpl
-@Inject
-constructor(
+@ContributesBinding(AppScope::class)
+class ExceptionPixelImpl @Inject constructor(
     private val pixel: Pixel,
     private val rootExceptionFinder: RootExceptionFinder
 ): ExceptionPixel {
