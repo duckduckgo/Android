04aa6b1c0e49ce9f9b3df69b24cb76124ebab042
Cristian Monforte <cristian@duckduckgo.com>
2022-10-07 13:04:41 +0200

    moves OfflinePixel interface to :api module

--- statistics/statistics-api/src/main/java/com/duckduckgo/app/statistics/api/OfflinePixel.kt
+++ statistics/statistics-api/src/main/java/com/duckduckgo/app/statistics/api/OfflinePixel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 DuckDuckGo
+ * Copyright (c) 2022 DuckDuckGo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,3 +40,4 @@ import io.reactivex.Completable
 interface OfflinePixel {
     fun send(): Completable
 }
+
