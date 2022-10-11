0de5169dc11dbce1b5ddc9deb192800916f2fe60
Cristian Monforte <cristian@duckduckgo.com>
2022-10-11 15:16:19 +0200

    introduce unique pixel when data cleared

--- app/src/main/java/com/duckduckgo/app/global/view/ClearPersonalDataAction.kt
+++ app/src/main/java/com/duckduckgo/app/global/view/ClearPersonalDataAction.kt
@@ -25,6 +25,7 @@ import com.duckduckgo.adclick.api.AdClickManager
 import com.duckduckgo.app.browser.WebDataManager
 import com.duckduckgo.app.browser.cookies.ThirdPartyCookieManager
 import com.duckduckgo.app.fire.AppCacheClearer
+import com.duckduckgo.app.fire.ClearDataPixel
 import com.duckduckgo.app.fire.FireActivity
 import com.duckduckgo.app.fire.UnsentForgetAllPixelStore
 import com.duckduckgo.app.fire.fireproofwebsite.data.FireproofWebsiteRepositoryAPI
@@ -67,7 +68,8 @@ class ClearPersonalDataAction(
     private val adClickManager: AdClickManager,
     private val fireproofWebsiteRepository: FireproofWebsiteRepositoryAPI,
     private val sitePermissionsManager: SitePermissionsManager,
-    private val dispatchers: DispatcherProvider = DefaultDispatcherProvider()
+    private val dispatchers: DispatcherProvider = DefaultDispatcherProvider(),
+    private val clearDataPixel: ClearDataPixel
 ) : ClearDataAction {
 
     override fun killAndRestartProcess(notifyDataCleared: Boolean) {
@@ -85,6 +87,7 @@ class ClearPersonalDataAction(
         shouldFireDataClearPixel: Boolean
     ) {
         withContext(Dispatchers.IO) {
+            clearDataPixel.onDataCleared()
             val fireproofDomains = fireproofWebsiteRepository.fireproofWebsitesSync().map { it.domain }
             cookieManager.flush()
             geoLocationPermissions.clearAllButFireproofed()
@@ -103,6 +106,7 @@ class ClearPersonalDataAction(
     @WorkerThread
     override suspend fun clearTabsAsync(appInForeground: Boolean) {
         withContext(dispatchers.io()) {
+            clearDataPixel.onDataCleared()
             Timber.i("Clearing tabs")
             dataManager.clearWebViewSessions()
             tabRepository.deleteAll()
