d2ee41a2da39dfb57ef47fd597cdafa5d85035ed
Cristian Monforte <cristian@duckduckgo.com>
2022-10-19 11:36:57 +0200

    document api

--- browser-api/src/main/java/com/duckduckgo/app/tabs/BrowserNav.kt
+++ browser-api/src/main/java/com/duckduckgo/app/tabs/BrowserNav.kt
@@ -19,6 +19,10 @@ package com.duckduckgo.app.tabs
 import android.content.Context
 import android.content.Intent
 
+/**
+ * Public interface to provide navigation Intents related to browser screen
+ */
 interface BrowserNav {
+    // opens url on a new tab
     fun openInNewTab(context: Context, url: String): Intent
 }
