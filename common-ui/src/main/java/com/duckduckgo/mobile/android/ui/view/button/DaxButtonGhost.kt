c32090c6531c77bd40b23069f6ff899df759dc23
David <malmstein@gmail.com>
2022-11-09 14:24:58 +0100

    updated dialogs to latest specs

--- common-ui/src/main/java/com/duckduckgo/mobile/android/ui/view/button/DaxButtonGhost.kt
+++ common-ui/src/main/java/com/duckduckgo/mobile/android/ui/view/button/DaxButtonGhost.kt
@@ -22,7 +22,7 @@ import com.duckduckgo.mobile.android.R
 
 class DaxButtonGhost @JvmOverloads constructor(
     ctx: Context,
-    attrs: AttributeSet,
+    attrs: AttributeSet?,
     defStyleAttr: Int = R.attr.daxButtonGhost
 ) : DaxButton(
     ctx,
