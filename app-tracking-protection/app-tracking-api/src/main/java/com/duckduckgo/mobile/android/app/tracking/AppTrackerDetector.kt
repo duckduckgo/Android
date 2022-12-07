8bd2f907f9608d0cd470600db965b0fb841ad3e2
Aitor Viana <aitorvs@gmail.com>
2022-12-07 01:43:57 +0000

    Add App Tracking Detector API (#2607)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
        3. Make sure these changes are tested in API 23 and API 26
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    -->
    
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1203463758229433/f
    
    ### Description
    Create a public AppTrackerDetector API that can be used in other places.
    The API is created in the app-tracking-api module and implemented in the
    vpn-impl module
    
    ### Steps to test this PR
    Smoke tests for AppTP

---
+++ app-tracking-protection/app-tracking-api/src/main/java/com/duckduckgo/mobile/android/app/tracking/AppTrackerDetector.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.mobile.android.app.tracking
+
+interface AppTrackerDetector {
+    /**
+     * Evaluates whether the specified domain requested by the specified uid is a tracker
+     *
+     * @param domain the domain to evaluate
+     * @param uid the uid of the app requesting the domain
+     *
+     * @return [AppTracker] if the request is a tracker, null otherwise
+     */
+    fun evaluate(domain: String, uid: Int): AppTracker?
+
+    data class AppTracker(
+        val domain: String,
+        val uid: Int,
+        val trackerCompanyDisplayName: String,
+        val trackingAppId: String,
+        val trackingAppName: String,
+    )
+}
