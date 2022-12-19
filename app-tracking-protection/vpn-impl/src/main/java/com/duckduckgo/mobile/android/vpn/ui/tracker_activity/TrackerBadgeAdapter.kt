58ab9812383bd9dbd872aba1f03ceac0ab0df562
David <malmstein@gmail.com>
2022-12-19 14:26:52 +0100

    kotlin format

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/TrackerBadgeAdapter.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/TrackerBadgeAdapter.kt
@@ -17,7 +17,6 @@
 package com.duckduckgo.mobile.android.vpn.ui.tracker_activity
 
 import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
