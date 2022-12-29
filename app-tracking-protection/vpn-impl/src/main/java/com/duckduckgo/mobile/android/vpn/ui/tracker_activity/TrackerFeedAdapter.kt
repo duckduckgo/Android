0828562c1a98c36d50440b674f82f5bdadd9655e
nalcalag <nalcalag@gmail.com>
2022-12-29 17:53:14 +0000

    couple of UI tweaks

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/TrackerFeedAdapter.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/TrackerFeedAdapter.kt
@@ -35,7 +35,6 @@ import com.duckduckgo.app.global.extensions.safeGetApplicationIcon
 import com.duckduckgo.mobile.android.ui.TextDrawable
 import com.duckduckgo.mobile.android.ui.recyclerviewext.StickyHeaders
 import com.duckduckgo.mobile.android.ui.view.gone
-import com.duckduckgo.mobile.android.ui.view.hide
 import com.duckduckgo.mobile.android.ui.view.listitem.SectionHeaderListItem
 import com.duckduckgo.mobile.android.ui.view.show
 import com.duckduckgo.mobile.android.vpn.R
@@ -166,7 +165,7 @@ class TrackerFeedAdapter @Inject constructor(
 
         fun bind(
             tracker: TrackerFeedItem.TrackerFeedData?,
-            onAppClick: (TrackerFeedItem.TrackerFeedData) -> Unit
+            onAppClick: (TrackerFeedItem.TrackerFeedData) -> Unit,
         ) {
             tracker?.let { item ->
                 with(activityMessage) {
