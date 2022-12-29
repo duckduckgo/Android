67f022dab0030411549ab97960a63629878cdce9
Noelia Alcala <nalcalag@gmail.com>
2022-12-29 18:46:44 +0100

    ADS: Share Feedback section (#2670)
    Task/Issue URL: https://app.asana.com/0/0/1203467500325943/f
    
    **Description**
    Migration of share feedback screens to new components
    
    **_Design Review_** in
    https://app.asana.com/0/1195897901771673/1203575646431883

--- app/src/main/java/com/duckduckgo/app/feedback/ui/negative/mainreason/MainReasonNegativeFeedbackFragment.kt
+++ app/src/main/java/com/duckduckgo/app/feedback/ui/negative/mainreason/MainReasonNegativeFeedbackFragment.kt
@@ -17,13 +17,11 @@
 package com.duckduckgo.app.feedback.ui.negative.mainreason
 
 import android.os.Bundle
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.duckduckgo.anvil.annotations.InjectWith
 import com.duckduckgo.app.browser.R
 import com.duckduckgo.app.browser.databinding.ContentFeedbackNegativeDisambiguationMainReasonBinding
 import com.duckduckgo.app.feedback.ui.common.FeedbackFragment
-import com.duckduckgo.app.feedback.ui.common.FeedbackItemDecoration
 import com.duckduckgo.app.feedback.ui.negative.FeedbackType.MainReason
 import com.duckduckgo.app.feedback.ui.negative.FeedbackTypeDisplay
 import com.duckduckgo.app.feedback.ui.negative.FeedbackTypeDisplay.FeedbackTypeMainReasonDisplay
@@ -58,7 +56,6 @@ class MainReasonNegativeFeedbackFragment : FeedbackFragment(R.layout.content_fee
         activity?.let {
             binding.recyclerView.layoutManager = LinearLayoutManager(it)
             binding.recyclerView.adapter = recyclerAdapter
-            binding.recyclerView.addItemDecoration(FeedbackItemDecoration(ContextCompat.getDrawable(it, R.drawable.feedback_list_divider)!!))
 
             val listValues = getMainReasonsDisplayText()
             recyclerAdapter.submitList(listValues)
