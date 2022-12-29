4df9cc7ffdfc3631383b52a3c5424af3bd1e1684
Noelia Alcala <nalcalag@gmail.com>
2022-12-29 18:46:44 +0100

    ADS: Share Feedback section (#2670)
    Task/Issue URL: https://app.asana.com/0/0/1203467500325943/f
    
    **Description**
    Migration of share feedback screens to new components
    
    **_Design Review_** in
    https://app.asana.com/0/1195897901771673/1203575646431883

--- app/src/main/java/com/duckduckgo/app/feedback/ui/negative/subreason/SubReasonAdapter.kt
+++ app/src/main/java/com/duckduckgo/app/feedback/ui/negative/subreason/SubReasonAdapter.kt
@@ -21,8 +21,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.duckduckgo.app.browser.databinding.ItemFeedbackReasonBinding
 import com.duckduckgo.app.feedback.ui.negative.FeedbackTypeDisplay.FeedbackTypeSubReasonDisplay
+import com.duckduckgo.mobile.android.databinding.RowOneLineListItemBinding
 
 class SubReasonAdapter(private val itemClickListener: (FeedbackTypeSubReasonDisplay) -> Unit) :
     ListAdapter<FeedbackTypeSubReasonDisplay, SubReasonAdapter.ViewHolder>(DiffCallback()) {
@@ -48,7 +48,7 @@ class SubReasonAdapter(private val itemClickListener: (FeedbackTypeSubReasonDisp
         viewType: Int,
     ): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemFeedbackReasonBinding.inflate(inflater, parent, false)
+        val binding = RowOneLineListItemBinding.inflate(inflater, parent, false)
         return ViewHolder(binding)
     }
 
@@ -59,14 +59,16 @@ class SubReasonAdapter(private val itemClickListener: (FeedbackTypeSubReasonDisp
         holder.bind(getItem(position), itemClickListener)
     }
 
-    data class ViewHolder(val binding: ItemFeedbackReasonBinding) : RecyclerView.ViewHolder(binding.root) {
+    data class ViewHolder(val binding: RowOneLineListItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(
             reason: FeedbackTypeSubReasonDisplay,
             clickListener: (FeedbackTypeSubReasonDisplay) -> Unit,
         ) {
-            binding.reason.text = binding.root.context.getString(reason.listDisplayResId)
-            binding.root.setOnClickListener { clickListener(reason) }
+            val listItem = binding.root
+            listItem.hideLeadingIcon()
+            listItem.setPrimaryText(binding.root.context.getString(reason.listDisplayResId))
+            listItem.setOnClickListener { clickListener(reason) }
         }
     }
 }
