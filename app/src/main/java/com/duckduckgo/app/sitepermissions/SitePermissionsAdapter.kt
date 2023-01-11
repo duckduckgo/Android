671a1bb2947fa05286eeabc082e17bd1f3dc3184
David González <malmstein@gmail.com>
2023-01-11 15:23:18 +0100

    kotlin format

--- app/src/main/java/com/duckduckgo/app/sitepermissions/SitePermissionsAdapter.kt
+++ app/src/main/java/com/duckduckgo/app/sitepermissions/SitePermissionsAdapter.kt
@@ -20,7 +20,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
-import android.widget.TextView
 import androidx.annotation.StringRes
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.LifecycleOwner
