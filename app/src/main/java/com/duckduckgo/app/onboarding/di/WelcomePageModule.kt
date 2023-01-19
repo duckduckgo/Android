7cc082a79a2296c6ba8f14850b96e8b63e5ce774
Ana Capatina <anikiki@users.noreply.github.com>
2023-01-19 16:17:25 +0000

    Implementation task: Add pixel (updated to use OS version) (#2742)
    Task/Issue URL: https://app.asana.com/0/0/1203712351528765/f
    
    ### Description
    Added `os_version_13_or_above` param for `m_db_s` and `m_db_ns` pixels
    fired from onboarding only.
    
    ### Steps to test this PR
    
    Android 13 
    
    - Scenario: Set DDG as default browser from obnoarding
    - [x] Fresh install from this branch.
    - [x] Look at the logs and filter by `Pixel sent: m_db_s with params`.
    - [x] Start onboarding and notice the `Set DDG as default browser`.
    Choose to set it.
    - [x] Check that you see in the logs `Pixel sent: m_db_s with params:
    {fo=true, os_version_13_or_above=true}`.
    
    
    - Scenario: Set DDG as browser from Settings
    - [x] Go to the Settings screen from the menu.
    - [x] Notice the `Set as Default Browser` toggle is ON. Toggle OFF /
    choose a different default browser and go back.
    - [x] Change the toggle to ON and set DDG as default browser. 
    - [x] Check that you see in the logs `Pixel sent: m_db_s with params:
    {fo=false}`.
    
    
    - Scenario: Don't set DDG as default browser from obnoarding
    - [x] Fresh install from this branch.
    - [x] Look at the logs and filter by `Pixel sent: m_db_ns with params`.
    - [x] Start onboarding and notice the `Set DDG as default browser`.
    Choose and set any other browser as default.
    - [x] Check that you see in the logs `Pixel sent: m_db_ns with params:
    {fo=true, os_version_13_or_above=true}`.
    
    
    Any version strictly below Android 13
    
    - Scenario: Set DDG as default browser from obnoarding
    - [x] Fresh install from this branch.
    - [x] Look at the logs and filter by `Pixel sent: m_db_s with params`.
    - [x] Start onboarding and notice the `Set DDG as default browser`.
    Choose to set it.
    - [x] Check that you see in the logs `Pixel sent: m_db_s with params:
    {fo=true, dbo=d, os_version_13_or_above=false}`.
    
    
    - Scenario: Set DDG as browser from Settings
    - [x] Go to the Settings screen from the menu.
    - [x] Notice the `Set as Default Browser` toggle is ON. Toggle OFF /
    choose a different default browser and go back.
    - [x] Change the toggle to ON and set DDG as default browser. 
    - [x] Check that you see in the logs `Pixel sent: m_db_s with params:
    {fo=false}`.
    
    
    - Scenario: Don't set DDG as default browser from obnoarding
    - [x] Fresh install from this branch.
    - [x] Look at the logs and filter by `Pixel sent: m_db_ns with params`.
    - [x] Start onboarding and notice the `Set DDG as default browser`.
    Choose and set any other browser as default.
    - [x] Check that you see in the logs `Pixel sent: m_db_ns with params:
    {fo=true, os_version_13_or_above=false}`.
    
    ### NO UI changes

--- app/src/main/java/com/duckduckgo/app/onboarding/di/WelcomePageModule.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/di/WelcomePageModule.kt
@@ -35,7 +35,8 @@ class WelcomePageModule {
         context: Context,
         pixel: Pixel,
         defaultRoleBrowserDialog: DefaultRoleBrowserDialog,
-    ) = WelcomePageViewModelFactory(appInstallStore, context, pixel, defaultRoleBrowserDialog)
+        appBuildConfig: AppBuildConfig,
+    ) = WelcomePageViewModelFactory(appInstallStore, context, pixel, defaultRoleBrowserDialog, appBuildConfig)
 
     @Provides
     fun defaultRoleBrowserDialog(
