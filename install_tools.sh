117e36938d0f6f592e8f57ac2e613f7749b6a7bc
David González <malmstein@gmail.com>
2023-01-20 14:03:03 +0100

    Update install script NDK to 21 (#2739)
    Task/Issue URL:
    https://app.asana.com/0/1157893581871903/1203769237130039
    
    ### Description
    This PR updates NDK to 25, since the version we were using is no longer
    available
    
    ### Steps to test this PR
    Usual smoke tests

--- install_tools.sh
+++ install_tools.sh
@@ -3,7 +3,7 @@ echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/androi
 echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license";
 echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license";
 echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "$ANDROID_HOME/licenses/android-sdk-license";
-sdkmanager 'ndk;21.0.6113669'
+sdkmanager 'ndk;21.4.7075529'
 sdkmanager tools;
 yes | sdkmanager --licenses;
 echo "Fetching ndk-bundle. Suppressing output to avoid travis 4MG size limit";
