5075baf5c8658a54bed9cc6c5b751d975503eb31
David González <malmstein@gmail.com>
2023-02-06 14:02:14 +0100

    added new entities and relations table

---
+++ sync/sync-store/src/main/java/com/duckduckgo/sync/store/RoomEntities.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.sync.store
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "entities")
+data class Entity(
+    @PrimaryKey var id: String,
+    var title: String,
+    var url: String?,
+    @Embedded var type: EntityType
+)
+
+sealed class EntityType {
+    object Bookmark : EntityType()
+    object Folder : EntityType()
+}
+
+@Entity(tableName = "relations")
+data class Relation(
+    @PrimaryKey var id: String,
+    var children: List<String>
+)
