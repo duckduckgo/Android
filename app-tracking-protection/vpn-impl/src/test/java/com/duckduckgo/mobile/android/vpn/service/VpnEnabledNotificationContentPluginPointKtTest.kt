2bd099b9b0c886dc9e2a6a0f04746eecbd8596ba
Aitor Viana <aitorvs@gmail.com>
2023-02-09 11:07:07 +0000

    Fix NoSuchElementException in notification content plugin (#2828)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1203928902312895/f
    
    ### Description
    See [asana task](https://app.asana.com/0/488551667048375/1203928902312895/f) for description
    
    ### Steps to test this PR
    _AppTP Notifications_
    - [x] fresh install from this branch
    - [x] launch app and enable AppTP
    - [x] verify ongoing notification shows and text is `Scanning for tracking activity… beep… boop`
    - [x] open any one app that tracks
    - [x] verify ongoing notification shows and text is `Tracking attempts blocked in 1 app (past hour).`
    - [x] open any second app that tracks
    - [x] verify ongoing notification shows and text is `Tracking attempts blocked across 2 apps (past hour).`
    - [x] Force close the app
    - [x] notification should disappear
    - [x] re-open app
    - [x] verify ongoing notification shows and text is `Tracking attempts blocked across 2 apps (past hour).`
    - [x] verify tapping on the notification takes the user to AppTP screen

---
+++ app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/service/VpnEnabledNotificationContentPluginPointKtTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.mobile.android.vpn.service
+
+import com.duckduckgo.app.global.plugins.PluginPoint
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class VpnEnabledNotificationContentPluginPointKtTest {
+    @Test
+    fun whenEmptyPluginPointThenReturnNull() {
+        assertNull(createPluginPoint(emptyList()).getHighestPriorityPlugin())
+    }
+
+    @Test
+    fun whenInactivePluginsThenReturnNull() {
+        assertNull(
+            createPluginPoint(
+                listOf(
+                    FakeVpnEnabledNotificationContentPlugin(isActive = false),
+                    FakeVpnEnabledNotificationContentPlugin(isActive = false),
+                ),
+            ).getHighestPriorityPlugin(),
+        )
+    }
+
+    @Test
+    fun whenActivePluginsThenReturnThemInPriorityOrder() {
+        val plugin = createPluginPoint(
+            listOf(
+                FakeVpnEnabledNotificationContentPlugin(isActive = true, VpnEnabledNotificationContentPlugin.VpnEnabledNotificationPriority.NORMAL),
+                FakeVpnEnabledNotificationContentPlugin(isActive = true, VpnEnabledNotificationContentPlugin.VpnEnabledNotificationPriority.LOW),
+                FakeVpnEnabledNotificationContentPlugin(isActive = true, VpnEnabledNotificationContentPlugin.VpnEnabledNotificationPriority.HIGH),
+            ),
+        ).getHighestPriorityPlugin()
+
+        assertEquals(VpnEnabledNotificationContentPlugin.VpnEnabledNotificationPriority.HIGH, plugin?.getPriority())
+    }
+
+    private fun createPluginPoint(plugins: List<VpnEnabledNotificationContentPlugin>): PluginPoint<VpnEnabledNotificationContentPlugin> {
+        return object : PluginPoint<VpnEnabledNotificationContentPlugin> {
+            override fun getPlugins(): Collection<VpnEnabledNotificationContentPlugin> {
+                return plugins
+            }
+        }
+    }
+}
