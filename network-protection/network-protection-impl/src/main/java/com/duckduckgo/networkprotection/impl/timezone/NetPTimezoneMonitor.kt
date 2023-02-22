081ab10b4c6f41227cbd4c8f7d513fe0faf12d5f
Aitor Viana <aitorvs@gmail.com>
2023-02-22 10:29:11 +0000

    Re-connect to closest server when TZ changes (#52)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1204017339801850/f
    
    ### Description
    Re-connect to closest server when TZ changes
    
    ### Steps to test this PR
    - [x] install from this branch
    - [x] enable NetP
    - [x] verify the proper server is selected according to your TZ
    - [x] Go to Android settings -> Date & Time -> change timezone to GMT-9:00
    - [x] swipe down and click on NetP notification
    - [x] verify selected server is El Segundo
    - [x] Go to Android settings -> Date & Time -> change timezone to GMT-4:00
    - [x] swipe down and click on NetP notification
    - [x] verify selected server is Newark
    - [x] Go to Android settings -> Date & Time -> use network provided timezone
    - [x] swipe down and click on NetP notification
    - [x] verify selected server is the one corresponding to your TZ

---
+++ network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/timezone/NetPTimezoneMonitor.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.networkprotection.impl.timezone
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import com.duckduckgo.di.scopes.VpnScope
+import com.duckduckgo.mobile.android.vpn.VpnFeaturesRegistry
+import com.duckduckgo.mobile.android.vpn.service.VpnServiceCallbacks
+import com.duckduckgo.mobile.android.vpn.state.VpnStateMonitor
+import com.duckduckgo.networkprotection.impl.NetPVpnFeature
+import com.squareup.anvil.annotations.ContributesMultibinding
+import dagger.SingleInstanceIn
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import logcat.logcat
+
+@SingleInstanceIn(VpnScope::class)
+@ContributesMultibinding(
+    scope = VpnScope::class,
+    boundType = VpnServiceCallbacks::class,
+)
+class NetPTimezoneMonitor @Inject constructor(
+    private val vpnFeaturesRegistry: VpnFeaturesRegistry,
+    private val context: Context,
+) : BroadcastReceiver(), VpnServiceCallbacks {
+    override fun onVpnStarted(coroutineScope: CoroutineScope) {
+        if (!vpnFeaturesRegistry.isFeatureRegistered(NetPVpnFeature.NETP_VPN)) {
+            logcat { "NetP not enabled, skip registering timezone monitor" }
+            return
+        }
+
+        logcat { "Registering NetP TimeZone monitor" }
+        register()
+    }
+
+    override fun onVpnStopped(coroutineScope: CoroutineScope, vpnStopReason: VpnStateMonitor.VpnStopReason) {
+        logcat { "Unregistering NetP TimeZone monitor" }
+        unregister()
+    }
+
+    override fun onReceive(context: Context?, intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_TIMEZONE_CHANGED -> {
+                val pendingResult = goAsync()
+                execAsync(pendingResult) {
+                    vpnFeaturesRegistry.refreshFeature(NetPVpnFeature.NETP_VPN)
+                }
+            }
+        }
+    }
+
+    private fun register() {
+        runCatching { context.unregisterReceiver(this) }
+
+        IntentFilter().apply {
+            addAction(Intent.ACTION_TIMEZONE_CHANGED)
+        }.run {
+            context.registerReceiver(this@NetPTimezoneMonitor, this)
+        }
+    }
+
+    private fun unregister() {
+        runCatching { context.unregisterReceiver(this) }
+    }
+}
+
+@Suppress("NoHardcodedCoroutineDispatcher")
+private fun execAsync(
+    pendingResult: BroadcastReceiver.PendingResult?,
+    coroutineScope: CoroutineScope = GlobalScope,
+    block: suspend () -> Unit,
+) {
+    coroutineScope.launch(Dispatchers.IO) {
+        try {
+            block()
+        } finally {
+            // Always call finish(), even if the coroutineScope was cancelled
+            pendingResult?.finish()
+        }
+    }
+}
