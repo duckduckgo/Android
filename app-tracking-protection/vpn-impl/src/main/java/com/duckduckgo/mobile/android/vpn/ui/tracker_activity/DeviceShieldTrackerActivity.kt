b09ec8decace1979297b87ed9203d086c256c41a
Ana Capatina <anikiki@gmail.com>
2023-03-10 12:40:10 +0000

    Updated assets to match the new design.

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/DeviceShieldTrackerActivity.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/DeviceShieldTrackerActivity.kt
@@ -516,7 +516,7 @@ class DeviceShieldTrackerActivity :
         if (runningState.state == VpnRunningState.ENABLED) {
             binding.deviceShieldTrackerBlockingTrackersDescription.text =
                 resources.getString(R.string.atp_ActivityBlockingTrackersEnabledDescription)
-            binding.deviceShieldTrackerPhoneAndLockImage.setImageResource(R.drawable.phone_and_lock_enabled)
+            binding.deviceShieldTrackerShieldImage.setImageResource(R.drawable.apptp_shield_enabled)
             if (runningState.alwaysOnState.isAlwaysOnLockedDown()) {
                 binding.deviceShieldTrackerLabelEnabled.gone()
 
@@ -548,7 +548,7 @@ class DeviceShieldTrackerActivity :
         } else {
             binding.deviceShieldTrackerBlockingTrackersDescription.text =
                 resources.getString(R.string.atp_ActivityBlockingTrackersDisabledDescription)
-            binding.deviceShieldTrackerPhoneAndLockImage.setImageResource(R.drawable.phone_and_lock_disabled)
+            binding.deviceShieldTrackerShieldImage.setImageResource(R.drawable.apptp_shield_disabled)
             binding.deviceShieldTrackerLabelEnabled.gone()
 
             val (disabledLabel, annotation) = if (runningState.stopReason == REVOKED) {
