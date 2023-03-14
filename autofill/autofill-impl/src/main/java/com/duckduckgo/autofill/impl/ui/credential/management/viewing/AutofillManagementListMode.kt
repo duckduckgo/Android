f49a8d556a5a4281c113a76a00ff900e074f71ad
Craig Russell <CDRussell@users.noreply.github.com>
2023-03-14 11:43:25 +0000

    Translations for credential management placeholder text when autofill disabled (#2933)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
        3. Make sure these changes are tested in API 23 and API 26
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    -->
    
    Task/Issue URL: https://app.asana.com/0/0/1204136958002926/f
    
    ### Description
    Translations for the placeholder text in autofill management screen,
    when autofill disabled and there are no logins
    
    ---------
    
    Co-authored-by: Marcos Holgado <marcosh@duckduckgo.com>

--- autofill/autofill-impl/src/main/java/com/duckduckgo/autofill/impl/ui/credential/management/viewing/AutofillManagementListMode.kt
+++ autofill/autofill-impl/src/main/java/com/duckduckgo/autofill/impl/ui/credential/management/viewing/AutofillManagementListMode.kt
@@ -180,7 +180,7 @@ class AutofillManagementListMode : DuckDuckGoFragment(R.layout.fragment_autofill
                 viewModel.viewState.collect { state ->
                     binding.enabledToggle.quietlySetIsChecked(state.autofillEnabled, globalAutofillToggleListener)
                     state.logins?.let {
-                        credentialsListUpdated(it, state.credentialSearchQuery)
+                        credentialsListUpdated(it, state.credentialSearchQuery, state.autofillEnabled)
                         parentActivity()?.invalidateOptionsMenu()
                     }
 
@@ -220,9 +220,10 @@ class AutofillManagementListMode : DuckDuckGoFragment(R.layout.fragment_autofill
     private fun credentialsListUpdated(
         credentials: List<LoginCredentials>,
         credentialSearchQuery: String,
+        autofillEnabled: Boolean,
     ) {
         if (credentials.isEmpty() && credentialSearchQuery.isEmpty()) {
-            showEmptyCredentialsPlaceholders()
+            showEmptyCredentialsPlaceholders(autofillEnabled)
         } else if (credentials.isEmpty()) {
             showNoResultsPlaceholders(credentialSearchQuery)
         } else {
@@ -236,9 +237,16 @@ class AutofillManagementListMode : DuckDuckGoFragment(R.layout.fragment_autofill
         adapter.showNoMatchingSearchResults(query)
     }
 
-    private fun showEmptyCredentialsPlaceholders() {
-        binding.emptyStateLayout.emptyStateContainer.show()
+    private fun showEmptyCredentialsPlaceholders(autofillEnabled: Boolean) {
         binding.logins.gone()
+        with(binding.emptyStateLayout) {
+            emptyStateContainer.show()
+            emptyPlaceholderTitle.text = if (autofillEnabled) {
+                getString(R.string.credentialManagementNoLoginsSavedTitle)
+            } else {
+                getString(R.string.credentialManagementNoLoginsSavedAutofillDisabledTitle)
+            }
+        }
     }
 
     private fun renderCredentialList(credentials: List<LoginCredentials>) {
