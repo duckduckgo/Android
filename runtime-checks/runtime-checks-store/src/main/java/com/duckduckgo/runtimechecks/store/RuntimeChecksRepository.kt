741aa9c014c40639512aab325ecdebfc3a7f4d8e
Josh Leibstein <joshliebe@gmail.com>
2023-03-15 14:50:14 +0000

    Add runtime checks module

---
+++ runtime-checks/runtime-checks-store/src/main/java/com/duckduckgo/runtimechecks/store/RuntimeChecksRepository.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.runtimechecks.store
+
+import com.duckduckgo.app.global.DispatcherProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+interface RuntimeChecksRepository {
+    fun updateAll(
+        runtimeChecksEntity: RuntimeChecksEntity,
+    )
+    var runtimeChecksEntity: RuntimeChecksEntity
+}
+
+class RealRuntimeChecksRepository constructor(
+    val database: RuntimeChecksDatabase,
+    private val coroutineScope: CoroutineScope,
+    val dispatcherProvider: DispatcherProvider,
+) : RuntimeChecksRepository {
+
+    private val runtimeChecksDao: RuntimeChecksDao = database.runtimeChecksDao()
+    override var runtimeChecksEntity = RuntimeChecksEntity(json = EMPTY_JSON)
+
+    init {
+        coroutineScope.launch(dispatcherProvider.io()) {
+            loadToMemory()
+        }
+    }
+
+    override fun updateAll(runtimeChecksEntity: RuntimeChecksEntity) {
+        runtimeChecksDao.updateAll(runtimeChecksEntity)
+        loadToMemory()
+    }
+
+    private fun loadToMemory() {
+        runtimeChecksEntity =
+            runtimeChecksDao.get() ?: RuntimeChecksEntity(json = EMPTY_JSON)
+    }
+
+    companion object {
+        const val EMPTY_JSON = "{}"
+    }
+}
