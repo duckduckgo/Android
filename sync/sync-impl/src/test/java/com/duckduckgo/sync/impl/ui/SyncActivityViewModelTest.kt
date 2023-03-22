1adf63a89a14e2bb35469614ca3be397a235a585
Cristian Monforte <cristian@duckduckgo.com>
2023-03-22 11:36:53 +0100

    Sync Main activity skeleton (#2967)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
        3. Make sure these changes are tested in API 23 and API 26
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    -->
    
    Task/Issue URL: https://app.asana.com/0/0/1204195738264046/f 
    
    ### Description
    Implement skeleton for sync main activity
    Just UI elements
    Will show toggle with expected state
    If user signed in, show QR
    Other parts of app are not functional
    
    ### Steps to test this PR
    
    _Feature 1_ (to create or delete an account, use sync internal settings)
    - [x] go to sync activity
    - [x] if account exists toggle should be on
    - [x] QR should be visible
    - [x] if account doesn't exist, toggle should be off
    - [x] account info should not be visible
    - [x] hint is shown below toggle
    
    ### UI changes
    | Before  | After |
    | ------ | ----- |
    !(Upload before screenshot)|(Upload after screenshot)|

---
+++ sync/sync-impl/src/test/java/com/duckduckgo/sync/impl/ui/SyncActivityViewModelTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.sync.impl.ui
+
+import app.cash.turbine.test
+import com.duckduckgo.app.CoroutineTestRule
+import com.duckduckgo.sync.impl.SyncRepository
+import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command
+import com.duckduckgo.sync.impl.ui.SyncActivityViewModel.Command.LaunchDeviceSetupFlow
+import java.lang.String.format
+import kotlin.reflect.KClass
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+class SyncActivityViewModelTest {
+
+    @get:Rule
+    val coroutineTestRule: CoroutineTestRule = CoroutineTestRule()
+
+    private val syncRepository: SyncRepository = mock()
+    private val testee = SyncActivityViewModel(
+        syncRepository = syncRepository,
+        dispatchers = coroutineTestRule.testDispatcherProvider,
+    )
+
+    @Test
+    fun whenUserSignedInThenDeviceSyncViewStateIsEnabled() = runTest {
+        whenever(syncRepository.isSignedIn()).thenReturn(true)
+
+        testee.viewState().test {
+            val viewState = awaitItem()
+            assertTrue(viewState.isDeviceSyncEnabled)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun whenToggleEnabledThenLaunchSetupFlow() = runTest {
+        testee.onToggleClicked(true)
+
+        testee.commands().test {
+            awaitItem().assertCommandType(LaunchDeviceSetupFlow::class)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    private fun Command.assertCommandType(expectedType: KClass<out Command>) {
+        assertTrue(format("Unexpected command type: %s", this::class.simpleName), this::class == expectedType)
+    }
+}
