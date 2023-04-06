9388144b7ea512d7139c66d7ff198373cdd24ce2
Aitor Viana <aitorvs@gmail.com>
2023-04-06 16:59:49 +0100

    Icon colors based on tracker name (#3034)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1203581260317501/f
    
    ### Description
    When we don't have the icon of the tracking company we create an icon on
    the fly with the first letter but fixed background color.
    
    This PR changes the logic so that the color is picked from a color
    pallete and calculated directly from the company name text
    
    ### Steps to test this PR
    Smoke tests for AppTP
    
    _Icon colors_
    - [x] install from this branch
    - [x] enable AppTP
    - [x] install termux app
    - [x] execute `curl -v <domain> &` for the following domains, ie. `https://logs.datadoghq.com` and `https://mobile.adsafeprotected.com`
    - [x] go to AppTP activity screen
    - [x] verify icons have colors
    - [x] go to AppTP detail screen (tap on the termux entry)
    - [x] verify icons have the same color
    - [x] close and relaunch the app
    - [x] verify the colors continue to be the same in above screens

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/AppTPCompanyDetailsAdapter.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/AppTPCompanyDetailsAdapter.kt
@@ -17,7 +17,6 @@
 package com.duckduckgo.mobile.android.vpn.ui.tracker_activity
 
 import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,11 +26,11 @@ import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.duckduckgo.mobile.android.ui.TextDrawable
 import com.duckduckgo.mobile.android.ui.view.Chip
 import com.duckduckgo.mobile.android.ui.view.gone
 import com.duckduckgo.mobile.android.ui.view.show
 import com.duckduckgo.mobile.android.vpn.R
+import com.duckduckgo.mobile.android.vpn.ui.util.TextDrawable
 import java.util.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -120,13 +119,13 @@ class AppTPCompanyDetailsAdapter : RecyclerView.Adapter<AppTPCompanyDetailsAdapt
             }
         }
 
-        var badgeImage: ImageView = view.findViewById(R.id.tracking_company_icon)
-        var companyName: TextView = view.findViewById(R.id.tracking_company_name)
-        var trackingAttempts: TextView = view.findViewById(R.id.tracking_company_attempts)
-        var showMore: TextView = view.findViewById(R.id.tracking_company_show_more)
-        var topSignalsLayout: LinearLayout = view.findViewById(R.id.tracking_company_top_signals)
-        var bottomSignalsLayout: LinearLayout = view.findViewById(R.id.tracking_company_bottom_signals)
-        var showLess: TextView = view.findViewById(R.id.tracking_company_show_less)
+        private var badgeImage: ImageView = view.findViewById(R.id.tracking_company_icon)
+        private var companyName: TextView = view.findViewById(R.id.tracking_company_name)
+        private var trackingAttempts: TextView = view.findViewById(R.id.tracking_company_attempts)
+        private var showMore: TextView = view.findViewById(R.id.tracking_company_show_more)
+        private var topSignalsLayout: LinearLayout = view.findViewById(R.id.tracking_company_top_signals)
+        private var bottomSignalsLayout: LinearLayout = view.findViewById(R.id.tracking_company_bottom_signals)
+        private var showLess: TextView = view.findViewById(R.id.tracking_company_show_less)
 
         fun bind(
             companyDetails: AppTPCompanyTrackersViewModel.CompanyTrackingDetails,
@@ -139,7 +138,7 @@ class AppTPCompanyDetailsAdapter : RecyclerView.Adapter<AppTPCompanyDetailsAdapt
                         .beginConfig()
                         .fontSize(50)
                         .endConfig()
-                        .buildRound(companyDetails.companyName.take(1), Color.DKGRAY),
+                        .buildRound(companyDetails.companyName.take(1)),
                 )
             } else {
                 badgeImage.setImageResource(badge)
