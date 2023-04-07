5172488210673beb4327b600c7ec4158bbc81e2b
David González <malmstein@gmail.com>
2023-04-07 11:23:27 +0200

    Sync Engine: Plugins

---
+++ sync/sync-api/src/main/java/com/duckduckgo/sync/api/SyncEngine.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.sync.api
+
+interface SyncablePlugin{
+
+    /**
+     * Used by the SyncClient to get all the updates from each syncable feature
+     * since a specific time
+     * This data that will be sent to the Sync API
+     */
+    fun getChanges(since: String): SyncChanges
+
+    /**
+     * Changes from Sync Client have been received
+     * Each feature is responsible for merging and solving conflicts
+     */
+    fun syncChanges(changes: List<SyncChanges>, timestamp: String)
+
+}
+
+data class SyncChanges(val type: SyncableType, val updatesJSON: String)
+
+enum class SyncableType {
+    BOOKMARKS
+}
