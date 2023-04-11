1178ec43077d5c2f627f142158accdb92bfa6f85
Kate Manning <20972610+laghee@users.noreply.github.com>
2023-04-11 23:47:00 +0200

    Revert version

--- app-tracking-protection/vpn-store/src/test/java/com/duckduckgo/mobile/android/vpn/trackers/AppTrackerJsonParserTest.kt
+++ app-tracking-protection/vpn-store/src/test/java/com/duckduckgo/mobile/android/vpn/trackers/AppTrackerJsonParserTest.kt
@@ -30,7 +30,7 @@ class AppTrackerJsonParserTest {
         val json = loadText(javaClass.classLoader!!, "full_app_trackers_blocklist.json")
         val blocklist = AppTrackerJsonParser.parseAppTrackerJson(moshi, json)
 
-        Assert.assertEquals("1680299195572", blocklist.version)
+        Assert.assertEquals("1639624032609", blocklist.version)
         Assert.assertEquals(302, blocklist.trackers.count())
         Assert.assertEquals(406, blocklist.packages.count())
         Assert.assertEquals(130, blocklist.entities.count())
