c809edac3c98edd42331084cafa965b4c963ccd4
Ana Capatina <anikiki@users.noreply.github.com>
2023-04-17 11:15:35 +0100

    Add ATB to relevant pixels (#3061)
    Task/Issue URL: https://app.asana.com/0/0/1204253343689086/f
    
    ### Description
    Added 2 new temporary pixels and temporary ATB info.
    
    ### Steps to test this PR
    See https://app.asana.com/0/0/1204253343689086/1204389170821809/f
    
    ### NO UI changes

--- app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/pixels/RealDeviceShieldPixelsTest.kt
+++ app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/pixels/RealDeviceShieldPixelsTest.kt
@@ -47,6 +47,7 @@ class RealDeviceShieldPixelsTest {
         deviceShieldPixels.deviceShieldEnabledOnSearch()
 
         verify(pixel).fire(DeviceShieldPixelNames.ATP_ENABLE_UPON_SEARCH_DAILY.pixelName)
+        verify(pixel, times(2)).fire(DeviceShieldPixelNames.ATP_ENABLE_UPON_SEARCH.pixelName)
         verifyNoMoreInteractions(pixel)
     }
 
@@ -56,6 +57,7 @@ class RealDeviceShieldPixelsTest {
         deviceShieldPixels.deviceShieldDisabledOnSearch()
 
         verify(pixel).fire(DeviceShieldPixelNames.ATP_DISABLE_UPON_SEARCH_DAILY.pixelName)
+        verify(pixel, times(2)).fire(DeviceShieldPixelNames.ATP_DISABLE_UPON_SEARCH.pixelName)
         verifyNoMoreInteractions(pixel)
     }
 
