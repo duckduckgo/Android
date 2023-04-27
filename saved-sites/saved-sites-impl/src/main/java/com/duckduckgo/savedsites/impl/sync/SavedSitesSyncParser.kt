32c9d25bce74016ebde2fb7ef1bb92b3c101e92b
David González <malmstein@gmail.com>
2023-04-28 00:24:24 +0200

    parsing favorites

--- saved-sites/saved-sites-impl/src/main/java/com/duckduckgo/savedsites/impl/sync/SavedSitesSyncParser.kt
+++ saved-sites/saved-sites-impl/src/main/java/com/duckduckgo/savedsites/impl/sync/SavedSitesSyncParser.kt
@@ -18,6 +18,7 @@ package com.duckduckgo.savedsites.impl.sync
 
 import com.duckduckgo.di.scopes.AppScope
 import com.duckduckgo.savedsites.api.SavedSitesRepository
+import com.duckduckgo.savedsites.api.models.SavedSitesNames
 import com.duckduckgo.sync.api.SyncCrypto
 import com.duckduckgo.sync.api.engine.SyncChanges
 import com.duckduckgo.sync.api.engine.SyncParser
@@ -25,6 +26,9 @@ import com.duckduckgo.sync.api.engine.SyncablePlugin
 import com.duckduckgo.sync.api.engine.SyncableType.BOOKMARKS
 import com.squareup.anvil.annotations.ContributesBinding
 import com.squareup.anvil.annotations.ContributesMultibinding
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import timber.log.Timber
 import javax.inject.Inject
 
 @ContributesMultibinding(scope = AppScope::class, boundType = SyncablePlugin::class)
@@ -50,7 +54,21 @@ class SavedSitesSyncParser @Inject constructor(
             return SyncChanges(BOOKMARKS, "")
         }
 
-        return SyncChanges(BOOKMARKS, "")
+        val updates = mutableListOf<SyncBookmarkEntry>()
+        // favorites (we don't add individual items, they are added as we go through bookmark folders)
+        if (hasFavorites) {
+            val favorites = repository.getFavoritesSync()
+            updates.add(
+                SyncBookmarkEntry.asFolder(
+                    id = SavedSitesNames.FAVORITES_ROOT,
+                    title = syncCrypto.encrypt(SavedSitesNames.FAVORITES_NAME),
+                    children = favorites.map { it.id },
+                    deleted = null,
+                ),
+            )
+        }
+
+        return formatUpdates(updates)
     }
 
     override fun getChanges(since: String): SyncChanges {
@@ -58,9 +76,62 @@ class SavedSitesSyncParser @Inject constructor(
         return parseChanges(since)
     }
 
+    private fun formatUpdates(updates: List<SyncBookmarkEntry>): SyncChanges {
+        val bookmarkUpdates = SyncBookmarkUpdates(updates)
+        val patch = SyncDataRequest(bookmarkUpdates)
+        val allDataJSON = Adapters.patchAdapter.toJson(patch)
+
+        return SyncChanges(BOOKMARKS, allDataJSON)
+    }
+
     override fun syncChanges(
         changes: List<SyncChanges>,
         timestamp: String,
     ) {
     }
+
+    private class Adapters {
+        companion object {
+            private val moshi = Moshi.Builder().build()
+            val patchAdapter: JsonAdapter<SyncDataRequest> =
+                moshi.adapter(SyncDataRequest::class.java)
+        }
+    }
 }
+
+data class SyncBookmarkPage(val url: String)
+data class SyncFolderChildren(val children: List<String>)
+
+data class SyncBookmarkEntry(
+    val id: String,
+    val title: String,
+    val page: SyncBookmarkPage?,
+    val folder: SyncFolderChildren?,
+    val deleted: String?,
+) {
+    companion object {
+        fun asBookmark(
+            id: String,
+            title: String,
+            url: String,
+            deleted: String?,
+        ): SyncBookmarkEntry {
+            return SyncBookmarkEntry(id, title, SyncBookmarkPage(url), null, deleted)
+        }
+
+        fun asFolder(
+            id: String,
+            title: String,
+            children: List<String>,
+            deleted: String?,
+        ): SyncBookmarkEntry {
+            return SyncBookmarkEntry(id, title, null, SyncFolderChildren(children), deleted)
+        }
+    }
+}
+
+fun SyncBookmarkEntry.isFolder(): Boolean = this.folder != null
+fun SyncBookmarkEntry.isBookmark(): Boolean = this.page != null
+
+class SyncDataRequest(val bookmarks: SyncBookmarkUpdates)
+class SyncBookmarkUpdates(val updates: List<SyncBookmarkEntry>)
