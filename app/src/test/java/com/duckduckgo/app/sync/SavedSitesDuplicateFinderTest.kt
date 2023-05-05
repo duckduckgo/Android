b827fd408bd58f592b8a14f36c3bef8967e79598
David González <malmstein@gmail.com>
2023-05-05 09:47:47 +0200

    added room testing for parser

---
+++ app/src/test/java/com/duckduckgo/app/sync/SavedSitesDuplicateFinderTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.sync
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.duckduckgo.app.CoroutineTestRule
+import com.duckduckgo.app.global.db.AppDatabase
+import com.duckduckgo.savedsites.api.SavedSitesRepository
+import com.duckduckgo.savedsites.impl.RealSavedSitesRepository
+import com.duckduckgo.savedsites.store.SavedSitesEntitiesDao
+import com.duckduckgo.savedsites.store.SavedSitesRelationsDao
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * TODO: Move this test out of app module and into saved-sites module once AppDatabase is in its own module
+ */
+@RunWith(AndroidJUnit4::class)
+class SavedSitesDuplicateFinderTest {
+    @get:Rule
+    @Suppress("unused")
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule()
+
+    private lateinit var db: AppDatabase
+    private lateinit var repository: SavedSitesRepository
+    private lateinit var savedSitesEntitiesDao: SavedSitesEntitiesDao
+    private lateinit var savedSitesRelationsDao: SavedSitesRelationsDao
+
+    @Before
+    fun setup() {
+        db = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().targetContext, AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        savedSitesEntitiesDao = db.syncEntitiesDao()
+        savedSitesRelationsDao = db.syncRelationsDao()
+
+        repository = RealSavedSitesRepository(savedSitesEntitiesDao, savedSitesRelationsDao)
+    }
+
+    @Test
+    fun whenBookmarkAlreadyExistsThenDuplicateIsFound(){
+
+        
+
+    }
+}
