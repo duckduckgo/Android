4d5e6284d5ebdd004616482a29fe8a2417a4b8c4
Noelia Alcala <nalcalag@gmail.com>
2023-05-26 15:14:56 +0100

    Remove Onboarding Customization Experiment (#3198)
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1204650250821526/f
    
    ### Description
    Remove onboarding customization experiment from the code
    
    ### Steps to test this PR
    
    - **Fresh** install from this branch
    - [x] Check onboarding flow matches control one
            1. Allow notifications bottomsheet
            2. Welcome animation
            3. Welcome dax dialog with primary cta button
    4. When button is tapped -> Set as default alert (if not default
    already)

--- app/src/main/java/com/duckduckgo/app/onboarding/di/WelcomePageModule.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/di/WelcomePageModule.kt
@@ -21,7 +21,6 @@ import com.duckduckgo.app.global.DefaultRoleBrowserDialog
 import com.duckduckgo.app.global.RealDefaultRoleBrowserDialog
 import com.duckduckgo.app.global.install.AppInstallStore
 import com.duckduckgo.app.onboarding.ui.page.WelcomePageViewModelFactory
-import com.duckduckgo.app.statistics.VariantManager
 import com.duckduckgo.app.statistics.pixels.Pixel
 import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import dagger.Module
@@ -36,8 +35,7 @@ class WelcomePageModule {
         context: Context,
         pixel: Pixel,
         defaultRoleBrowserDialog: DefaultRoleBrowserDialog,
-        variantManager: VariantManager,
-    ) = WelcomePageViewModelFactory(appInstallStore, context, pixel, defaultRoleBrowserDialog, variantManager)
+    ) = WelcomePageViewModelFactory(appInstallStore, context, pixel, defaultRoleBrowserDialog)
 
     @Provides
     fun defaultRoleBrowserDialog(
