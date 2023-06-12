cd41e1046f4d998de37238b5154000b74705b2cc
Ana Capatina <anikiki@users.noreply.github.com>
2023-06-12 12:49:31 +0100

    Create the new "Permissions and Privacy" screen (#3230)
    Task/Issue URL: https://app.asana.com/0/0/1204667877992130/f 
    
    ### Description
    Created the new "Permissions and Privacy" screen.
    
    ### Steps to test this PR
    
    - [ ] Install from  this branch and go to the `Settings` screen. 
    - [ ] Tap on the `Permissions and Privacy` item and notice the new
    screen.
    - [ ] Tap on each item on the `Permissions and Privacy` screen and
    notice they behave as before.
    
    ### UI changes
    | New screen  | New screen scrolled to bottom |
    | ------ | ----- |
    
    |![permissions_and_privacy_1](https://github.com/duckduckgo/Android/assets/7963079/b87e9ec3-4351-425d-abb2-3f0d619f14b6)|![permissions_and_privacy_2](https://github.com/duckduckgo/Android/assets/7963079/34313aea-e906-4b36-88ce-e369394d6599)|

---
+++ app/src/main/java/com/duckduckgo/app/appearance/ChangeIconContract.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.appearance
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import com.duckduckgo.app.icon.ui.ChangeIconActivity
+
+class ChangeIconContract : ActivityResultContract<Void?, Boolean>() {
+    override fun createIntent(
+        context: Context,
+        input: Void?,
+    ): Intent {
+        return ChangeIconActivity.intent(context)
+    }
+
+    override fun parseResult(
+        resultCode: Int,
+        intent: Intent?,
+    ): Boolean {
+        return resultCode == Activity.RESULT_OK
+    }
+}
