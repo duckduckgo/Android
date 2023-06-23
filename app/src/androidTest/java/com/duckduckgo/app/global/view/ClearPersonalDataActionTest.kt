c442730d029611eb43e75f15a94da632b45dd35e
David González <malmstein@gmail.com>
2023-06-23 10:50:01 -0400

    Saved Sites: prune objects on app launch / fire button (#3276)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
        3. Make sure these changes are tested in API 23 and API 26
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    -->
    
    Task/Issue URL:
    https://app.asana.com/0/1201493110486074/1204879487866164/f
    Task/Issue URL:
    https://app.asana.com/0/1201493110486074/1204874751768176/f
    ### Description:
    
    ### Steps to test this PR
    
    _Sync Disabled - App open clearing_
    - [ ] Open app and add some bookmarks
    - [ ] Delete bookmark
    - [ ] Bookmark should still be in the DB with deleted flag enabled, and
    title / url removed (use AppInspector)
    - [ ] Force close the app, and open it again
    - [ ] Bookmark should be done from the DB 
    
    _Sync Disabled - Fire button_
    - [ ] Open app and add some bookmarks
    - [ ] Ensure Fire Button config is set to clear all data
    - [ ] Delete bookmark
    - [ ] Bookmark should still be in the DB with deleted flag enabled, and
    title / url removed (use AppInspector)
    - [ ] Tap on the fire button
    - [ ] Bookmark should be done from the DB

--- app/src/androidTest/java/com/duckduckgo/app/global/view/ClearPersonalDataActionTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/global/view/ClearPersonalDataActionTest.kt
@@ -30,7 +30,9 @@ import com.duckduckgo.app.location.GeoLocationPermissions
 import com.duckduckgo.app.settings.db.SettingsDataStore
 import com.duckduckgo.app.tabs.model.TabRepository
 import com.duckduckgo.cookies.api.DuckDuckGoCookieManager
+import com.duckduckgo.savedsites.api.SavedSitesRepository
 import com.duckduckgo.site.permissions.api.SitePermissionsManager
+import com.duckduckgo.sync.api.DeviceSyncState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
@@ -39,6 +41,7 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
@@ -57,6 +60,8 @@ class ClearPersonalDataActionTest {
     private val mockThirdPartyCookieManager: ThirdPartyCookieManager = mock()
     private val mockAdClickManager: AdClickManager = mock()
     private val mockFireproofWebsiteRepository: FireproofWebsiteRepository = mock()
+    private val mockDeviceSyncState: DeviceSyncState = mock()
+    private val mockSavedSitesRepository: SavedSitesRepository = mock()
     private val mockSitePermissionsManager: SitePermissionsManager = mock()
 
     private val fireproofWebsites: LiveData<List<FireproofWebsiteEntity>> = MutableLiveData()
@@ -75,9 +80,12 @@ class ClearPersonalDataActionTest {
             thirdPartyCookieManager = mockThirdPartyCookieManager,
             adClickManager = mockAdClickManager,
             fireproofWebsiteRepository = mockFireproofWebsiteRepository,
+            deviceSyncState = mockDeviceSyncState,
+            savedSitesRepository = mockSavedSitesRepository,
             sitePermissionsManager = mockSitePermissionsManager,
         )
         whenever(mockFireproofWebsiteRepository.getFireproofWebsites()).thenReturn(fireproofWebsites)
+        whenever(mockDeviceSyncState.isUserSignedInOnDevice()).thenReturn(true)
     }
 
     @Test
@@ -126,5 +134,18 @@ class ClearPersonalDataActionTest {
     fun whenClearCalledThenThirdPartyCookieSitesAreCleared() = runTest {
         testee.clearTabsAndAllDataAsync(appInForeground = false, shouldFireDataClearPixel = false)
         verify(mockThirdPartyCookieManager).clearAllData()
+    }
+
+    @Test
+    fun whenClearCalledAndSyncEnabledThenSavedSitesDoesNotPruneDeleted() = runTest {
+        testee.clearTabsAndAllDataAsync(appInForeground = false, shouldFireDataClearPixel = false)
+        verifyNoInteractions(mockSavedSitesRepository)
+    }
+
+    @Test
+    fun whenClearCalledAndSyncDisabledThenSavedSitesDoesNotPruneDeleted() = runTest {
+        whenever(mockDeviceSyncState.isUserSignedInOnDevice()).thenReturn(false)
+        testee.clearTabsAndAllDataAsync(appInForeground = false, shouldFireDataClearPixel = false)
+        verify(mockSavedSitesRepository).pruneDeleted()
     }
 }
