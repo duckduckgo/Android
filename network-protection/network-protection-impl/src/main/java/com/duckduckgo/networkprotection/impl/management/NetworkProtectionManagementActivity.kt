ba2413b5ce8216236fc7202b40a70e86beba0b3b
Karl Dimla <klmbdimla@gmail.com>
2023-07-12 15:43:25 +0200

    Implement about pages

--- network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/management/NetworkProtectionManagementActivity.kt
+++ network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/management/NetworkProtectionManagementActivity.kt
@@ -43,6 +43,8 @@ import com.duckduckgo.navigation.api.GlobalActivityStarter
 import com.duckduckgo.networkprotection.api.NetPAppExclusionListNoParams
 import com.duckduckgo.networkprotection.api.NetworkProtectionManagementScreenNoParams
 import com.duckduckgo.networkprotection.impl.R
+import com.duckduckgo.networkprotection.impl.about.NetpAboutVpnActivity
+import com.duckduckgo.networkprotection.impl.about.NetpFaqActivity
 import com.duckduckgo.networkprotection.impl.databinding.ActivityNetpManagementBinding
 import com.duckduckgo.networkprotection.impl.management.NetworkProtectionManagementViewModel.AlertState.None
 import com.duckduckgo.networkprotection.impl.management.NetworkProtectionManagementViewModel.AlertState.ShowAlwaysOnLockdownEnabled
@@ -107,6 +109,14 @@ class NetworkProtectionManagementActivity : DuckDuckGoActivity() {
 
         binding.settings.settingsExclusion.setClickListener {
             globalActivityStarter.start(this, NetPAppExclusionListNoParams)
+        }
+
+        binding.about.aboutVpn.setClickListener {
+            startActivity(NetpAboutVpnActivity.intent(this))
+        }
+
+        binding.about.aboutFaq.setClickListener {
+            startActivity(NetpFaqActivity.intent(this))
         }
     }
 
