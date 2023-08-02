7084866c005d9c1ed4cf9f6e8cd747bbcdd9d71c
Aitor Viana <aitorvs@gmail.com>
2023-08-02 16:15:25 +0100

    Clean up code from dns-base blocking (#3402)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1205165295333462/f
    
    ### Description
    Code Clean up
    
    ### Steps to test this PR
    Test AppTP and NP in different network configs, ie:
    * WIFI
    * CELL
    * network switch WIFI<>CELL
    * Private DNS

--- app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/integration/NgVpnNetworkStackTest.kt
+++ app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/integration/NgVpnNetworkStackTest.kt
@@ -21,9 +21,6 @@ import com.duckduckgo.app.CoroutineTestRule
 import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import com.duckduckgo.mobile.android.app.tracking.AppTrackerDetector
 import com.duckduckgo.mobile.android.vpn.apps.TrackingProtectionAppsRepository
-import com.duckduckgo.mobile.android.vpn.feature.AppTpFeatureConfig
-import com.duckduckgo.mobile.android.vpn.feature.FakeAppTpFeatureConfig
-import com.duckduckgo.mobile.android.vpn.network.FakeDnsProvider
 import com.duckduckgo.vpn.network.api.*
 import com.duckduckgo.vpn.network.api.AddressRR
 import com.duckduckgo.vpn.network.api.DnsRR
@@ -55,14 +52,12 @@ class NgVpnNetworkStackTest {
     private val appTrackerDetector: AppTrackerDetector = mock()
     private val trackingProtectionAppsRepository: TrackingProtectionAppsRepository = mock()
 
-    private lateinit var appTpFeatureConfig: AppTpFeatureConfig
     private lateinit var ngVpnNetworkStack: NgVpnNetworkStack
 
     @Before
     fun setup() {
         whenever(vpnNetwork.create()).thenReturn(111)
         whenever(vpnNetwork.mtu()).thenReturn(1500)
-        appTpFeatureConfig = FakeAppTpFeatureConfig()
 
         ngVpnNetworkStack = NgVpnNetworkStack(
             appBuildConfig,
@@ -70,8 +65,6 @@ class NgVpnNetworkStackTest {
             runtime,
             appTrackerDetector,
             trackingProtectionAppsRepository,
-            appTpFeatureConfig,
-            FakeDnsProvider(),
         )
     }
 
