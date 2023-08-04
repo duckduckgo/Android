6dcb6cb4653c16302601d58dd4d0402993a7e533
Craig Russell <CDRussell@users.noreply.github.com>
2023-08-04 09:36:50 +0100

    Update UI of email protection bottom sheet

--- app/src/main/java/com/duckduckgo/app/browser/autofill/AutofillCredentialsSelectionResultHandler.kt
+++ app/src/main/java/com/duckduckgo/app/browser/autofill/AutofillCredentialsSelectionResultHandler.kt
@@ -16,7 +16,9 @@
 
 package com.duckduckgo.app.browser.autofill
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
@@ -26,11 +28,17 @@ import com.duckduckgo.app.di.AppCoroutineScope
 import com.duckduckgo.app.global.DefaultDispatcherProvider
 import com.duckduckgo.app.global.DispatcherProvider
 import com.duckduckgo.app.statistics.pixels.Pixel
+import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import com.duckduckgo.autofill.api.AutofillCapabilityChecker
 import com.duckduckgo.autofill.api.CredentialAutofillPickerDialog
 import com.duckduckgo.autofill.api.CredentialSavePickerDialog
 import com.duckduckgo.autofill.api.CredentialUpdateExistingCredentialsDialog
 import com.duckduckgo.autofill.api.CredentialUpdateExistingCredentialsDialog.CredentialUpdateType
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog.UseEmailResultType
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog.UseEmailResultType.DoNotUseEmailProtection
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog.UseEmailResultType.UsePersonalEmailAddress
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog.UseEmailResultType.UsePrivateAliasAddress
 import com.duckduckgo.autofill.api.ExistingCredentialMatchDetector
 import com.duckduckgo.autofill.api.UseGeneratedPasswordDialog
 import com.duckduckgo.autofill.api.domain.app.LoginCredentials
@@ -66,6 +74,7 @@ class AutofillCredentialsSelectionResultHandler @Inject constructor(
     private val autoSavedLoginsMonitor: AutomaticSavedLoginsMonitor,
     private val existingCredentialMatchDetector: ExistingCredentialMatchDetector,
     private val autofillCapabilityChecker: AutofillCapabilityChecker,
+    private val appBuildConfig: AppBuildConfig,
 ) {
 
     suspend fun processAutofillCredentialSelectionResult(
@@ -316,6 +325,26 @@ class AutofillCredentialsSelectionResultHandler @Inject constructor(
 
     fun processSaveOrUpdatePromptShown() {
         autofillDialogSuppressor.autofillSaveOrUpdateDialogVisibilityChanged(visible = true)
+    }
+
+    @SuppressLint("NewApi")
+    fun processEmailProtectionSelectEmailChoice(
+        result: Bundle,
+        viewModel: BrowserTabViewModel,
+        tabId: String,
+    ) {
+        val userSelection: UseEmailResultType = if (appBuildConfig.sdkInt >= VERSION_CODES.TIRAMISU) {
+            result.getParcelable(EmailProtectionChooserDialog.KEY_RESULT, UseEmailResultType::class.java)!!
+        } else {
+            result.getParcelable(EmailProtectionChooserDialog.KEY_RESULT)!!
+        }
+        val originalUrl = result.getString(EmailProtectionChooserDialog.KEY_URL)!!
+
+        when (userSelection) {
+            UsePersonalEmailAddress -> viewModel.useAddress(originalUrl)
+            UsePrivateAliasAddress -> viewModel.consumeAlias(originalUrl)
+            DoNotUseEmailProtection -> viewModel.cancelAutofillTooltip()
+        }
     }
 
     interface CredentialInjector {
---
+++ autofill/autofill-impl/src/main/java/com/duckduckgo/app/email/EmailAutofillTooltipFragment.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.email
+
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResult
+import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.app.global.extensions.html
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog
+import com.duckduckgo.autofill.api.EmailProtectionChooserDialog.UseEmailResultType
+import com.duckduckgo.autofill.impl.R
+import com.duckduckgo.autofill.impl.databinding.ContentAutofillTooltipBinding
+import com.duckduckgo.autofill.impl.ui.credential.dialog.animateClosed
+import com.duckduckgo.di.scopes.FragmentScope
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.android.support.AndroidSupportInjection
+import timber.log.Timber
+
+@InjectWith(FragmentScope::class)
+class EmailAutofillTooltipFragment : BottomSheetDialogFragment(), EmailProtectionChooserDialog {
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    /**
+     * To capture all the ways the BottomSheet can be dismissed, we might end up with onCancel being called when we don't want it
+     * This flag is set to true when taking an action which dismisses the dialog, but should not be treated as a cancellation.
+     */
+    private var ignoreCancellationEvents = false
+
+    override fun getTheme(): Int = R.style.AutofillBottomSheetDialogTheme
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // pixelNameDialogEvent(Shown)?.let { pixel.fire(it) }
+
+        val binding = ContentAutofillTooltipBinding.inflate(inflater, container, false)
+        configureViews(binding)
+        return binding.root
+    }
+
+    private fun configureViews(binding: ContentAutofillTooltipBinding) {
+        (dialog as BottomSheetDialog).behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        configureCloseButton(binding)
+        configureEmailButtons(binding)
+    }
+
+    private fun configureEmailButtons(binding: ContentAutofillTooltipBinding) {
+        context?.let {
+            binding.primaryCta.setPrimaryText(getPersonalAddress().html(it).toString())
+
+            binding.secondaryCta.setOnClickListener {
+                returnResult(UseEmailResultType.UsePrivateAliasAddress)
+                dismiss()
+            }
+
+            binding.primaryCta.setOnClickListener {
+                returnResult(UseEmailResultType.UsePersonalEmailAddress)
+                dismiss()
+            }
+        }
+    }
+
+    private fun returnResult(resultType: UseEmailResultType) {
+        val result = Bundle().also {
+            it.putString(EmailProtectionChooserDialog.KEY_URL, getOriginalUrl())
+            it.putParcelable(EmailProtectionChooserDialog.KEY_RESULT, resultType)
+        }
+
+        parentFragment?.setFragmentResult(EmailProtectionChooserDialog.resultKey(getTabId()), result)
+    }
+
+    private fun configureCloseButton(binding: ContentAutofillTooltipBinding) {
+        binding.closeButton.setOnClickListener { animateClosed() }
+        returnResult(UseEmailResultType.DoNotUseEmailProtection)
+    }
+
+    private fun animateClosed() {
+        (dialog as BottomSheetDialog).animateClosed()
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        if (ignoreCancellationEvents) {
+            Timber.v("onCancel: Ignoring cancellation event")
+            return
+        }
+
+        Timber.v("onCancel: GeneratePasswordDialogCancel. User declined to use generated password")
+
+        // pixelNameDialogEvent(Dismissed)?.let { pixel.fire(it) }
+
+        returnResult(UseEmailResultType.DoNotUseEmailProtection)
+    }
+
+    private fun getPersonalAddress() = arguments?.getString(EmailProtectionChooserDialog.KEY_ADDRESS)!!
+    private fun getOriginalUrl() = arguments?.getString(EmailProtectionChooserDialog.KEY_URL)!!
+    private fun getTabId() = arguments?.getString(EmailProtectionChooserDialog.KEY_TAB_ID)!!
+
+    companion object {
+        fun instance(personalDuckAddress: String, url: String, tabId: String): EmailAutofillTooltipFragment {
+            val fragment = EmailAutofillTooltipFragment()
+            fragment.arguments = Bundle().also {
+                it.putString(EmailProtectionChooserDialog.KEY_ADDRESS, personalDuckAddress)
+                it.putString(EmailProtectionChooserDialog.KEY_URL, url)
+                it.putString(EmailProtectionChooserDialog.KEY_TAB_ID, tabId)
+            }
+            return fragment
+        }
+    }
+}
