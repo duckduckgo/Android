abba9945b85edfe21eec286d2a39d7bc9327ae5b
Cristian Monforte <cristian@duckduckgo.com>
2023-09-07 21:48:51 +0200

    add java docs to api interfaces

--- settings/settings-api/src/main/java/com/duckduckgo/settings/api/SyncSettingsListener.kt
+++ settings/settings-api/src/main/java/com/duckduckgo/settings/api/SyncSettingsListener.kt
@@ -16,7 +16,15 @@
 
 package com.duckduckgo.settings.api
 
-//TODO: document api
+/**
+ * Interface to notify when a setting was changed by the user.
+ */
 interface SyncSettingsListener {
+
+    /**
+     * Should be called when a setting was changed by the user.
+     * @param settingKey the unique identifier of the setting that was changed.
+     * It should be the same as @see SyncableSetting.key
+     */
     fun onSettingChanged(settingKey: String)
 }
--- settings/settings-api/src/main/java/com/duckduckgo/settings/api/SyncableSetting.kt
+++ settings/settings-api/src/main/java/com/duckduckgo/settings/api/SyncableSetting.kt
@@ -16,14 +16,45 @@
 
 package com.duckduckgo.settings.api
 
-//TODO: document api
+/**
+ * A setting that can be synced.
+ * Implement this interface to provide a way to sync a setting with the Sync Service.
+ */
 interface SyncableSetting {
+    /**
+     * This is the unique identifier of the setting to be used as a key in the Sync Setting collection.
+     */
     val key: String
+
+    /**
+     * Setting value represented as a string.
+     * If the setting is not set or has no value, null should be returned.
+     */
     fun getValue(): String?
+
+    /**
+     * Should save a new value for the setting.
+     * @param value the new value to be stored in the local store. If value not present or deleted, will be null.
+     * @return true if local value was changed, false otherwise.
+     */
     fun save(value: String?): Boolean
 
+    /**
+     * Should provide a way to merge remote value with local value on First sync (Deduplication).
+     * @param value the remote value to be merged with the local value. If remote value not present or deleted, will be null.
+     * @return true if local value was changed, false otherwise.
+     */
     fun mergeRemote(value: String?): Boolean
 
+    /**
+     * Should provide a way to register to changes applied by Sync Service.
+     * When listener notified means that the setting value was changed by the Sync Service.
+     */
     fun registerToRemoteChanges(onDataChanged: () -> Unit)
+
+    /**
+     * Should provide a way to register to changes applied by the user.
+     * When called means that the user changed the setting value, and needs to be synced.
+     */
     fun onSettingChanged()
 }
