64adca591fff84122cc57a99bc510ded435ef76b
Josh Leibstein <joshliebe@gmail.com>
2023-09-27 10:20:58 +0100

    Change image download location to DIRECTORY_DOWNLOADS (#3581)

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -2575,7 +2575,7 @@ class BrowserTabFragment :
     ) {
         pendingFileDownload = PendingFileDownload(
             url = url,
-            subfolder = Environment.DIRECTORY_PICTURES,
+            subfolder = Environment.DIRECTORY_DOWNLOADS,
         )
 
         if (hasWriteStoragePermission()) {
