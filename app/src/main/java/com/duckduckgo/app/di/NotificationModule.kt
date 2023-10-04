8d7ba002cbacb36acc5a3dab9018bea83b41ea76
Noelia Alcala <nalcalag@gmail.com>
2023-10-04 18:43:06 +0200

    Deleted unused import

--- app/src/main/java/com/duckduckgo/app/di/NotificationModule.kt
+++ app/src/main/java/com/duckduckgo/app/di/NotificationModule.kt
@@ -30,7 +30,6 @@ import com.duckduckgo.app.notification.model.PrivacyProtectionNotification
 import com.duckduckgo.app.notification.model.SchedulableNotificationPlugin
 import com.duckduckgo.app.privacy.db.PrivacyProtectionCountDao
 import com.duckduckgo.app.settings.db.SettingsDataStore
-import com.duckduckgo.app.statistics.VariantManager
 import com.duckduckgo.di.scopes.AppScope
 import dagger.Module
 import dagger.Provides
