9132c4a1a6972a2b7cdd3d1efaf41074fd45e0cf
Josh Leibstein <joshliebe@gmail.com>
2023-10-18 18:21:14 +0100

    Set zh to 1

--- statistics/src/main/java/com/duckduckgo/app/statistics/VariantManager.kt
+++ statistics/src/main/java/com/duckduckgo/app/statistics/VariantManager.kt
@@ -47,10 +47,10 @@ interface VariantManager {
             Variant(key = "se", weight = 0.0, features = emptyList(), filterBy = { isSerpRegionToggleCountry() }),
 
             // Experiment: Ask for Default Browser More Than Once
-            Variant(key = "zh", weight = 0.0, features = emptyList(), filterBy = { isDefaultBrowserExperimentCountry() }),
+            Variant(key = "zh", weight = 1.0, features = emptyList(), filterBy = { isDefaultBrowserExperimentCountry() }),
             Variant(
                 key = "zj",
-                weight = 1.0,
+                weight = 0.0,
                 features = listOf(AskForDefaultBrowserMoreThanOnce),
                 filterBy = { isDefaultBrowserExperimentCountry() },
             ),
