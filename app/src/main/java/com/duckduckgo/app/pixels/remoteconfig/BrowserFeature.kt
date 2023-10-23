c980e04235804594444ba173f0fc959bbd6cbf9c
Ana Capatina <anikiki@users.noreply.github.com>
2023-10-23 19:52:01 +0300

    Android:  Add the full webview version to the ml pixel under a feature flag (#3720)
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1205724589991799/f
    
    ### Description
    Added the full webview version to the ml pixel under a feature flag.
    
    ### Steps to test this PR
    
    - [ ] Sample privacy-config in task description. Use the URL as value
    for `PRIVACY_REMOTE_CONFIG_URL` for testing.
    - [ ] When both `browserTelemetry` and `collectFullWebViewVersion`
    enabled you should see the full webview version sent with the `ml`
    pixel.
    
    ### NO UI changes

---
+++ app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/BrowserFeature.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.pixels.remoteconfig
+
+import com.duckduckgo.anvil.annotations.ContributesRemoteFeature
+import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.feature.toggles.api.Toggle
+
+/**
+ * This is the class that represents the browser feature flags
+ */
+@ContributesRemoteFeature(
+    scope = AppScope::class,
+    featureName = "browser",
+)
+interface BrowserFeature {
+
+    /**
+     * @return `true` when the remote config has the global "browser" feature flag enabled
+     * If the remote feature is not present defaults to `false`
+     */
+    @Toggle.DefaultValue(false)
+    fun self(): Toggle
+
+    /**
+     * @return `true` when the remote config has the global "collectFullWebViewVersion" browser sub-feature flag enabled
+     * If the remote feature is not present defaults to `false`
+     */
+    @Toggle.DefaultValue(false)
+    fun collectFullWebViewVersion(): Toggle
+}
--- statistics/src/main/java/com/duckduckgo/app/statistics/pixels/Pixel.kt
+++ statistics/src/main/java/com/duckduckgo/app/statistics/pixels/Pixel.kt
@@ -54,6 +54,7 @@ interface Pixel {
         const val COHORT = "cohort"
         const val LAST_USED_DAY = "duck_address_last_used"
         const val WEBVIEW_VERSION = "webview_version"
+        const val WEBVIEW_FULL_VERSION = "webview_full_version"
         const val OS_VERSION = "os_version"
         const val DEFAULT_BROWSER = "default_browser"
         const val EMAIL = "email"
