9689af1bd7923b37b814b03533e29ac17f049f03
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2023-10-24 09:41:12 +0200

    implement new breakage category specification

--- app/src/main/java/com/duckduckgo/app/brokensite/model/BrokenSite.kt
+++ app/src/main/java/com/duckduckgo/app/brokensite/model/BrokenSite.kt
@@ -40,25 +40,25 @@ sealed class BrokenSiteCategory(
     @StringRes val category: Int,
     val key: String,
 ) {
-    object ImagesCategory : BrokenSiteCategory(R.string.brokenSiteCategoryImages, IMAGES_CATEGORY_KEY)
+    object BlockedCategory : BrokenSiteCategory(R.string.brokenSiteCategoryBlocked, BLOCKED_CATEGORY_KEY)
+    object LayoutCategory : BrokenSiteCategory(R.string.brokenSiteCategoryLayout, LAYOUT_CATEGORY_KEY)
+    object EmptySpacesCategory : BrokenSiteCategory(R.string.brokenSiteCategoryEmptySpaces, EMPTY_SPACES_CATEGORY_KEY)
+    object ShoppingCategory : BrokenSiteCategory(R.string.brokenSiteCategoryShopping, SHOPPING_CATEGORY_KEY)
     object PaywallCategory : BrokenSiteCategory(R.string.brokenSiteCategoryPaywall, PAYWALL_CATEGORY_KEY)
     object CommentsCategory : BrokenSiteCategory(R.string.brokenSiteCategoryComments, COMMENTS_CATEGORY_KEY)
     object VideosCategory : BrokenSiteCategory(R.string.brokenSiteCategoryVideos, VIDEOS_CATEGORY_KEY)
-    object LinksCategory : BrokenSiteCategory(R.string.brokenSiteCategoryLinks, LINKS_CATEGORY_KEY)
-    object ContentCategory : BrokenSiteCategory(R.string.brokenSiteCategoryContent, CONTENT_CATEGORY_KEY)
     object LoginCategory : BrokenSiteCategory(R.string.brokenSiteCategoryLogin, LOGIN_CATEGORY_KEY)
-    object UnsupportedCategory : BrokenSiteCategory(R.string.brokenSiteCategoryUnsupported, UNSUPPORTED_CATEGORY_KEY)
     object OtherCategory : BrokenSiteCategory(R.string.brokenSiteCategoryOther, OTHER_CATEGORY_KEY)
 
     companion object {
-        const val IMAGES_CATEGORY_KEY = "images"
+        const val BLOCKED_CATEGORY_KEY = "blocked"
+        const val LAYOUT_CATEGORY_KEY = "layout"
+        const val EMPTY_SPACES_CATEGORY_KEY = "empty-spaces"
+        const val SHOPPING_CATEGORY_KEY = "shopping"
         const val PAYWALL_CATEGORY_KEY = "paywall"
         const val COMMENTS_CATEGORY_KEY = "comments"
         const val VIDEOS_CATEGORY_KEY = "videos"
-        const val LINKS_CATEGORY_KEY = "links"
-        const val CONTENT_CATEGORY_KEY = "content"
         const val LOGIN_CATEGORY_KEY = "login"
-        const val UNSUPPORTED_CATEGORY_KEY = "unsupported"
         const val OTHER_CATEGORY_KEY = "other"
     }
 }
