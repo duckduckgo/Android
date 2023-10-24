96fece7faf06cc7f751bd6bbacbd8460bce1380b
Craig Russell <CDRussell@users.noreply.github.com>
2023-10-24 17:48:59 +0100

    Add autofill default state param to key autofill pixels

--- app/src/main/java/com/duckduckgo/app/settings/SettingsViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/settings/SettingsViewModel.kt
@@ -256,7 +256,6 @@ class SettingsViewModel @Inject constructor(
 
     fun onAutofillSettingsClick() {
         viewModelScope.launch { command.send(Command.LaunchAutofillSettings) }
-        pixel.fire(SETTINGS_AUTOFILL_MANAGEMENT_OPENED)
     }
 
     fun onAccessibilitySettingClicked() {
