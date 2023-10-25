58e38f37586eec2345ca97483a92250849cb7262
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2023-10-25 21:20:46 +0200

    run UserAllowListRepositoryTest using robolectric

--- app/src/test/java/com/duckduckgo/app/privacy/db/UserAllowListRepositoryTest.kt
+++ app/src/test/java/com/duckduckgo/app/privacy/db/UserAllowListRepositoryTest.kt
@@ -18,6 +18,7 @@ package com.duckduckgo.app.privacy.db
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.duckduckgo.app.CoroutineTestRule
 import com.duckduckgo.app.global.db.AppDatabase
@@ -30,8 +31,10 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.MockitoAnnotations
 
+@RunWith(AndroidJUnit4::class)
 class UserAllowListRepositoryTest {
 
     @ExperimentalCoroutinesApi
