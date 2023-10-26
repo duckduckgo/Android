11307ea78dbc7cdca06c578964d00d8932a855e1
Craig Russell <CDRussell@users.noreply.github.com>
2023-10-26 16:03:29 +0100

    De-duplicate login suggestions offered to a user for autofilling credentials (#3702)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    
    At a minimum, make sure your changes are tested in API 23 and one of the
    more recent API levels available.
    -->
    
    Task/Issue URL:
    https://app.asana.com/0/1202552961248957/1205369440069078/f
    
    ### Description
    De-duplicates logins which are offered for autofilling into a page. The
    de-duplication only happens at the UI layer, and only for the
    bottom-sheet autofill prompt (duplicates continue to be supported in the
    DB and in the login management screens).
    
    Rules for de-duplicating are described in [Validating business rules for
    de-duplicating
    logins](https://app.asana.com/0/1205729146579121/1205729146579121/f),
    but to summarise:
    
    - only considered a duplicate if both username and password are perfect
    matches
    - de-deduplication is applied at the last step before presenting the
    autofill login options, meaning all of the existing rules on which saved
    logins are considered still apply; this just filters that list down if
    it contains duplicates.
    - when de-duplication happens, it might be that saved logins had
    multiple subdomains (e.g., a login saved for `a.example.com` and
    `example.com` both had the same username/password). In this case, we
    favour the _best match_ and pick that one to show to the user from the
    duplicates.
    
    
    ### Steps to test this PR
    
    Install fresh `internal` build.
    
    #### Duplicates on the same E-TLD+1 and subdomain
    - [ ] Add a login (`domain=fill.dev`, `username=user`,
    `password=password`)
    - [ ] Add a login (`domain=fill.dev`, `username=user`,
    `password=password`) (a duplicate)
    - [ ] Visit https://fill.dev/form/login-simple and verify you only see
    one option in the autofill prompt
    
    #### Duplicates on the same E-TLD+1 / different subdomain
    - [ ] Edit one of the saved logins to add a subdomain
    (`domain=example.fill.dev`, `username=user`, `password=password`)
    - [ ] Visit https://fill.dev/form/login-simple and verify you only see
    one option in the autofill prompt
    - [ ] Verify you do **not** see "From example.fill.dev" (this confirms
    it picked the _best_ match out of the dupes)
    
    #### Duplicates across subdomains (same E-TLD+1)
    - [ ] Edit the saved login that still has `fill.dev` to now also have a
    subdomain (`domain=foo.fill.dev`, `username=user`, `password=password`)
    - [ ] Visit https://fill.dev/form/login-simple and verify you only see
    one option in the autofill prompt
    - [ ] Verify you **do** see the subdomain that is offered. e.g., "From
    foo.fill.dev" (it will pick the one that was modified most recently)
    
    #### Mis-matching username means no de-duplication
    - [ ] Delete all logins
    - [ ] Add a login (`domain=fill.dev`, `username=user`,
    `password=password`)
    - [ ] Add a login (`domain=fill.dev`, `username=different`,
    `password=password`) (different username)
    - [ ] Visit https://fill.dev/form/login-simple and verify you see two
    options offered (no de-duplication)
    
    ### Mis-matching password means no de-duplication
    - [ ] Delete all logins
    - [ ] Add a login (`domain=fill.dev`, `username=user`,
    `password=password`)
    - [ ] Add a login (`domain=fill.dev`, `username=user`,
    `password=different`) (different password)
    - [ ] Visit https://fill.dev/form/login-simple and verify you see two
    options offered (no de-duplication)
    
    
    
    ### UI changes
    
    ![combined](https://github.com/duckduckgo/Android/assets/1336281/d54b817f-a084-4e7a-bf32-9e1c75653f28)

---
+++ autofill/autofill-impl/src/main/java/com/duckduckgo/autofill/impl/deduper/AutofillDeduplicationBestMatchFinder.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.autofill.impl.deduper
+
+import com.duckduckgo.autofill.api.domain.app.LoginCredentials
+import com.duckduckgo.autofill.impl.deduper.AutofillDeduplicationMatchTypeDetector.MatchType.NotAMatch
+import com.duckduckgo.autofill.impl.deduper.AutofillDeduplicationMatchTypeDetector.MatchType.PartialMatch
+import com.duckduckgo.autofill.impl.deduper.AutofillDeduplicationMatchTypeDetector.MatchType.PerfectMatch
+import com.duckduckgo.autofill.impl.urlmatcher.AutofillUrlMatcher
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesBinding
+import javax.inject.Inject
+
+interface AutofillDeduplicationBestMatchFinder {
+
+    fun findBestMatch(
+        originalUrl: String,
+        logins: List<LoginCredentials>,
+    ): LoginCredentials?
+}
+
+@ContributesBinding(AppScope::class)
+class RealAutofillDeduplicationBestMatchFinder @Inject constructor(
+    private val urlMatcher: AutofillUrlMatcher,
+    private val matchTypeDetector: AutofillDeduplicationMatchTypeDetector,
+) : AutofillDeduplicationBestMatchFinder {
+
+    override fun findBestMatch(
+        originalUrl: String,
+        logins: List<LoginCredentials>,
+    ): LoginCredentials? {
+        // perfect matches are those where the subdomain and e-tld+1 match
+        val perfectMatches = mutableListOf<LoginCredentials>()
+
+        // partial matches are those where only e-tld+1 matches
+        val partialMatches = mutableListOf<LoginCredentials>()
+
+        // non-matches are those where neither subdomain nor e-tld+1 match
+        val nonMatches = mutableListOf<LoginCredentials>()
+
+        categoriseEachLogin(logins, originalUrl, perfectMatches, partialMatches, nonMatches)
+
+        if (perfectMatches.isEmpty() && partialMatches.isEmpty() && nonMatches.isEmpty()) {
+            return null
+        }
+
+        return if (perfectMatches.isNotEmpty()) {
+            bestPerfectMatch(perfectMatches)
+        } else if (partialMatches.isNotEmpty()) {
+            bestPartialMatch(partialMatches)
+        } else {
+            bestNonMatch(nonMatches)
+        }
+    }
+
+    private fun categoriseEachLogin(
+        logins: List<LoginCredentials>,
+        originalUrl: String,
+        perfectMatches: MutableList<LoginCredentials>,
+        partialMatches: MutableList<LoginCredentials>,
+        nonMatches: MutableList<LoginCredentials>,
+    ) {
+        logins.forEach {
+            when (matchTypeDetector.detectMatchType(originalUrl, it)) {
+                PerfectMatch -> perfectMatches.add(it)
+                PartialMatch -> partialMatches.add(it)
+                NotAMatch -> nonMatches.add(it)
+            }
+        }
+    }
+
+    private fun bestPerfectMatch(perfectMatches: List<LoginCredentials>): LoginCredentials {
+        return perfectMatches.sortedWith(AutofillDeduplicationLoginComparator()).first()
+    }
+
+    private fun bestPartialMatch(partialMatches: MutableList<LoginCredentials>): LoginCredentials {
+        return partialMatches.sortedWith(AutofillDeduplicationLoginComparator()).first()
+    }
+
+    private fun bestNonMatch(nonMatches: MutableList<LoginCredentials>): LoginCredentials {
+        return nonMatches.sortedWith(AutofillDeduplicationLoginComparator()).first()
+    }
+}
---
+++ autofill/autofill-impl/src/test/java/com/duckduckgo/autofill/impl/deduper/RealLoginDeduplicatorBestMatchFinderTest.kt
@@ -0,0 +1,88 @@
+package com.duckduckgo.autofill.impl.deduper
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.duckduckgo.autofill.api.domain.app.LoginCredentials
+import com.duckduckgo.autofill.impl.encoding.TestUrlUnicodeNormalizer
+import com.duckduckgo.autofill.impl.urlmatcher.AutofillDomainNameUrlMatcher
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RealLoginDeduplicatorBestMatchFinderTest {
+
+    private val urlMatcher = AutofillDomainNameUrlMatcher(TestUrlUnicodeNormalizer())
+    private val matchTypeDetector = RealAutofillDeduplicationMatchTypeDetector(urlMatcher)
+    private val testee = RealAutofillDeduplicationBestMatchFinder(
+        urlMatcher = urlMatcher,
+        matchTypeDetector = matchTypeDetector,
+    )
+
+    @Test
+    fun whenEmptyListThenNoBestMatchFound() {
+        assertNull(testee.findBestMatch("", emptyList()))
+    }
+
+    @Test
+    fun whenSinglePerfectMatchThenThatIsReturnedAsBestMatch() {
+        val input = listOf(
+            LoginCredentials(id = 0, domain = "example.com", username = "username", password = "password"),
+        )
+        val result = testee.findBestMatch("example.com", input)
+        assertNotNull(result)
+    }
+
+    @Test
+    fun whenMultiplePerfectMatchesMostRecentlyModifiedIsReturned() {
+        val input = listOf(
+            creds("example.com", 1000),
+            creds("example.com", 2000),
+        )
+        val result = testee.findBestMatch("example.com", input)
+        assertEquals(2000L, result!!.lastUpdatedMillis)
+    }
+
+    @Test
+    fun whenMultiplePartialMatchesWithSameTimestampThenDomainAlphabeticallyFirstReturned() {
+        val input = listOf(
+            creds("a.example.com", 2000),
+            creds("b.example.com", 2000),
+        )
+        val result = testee.findBestMatch("example.com", input)
+        assertEquals("a.example.com", result!!.domain)
+    }
+
+    @Test
+    fun whenSingleNonMatchThenReturnedAsBestMatch() {
+        val input = listOf(
+            creds("not-a-match.com", 2000),
+        )
+        val result = testee.findBestMatch("example.com", input)
+        assertEquals("not-a-match.com", result!!.domain)
+    }
+
+    @Test
+    fun whenMultipleNonMatchesThenMostRecentlyModifiedIsReturned() {
+        val input = listOf(
+            creds("not-a-match.com", 2000),
+            creds("also-not-a-match.com", 1000),
+        )
+        val result = testee.findBestMatch("example.com", input)
+        assertEquals("not-a-match.com", result!!.domain)
+    }
+
+    @Test
+    fun whenMatchesFromAllTypesThenMatchInPerfectReturnedRegardlessOfTimestamps() {
+        val input = listOf(
+            creds("perfect-match.com", 1000),
+            creds("imperfect-match.com", 3000),
+            creds("not-a-match.com", 2000),
+        )
+        val result = testee.findBestMatch("perfect-match.com", input)
+        assertEquals("perfect-match.com", result!!.domain)
+    }
+
+    private fun creds(domain: String, lastModified: Long?): LoginCredentials {
+        return LoginCredentials(domain = domain, lastUpdatedMillis = lastModified, username = "", password = "")
+    }
+}
