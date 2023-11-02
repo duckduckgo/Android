82ec0b5b1fba66631d15d53422355c7be7a1890d
Ana Capatina <anikiki@users.noreply.github.com>
2023-11-02 15:09:50 +0000

    Android implementation: Report previously submitted site breakage reports (#3782)
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1205833527774191/f
    
    ### Description
    Added broken site last sent report. Created modules for the broken-site
    feature.
    
    ### Steps to test this PR
    
    - [x] Install from this branch.
    - [x] Navigate to a website and report it as broken from the overflow
    menu -> `Report Broken Site`
    - [x] Filter logcat by `epbf` and check a pixel is sent and it contains
    a param called lastSentDay with no value.
    - [x] In the IDE Open `App Inspection` and check the `broken-site`
    database exists and an entry is added to the
    `broken-site-last-sent-report` table. You should see there the 6 letter
    hash prefix for the hostname and a timestamp.
    - [x] Navigate to the same website and send another report.
    - [x] Filter logcat by `epbf` and check a pixel is sent and it contains
    a param called lastSentDay with the date as today.
    - [x] In the IDE Open `App Inspection` you should see the same entry
    with an updated timestamp.
    - [x] Change the date on the device to 31 days in advance.
    - [x] In the IDE Open `App Inspection` you should not see any entries as
    the cleanup job removed them for being more than 30 days old.
    
    ### NO UI changes

---
+++ broken-site/broken-site-impl/src/main/java/com/duckduckgo/brokensite/impl/CleanupBrokenSiteLastSentReportWorker.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2023 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.brokensite.impl
+
+import android.content.Context
+import androidx.lifecycle.LifecycleOwner
+import androidx.work.BackoffPolicy
+import androidx.work.CoroutineWorker
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import com.duckduckgo.anvil.annotations.ContributesWorker
+import com.duckduckgo.app.global.DispatcherProvider
+import com.duckduckgo.app.lifecycle.MainProcessLifecycleObserver
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesMultibinding
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+@ContributesWorker(AppScope::class)
+class CleanupBrokenSiteLastSentReportWorker(context: Context, workerParameters: WorkerParameters) :
+    CoroutineWorker(context, workerParameters) {
+
+    @Inject
+    lateinit var brokenSiteReportRepository: BrokenSiteReportRepository
+
+    @Inject
+    lateinit var dispatchers: DispatcherProvider
+
+    override suspend fun doWork(): Result {
+        return withContext(dispatchers.io()) {
+            brokenSiteReportRepository.cleanupOldEntries()
+
+            return@withContext Result.success()
+        }
+    }
+}
+
+@ContributesMultibinding(
+    scope = AppScope::class,
+    boundType = MainProcessLifecycleObserver::class,
+)
+class CleanupBrokenSiteLastSentReportWorkerScheduler @Inject constructor(
+    private val workManager: WorkManager,
+) : MainProcessLifecycleObserver {
+
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+        Timber.v("Scheduling cleanup broken site report worker")
+        val workerRequest = PeriodicWorkRequestBuilder<CleanupBrokenSiteLastSentReportWorker>(1, TimeUnit.DAYS)
+            .addTag(CLEANUP_BROKEN_SITE_REPORT_WORKER_TAG)
+            .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, 10, TimeUnit.MINUTES)
+            .build()
+        workManager.enqueueUniquePeriodicWork(CLEANUP_BROKEN_SITE_REPORT_WORKER_TAG, ExistingPeriodicWorkPolicy.REPLACE, workerRequest)
+    }
+
+    companion object {
+        private const val CLEANUP_BROKEN_SITE_REPORT_WORKER_TAG = "CLEANUP_BROKEN_SITE_REPORT_WORKER_TAG"
+    }
+}
