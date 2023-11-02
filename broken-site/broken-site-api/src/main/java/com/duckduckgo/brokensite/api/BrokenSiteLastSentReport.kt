fb436aa7701848f19b761ad546d1d57b207ddbb6
Ana Capatina <anikiki@gmail.com>
2023-11-02 10:38:51 +0000

    Added docs.

--- broken-site/broken-site-api/src/main/java/com/duckduckgo/brokensite/api/BrokenSiteLastSentReport.kt
+++ broken-site/broken-site-api/src/main/java/com/duckduckgo/brokensite/api/BrokenSiteLastSentReport.kt
@@ -18,6 +18,17 @@ package com.duckduckgo.brokensite.api
 
 interface BrokenSiteLastSentReport {
 
+    /**
+     * Retrieves the date when a broken site report was last sent for the specified hostname. The 'broken_site_last_sent_report' table in
+     * the 'broken_site' database stores this date.
+     * @return a string containing the date in the format 'yyyy-MM-dd', or null if a report was not sent in the last 30 days.
+     */
     suspend fun getLastSentDay(hostname: String): String?
+
+    /**
+     * Sets / updates the last sent date for a broken site report associated with the provided hostname, setting it to the current date.
+     * The 'broken_site_last_sent_report' table in the 'broken_site' database stores this updated date along with a prefix
+     * of the hashed hostname.
+     */
     fun setLastSentDay(hostname: String)
 }
