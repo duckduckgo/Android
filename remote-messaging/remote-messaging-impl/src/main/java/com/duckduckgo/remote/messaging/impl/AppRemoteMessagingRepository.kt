b85d854ebb1f6dad14c37f10518fa8c95ca78391
Josh Leibstein <joshliebe@gmail.com>
2023-11-02 15:13:59 +0000

    Only show default browser experiment if not default browser

--- remote-messaging/remote-messaging-impl/src/main/java/com/duckduckgo/remote/messaging/impl/AppRemoteMessagingRepository.kt
+++ remote-messaging/remote-messaging-impl/src/main/java/com/duckduckgo/remote/messaging/impl/AppRemoteMessagingRepository.kt
@@ -58,8 +58,9 @@ class AppRemoteMessagingRepository(
     override fun messageFlow(): Flow<RemoteMessage?> {
         // Experiment: Ask for Default Browser More Than Once
         val daysSinceInstalled = userBrowserProperties.daysSinceInstalled()
+        val isDefaultBrowser = userBrowserProperties.defaultBrowser()
         return remoteMessagesDao.messagesFlow().distinctUntilChanged().map {
-            if (daysSinceInstalled != 0L && daysSinceInstalled != 1L && daysSinceInstalled != 2L) return@map null
+            if (daysSinceInstalled != 0L && daysSinceInstalled != 1L && daysSinceInstalled != 2L || isDefaultBrowser) return@map null
             if (it == null || it.message.isEmpty()) return@map null
 
             val message = messageMapper.fromMessage(it.message) ?: return@map null
