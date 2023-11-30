618be44529717045f35dfe5db11f67bf65d41723
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2023-11-30 14:25:58 +0100

    remember popup was dismissed for given domain and don't show it again for 24h

--- privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupManagerImpl.kt
+++ privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupManagerImpl.kt
@@ -25,6 +25,8 @@ import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupUiEvent
 import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupUiEvent.DISMISSED
 import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupUiEvent.DISMISS_CLICKED
 import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupViewState
+import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupManagerImpl.PopupDismissed.DismissedAt
+import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupManagerImpl.PopupDismissed.NotDismissed
 import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
@@ -33,10 +35,10 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
@@ -44,12 +46,16 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.threeten.bp.Duration
+import org.threeten.bp.Instant
 
 @ContributesBinding(FragmentScope::class)
 class PrivacyProtectionsPopupManagerImpl @Inject constructor(
     @AppCoroutineScope private val appCoroutineScope: CoroutineScope,
     private val featureAvailability: PrivacyProtectionsPopupFeatureAvailability,
     private val protectionsStateProvider: ProtectionsStateProvider,
+    private val timeProvider: TimeProvider,
+    private val repository: PrivacyProtectionsPopupRepository,
 ) : PrivacyProtectionsPopupManager {
 
     private val state = MutableStateFlow(
@@ -59,6 +65,7 @@ class PrivacyProtectionsPopupManagerImpl @Inject constructor(
             refreshTriggered = false,
             domain = null,
             hasHttpErrorCodes = false,
+            popupDismissed = null,
         ),
     )
 
@@ -77,18 +84,18 @@ class PrivacyProtectionsPopupManagerImpl @Inject constructor(
     override fun onUiEvent(event: PrivacyProtectionsPopupUiEvent) {
         when (event) {
             DISMISSED -> {
-                state.update { oldState -> oldState.copy(refreshTriggered = false) }
+                dismissPopup()
             }
 
             DISMISS_CLICKED -> {
                 // TODO pixel
-                state.update { oldState -> oldState.copy(refreshTriggered = false) }
+                dismissPopup()
             }
 
             DISABLE_PROTECTIONS_CLICKED -> {
                 // TODO pixel
                 // TODO add to allowlist
-                state.update { oldState -> oldState.copy(refreshTriggered = false) }
+                dismissPopup()
             }
         }
     }
@@ -110,9 +117,20 @@ class PrivacyProtectionsPopupManagerImpl @Inject constructor(
                     protectionsEnabled = null,
                     domain = newDomain,
                     hasHttpErrorCodes = httpErrorCodes.isNotEmpty(),
+                    popupDismissed = null,
                 )
             } else {
                 oldState.copy(hasHttpErrorCodes = httpErrorCodes.isNotEmpty())
+            }
+        }
+    }
+
+    private fun dismissPopup() {
+        val popupDismissedAt = timeProvider.getCurrentTime()
+
+        state.value.domain?.let { domain ->
+            appCoroutineScope.launch {
+                repository.setPopupDismissTime(domain, popupDismissedAt)
             }
         }
     }
@@ -137,7 +155,22 @@ class PrivacyProtectionsPopupManagerImpl @Inject constructor(
                 .onEach { protectionsEnabled ->
                     state.update { it.copy(protectionsEnabled = protectionsEnabled) }
                 }
-                .collect()
+                .launchIn(this)
+
+            state.map { it.domain }
+                .distinctUntilChanged()
+                .flatMapLatest { domain ->
+                    if (domain != null) {
+                        repository.getPopupDismissTime(domain)
+                    } else {
+                        flowOf(null)
+                    }
+                }
+                .map { dismissedAt -> if (dismissedAt != null) DismissedAt(dismissedAt) else NotDismissed }
+                .onEach { popupDismissed ->
+                    state.update { it.copy(popupDismissed = popupDismissed) }
+                }
+                .launchIn(this)
         }
     }
 
@@ -152,15 +185,35 @@ class PrivacyProtectionsPopupManagerImpl @Inject constructor(
         val refreshTriggered: Boolean,
         val domain: String?,
         val hasHttpErrorCodes: Boolean,
+        val popupDismissed: PopupDismissed?,
     )
 
+    private sealed class PopupDismissed {
+        data object NotDismissed : PopupDismissed()
+        data class DismissedAt(val timestamp: Instant) : PopupDismissed()
+    }
+
     private fun createViewState(state: State): PrivacyProtectionsPopupViewState = with(state) {
+        val popupDismissed = when (popupDismissed) {
+            is DismissedAt -> {
+                popupDismissed.timestamp + DISMISS_REMEMBER_DURATION > timeProvider.getCurrentTime()
+            }
+
+            NotDismissed -> false
+            null -> null
+        }
+
         val shouldShowPopup = featureAvailable &&
             refreshTriggered &&
             protectionsEnabled == true &&
             domain != null &&
-            !hasHttpErrorCodes
+            !hasHttpErrorCodes &&
+            popupDismissed == false
 
         return PrivacyProtectionsPopupViewState(visible = shouldShowPopup)
+    }
+
+    private companion object {
+        val DISMISS_REMEMBER_DURATION: Duration = Duration.ofDays(1)
     }
 }
