c53096d4eca5870a19fbb4646de910edbf4424af
Aitor Viana <aitorvs@gmail.com>
2023-12-06 15:13:17 +0000

    Fix db lock in fingerprint protection (#3971)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1206101789026061/f
    
    ### Description
    Fix the DB lock crash we've got when calling multiple times the storeNewSeed() method.
    
    The db table was not really necessary so we've removed it and we keed the seed in memory now. Meeting the same requirements as before
    
    ### Steps to test this PR
    
    _Test app update_
    - [x] install from develop
    - [x] update from this branch
    - [x] verify the `FingerprintProtectionDatabase` is not cleared and only the `fingerprint_protection_seed` db table is gone
    - [x] Smoke test fingerprint protection
    
    _Test app fresh install_
    - [x] install from this branch
    - [x] verify the FingerprintProtectionDatabase is created and `fingerprint_protection_seed` is not there
    - [x] Smoke test fingerprint protection

--- fingerprint-protection/fingerprint-protection-store/src/main/java/com/duckduckgo/fingerprintprotection/store/FingerprintProtectionDatabase.kt
+++ fingerprint-protection/fingerprint-protection-store/src/main/java/com/duckduckgo/fingerprintprotection/store/FingerprintProtectionDatabase.kt
@@ -19,23 +19,22 @@ package com.duckduckgo.fingerprintprotection.store
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.duckduckgo.fingerprintprotection.store.features.fingerprintingbattery.FingerprintingBatteryDao
 import com.duckduckgo.fingerprintprotection.store.features.fingerprintingcanvas.FingerprintingCanvasDao
 import com.duckduckgo.fingerprintprotection.store.features.fingerprintinghardware.FingerprintingHardwareDao
 import com.duckduckgo.fingerprintprotection.store.features.fingerprintingscreensize.FingerprintingScreenSizeDao
 import com.duckduckgo.fingerprintprotection.store.features.fingerprintingtemporarystorage.FingerprintingTemporaryStorageDao
-import com.duckduckgo.fingerprintprotection.store.seed.FingerprintProtectionSeedDao
 
 @Database(
     exportSchema = true,
-    version = 1,
+    version = 2,
     entities = [
         FingerprintingBatteryEntity::class,
         FingerprintingCanvasEntity::class,
         FingerprintingHardwareEntity::class,
         FingerprintingScreenSizeEntity::class,
         FingerprintingTemporaryStorageEntity::class,
-        FingerprintProtectionSeedEntity::class,
     ],
 )
 abstract class FingerprintProtectionDatabase : RoomDatabase() {
@@ -44,7 +43,13 @@ abstract class FingerprintProtectionDatabase : RoomDatabase() {
     abstract fun fingerprintingHardwareDao(): FingerprintingHardwareDao
     abstract fun fingerprintingScreenSizeDao(): FingerprintingScreenSizeDao
     abstract fun fingerprintingTemporaryStorageDao(): FingerprintingTemporaryStorageDao
-    abstract fun fingerprintProtectionSeedDao(): FingerprintProtectionSeedDao
+}
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DROP TABLE IF EXISTS `fingerprint_protection_seed`")
+    }
 }
 
-val ALL_MIGRATIONS = emptyArray<Migration>()
+val ALL_MIGRATIONS = listOf(
+    MIGRATION_1_2,
+)
--- fingerprint-protection/fingerprint-protection-store/src/test/java/com/duckduckgo/fingerprintprotection/store/seed/RealFingerprintProtectionSeedRepositoryTest.kt
+++ fingerprint-protection/fingerprint-protection-store/src/test/java/com/duckduckgo/fingerprintprotection/store/seed/RealFingerprintProtectionSeedRepositoryTest.kt
@@ -16,99 +16,28 @@
 
 package com.duckduckgo.fingerprintprotection.store.seed
 
-import com.duckduckgo.common.test.CoroutineTestRule
-import com.duckduckgo.fingerprintprotection.store.FingerprintProtectionDatabase
-import com.duckduckgo.fingerprintprotection.store.FingerprintProtectionSeedEntity
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.mockito.kotlin.argumentCaptor
-import org.mockito.kotlin.inOrder
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.times
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
 
-@ExperimentalCoroutinesApi
 class RealFingerprintProtectionSeedRepositoryTest {
-    @get:Rule var coroutineRule = CoroutineTestRule()
-
     private lateinit var testee: RealFingerprintProtectionSeedRepository
-
-    private val mockDatabase: FingerprintProtectionDatabase = mock()
-    private val mockFingerprintProtectionSeedDao: FingerprintProtectionSeedDao = mock()
 
     @Before
     fun before() {
-        whenever(mockFingerprintProtectionSeedDao.get()).thenReturn(null)
-        whenever(mockDatabase.fingerprintProtectionSeedDao()).thenReturn(mockFingerprintProtectionSeedDao)
+        testee = RealFingerprintProtectionSeedRepository()
     }
 
     @Test
-    fun whenInitializedThenStoreNewSeedAndLoadToMemory() =
-        runTest {
-            whenever(mockFingerprintProtectionSeedDao.get()).thenReturn(FingerprintProtectionSeedEntity(seed = "1234"))
-            testee =
-                RealFingerprintProtectionSeedRepository(
-                    mockDatabase,
-                    TestScope(),
-                    coroutineRule.testDispatcherProvider,
-                )
-            assertEquals("1234", testee.fingerprintProtectionSeedEntity.seed)
-
-            whenever(mockFingerprintProtectionSeedDao.get()).thenReturn(FingerprintProtectionSeedEntity(seed = "5678"))
-            testee =
-                RealFingerprintProtectionSeedRepository(
-                    mockDatabase,
-                    TestScope(),
-                    coroutineRule.testDispatcherProvider,
-                )
-            assertEquals("5678", testee.fingerprintProtectionSeedEntity.seed)
-
-            verify(mockFingerprintProtectionSeedDao, times(2)).get()
-
-            val captor = argumentCaptor<FingerprintProtectionSeedEntity>()
-            val captor2 = argumentCaptor<FingerprintProtectionSeedEntity>()
-
-            mockFingerprintProtectionSeedDao.inOrder {
-                verify().updateAll(captor.capture())
-                verify().updateAll(captor2.capture())
-            }
-            val seed = captor.firstValue.seed
-            val seed2 = captor2.firstValue.seed
-
-            assertNotEquals(seed, seed2)
-        }
+    fun whenInitializedRandomSeedIsSet() {
+        assertFalse(testee.seed.isBlank())
+    }
 
     @Test
-    fun whenStoreNewSeedThenStoreNewSeedAndLoadToMemory() =
-        runTest {
-            whenever(mockFingerprintProtectionSeedDao.get()).thenReturn(FingerprintProtectionSeedEntity(seed = "1234"))
-            testee =
-                RealFingerprintProtectionSeedRepository(
-                    mockDatabase,
-                    TestScope(),
-                    coroutineRule.testDispatcherProvider,
-                )
-            testee.storeNewSeed()
-
-            verify(mockFingerprintProtectionSeedDao, times(2)).get()
-
-            val captor = argumentCaptor<FingerprintProtectionSeedEntity>()
-            val captor2 = argumentCaptor<FingerprintProtectionSeedEntity>()
-
-            mockFingerprintProtectionSeedDao.inOrder {
-                verify().updateAll(captor.capture())
-                verify().updateAll(captor2.capture())
-            }
-            val seed = captor.firstValue.seed
-            val seed2 = captor2.firstValue.seed
-
-            assertNotEquals(seed, seed2)
-        }
+    fun whenStoredNewSeedThenSeedIsRefreshed() {
+        val oldSeed = testee.seed
+        testee.storeNewSeed()
+        assertNotEquals(oldSeed, testee.seed)
+    }
 }
