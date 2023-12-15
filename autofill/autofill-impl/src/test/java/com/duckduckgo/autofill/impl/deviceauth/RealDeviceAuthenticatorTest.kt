edae8ba42c2ec4e9ec754bdf9a39b3b1b72d5c3d
Craig Russell <CDRussell@users.noreply.github.com>
2023-12-15 10:25:37 +0000

    Update autofill copy, change logins->passwords globally (#3966)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    
    At a minimum, make sure your changes are tested in API 23 and one of the
    more recent API levels available.
    -->
    
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1205931258794166/f
    
    ### Description
    Updates our autofill-related terminology around saved logins... these
    will now be referred to as `passwords` instead of `logins`.
    
    Several preceding PRs have made similar incremental changes already, and
    this one should take care of the remaining loose ends.
    - Most of the footprint of change is caused by removing existing (now
    outdated) translations.
    - New translations will follow separately.
    
    ### Steps to test this PR
    This is QA-optional, as you should be able to assess the change from the
    PR diff. If you do want to do extra testing:
    
    - [ ] Smoke test autofill functionality (now called `Passwords` in menu)
    - [ ] Make sure there are no remaining references to `Logins` in the UI

--- autofill/autofill-impl/src/test/java/com/duckduckgo/autofill/impl/deviceauth/RealDeviceAuthenticatorTest.kt
+++ autofill/autofill-impl/src/test/java/com/duckduckgo/autofill/impl/deviceauth/RealDeviceAuthenticatorTest.kt
@@ -20,8 +20,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import com.duckduckgo.autofill.impl.R
-import com.duckduckgo.autofill.impl.deviceauth.DeviceAuthenticator.Features.AUTOFILL_TO_ACCESS_CREDENTIALS
-import com.duckduckgo.autofill.impl.deviceauth.DeviceAuthenticator.Features.AUTOFILL_TO_USE_CREDENTIALS
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -95,39 +93,20 @@ class RealDeviceAuthenticatorTest {
 
     @Test
     fun whenAuthenticateToAccessCredentialsIsCalledWithFragmentThenLaunchAuthLauncher() {
-        testee.authenticate(AUTOFILL_TO_ACCESS_CREDENTIALS, fragment) {}
+        testee.authenticate(fragment) {}
         verify(authLauncher).launch(eq(R.string.autofill_auth_text_for_access), eq(fragment), any())
-    }
-
-    @Test
-    fun whenAuthenticateToUseCredentialsIsCalledWithFragmentThenLaunchAuthLauncher() {
-        testee.authenticate(AUTOFILL_TO_USE_CREDENTIALS, fragment) {}
-        verify(authLauncher).launch(eq(R.string.autofill_auth_text_for_using), eq(fragment), any())
     }
 
     @Test
     fun whenAuthenticateToAccessCredentialsIsCalledWithActivityThenLaunchAuthLauncher() {
-        testee.authenticate(AUTOFILL_TO_ACCESS_CREDENTIALS, fragment) {}
+        testee.authenticate(fragment) {}
         verify(authLauncher).launch(eq(R.string.autofill_auth_text_for_access), eq(fragment), any())
-    }
-
-    @Test
-    fun whenAuthenticateToUseCredentialsIsCalledWithActivityThenLaunchAuthLauncher() {
-        testee.authenticate(AUTOFILL_TO_USE_CREDENTIALS, fragment) {}
-        verify(authLauncher).launch(eq(R.string.autofill_auth_text_for_using), eq(fragment), any())
-    }
-
-    @Test
-    fun whenAuthGracePeriodActiveThenNoDeviceAuthLaunchedWhenUsingCredentials() {
-        whenever(autofillAuthorizationGracePeriod.isAuthRequired()).thenReturn(false)
-        testee.authenticate(AUTOFILL_TO_USE_CREDENTIALS, fragmentActivity) {}
-        verifyAuthNotLaunched()
     }
 
     @Test
     fun whenAuthGracePeriodActiveThenNoDeviceAuthLaunchedWhenAccessingCredentials() {
         whenever(autofillAuthorizationGracePeriod.isAuthRequired()).thenReturn(false)
-        testee.authenticate(AUTOFILL_TO_ACCESS_CREDENTIALS, fragmentActivity) {}
+        testee.authenticate(fragmentActivity) {}
         verifyAuthNotLaunched()
     }
 
