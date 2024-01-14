e46a1422e030e404dd4c9ac7f01fb872ae444a6c
denizgencay <denizgencay35@gmail.com>
2024-01-14 16:31:25 +0300

    Saving safe gaze count to local storage implemented.

--- app/src/main/java/com/duckduckgo/app/browser/safe_gaze/SafeGazeJsInterface.kt
+++ app/src/main/java/com/duckduckgo/app/browser/safe_gaze/SafeGazeJsInterface.kt
@@ -1,26 +1,31 @@
-/*
- * Copyright (c) 2024 DuckDuckGo
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.duckduckgo.app.browser.safe_gaze
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.webkit.JavascriptInterface
 
-class SafeGazeJsInterface {
+class SafeGazeJsInterface(context: Context) {
+
+    private val preferences: SharedPreferences =
+        context.getSharedPreferences("safe_gaze_prefs", Context.MODE_PRIVATE)
+
     @JavascriptInterface
     fun sendMessage(message: String) {
         println("Message send from js side is -> $message")
+    }
+
+    @JavascriptInterface
+    fun safeGazeImageCounter() {
+        val currentCounter = getCounterValue()
+        val newCounter = currentCounter + 1
+        saveGlobalCounterValue(newCounter)
+    }
+
+    private fun saveGlobalCounterValue(value: Int) {
+        preferences.edit().putInt("safe_gaze_global_counter", value).apply()
+    }
+
+    private fun getCounterValue(): Int {
+        return preferences.getInt("safe_gaze_global_counter", 0)
     }
 }
