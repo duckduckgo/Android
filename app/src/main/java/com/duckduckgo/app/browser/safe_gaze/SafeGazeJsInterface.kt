b69892f0d7c8d298ce2f67c0fbe9335a267f5a18
denizgencay <denizgencay35@gmail.com>
2024-01-14 19:59:13 +0300

    Total count for Safe Gaze implemented.

--- app/src/main/java/com/duckduckgo/app/browser/safe_gaze/SafeGazeJsInterface.kt
+++ app/src/main/java/com/duckduckgo/app/browser/safe_gaze/SafeGazeJsInterface.kt
@@ -7,25 +7,21 @@ import android.webkit.JavascriptInterface
 class SafeGazeJsInterface(context: Context) {
 
     private val preferences: SharedPreferences =
-        context.getSharedPreferences("safe_gaze_prefs", Context.MODE_PRIVATE)
+        context.getSharedPreferences("safe_gaze_preferences", Context.MODE_PRIVATE)
 
     @JavascriptInterface
     fun sendMessage(message: String) {
         println("Message send from js side is -> $message")
-    }
-
-    @JavascriptInterface
-    fun safeGazeImageCounter() {
         val currentCounter = getCounterValue()
         val newCounter = currentCounter + 1
         saveGlobalCounterValue(newCounter)
     }
 
     private fun saveGlobalCounterValue(value: Int) {
-        preferences.edit().putInt("safe_gaze_global_counter", value).apply()
+        preferences.edit().putInt("all_time_cencored_count", value).apply()
     }
 
     private fun getCounterValue(): Int {
-        return preferences.getInt("safe_gaze_global_counter", 0)
+        return preferences.getInt("all_time_cencored_count", 0)
     }
 }
