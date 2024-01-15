c377faf7d3a44a2b6faa2bf57e89fe9e3c364aac
Asif Ahmed <asifddlks@hotmail.com>
2024-01-15 15:29:27 +0600

    Replace asil URL in Learn more in global privacy control (url with directories)

--- app/src/main/java/com/duckduckgo/app/globalprivacycontrol/ui/GlobalPrivacyControlViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/globalprivacycontrol/ui/GlobalPrivacyControlViewModel.kt
@@ -80,6 +80,6 @@ class GlobalPrivacyControlViewModel @Inject constructor(
     }
 
     companion object {
-        const val LEARN_MORE_URL = "https://asil.co"
+        const val LEARN_MORE_URL = "https://asil.co/asil-help-pages/privacy/gpc/"
     }
 }
