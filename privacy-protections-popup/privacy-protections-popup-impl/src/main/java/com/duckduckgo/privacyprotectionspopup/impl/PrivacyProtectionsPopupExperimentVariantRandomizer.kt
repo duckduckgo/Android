35616feb6b41fda0bcba29b38ffbbeda7f381144
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2024-01-25 16:13:03 +0100

    force control experiment group in UI tests

--- privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupExperimentVariantRandomizer.kt
+++ privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupExperimentVariantRandomizer.kt
@@ -16,7 +16,9 @@
 
 package com.duckduckgo.privacyprotectionspopup.impl
 
+import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import com.duckduckgo.di.scopes.FragmentScope
+import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupExperimentVariant.CONTROL
 import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
 import org.apache.commons.math3.random.RandomDataGenerator
@@ -26,9 +28,13 @@ interface PrivacyProtectionsPopupExperimentVariantRandomizer {
 }
 
 @ContributesBinding(FragmentScope::class)
-class PrivacyProtectionsPopupExperimentVariantRandomizerImpl @Inject constructor() : PrivacyProtectionsPopupExperimentVariantRandomizer {
+class PrivacyProtectionsPopupExperimentVariantRandomizerImpl @Inject constructor(
+    private val buildConfig: AppBuildConfig,
+) : PrivacyProtectionsPopupExperimentVariantRandomizer {
 
     override fun getRandomVariant(): PrivacyProtectionsPopupExperimentVariant {
+        if (buildConfig.isDefaultVariantForced) return CONTROL
+
         val variants = PrivacyProtectionsPopupExperimentVariant.entries
         val randomIndex = RandomDataGenerator().nextInt(0, variants.lastIndex)
         return variants[randomIndex]
