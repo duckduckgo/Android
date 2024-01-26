Apetree100122/duck-duck-Andoid

Sync: Critical Path Tests

.maestro/sync_flows/bookmark_domains.js
@@ -1,0 +1,3 @@
+output.domains = {
+    domains: ["https://www.example.com", "fill.dev/example", "192.168.0.100"],
+}

duckduckgo/Android

.maestro/sync_flows/bookmark_domains.js
@@ -1,0 +1,3 @@
+output.bookmarks = {
+    domains: ["https://www.example.com", "fill.dev/example", "192.168.0.100"],
+}