19c03299543e557fe9956e573e4ae27df8105e83
Aitor Viana <aitorvs@gmail.com>
2024-02-06 14:18:38 +0000

    Fix bug when computing the terrible latency bottom band (#4163)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1206514199358868
    
    ### Description
    Fix bug when calculating bottom band for terrible latency
    
    ### Steps to test this PR
    QA optional, code review and test pass

--- network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/connectionclass/ConnectionClassManager.kt
+++ network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/connectionclass/ConnectionClassManager.kt
@@ -105,7 +105,7 @@ class ConnectionClassManager @Inject constructor(
         val currentQuality = currentConnectionQuality.get() ?: return false
         val (bottomOfBand, topOfBand) = when (currentQuality) {
             ConnectionQuality.TERRIBLE -> {
-                ConnectionQuality.MODERATE.max to ConnectionQuality.POOR.max
+                ConnectionQuality.POOR.max to Double.MAX_VALUE
             }
             ConnectionQuality.POOR -> {
                 ConnectionQuality.MODERATE.max to ConnectionQuality.POOR.max
@@ -126,7 +126,7 @@ class ConnectionClassManager @Inject constructor(
         }
 
         val average = latencyMeasurements.average
-        return if (average > topOfBand * HYSTERESIS_TOP_MULTIPLIER) {
+        return if (average / HYSTERESIS_TOP_MULTIPLIER > topOfBand) { // avoid Double.MAX_VALUE overflow
             true
         } else if (average < bottomOfBand * HYSTERESIS_BOTTOM_MULTIPLIER) {
             true
