4a0c5d82c88e78fb7c27634708c543fa452d6902
Cristian Monforte <cristian@duckduckgo.com>
2024-02-29 11:19:31 +0100

    support deeplink navigation

--- anvil/anvil-compiler/src/main/java/com/duckduckgo/anvil/compiler/ContributeToActivityStarterCodeGenerator.kt
+++ anvil/anvil-compiler/src/main/java/com/duckduckgo/anvil/compiler/ContributeToActivityStarterCodeGenerator.kt
@@ -68,14 +68,20 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
         val content = FileSpec.buildFile(generatedPackage, moduleClassName) {
             for (annotation in mapperAnnotations) {
                 val paramsType = annotation.paramsTypeOrNull()!!
-                addType(createMapperClass(paramsType, vmClass, module)).build()
+                val screenName = annotation.screenNameOrNull().orEmpty()
+                addType(createMapperClass(paramsType, screenName, vmClass, module)).build()
             }
         }
 
         return createGeneratedFile(codeGenDir, generatedPackage, moduleClassName, content)
     }
 
-    private fun createMapperClass(paramsType: ClassReference, vmClass: ClassReference.Psi, module: ModuleDescriptor): TypeSpec {
+    private fun createMapperClass(
+        paramsType: ClassReference,
+        screenName: String,
+        vmClass: ClassReference.Psi,
+        module: ModuleDescriptor,
+    ): TypeSpec {
         val moduleClassName = "${vmClass.shortName}_${paramsType.shortName}_Mapper"
 
         return TypeSpec.classBuilder(moduleClassName)
@@ -86,6 +92,11 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
             )
             .addSuperinterface(paramToActivityMapperFqName.asClassName(module))
             .addPrimaryConstructor()
+            .addProperty(
+                PropertySpec.builder("moshi", moshi.asClassName(module), KModifier.PRIVATE)
+                    .initializer("Moshi.Builder().add(%T()).build()", kotlinJsonObjectAdapter.asClassName(module))
+                    .build(),
+            )
             .addFunction(
                 FunSpec.builder("map")
                     .addModifiers(KModifier.OVERRIDE)
@@ -111,6 +122,39 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
                     )
                     .build(),
             )
+            .addFunction(
+                FunSpec.builder("map")
+                    .addModifiers(KModifier.OVERRIDE)
+                    .addParameter(
+                        "deeplinkActivityParams",
+                        deeplinkActivityParamsFqName.asClassName(module),
+                    )
+                    .returns(
+                        activityParamsFqName.asClassName(module).copy(nullable = true),
+                    )
+                    .addCode(
+                        """
+                            val screenName = deeplinkActivityParams.screenName.takeUnless { it.isEmpty() } ?: return null
+                            return if (screenName == %S) {
+                                if (deeplinkActivityParams.jsonArguments.isEmpty()) {
+                                    val instance = tryCreateObjectInstance(%T::class.java)
+                                    if (instance != null) {
+                                        return instance
+                                    }
+                                }
+                                tryCreateActivityParams(%T::class.java, deeplinkActivityParams)
+                            } else {
+                                null
+                            }
+                        """.trimIndent(),
+                        screenName,
+                        paramsType.asClassName(),
+                        paramsType.asClassName(),
+                    )
+                    .build(),
+            )
+            .addFunction(createTryCreateObjectInstance(module))
+            .addFunction(createTryCreateActivityParamsInstance(module))
             .build()
     }
 
@@ -126,11 +170,61 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
         return argumentAt("paramsType", 0)?.value()
     }
 
+    private fun AnnotationReference.screenNameOrNull(): String? {
+        return argumentAt("screenName", 1)?.value() as? String?
+    }
+
+    private fun createTryCreateObjectInstance(module: ModuleDescriptor): FunSpec {
+        return FunSpec.builder("tryCreateObjectInstance")
+            .addModifiers(KModifier.PRIVATE)
+            .addParameter("clazz", Class::class.asClassName().parameterizedBy(WildcardTypeName.producerOf(activityParamsFqName.asClassName(module))))
+            .addCode(
+                CodeBlock.builder()
+                    .add(
+                        """
+                            return kotlin.runCatching {
+                                %T.getRawType(clazz).kotlin.objectInstance as %T
+                            }.getOrNull()
+                        """.trimIndent(),
+                        moshiTypes.asClassName(module),
+                        activityParamsFqName.asClassName(module),
+                    ).build(),
+            )
+            .returns(activityParamsFqName.asClassName(module).copy(nullable = true))
+            .build()
+    }
+
+    private fun createTryCreateActivityParamsInstance(module: ModuleDescriptor): FunSpec {
+        return FunSpec.builder("tryCreateActivityParams")
+            .addModifiers(KModifier.PRIVATE)
+            .addParameter("clazz", Class::class.asClassName().parameterizedBy(WildcardTypeName.producerOf(activityParamsFqName.asClassName(module))))
+            .addParameter(
+                "deeplinkActivityParams",
+                deeplinkActivityParamsFqName.asClassName(module),
+            )
+            .addCode(
+                CodeBlock.builder()
+                    .add(
+                        """
+                            return kotlin.runCatching {
+                                moshi.adapter(clazz).fromJson(deeplinkActivityParams.jsonArguments)
+                            }.getOrNull()
+                        """.trimIndent(),
+                    ).build(),
+            )
+            .returns(activityParamsFqName.asClassName(module).copy(nullable = true))
+            .build()
+    }
+
     companion object {
         private val appScopeFqName = FqName("com.duckduckgo.di.scopes.AppScope")
         private val paramToActivityMapperFqName = FqName("com.duckduckgo.navigation.api.GlobalActivityStarter.ParamToActivityMapper")
         private val activityParamsFqName = FqName("com.duckduckgo.navigation.api.GlobalActivityStarter.ActivityParams")
+        private val deeplinkActivityParamsFqName = FqName("com.duckduckgo.navigation.api.GlobalActivityStarter.DeeplinkActivityParams")
         private val appCompatActivityFqName = FqName("androidx.appcompat.app.AppCompatActivity")
         private val duckduckgoActivityFqName = FqName("com.duckduckgo.common.ui.DuckDuckGoActivity")
+        private val moshi = FqName("com.squareup.moshi.Moshi")
+        private val kotlinJsonObjectAdapter = FqName("com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory")
+        private val moshiTypes = FqName("com.squareup.moshi.Types")
     }
 }
--- navigation/navigation-impl/src/test/java/com/duckduckgo/navigation/impl/GlobalActivityStarterImplTest.kt
+++ navigation/navigation-impl/src/test/java/com/duckduckgo/navigation/impl/GlobalActivityStarterImplTest.kt
@@ -21,10 +21,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.duckduckgo.navigation.api.GlobalActivityStarter
+import com.duckduckgo.navigation.api.GlobalActivityStarter.ActivityParams
+import com.duckduckgo.navigation.api.GlobalActivityStarter.DeeplinkActivityParams
 import com.duckduckgo.navigation.api.getActivityParams
-import java.lang.IllegalArgumentException
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.any
@@ -35,13 +38,8 @@ import org.mockito.kotlin.verify
 @RunWith(AndroidJUnit4::class)
 class GlobalActivityStarterImplTest {
 
-    private lateinit var globalActivityStarter: GlobalActivityStarter
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
-
-    @Before
-    fun setup() {
-        globalActivityStarter = GlobalActivityStarterImpl(setOf(TestActivityMapper()))
-    }
+    private val globalActivityStarter = GlobalActivityStarterImpl(setOf(TestActivityWithParamsMapper(), TestActivityNoParamsMapper()))
 
     @Test
     fun whenStartIntentNotFoundActivityThenReturnNull() {
@@ -49,8 +47,29 @@ class GlobalActivityStarterImplTest {
     }
 
     @Test
-    fun whenStartIntentNotFoundActivityThenReturnIntent() {
+    fun whenStartIntentNotFoundDeeplinkActivityThenReturnNull() {
+        assertNull(globalActivityStarter.startIntent(context, notFoundDeeplinkParams))
+    }
+
+    @Test
+    fun whenStartIntentWithParamsFindsActivityThenReturnIntent() {
         val intent = globalActivityStarter.startIntent(context, TestParams("test"))
+
+        assertNotNull(intent)
+        assertEquals("test", intent?.getActivityParams(TestParams::class.java)?.value)
+    }
+
+    @Test
+    fun whenStartIntentWithDeeplinkNoParamsFindsActivityThenReturnIntent() {
+        val intent = globalActivityStarter.startIntent(context, DeeplinkActivityParams("screenTest"))
+
+        assertNotNull(intent)
+        assertNotNull(intent?.getActivityParams(TestNoParams::class.java))
+    }
+
+    @Test
+    fun whenStartIntentWithDeeplinkParamsFindsActivityThenReturnIntent() {
+        val intent = globalActivityStarter.startIntent(context, DeeplinkActivityParams("screenTest", jsonArguments = "{\"value\": \"test\"}"))
 
         assertNotNull(intent)
         assertEquals("test", intent?.getActivityParams(TestParams::class.java)?.value)
@@ -61,24 +80,100 @@ class GlobalActivityStarterImplTest {
         globalActivityStarter.start(context, NotFoundParams)
     }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun whenStartNotFoundDeeplinkActivityThenThrow() {
+        globalActivityStarter.start(context, notFoundDeeplinkParams)
+    }
+
     @Test
-    fun whenStartNotFoundActivityThenSucceeds() {
+    fun whenStartWithParamsFindsActivityThenSucceeds() {
         val context: Context = mock()
         globalActivityStarter.start(context, TestParams("test"))
+
+        verify(context).startActivity(any(), anyOrNull())
+    }
+
+    @Test
+    fun whenStartWithDeeplinkParamsArgumentsFindsActivityThenSucceeds() {
+        val context: Context = mock()
+        globalActivityStarter.start(context, DeeplinkActivityParams("screenTest", jsonArguments = "{\"value\": \"test\"}"))
+
+        verify(context).startActivity(any(), anyOrNull())
+    }
+
+    @Test
+    fun whenStartWithDeeplinkNoParamsArgumentsFindsActivityThenSucceeds() {
+        val context: Context = mock()
+        globalActivityStarter.start(context, DeeplinkActivityParams("screenTest"))
 
         verify(context).startActivity(any(), anyOrNull())
     }
 }
 
 private class TestActivity : AppCompatActivity()
-private data class TestParams(val value: String) : GlobalActivityStarter.ActivityParams
-private object NotFoundParams : GlobalActivityStarter.ActivityParams
-private class TestActivityMapper : GlobalActivityStarter.ParamToActivityMapper {
-    override fun map(activityParams: GlobalActivityStarter.ActivityParams): Class<out AppCompatActivity>? {
+private data class TestParams(val value: String) : ActivityParams
+private object NotFoundParams : ActivityParams
+
+private val notFoundDeeplinkParams = DeeplinkActivityParams("notFoundScreen")
+private object TestNoParams : ActivityParams
+
+private class TestActivityNoParamsMapper : GlobalActivityStarter.ParamToActivityMapper {
+    override fun map(activityParams: ActivityParams): Class<out AppCompatActivity>? {
+        return if (activityParams is TestNoParams) {
+            TestActivity::class.java
+        } else {
+            null
+        }
+    }
+
+    override fun map(deeplinkActivityParams: DeeplinkActivityParams): ActivityParams? {
+        val screenName = deeplinkActivityParams.screenName.takeUnless { it.isEmpty() } ?: return null
+        return if (screenName == "screenTest") {
+            TestNoParams
+        } else {
+            null
+        }
+    }
+}
+
+private class TestActivityWithParamsMapper : GlobalActivityStarter.ParamToActivityMapper {
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    override fun map(activityParams: ActivityParams): Class<out AppCompatActivity>? {
         return if (activityParams is TestParams) {
             TestActivity::class.java
         } else {
             null
         }
+    }
+
+    override fun map(deeplinkActivityParams: DeeplinkActivityParams): ActivityParams? {
+        val screenName = deeplinkActivityParams.screenName.takeUnless { it.isEmpty() } ?: return null
+        return if (screenName == "screenTest") {
+            if (deeplinkActivityParams.jsonArguments.isEmpty()) {
+                val instance = tryCreateObjectInstance(TestParams::class.java)
+                if (instance != null) {
+                    return instance
+                }
+            }
+            tryCreateActivityParams(TestParams::class.java, deeplinkActivityParams.jsonArguments)
+        } else {
+            null
+        }
+    }
+
+    private fun tryCreateObjectInstance(clazz: Class<out ActivityParams>): ActivityParams? {
+        return kotlin.runCatching {
+            Types.getRawType(clazz).kotlin.objectInstance as ActivityParams
+        }.getOrNull()
+    }
+
+    private fun tryCreateActivityParams(
+        clazz: Class<out ActivityParams>,
+        jsonArguments: String,
+    ): ActivityParams? {
+        return kotlin.runCatching {
+            moshi.adapter(clazz).fromJson(jsonArguments)
+        }.getOrNull()
     }
 }
