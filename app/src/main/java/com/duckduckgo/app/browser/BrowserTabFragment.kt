001a857b32f73538d207bcbba07b5b4f31af2fd2
Ana Capatina <anikiki@gmail.com>
2024-03-05 13:02:57 +0000

    Prevented Dax dialogs from being shown in the Custom Tab.

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -3614,7 +3614,7 @@ class BrowserTabFragment :
         }
 
         fun renderCtaViewState(viewState: CtaViewState) {
-            if (isHidden) {
+            if (isHidden || isActiveCustomTab()) {
                 return
             }
 
