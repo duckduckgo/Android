2d198f414ef9bfab3f4c72b3535ed4adaf6b15f1
Karl Dimla <klmbdimla@gmail.com>
2024-03-11 17:34:54 +0100

    Refactor other classes depending onn VPNFeatureRegistry

--- app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/integration/VpnNetworkStackProviderTest.kt
+++ app-tracking-protection/vpn-impl/src/test/java/com/duckduckgo/mobile/android/vpn/integration/VpnNetworkStackProviderTest.kt
@@ -17,10 +17,8 @@
 package com.duckduckgo.mobile.android.vpn.integration
 
 import com.duckduckgo.common.utils.plugins.PluginPoint
+import com.duckduckgo.mobile.android.app.tracking.AppTrackingProtection
 import com.duckduckgo.mobile.android.vpn.AppTpVpnFeature
-import com.duckduckgo.mobile.android.vpn.FakeVpnFeaturesRegistry
-import com.duckduckgo.mobile.android.vpn.VpnFeature
-import com.duckduckgo.mobile.android.vpn.VpnFeaturesRegistry
 import com.duckduckgo.mobile.android.vpn.network.FakeVpnNetworkStack
 import com.duckduckgo.mobile.android.vpn.network.VpnNetworkStack
 import kotlinx.coroutines.test.runTest
@@ -33,7 +31,7 @@ import org.mockito.kotlin.whenever
 class VpnNetworkStackProviderTest {
 
     private val vpnNetworkStacks: PluginPoint<VpnNetworkStack> = mock()
-    private lateinit var vpnFeaturesRegistry: VpnFeaturesRegistry
+    private val appTrackingProtection: AppTrackingProtection = mock()
 
     private lateinit var vpnNetworkStackProvider: VpnNetworkStackProvider
     private val fakeVpnNetworkStack = FakeVpnNetworkStack(AppTpVpnFeature.APPTP_VPN.featureName)
@@ -41,19 +39,19 @@ class VpnNetworkStackProviderTest {
     @Before
     fun setup() {
         whenever(vpnNetworkStacks.getPlugins()).thenReturn(listOf(fakeVpnNetworkStack))
-        vpnFeaturesRegistry = FakeVpnFeaturesRegistry()
 
-        vpnNetworkStackProvider = VpnNetworkStackProviderImpl(vpnNetworkStacks, vpnFeaturesRegistry)
+        vpnNetworkStackProvider = VpnNetworkStackProviderImpl(vpnNetworkStacks, appTrackingProtection)
     }
 
     @Test
     fun whenProvideNetworkStackAndNoFeaturesRegisteredThenThrowsException() = runTest {
+        whenever(appTrackingProtection.isEnabled()).thenReturn(false)
         assertEquals(VpnNetworkStack.EmptyVpnNetworkStack, vpnNetworkStackProvider.provideNetworkStack())
     }
 
     @Test
     fun whenProviderNetworkStackAndAppTpRegisteredThenReturnNetworkStack() = runTest {
-        vpnFeaturesRegistry.registerFeature(AppTpVpnFeature.APPTP_VPN)
+        whenever(appTrackingProtection.isEnabled()).thenReturn(true)
         val networkStack = vpnNetworkStackProvider.provideNetworkStack()
 
         assertEquals(fakeVpnNetworkStack, networkStack)
@@ -61,18 +59,8 @@ class VpnNetworkStackProviderTest {
 
     @Test
     fun whenProviderNetworkStackAndUnknownFeatureRegisteredThenThrowsException() = runTest {
-        vpnFeaturesRegistry.registerFeature(VpnFeature { "unknown" })
+        whenever(appTrackingProtection.isEnabled()).thenReturn(false)
         vpnNetworkStackProvider.provideNetworkStack()
         assertEquals(VpnNetworkStack.EmptyVpnNetworkStack, vpnNetworkStackProvider.provideNetworkStack())
-    }
-
-    @Test
-    fun whenProviderNetworkStackAndFeaturesContainAppTpThenReturnNetworkStack() = runTest {
-        vpnFeaturesRegistry.registerFeature(VpnFeature { "unknown" })
-        vpnFeaturesRegistry.registerFeature(AppTpVpnFeature.APPTP_VPN)
-
-        val networkStack = vpnNetworkStackProvider.provideNetworkStack()
-
-        assertEquals(fakeVpnNetworkStack, networkStack)
     }
 }
