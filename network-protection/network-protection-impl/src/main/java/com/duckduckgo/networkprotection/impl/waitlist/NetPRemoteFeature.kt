4b3adacce97ec07ad664abe62c57598ac318f046
Aitor Viana <aitorvs@gmail.com>
2024-03-12 13:14:04 +0000

    Fix vpn disable notification wrongly showing upon beta ending (#4290)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1206806997674078/f
    
    ### Description
    See Asana task
    
    ### Steps to test this PR
    See [test cases](https://app.asana.com/0/0/1206806997674080/f)

--- network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/waitlist/NetPRemoteFeature.kt
+++ network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/waitlist/NetPRemoteFeature.kt
@@ -16,13 +16,29 @@
 
 package com.duckduckgo.networkprotection.impl.waitlist
 
+import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.duckduckgo.anvil.annotations.ContributesRemoteFeature
+import com.duckduckgo.app.di.AppCoroutineScope
+import com.duckduckgo.common.utils.DispatcherProvider
 import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.feature.toggles.api.RemoteFeatureStoreNamed
 import com.duckduckgo.feature.toggles.api.Toggle
+import com.duckduckgo.feature.toggles.api.Toggle.State
+import com.duckduckgo.mobile.android.vpn.prefs.VpnSharedPreferencesProvider
+import com.squareup.anvil.annotations.ContributesBinding
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.SingleInstanceIn
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @ContributesRemoteFeature(
     scope = AppScope::class,
     featureName = "networkProtection",
+    toggleStore = NetPRemoteFeatureStore::class,
 )
 interface NetPRemoteFeature {
     @Toggle.DefaultValue(false)
@@ -39,4 +55,47 @@ interface NetPRemoteFeature {
      */
     @Toggle.DefaultValue(true)
     fun waitlistBetaActive(): Toggle
+}
+
+@ContributesBinding(AppScope::class)
+@SingleInstanceIn(AppScope::class)
+@RemoteFeatureStoreNamed(NetPRemoteFeature::class)
+class NetPRemoteFeatureStore @Inject constructor(
+    @AppCoroutineScope private val coroutineScope: CoroutineScope,
+    private val dispatcherProvider: DispatcherProvider,
+    private val vpnSharedPreferencesProvider: VpnSharedPreferencesProvider,
+    moshi: Moshi,
+) : Toggle.Store {
+
+    private val preferences: SharedPreferences by lazy {
+        // migrate old values to new multiprocess shared prefs
+        vpnSharedPreferencesProvider.getSharedPreferences(FILENAME, multiprocess = true, migrate = true)
+    }
+    private val stateAdapter: JsonAdapter<State> by lazy {
+        moshi.newBuilder().add(KotlinJsonAdapterFactory()).build().adapter(State::class.java)
+    }
+
+    override fun set(
+        key: String,
+        state: State,
+    ) {
+        preferences.save(key, state)
+    }
+
+    override fun get(key: String): State? {
+        return preferences.getString(key, null)?.let {
+            stateAdapter.fromJson(it)
+        }
+    }
+
+    private fun SharedPreferences.save(key: String, state: State) {
+        coroutineScope.launch(dispatcherProvider.io()) {
+            edit(commit = true) { putString(key, stateAdapter.toJson(state)) }
+        }
+    }
+
+    companion object {
+        // This is the backwards compatible value
+        const val FILENAME = "com.duckduckgo.feature.toggle.networkProtection"
+    }
 }
