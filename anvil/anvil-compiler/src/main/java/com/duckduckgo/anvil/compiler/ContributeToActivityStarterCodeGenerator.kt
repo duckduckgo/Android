e1328b5ea524d89ab848eda3dd32ebda07c0cdd7
Cristian Monforte <cristian@duckduckgo.com>
2024-03-12 16:42:13 +0100

    support deeplink navigation (#4259)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    
    At a minimum, make sure your changes are tested in API 23 and one of the
    more recent API levels available.
    -->
    
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1206775412000490/f
    
    ### Description
    Adds support for deeplinks into our navigation api.
    
    This follows specs in
    https://app.asana.com/0/1202552961248957/1206494675697774/f:
    - new data class `DeeplinkActivityParams`, where we can indicate
    `screenName` and `jsonArguments`(optional) in case it requires extra
    params.
    - support navigation using `DeeplinkActivityParams` 
    - adds new method into `ParamToActivityMapper` that maps
    `DeeplinkActivityParams` to `ActivityParams``
    - add mapping logic into codegen
    
    ### Steps to test this PR
    
    For testing this we will need to introduce some changes so we can test
    navigation via deeplinks.
    I've pushed another branch with the required changes to make it easy.
    
    _Feature 1_
    - [x] checkout and install this branch:
    `feature/cristian/nav_api_deeplink_test_scenario`
    - [x] open the browser
    - [x] access Bookmarks via overflow menu from Browser Tab
    - [x] ensure you access bookmarks screen
    - [x] Go to `BrowserActivity`, comment L:498 and un-comment L:499 (to
    try navigation with params)
    - [x] install changes
    - [x] access Bookmarks via overflow menu from Browser Tab
    - [x] ensure you access bookmarks screen and you see a Toast!
    
    ### UI changes
    | Before  | After |
    | ------ | ----- |
    !(Upload before screenshot)|(Upload after screenshot)|

--- anvil/anvil-compiler/src/main/java/com/duckduckgo/anvil/compiler/ContributeToActivityStarterCodeGenerator.kt
+++ anvil/anvil-compiler/src/main/java/com/duckduckgo/anvil/compiler/ContributeToActivityStarterCodeGenerator.kt
@@ -68,14 +68,20 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
         val content = FileSpec.buildFile(generatedPackage, moduleClassName) {
             for (annotation in mapperAnnotations) {
                 val paramsType = annotation.paramsTypeOrNull()!!
-                addType(createMapperClass(paramsType, vmClass, module)).build()
+                val screenName = annotation.screenNameOrNull().orEmpty()
+                addType(createMapperClass(paramsType, screenName, vmClass, module)).build()
             }
         }
 
         return createGeneratedFile(codeGenDir, generatedPackage, moduleClassName, content)
     }
 
-    private fun createMapperClass(paramsType: ClassReference, vmClass: ClassReference.Psi, module: ModuleDescriptor): TypeSpec {
+    private fun createMapperClass(
+        paramsType: ClassReference,
+        screenName: String,
+        vmClass: ClassReference.Psi,
+        module: ModuleDescriptor,
+    ): TypeSpec {
         val moduleClassName = "${vmClass.shortName}_${paramsType.shortName}_Mapper"
 
         return TypeSpec.classBuilder(moduleClassName)
@@ -86,6 +92,11 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
             )
             .addSuperinterface(paramToActivityMapperFqName.asClassName(module))
             .addPrimaryConstructor()
+            .addProperty(
+                PropertySpec.builder("moshi", moshi.asClassName(module), KModifier.PRIVATE)
+                    .initializer("Moshi.Builder().add(%T()).build()", kotlinJsonObjectAdapter.asClassName(module))
+                    .build(),
+            )
             .addFunction(
                 FunSpec.builder("map")
                     .addModifiers(KModifier.OVERRIDE)
@@ -111,6 +122,15 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
                     )
                     .build(),
             )
+            .apply {
+                if (screenName.isBlank()) {
+                    addFunction(emptyDeepLinkMapper(module))
+                } else {
+                    addFunction(createDeeplinkMapper(module, paramsType, screenName))
+                    addFunction(createTryCreateObjectInstance(module))
+                    addFunction(createTryCreateActivityParamsInstance(module))
+                }
+            }
             .build()
     }
 
@@ -126,11 +146,120 @@ class ContributeToActivityStarterCodeGenerator : CodeGenerator {
         return argumentAt("paramsType", 0)?.value()
     }
 
+    private fun AnnotationReference.screenNameOrNull(): String? {
+        return argumentAt("screenName", 1)?.value() as? String?
+    }
+
+    private fun emptyDeepLinkMapper(module: ModuleDescriptor): FunSpec {
+        return FunSpec.builder("map")
+            .addModifiers(KModifier.OVERRIDE)
+            .addParameter(
+                "deeplinkActivityParams",
+                deeplinkActivityParamsFqName.asClassName(module),
+            )
+            .returns(
+                activityParamsFqName.asClassName(module).copy(nullable = true),
+            )
+            .addCode(
+                """
+                    return null            
+                """.trimIndent(),
+            )
+            .build()
+    }
+
+    private fun createDeeplinkMapper(module: ModuleDescriptor, paramsType: ClassReference, screenName: String): FunSpec {
+        return FunSpec.builder("map")
+            .addModifiers(KModifier.OVERRIDE)
+            .addParameter(
+                "deeplinkActivityParams",
+                deeplinkActivityParamsFqName.asClassName(module),
+            )
+            .returns(
+                activityParamsFqName.asClassName(module).copy(nullable = true),
+            )
+            .addCode(
+                """
+                    val screenName = deeplinkActivityParams.screenName
+                    if (screenName.isNullOrEmpty()) {
+                        return null
+                    }
+                    
+                    val definedScreenName = %S
+                    if (definedScreenName.isNullOrEmpty()) {
+                        return null
+                    }
+                    
+                    return if (screenName == definedScreenName) {
+                        if (deeplinkActivityParams.jsonArguments.isEmpty()) {
+                            val instance = tryCreateObjectInstance(%T::class.java)
+                            if (instance != null) {
+                                return instance
+                            }
+                        }
+                        tryCreateActivityParams(%T::class.java, deeplinkActivityParams)
+                    } else {
+                        null
+                    }
+                """.trimIndent(),
+                screenName,
+                paramsType.asClassName(),
+                paramsType.asClassName(),
+            )
+            .build()
+    }
+
+    private fun createTryCreateObjectInstance(module: ModuleDescriptor): FunSpec {
+        return FunSpec.builder("tryCreateObjectInstance")
+            .addModifiers(KModifier.PRIVATE)
+            .addParameter("clazz", Class::class.asClassName().parameterizedBy(WildcardTypeName.producerOf(activityParamsFqName.asClassName(module))))
+            .addCode(
+                CodeBlock.builder()
+                    .add(
+                        """
+                            return kotlin.runCatching {
+                                %T.getRawType(clazz).kotlin.objectInstance as %T
+                            }.getOrNull()
+                        """.trimIndent(),
+                        moshiTypes.asClassName(module),
+                        activityParamsFqName.asClassName(module),
+                    ).build(),
+            )
+            .returns(activityParamsFqName.asClassName(module).copy(nullable = true))
+            .build()
+    }
+
+    private fun createTryCreateActivityParamsInstance(module: ModuleDescriptor): FunSpec {
+        return FunSpec.builder("tryCreateActivityParams")
+            .addModifiers(KModifier.PRIVATE)
+            .addParameter("clazz", Class::class.asClassName().parameterizedBy(WildcardTypeName.producerOf(activityParamsFqName.asClassName(module))))
+            .addParameter(
+                "deeplinkActivityParams",
+                deeplinkActivityParamsFqName.asClassName(module),
+            )
+            .addCode(
+                CodeBlock.builder()
+                    .add(
+                        """
+                            return kotlin.runCatching {
+                                moshi.adapter(clazz).fromJson(deeplinkActivityParams.jsonArguments)
+                            }.getOrNull()
+                        """.trimIndent(),
+                    ).build(),
+            )
+            .returns(activityParamsFqName.asClassName(module).copy(nullable = true))
+            .build()
+    }
+
     companion object {
         private val appScopeFqName = FqName("com.duckduckgo.di.scopes.AppScope")
         private val paramToActivityMapperFqName = FqName("com.duckduckgo.navigation.api.GlobalActivityStarter.ParamToActivityMapper")
         private val activityParamsFqName = FqName("com.duckduckgo.navigation.api.GlobalActivityStarter.ActivityParams")
+        private val deeplinkActivityParamsFqName = FqName("com.duckduckgo.navigation.api.GlobalActivityStarter.DeeplinkActivityParams")
         private val appCompatActivityFqName = FqName("androidx.appcompat.app.AppCompatActivity")
         private val duckduckgoActivityFqName = FqName("com.duckduckgo.common.ui.DuckDuckGoActivity")
+        private val moshi = FqName("com.squareup.moshi.Moshi")
+        private val kotlinJsonObjectAdapter = FqName("com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory")
+        private val moshiTypes = FqName("com.squareup.moshi.Types")
     }
 }
