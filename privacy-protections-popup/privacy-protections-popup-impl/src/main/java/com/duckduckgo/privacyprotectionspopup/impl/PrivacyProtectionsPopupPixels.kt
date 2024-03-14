c7394672306aea50bb71a66d8142b187637f0455
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2024-03-14 12:46:54 +0100

    Remove temporary pixels for Privacy Protections Popup experiment (#4302)
    <!--
    Note: This checklist is a reminder of our shared engineering
    expectations.
    The items in Bold are required
    If your PR involves UI changes:
    1. Upload screenshots or screencasts that illustrate the changes before
    / after
    2. Add them under the UI changes section (feel free to add more columns
    if needed)
    If your PR does not involve UI changes, you can remove the **UI
    changes** section
    
    At a minimum, make sure your changes are tested in API 23 and one of the
    more recent API levels available.
    -->
    
    Task/Issue URL:
    https://app.asana.com/0/1205278999335242/1206451970300384/f
    
    ### Description
    This removes temporary pixels that were used to measure Privacy
    Protections Popup experiment.
    
    ### No UI changes

--- privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupPixels.kt
+++ privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupPixels.kt
@@ -16,27 +16,9 @@
 
 package com.duckduckgo.privacyprotectionspopup.impl
 
-import com.duckduckgo.app.di.AppCoroutineScope
-import com.duckduckgo.app.statistics.pixels.Pixel
 import com.duckduckgo.di.scopes.FragmentScope
-import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupExperimentExternalPixels
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.DO_NOT_SHOW_AGAIN_CLICKED
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.EXPERIMENT_VARIANT_ASSIGNED
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.PAGE_REFRESH_ON_POSSIBLE_BREAKAGE
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.PAGE_REFRESH_ON_POSSIBLE_BREAKAGE_DAILY
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.POPUP_DISMISSED_VIA_BUTTON
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.POPUP_DISMISSED_VIA_CLICK_OUTSIDE
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.POPUP_TRIGGERED
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.PRIVACY_DASHBOARD_OPENED
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.PRIVACY_DASHBOARD_OPENED_UNIQUE
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.PROTECTIONS_DISABLED
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.PROTECTIONS_DISABLED_UNIQUE
-import com.duckduckgo.privacyprotectionspopup.impl.PrivacyProtectionsPopupPixelName.Params.PARAM_POPUP_TRIGGER_COUNT
-import com.duckduckgo.privacyprotectionspopup.impl.store.PrivacyProtectionsPopupDataStore
 import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 interface PrivacyProtectionsPopupPixels {
     fun reportExperimentVariantAssigned()
@@ -50,73 +32,29 @@ interface PrivacyProtectionsPopupPixels {
 }
 
 @ContributesBinding(FragmentScope::class)
-class PrivacyProtectionsPopupPixelsImpl @Inject constructor(
-    private val pixelSender: Pixel,
-    private val paramsProvider: PrivacyProtectionsPopupExperimentExternalPixels,
-    @AppCoroutineScope private val appCoroutineScope: CoroutineScope,
-    private val dataStore: PrivacyProtectionsPopupDataStore,
-) : PrivacyProtectionsPopupPixels {
+class PrivacyProtectionsPopupPixelsImpl @Inject constructor() : PrivacyProtectionsPopupPixels {
 
     override fun reportExperimentVariantAssigned() {
-        appCoroutineScope.launch {
-            fire(EXPERIMENT_VARIANT_ASSIGNED)
-        }
     }
 
     override fun reportPopupTriggered() {
-        appCoroutineScope.launch {
-            fire(POPUP_TRIGGERED)
-        }
     }
 
     override fun reportProtectionsDisabled() {
-        appCoroutineScope.launch {
-            fire(PROTECTIONS_DISABLED)
-            fire(PROTECTIONS_DISABLED_UNIQUE)
-        }
     }
 
     override fun reportPrivacyDashboardOpened() {
-        appCoroutineScope.launch {
-            fire(PRIVACY_DASHBOARD_OPENED)
-            fire(PRIVACY_DASHBOARD_OPENED_UNIQUE)
-        }
     }
 
     override fun reportPopupDismissedViaButton() {
-        appCoroutineScope.launch {
-            fire(POPUP_DISMISSED_VIA_BUTTON)
-        }
     }
 
     override fun reportPopupDismissedViaClickOutside() {
-        appCoroutineScope.launch {
-            fire(POPUP_DISMISSED_VIA_CLICK_OUTSIDE)
-        }
     }
 
     override fun reportDoNotShowAgainClicked() {
-        appCoroutineScope.launch {
-            val params = mapOf(PARAM_POPUP_TRIGGER_COUNT to dataStore.getPopupTriggerCount().toString())
-            fire(DO_NOT_SHOW_AGAIN_CLICKED, params)
-        }
     }
 
     override fun reportPageRefreshOnPossibleBreakage() {
-        appCoroutineScope.launch {
-            fire(PAGE_REFRESH_ON_POSSIBLE_BREAKAGE)
-            fire(PAGE_REFRESH_ON_POSSIBLE_BREAKAGE_DAILY)
-        }
-    }
-
-    private suspend fun fire(
-        pixel: PrivacyProtectionsPopupPixelName,
-        params: Map<String, String> = emptyMap(),
-    ) {
-        pixelSender.fire(
-            pixel = pixel,
-            parameters = params + paramsProvider.getPixelParams(),
-            type = pixel.type,
-        )
     }
 }
