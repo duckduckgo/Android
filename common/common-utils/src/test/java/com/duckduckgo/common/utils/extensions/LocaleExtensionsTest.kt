df497fb293388d358db624c61035300ed934c046
joshliebe <joshliebe@gmail.com>
2024-04-22 12:21:41 +0100

    Improve voice search compatibility

---
+++ common/common-utils/src/test/java/com/duckduckgo/common/utils/extensions/LocaleExtensionsTest.kt
@@ -0,0 +1,31 @@
+package com.duckduckgo.common.utils.extensions
+
+import java.util.Locale
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class LocaleExtensionsTest {
+
+    @Test
+    fun whenHasUnicodeLocaleExtensionThenRemoveUnicodeLocaleExtension() {
+        val locale = Locale.Builder()
+            .setLanguage("en")
+            .setRegion("US")
+            .setExtension(Locale.UNICODE_LOCALE_EXTENSION, "test")
+            .build()
+
+        assertEquals("en-US-u-test", locale.toLanguageTag())
+        assertEquals("en-US", locale.toSanitizedLanguageTag())
+    }
+
+    @Test
+    fun whenDoesNotHaveUnicodeLocaleExtensionThenLanguageTagIsUnchanged() {
+        val locale = Locale.Builder()
+            .setLanguage("en")
+            .setRegion("US")
+            .build()
+
+        assertEquals("en-US", locale.toLanguageTag())
+        assertEquals("en-US", locale.toSanitizedLanguageTag())
+    }
+}
