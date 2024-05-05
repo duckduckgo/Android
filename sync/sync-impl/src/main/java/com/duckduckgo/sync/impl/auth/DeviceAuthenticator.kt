95467fe3a81d2791ee551826c3088e8213bb16f4
Cristian Monforte <cristian@duckduckgo.com>
2024-05-05 23:24:06 +0200

    Request device authentication for sync account related actions (#4466)
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1206876054968714/f
    
    ### Description
    
    ### Steps to test this PR
    
    _Sync disabled_
    - [x] Without any device authentication
    - [x] User should not be able to create/recover/join account
    - [x] dialog should be displayed
    - [x] button should take the user to settings
    
    _Sync enabled_
    - [x] Setup a device authentication
    - [x] create account
    - [x] Disable device authentication
    - [x] User should not be able to add a new device / store recovery code
    - [x] dialog should be displayed
    - [x] button should take the user to settings
    
    ### UI changes
    | Before  | After |
    | ------ | ----- |
    !(Upload before screenshot)|(Upload after screenshot)|
    
    ---------
    
    Co-authored-by: root <root@10.100.11.5>

--- sync/sync-impl/src/main/java/com/duckduckgo/sync/impl/auth/DeviceAuthenticator.kt
+++ sync/sync-impl/src/main/java/com/duckduckgo/sync/impl/auth/DeviceAuthenticator.kt
@@ -16,16 +16,23 @@
 
 package com.duckduckgo.sync.impl.auth
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.provider.Settings
 import androidx.annotation.UiThread
 import androidx.fragment.app.FragmentActivity
 import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.sync.impl.BuildConfig
 import com.duckduckgo.sync.impl.R
 import com.duckduckgo.sync.impl.auth.DeviceAuthenticator.AuthConfiguration
 import com.duckduckgo.sync.impl.auth.DeviceAuthenticator.AuthResult
 import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
+import timber.log.Timber
 
 interface DeviceAuthenticator {
     /**
@@ -43,6 +50,20 @@ interface DeviceAuthenticator {
         config: AuthConfiguration = AuthConfiguration(),
         onResult: (AuthResult) -> Unit,
     )
+
+    /**
+     * Returns true if the user has to authenticate to use sync. This is always true in production.
+     *
+     * When running some specific UI tests, this can be set to false with a build flag to allow us to have increased test coverage.
+     */
+    fun isAuthenticationRequired(): Boolean {
+        return BuildConfig.AUTH_REQUIRED
+    }
+
+    /**
+     * Launches the device authentication enrollment screen from system settings.
+     */
+    fun launchDeviceAuthEnrollment(context: Context)
 
     sealed class AuthResult {
         object Success : AuthResult()
@@ -91,5 +112,46 @@ class RealDeviceAuthenticator @Inject constructor(
         } else {
             onResult(AuthResult.Success)
         }
+    }
+
+    @SuppressLint("InlinedApi", "DEPRECATION")
+    override fun launchDeviceAuthEnrollment(context: Context) {
+        when {
+            appBuildConfig.manufacturer == "Xiaomi" -> {
+                // Issue on Xiaomi: https://stackoverflow.com/questions/68484485/intent-action-fingerprint-enroll-on-redmi-results-in-exception
+                SYSTEM_SETTINGS_ACTION.safeLaunchSettingsActivity(context, tryFallback = false)
+            }
+
+            appBuildConfig.sdkInt >= Build.VERSION_CODES.R -> {
+                Settings.ACTION_BIOMETRIC_ENROLL.safeLaunchSettingsActivity(context, tryFallback = true)
+            }
+
+            appBuildConfig.sdkInt >= Build.VERSION_CODES.P -> {
+                Settings.ACTION_FINGERPRINT_ENROLL.safeLaunchSettingsActivity(context, tryFallback = true)
+            }
+
+            else -> {
+                Settings.ACTION_SECURITY_SETTINGS.safeLaunchSettingsActivity(context, tryFallback = true)
+            }
+        }
+    }
+
+    /**
+     * Attempt to launch the given activity.
+     * If it fails because the activity wasn't found, try launching the main settings activity if tryFallback=true.
+     */
+    private fun String.safeLaunchSettingsActivity(context: Context, tryFallback: Boolean) {
+        try {
+            context.startActivity(Intent(this))
+        } catch (e: ActivityNotFoundException) {
+            Timber.w("%s. Trying fallback? %s", e.message, tryFallback)
+            if (tryFallback) {
+                SYSTEM_SETTINGS_ACTION.safeLaunchSettingsActivity(context, tryFallback = false)
+            }
+        }
+    }
+
+    companion object {
+        private const val SYSTEM_SETTINGS_ACTION = Settings.ACTION_SETTINGS
     }
 }
