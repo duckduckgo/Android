fa240a67351c21370189825d82f88955e2c6080b
Aitor Viana <aitorvs@gmail.com>
2024-05-25 10:54:14 +0100

    Provide a typealias ActivePluginPoint that ensures JvmSuppressWildcards added

--- common/common-utils/src/main/java/com/duckduckgo/common/utils/plugins/ActivePluginPoint.kt
+++ common/common-utils/src/main/java/com/duckduckgo/common/utils/plugins/ActivePluginPoint.kt
@@ -16,12 +16,44 @@
 
 package com.duckduckgo.common.utils.plugins
 
-/** A PluginPoint provides a list of plugins of a particular type T */
-interface ActivePluginPoint<out T : @JvmSuppressWildcards ActivePluginPoint.ActivePlugin> {
+@Deprecated(
+    message = "Use \"ActivePluginPoint\" instead to ensure \"JvmSuppressWildcards\" is not forgotten",
+    replaceWith = ReplaceWith("ActivePluginPoint"),
+)
+interface InternalActivePluginPoint<out T : @JvmSuppressWildcards ActivePlugin> {
     /** @return the list of plugins of type <T> */
     suspend fun getPlugins(): Collection<T>
-
-    interface ActivePlugin {
-        suspend fun isActive(): Boolean = true
-    }
 }
+
+/**
+ * Active plugins SHALL extend from [ActivePlugin]
+ *
+ * Usage:
+ * ```kotlin
+ * @ContributesActivePluginPoint(
+ *     scope = SomeScope::class,
+ * )
+ * interface MyActivePlugin : ActivePlugin {...}
+ *
+ * @ContributesActivePlugin(
+ *     scope = SomeScope::class,
+ *     boundType = MyActivePlugin::class,
+ * )
+ * class FooMyActivePlugin @Inject constructor() : MyActivePlugin {...}
+ * ```
+ */
+interface ActivePlugin {
+    suspend fun isActive(): Boolean = true
+}
+
+/**
+ * Use this typealias to collect your [ActivePlugin]s
+ *
+ * Usage:
+ * ```kotlin
+ * class MyClass @Inject constructor(
+ *   private val pp: ActivePluginPoint<MyActivePlugin>,
+ * ) {...}
+ * ```
+ */
+typealias ActivePluginPoint<T> = InternalActivePluginPoint<@JvmSuppressWildcards T>
