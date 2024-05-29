9272935c625b27a30085107edb4eeec31767f04e
Craig Russell <CDRussell@users.noreply.github.com>
2024-05-29 12:01:10 +0100

    Add developer settings for importing passwords from Google Password Manager

--- autofill/autofill-internal/src/main/java/com/duckduckgo/autofill/internal/AutofillInternalSettingsActivity.kt
+++ autofill/autofill-internal/src/main/java/com/duckduckgo/autofill/internal/AutofillInternalSettingsActivity.kt
@@ -16,15 +16,19 @@
 
 package com.duckduckgo.autofill.internal
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Lifecycle.State.STARTED
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.app.tabs.BrowserNav
 import com.duckduckgo.autofill.api.AutofillFeature
 import com.duckduckgo.autofill.api.AutofillScreens.AutofillSettingsScreen
 import com.duckduckgo.autofill.api.AutofillSettingsLaunchSource.InternalDevSettings
@@ -33,6 +37,8 @@ import com.duckduckgo.autofill.api.email.EmailManager
 import com.duckduckgo.autofill.impl.configuration.AutofillJavascriptEnvironmentConfiguration
 import com.duckduckgo.autofill.impl.email.incontext.store.EmailProtectionInContextDataStore
 import com.duckduckgo.autofill.impl.engagement.store.AutofillEngagementRepository
+import com.duckduckgo.autofill.impl.importing.CsvPasswordImporter
+import com.duckduckgo.autofill.impl.importing.CsvPasswordParser
 import com.duckduckgo.autofill.impl.reporting.AutofillSiteBreakageReportingDataStore
 import com.duckduckgo.autofill.impl.store.InternalAutofillStore
 import com.duckduckgo.autofill.impl.store.NeverSavedSiteRepository
@@ -76,6 +82,12 @@ class AutofillInternalSettingsActivity : DuckDuckGoActivity() {
     lateinit var autofillStore: InternalAutofillStore
 
     @Inject
+    lateinit var csvPasswordParser: CsvPasswordParser
+
+    @Inject
+    lateinit var browserNav: BrowserNav
+
+    @Inject
     lateinit var autofillPrefsStore: AutofillPrefsStore
 
     private val dateFormatter = SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.MEDIUM, SimpleDateFormat.MEDIUM)
@@ -100,6 +112,24 @@ class AutofillInternalSettingsActivity : DuckDuckGoActivity() {
 
     @Inject
     lateinit var reportBreakageDataStore: AutofillSiteBreakageReportingDataStore
+
+    @Inject
+    lateinit var csvPasswordImporter: CsvPasswordImporter
+
+    private val importCsvLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val fileUrl = data?.data
+
+            logcat { "cdr onActivityResult for CSV file request. resultCode=${result.resultCode}. uri=$fileUrl" }
+            if (fileUrl != null) {
+                lifecycleScope.launch {
+                    val insertedIds = csvPasswordImporter.importCsv(fileUrl)
+                    Toast.makeText(this@AutofillInternalSettingsActivity, "Imported ${insertedIds.size} passwords", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -168,6 +198,7 @@ class AutofillInternalSettingsActivity : DuckDuckGoActivity() {
         configureEngagementEventHandlers()
         configureReportBreakagesHandlers()
         configureDeclineCounterHandlers()
+        configureImportPasswordsEventHandlers()
     }
 
     private fun configureReportBreakagesHandlers() {
@@ -176,6 +207,22 @@ class AutofillInternalSettingsActivity : DuckDuckGoActivity() {
                 reportBreakageDataStore.clearAllReports()
             }
             Toast.makeText(this@AutofillInternalSettingsActivity, R.string.autofillDevSettingsReportBreakageHistoryCleared, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun configureImportPasswordsEventHandlers() {
+        binding.importPasswordsLaunchGooglePasswordWebpage.setClickListener {
+            val googlePasswordsUrl = "https://passwords.google.com/options?ep=1"
+            startActivity(browserNav.openInNewTab(this, googlePasswordsUrl))
+        }
+
+        binding.importPasswordsImportCsv.setClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "*/*"
+            }
+            importCsvLauncher.launch(intent)
         }
     }
 
