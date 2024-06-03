c54bdf52b41c0f4345deca4030fe7f9cc2b91cbd
David González <malmstein@gmail.com>
2024-06-03 13:53:58 +0200

    added test for RemoteMessageViewModel

--- app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
@@ -5432,10 +5432,6 @@ class BrowserTabViewModelTest {
         testee.ctaViewState.value = ctaViewState().copy(cta = cta)
     }
 
-    private suspend fun givenRemoteMessage(remoteMessage: RemoteMessage) {
-        remoteMessageFlow.send(remoteMessage)
-    }
-
     private fun aTabEntity(id: String): TabEntity {
         return TabEntity(tabId = id, position = 0)
     }
