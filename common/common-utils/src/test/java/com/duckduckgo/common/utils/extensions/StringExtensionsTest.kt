c1a0f3597a675908d1148cc565824048755ade2f
joshliebe <joshliebe@gmail.com>
2024-06-10 17:32:51 +0100

    Add additional tests

--- common/common-utils/src/test/java/com/duckduckgo/common/utils/extensions/StringExtensionsTest.kt
+++ common/common-utils/src/test/java/com/duckduckgo/common/utils/extensions/StringExtensionsTest.kt
@@ -12,6 +12,8 @@ class StringExtensionsTest {
         assertEquals(0, "2.1.3.5".compareSemanticVersion("2.1.3.5"))
         assertEquals(0, "1.0".compareSemanticVersion("1.0.0"))
         assertEquals(0, "1.0.0".compareSemanticVersion("1.0"))
+        assertEquals(0, "1.0.0".compareSemanticVersion("1.0"))
+        assertEquals(0, "126.0.6478.40".compareSemanticVersion("126.0.6478.40"))
     }
 
     @Test
@@ -23,6 +25,11 @@ class StringExtensionsTest {
         assertEquals(1, "1.0.1".compareSemanticVersion("1.0"))
         assertEquals(1, "1.1".compareSemanticVersion("1.0.0"))
         assertEquals(1, "1.0.0.1".compareSemanticVersion("1.0"))
+        assertEquals(1, "127.0.6478.40".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(1, "126.1.6478.40".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(1, "126.0.6479.40".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(1, "126.0.6478.41".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(1, "127.amazon-webview-v122-6261-tablet.6261.140.26".compareSemanticVersion("126.0.6478.40"))
     }
 
     @Test
@@ -33,6 +40,11 @@ class StringExtensionsTest {
         assertEquals(-1, "1.0".compareSemanticVersion("1.0.0.1"))
         assertEquals(-1, "1.0.0".compareSemanticVersion("1.0.0.1"))
         assertEquals(-1, "1".compareSemanticVersion("1.0.1"))
+        assertEquals(-1, "125.0.6478.40".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(-1, "125.1.6478.40".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(-1, "126.0.6477.40".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(-1, "126.0.6478.39".compareSemanticVersion("126.0.6478.40"))
+        assertEquals(-1, "125.amazon-webview-v122-6261-tablet.6261.140.26".compareSemanticVersion("126.0.6478.40"))
     }
 
     @Test
@@ -43,5 +55,8 @@ class StringExtensionsTest {
         assertNull("1.0.0".compareSemanticVersion("1.0.a"))
         assertNull("a.b.c.d".compareSemanticVersion("1.0.0"))
         assertNull("1".compareSemanticVersion("a.1.2"))
+        assertNull("126.amazon-webview-v122-6261-tablet.6261.140.26".compareSemanticVersion("126.0.6478.40"))
+        assertNull("126.0.6478a.40".compareSemanticVersion("126.0.6478.40"))
+        assertNull("126.0.6478.40a".compareSemanticVersion("126.0.6478.40"))
     }
 }
