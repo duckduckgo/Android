5e2151496cbd437b82f6c4abe34e3c05f7c70e42
joshliebe <joshliebe@gmail.com>
2024-06-12 18:31:43 +0100

    Delete APP_EMPTY_VIEW_WIDGET_LAUNCH pixel name

--- app/src/main/java/com/duckduckgo/app/pixels/AppPixelName.kt
+++ app/src/main/java/com/duckduckgo/app/pixels/AppPixelName.kt
@@ -72,7 +72,6 @@ enum class AppPixelName(override val pixelName: String) : Pixel.PixelName {
     APP_WIDGET_LAUNCH(pixelName = "m_w_l"),
     APP_FAVORITES_SEARCHBAR_WIDGET_LAUNCH(pixelName = "m_sfbw_l"),
     APP_FAVORITES_ITEM_WIDGET_LAUNCH(pixelName = "m_sfiw_l"),
-    APP_EMPTY_VIEW_WIDGET_LAUNCH(pixelName = "m_sfew_l"),
     APP_ASSIST_LAUNCH(pixelName = "m_a_l"),
     APP_SYSTEM_SEARCH_BOX_LAUNCH(pixelName = "m_ssb_l"),
     APP_THIRD_PARTY_LAUNCH(pixelName = "m_third_party_launch"),
