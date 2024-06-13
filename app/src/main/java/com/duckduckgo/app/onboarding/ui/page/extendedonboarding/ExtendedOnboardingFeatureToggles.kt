04d0630531a2f7a7bb3bb1295c84c3900ef80a67
Noelia Alcala <nalcalag@gmail.com>
2024-06-13 13:32:26 +0100

    Added Experiment annotation to sub-feature

--- app/src/main/java/com/duckduckgo/app/onboarding/ui/page/extendedonboarding/ExtendedOnboardingFeatureToggles.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/ui/page/extendedonboarding/ExtendedOnboardingFeatureToggles.kt
@@ -19,6 +19,7 @@ package com.duckduckgo.app.onboarding.ui.page.extendedonboarding
 import com.duckduckgo.anvil.annotations.ContributesRemoteFeature
 import com.duckduckgo.di.scopes.AppScope
 import com.duckduckgo.feature.toggles.api.Toggle
+import com.duckduckgo.feature.toggles.api.Toggle.Experiment
 
 @ContributesRemoteFeature(
     scope = AppScope::class,
@@ -33,5 +34,6 @@ interface ExtendedOnboardingFeatureToggles {
     fun aestheticUpdates(): Toggle
 
     @Toggle.DefaultValue(false)
+    @Experiment
     fun noBrowserCtas(): Toggle
 }
