702c589250fd17a4a512c2dae3cdf2fcb4272eca
David González <malmstein@gmail.com>
2024-06-17 11:39:17 +0200

    Disable Force Dark Mode when WebView version doesn't support it (#4653)
    Task/Issue URL:
    https://app.asana.com/0/1157893581871903/1207572180893002/f
    
    ### Description
    We enabled [✓ Android: Force dark mode in sites that don’t support
    it](https://app.asana.com/0/276630244458377/1206630026838095/f)
    This was causing the app crash in webview versions that didn’t support
    it. Fixed in[✓ setAlgorithmicDarkeningAllowed crashing the
    app](https://app.asana.com/0/1125189844152671/1207572075253654/f)
    After this fix, devices with webview versions that don’t support
    algorithmic darkening won’t see the feature.
    
    This PR removes this option in devices that don’t support it
    
    ### Steps to test this PR
    
    _WebView not supported_
    - [x] Install this branch in an emulator with an old webview version
    - [x] Open Settings / Appearance
    - [x] Ensure Dark theme is selected
    - [x] Verify that Force Dark Mode is not visible
    
    _WebView supported_
    - [x] Install this branch in an emulator with a new webview version
    - [x] Open Settings / Appearance
    - [x] Ensure Dark theme is selected
    - [x] Verify that Force Dark Mode is enabled
    - [x] Verify that turning it on enabled force dark mode

--- app/src/main/java/com/duckduckgo/app/appearance/AppearanceViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/appearance/AppearanceViewModel.kt
@@ -18,6 +18,7 @@ package com.duckduckgo.app.appearance
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.webkit.WebViewFeature
 import com.duckduckgo.anvil.annotations.ContributesViewModel
 import com.duckduckgo.app.icon.api.AppIcon
 import com.duckduckgo.app.pixels.AppPixelName
@@ -51,6 +52,7 @@ class AppearanceViewModel @Inject constructor(
         val appIcon: AppIcon = AppIcon.DEFAULT,
         val forceDarkModeEnabled: Boolean = false,
         val canForceDarkMode: Boolean = false,
+        val supportsForceDarkMode: Boolean = true,
     )
 
     sealed class Command {
@@ -69,7 +71,8 @@ class AppearanceViewModel @Inject constructor(
                     theme = themingDataStore.theme,
                     appIcon = settingsDataStore.appIcon,
                     forceDarkModeEnabled = settingsDataStore.experimentalWebsiteDarkMode,
-                    canForceDarkMode = themingDataStore.theme != DuckDuckGoTheme.LIGHT,
+                    canForceDarkMode = canForceDarkMode(),
+                    supportsForceDarkMode = WebViewFeature.isFeatureSupported(WebViewFeature.ALGORITHMIC_DARKENING),
                 ),
             )
         }
@@ -77,6 +80,10 @@ class AppearanceViewModel @Inject constructor(
 
     fun commands(): Flow<Command> {
         return command.receiveAsFlow()
+    }
+
+    private fun canForceDarkMode(): Boolean {
+        return themingDataStore.theme != DuckDuckGoTheme.LIGHT
     }
 
     fun userRequestedToChangeTheme() {
@@ -98,7 +105,7 @@ class AppearanceViewModel @Inject constructor(
         viewModelScope.launch(dispatcherProvider.io()) {
             themingDataStore.theme = selectedTheme
             withContext(dispatcherProvider.main()) {
-                viewState.emit(currentViewState().copy(theme = selectedTheme, forceDarkModeEnabled = selectedTheme != DuckDuckGoTheme.LIGHT))
+                viewState.emit(currentViewState().copy(theme = selectedTheme, forceDarkModeEnabled = canForceDarkMode()))
                 command.send(Command.UpdateTheme)
             }
         }
