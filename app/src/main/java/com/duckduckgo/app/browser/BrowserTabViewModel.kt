5b129313b0e9b9117aa482a34bb0ed5a7e2df864
David González <malmstein@gmail.com>
2024-06-26 14:09:48 +0200

    code format

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -175,7 +175,6 @@ import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupExperim
 import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupManager
 import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsPopupUiEvent
 import com.duckduckgo.privacyprotectionspopup.api.PrivacyProtectionsToggleUsageListener
-import com.duckduckgo.remote.messaging.api.RemoteMessage
 import com.duckduckgo.savedsites.api.SavedSitesRepository
 import com.duckduckgo.savedsites.api.models.BookmarkFolder
 import com.duckduckgo.savedsites.api.models.SavedSite
