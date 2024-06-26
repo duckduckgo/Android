610fe06144709173779c5859b0163c98efe80272
David González <malmstein@gmail.com>
2024-06-26 16:53:05 +0200

    fixing RMF model test

--- app/src/test/java/com/duckduckgo/app/browser/favorites/NewTabLegacyPageViewModelTest.kt
+++ app/src/test/java/com/duckduckgo/app/browser/favorites/NewTabLegacyPageViewModelTest.kt
@@ -2,35 +2,26 @@ package com.duckduckgo.app.browser.favorites
 
 import androidx.lifecycle.LifecycleOwner
 import app.cash.turbine.test
-import com.duckduckgo.app.browser.BrowserTabViewModel.HiddenBookmarksIds
+import com.duckduckgo.app.browser.favorites.NewTabLegacyPageViewModel.Command
 import com.duckduckgo.app.browser.remotemessage.CommandActionMapper
-import com.duckduckgo.app.browser.remotemessage.RemoteMessagingModel
-import com.duckduckgo.app.pixels.AppPixelName
-import com.duckduckgo.app.playstore.PlayStoreUtils
-import com.duckduckgo.app.statistics.pixels.Pixel
 import com.duckduckgo.common.test.CoroutineTestRule
-import com.duckduckgo.common.utils.DispatcherProvider
+import com.duckduckgo.common.utils.playstore.PlayStoreUtils
+import com.duckduckgo.remote.messaging.api.Action
 import com.duckduckgo.remote.messaging.api.Content
 import com.duckduckgo.remote.messaging.api.RemoteMessage
-import com.duckduckgo.remote.messaging.api.RemoteMessagingRepository
+import com.duckduckgo.remote.messaging.api.RemoteMessageModel
 import com.duckduckgo.savedsites.api.SavedSitesRepository
 import com.duckduckgo.savedsites.api.models.SavedSite.Bookmark
 import com.duckduckgo.savedsites.api.models.SavedSite.Favorite
 import com.duckduckgo.sync.api.engine.SyncEngine
 import java.util.UUID
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.never
 import org.mockito.Mockito.verify
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
@@ -41,46 +32,33 @@ class NewTabLegacyPageViewModelTest {
     var coroutinesTestRule = CoroutineTestRule()
 
     private var mockLifecycleOwner: LifecycleOwner = mock()
-    private var mockRemoteMessagingRepository: RemoteMessagingRepository = mock()
-    private var mockPlayStoreUtils: PlayStoreUtils = mock()
     private var mockSavedSitesRepository: SavedSitesRepository = mock()
     private var mockSyncEngine: SyncEngine = mock()
     private var mockCommandActionMapper: CommandActionMapper = mock()
-    private var mockPixel: Pixel = mock()
-
-    private lateinit var remoteMessagingModel: RemoteMessagingModel
-    private val remoteMessageFlow = Channel<RemoteMessage>()
+    private var mockPlaystoreUtils: PlayStoreUtils = mock()
+    private var mockRemoteMessageModel: RemoteMessageModel = mock()
 
     private lateinit var testee: NewTabLegacyPageViewModel
 
     @Before
     fun setUp() {
         whenever(mockSavedSitesRepository.getFavorites()).thenReturn(flowOf(emptyList()))
-        whenever(mockRemoteMessagingRepository.messageFlow()).thenReturn(remoteMessageFlow.consumeAsFlow())
-
-        remoteMessagingModel = givenRemoteMessagingModel(mockRemoteMessagingRepository, mockPixel, coroutinesTestRule.testDispatcherProvider)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(null))
 
         testee = NewTabLegacyPageViewModel(
             dispatchers = coroutinesTestRule.testDispatcherProvider,
-            remoteMessagingModel = { remoteMessagingModel },
-            playStoreUtils = mockPlayStoreUtils,
+            remoteMessagingModel = mockRemoteMessageModel,
+            playStoreUtils = mockPlaystoreUtils,
             savedSitesRepository = mockSavedSitesRepository,
             syncEngine = mockSyncEngine,
             commandActionMapper = mockCommandActionMapper,
         )
-
-        whenever(testee.hiddenIds).thenReturn(MutableStateFlow(HiddenBookmarksIds()))
-    }
-
-    @After
-    fun after() {
-        remoteMessageFlow.close()
     }
 
     @Test
     fun whenViewModelIsInitializedThenViewStateShouldEmitInitialState() = runTest {
         val remoteMessage = RemoteMessage("id1", Content.Small("", ""), emptyList(), emptyList())
-        givenRemoteMessage(remoteMessage)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(remoteMessage))
 
         testee.onStart(mockLifecycleOwner)
 
@@ -88,7 +66,7 @@ class NewTabLegacyPageViewModelTest {
             expectMostRecentItem().also {
                 assertEquals(it.message, remoteMessage)
                 assertTrue(it.favourites.isEmpty())
-                assertFalse(it.newMessage)
+                assertTrue(it.newMessage)
             }
         }
     }
@@ -111,69 +89,85 @@ class NewTabLegacyPageViewModelTest {
 
     @Test
     fun whenRemoteMessageShownThenFirePixelAndMarkAsShown() = runTest {
-        testee.onStart(mockLifecycleOwner)
-
         val remoteMessage = RemoteMessage("id1", Content.Small("", ""), emptyList(), emptyList())
-        givenRemoteMessage(remoteMessage)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(remoteMessage))
+
+        testee.onStart(mockLifecycleOwner)
 
         testee.onMessageShown()
 
-        verify(mockRemoteMessagingRepository).markAsShown(remoteMessage)
-        verify(mockPixel).fire(AppPixelName.REMOTE_MESSAGE_SHOWN_UNIQUE, mapOf("message" to "id1"))
-        verify(mockPixel).fire(AppPixelName.REMOTE_MESSAGE_SHOWN, mapOf("message" to "id1"))
+        verify(mockRemoteMessageModel).onMessageShown(remoteMessage)
     }
 
     @Test
     fun whenRemoteMessageCloseButtonClickedThenFirePixelAndDismiss() = runTest {
         val remoteMessage = RemoteMessage("id1", Content.Small("", ""), emptyList(), emptyList())
-        givenRemoteMessage(remoteMessage)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(remoteMessage))
+
+        testee.onStart(mockLifecycleOwner)
 
         testee.onMessageCloseButtonClicked()
 
-        verify(mockRemoteMessagingRepository).dismissMessage("id1")
-        verify(mockPixel).fire(AppPixelName.REMOTE_MESSAGE_DISMISSED, mapOf("message" to "id1"))
+        verify(mockRemoteMessageModel).onMessageDismissed(remoteMessage)
     }
 
     @Test
     fun whenRemoteMessagePrimaryButtonClickedThenFirePixelAndDismiss() = runTest {
         val remoteMessage = RemoteMessage("id1", Content.Small("", ""), emptyList(), emptyList())
-        givenRemoteMessage(remoteMessage)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(remoteMessage))
+
+        val action = Action.Dismiss
+        whenever(mockRemoteMessageModel.onPrimaryActionClicked(remoteMessage)).thenReturn(Action.Dismiss)
+        whenever(mockCommandActionMapper.asNewTabCommand(action)).thenReturn(NewTabLegacyPageViewModel.Command.DismissMessage)
+
+        testee.onStart(mockLifecycleOwner)
 
         testee.onMessagePrimaryButtonClicked()
 
-        verify(mockRemoteMessagingRepository).dismissMessage("id1")
-        verify(mockPixel).fire(AppPixelName.REMOTE_MESSAGE_PRIMARY_ACTION_CLICKED, mapOf("message" to "id1"))
+        testee.commands().test {
+            expectMostRecentItem().also {
+                assertEquals(it, Command.DismissMessage)
+            }
+        }
     }
 
     @Test
     fun whenRemoteMessageSecondaryButtonClickedThenFirePixelAndDismiss() = runTest {
         val remoteMessage = RemoteMessage("id1", Content.Small("", ""), emptyList(), emptyList())
-        givenRemoteMessage(remoteMessage)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(remoteMessage))
+
+        val action = Action.Dismiss
+        whenever(mockRemoteMessageModel.onSecondaryActionClicked(remoteMessage)).thenReturn(Action.Dismiss)
+        whenever(mockCommandActionMapper.asNewTabCommand(action)).thenReturn(NewTabLegacyPageViewModel.Command.DismissMessage)
+
+        testee.onStart(mockLifecycleOwner)
 
         testee.onMessageSecondaryButtonClicked()
 
-        verify(mockRemoteMessagingRepository).dismissMessage("id1")
-        verify(mockPixel).fire(AppPixelName.REMOTE_MESSAGE_SECONDARY_ACTION_CLICKED, mapOf("message" to "id1"))
+        testee.commands().test {
+            expectMostRecentItem().also {
+                assertEquals(it, Command.DismissMessage)
+            }
+        }
     }
 
     @Test
     fun whenRemoteMessageActionButtonClickedThenFirePixelAndDontDismiss() = runTest {
         val remoteMessage = RemoteMessage("id1", Content.Small("", ""), emptyList(), emptyList())
-        givenRemoteMessage(remoteMessage)
+        whenever(mockRemoteMessageModel.getActiveMessages()).thenReturn(flowOf(remoteMessage))
+
+        val action = Action.Dismiss
+        whenever(mockRemoteMessageModel.onActionClicked(remoteMessage)).thenReturn(Action.Dismiss)
+        whenever(mockCommandActionMapper.asNewTabCommand(action)).thenReturn(NewTabLegacyPageViewModel.Command.DismissMessage)
+
+        testee.onStart(mockLifecycleOwner)
 
         testee.onMessageActionButtonClicked()
 
-        verify(mockRemoteMessagingRepository, never()).dismissMessage("id1")
-        verify(mockPixel).fire(AppPixelName.REMOTE_MESSAGE_ACTION_CLICKED, mapOf("message" to "id1"))
-    }
-
-    private fun givenRemoteMessagingModel(
-        remoteMessagingRepository: RemoteMessagingRepository,
-        pixel: Pixel,
-        dispatchers: DispatcherProvider,
-    ) = RemoteMessagingModel(remoteMessagingRepository, pixel, dispatchers)
-
-    private suspend fun givenRemoteMessage(remoteMessage: RemoteMessage) {
-        remoteMessageFlow.send(remoteMessage)
+        testee.commands().test {
+            expectMostRecentItem().also {
+                assertEquals(it, Command.DismissMessage)
+            }
+        }
     }
 }
