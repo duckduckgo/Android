55fa9ed6ddecf7f573504a567f5cd2ddbd8ac4a4
Maxim Tsoy <maks.tsoy@gmail.com>
2024-07-10 12:36:14 +0200

    Bundle autoconsent rules in the JS bundle

--- autoconsent/autoconsent-impl/libs/userscript.js
+++ autoconsent/autoconsent-impl/libs/userscript.js
@@ -1,9 +1,12 @@
 import AutoConsent from '@duckduckgo/autoconsent';
+import * as rules from '@duckduckgo/autoconsent/rules/rules.json';
 
 const autoconsent = new AutoConsent(
     (message) => {
         AutoconsentAndroid.process(JSON.stringify(message));
     },
+    null,
+    rules,
 );
 window.autoconsentMessageCallback = (msg) => {
     autoconsent.receiveMessageCallback(msg);
