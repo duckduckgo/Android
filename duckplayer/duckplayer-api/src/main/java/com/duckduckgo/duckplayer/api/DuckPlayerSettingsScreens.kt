ac2594bac7cfd1f6048d9506b864f8a86534fe16
Cris Barreiro <cbarreiro@duckduckgo.com>
2024-07-10 19:36:30 +0200

    Add Duck Player settings

--- duckplayer/duckplayer-api/src/main/java/com/duckduckgo/duckplayer/api/DuckPlayerSettingsScreens.kt
+++ duckplayer/duckplayer-api/src/main/java/com/duckduckgo/duckplayer/api/DuckPlayerSettingsScreens.kt
@@ -14,28 +14,11 @@
  * limitations under the License.
  */
 
-package com.duckduckgo.settings.api
+package com.duckduckgo.duckplayer.api
 
-import android.content.Context
-import android.view.View
-
-/**
- * Use this interface to create a new plugin that will be used to display a specific settings section
- */
-interface SettingsPlugin {
-    /**
-     * This method returns a [View] that will be used as a setting item
-     * @return [View]
-     */
-    fun getView(context: Context): View
-}
+import com.duckduckgo.navigation.api.GlobalActivityStarter
 
 /**
- * This is the plugin for the subs settings
+ * Use this model to launch the Duck Player Settings screen
  */
-interface ProSettingsPlugin : SettingsPlugin
-
-/**
- * This is the plugin for Duck Player settings
- */
-interface DuckPlayerSettingsPlugin : SettingsPlugin
+object DuckPlayerSettingsNoParams : GlobalActivityStarter.ActivityParams
