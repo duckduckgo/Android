04fd19f734a59b38f604d2ee402c1a8ef61fdd7d
David González <malmstein@gmail.com>
2024-07-17 15:40:56 +0200

    more pixels test

--- saved-sites/saved-sites-impl/src/test/java/com/duckduckgo/savedsites/impl/newtab/FavouritesNewTabSettingsViewModelTest.kt
+++ saved-sites/saved-sites-impl/src/test/java/com/duckduckgo/savedsites/impl/newtab/FavouritesNewTabSettingsViewModelTest.kt
@@ -2,15 +2,18 @@ package com.duckduckgo.savedsites.impl.newtab
 
 import androidx.lifecycle.LifecycleOwner
 import app.cash.turbine.test
+import com.duckduckgo.app.statistics.pixels.Pixel
 import com.duckduckgo.common.test.CoroutineTestRule
 import com.duckduckgo.feature.toggles.api.Toggle
 import com.duckduckgo.feature.toggles.api.Toggle.State
+import com.duckduckgo.savedsites.impl.SavedSitesPixelName
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class FavouritesNewTabSettingsViewModelTest {
@@ -21,12 +24,14 @@ class FavouritesNewTabSettingsViewModelTest {
     private lateinit var testee: FavouritesNewTabSettingsViewModel
     private val setting: NewTabFavouritesSectionSetting = mock()
     private val lifecycleOwner: LifecycleOwner = mock()
+    private val pixels: Pixel = mock()
 
     @Before
     fun setup() {
         testee = FavouritesNewTabSettingsViewModel(
             coroutinesTestRule.testDispatcherProvider,
             setting,
+            pixels,
         )
     }
 
@@ -76,5 +81,45 @@ class FavouritesNewTabSettingsViewModelTest {
                 assertFalse(it.enabled)
             }
         }
+    }
+
+    @Test
+    fun whenSettingEnabledThenPixelFired() = runTest {
+        whenever(setting.self()).thenReturn(
+            object : Toggle {
+                override fun isEnabled(): Boolean {
+                    return false
+                }
+
+                override fun setEnabled(state: State) {
+                }
+
+                override fun getRawStoredState(): State {
+                    return State()
+                }
+            },
+        )
+        testee.onSettingEnabled(true)
+        verify(pixels).fire(SavedSitesPixelName.FAVOURITES_SECTION_TOGGLED_ON)
+    }
+
+    @Test
+    fun whenSettingDisabledThenPixelFired() = runTest {
+        whenever(setting.self()).thenReturn(
+            object : Toggle {
+                override fun isEnabled(): Boolean {
+                    return false
+                }
+
+                override fun setEnabled(state: State) {
+                }
+
+                override fun getRawStoredState(): State {
+                    return State()
+                }
+            },
+        )
+        testee.onSettingEnabled(false)
+        verify(pixels).fire(SavedSitesPixelName.FAVOURITES_SECTION_TOGGLED_OFF)
     }
 }
