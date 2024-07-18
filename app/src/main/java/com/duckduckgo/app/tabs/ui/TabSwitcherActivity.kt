2435aa71a726049a8cf73c83030e94b84cc391d8
Ondrej Ruttkay <ruttkay@gmail.com>
2024-07-18 15:27:30 +0200

    Optimize imports

--- app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherActivity.kt
+++ app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherActivity.kt
@@ -62,7 +62,6 @@ import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 @InjectWith(ActivityScope::class)
