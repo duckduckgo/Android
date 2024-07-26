33f74a21047496fad29b3767b8bf70b49d7d6b31
Cristian Monforte <cristian@duckduckgo.com>
2024-07-26 15:17:38 +0200

    Adds new FF for autofill on by default for existing users. Integrates FF into decider logic.

--- autofill/autofill-impl/src/test/java/com/duckduckgo/feature/toggles/api/toggle/AutofillTestFeature.kt
+++ autofill/autofill-impl/src/test/java/com/duckduckgo/feature/toggles/api/toggle/AutofillTestFeature.kt
@@ -28,6 +28,7 @@ class AutofillTestFeature : AutofillFeature {
     var canAccessCredentialManagement: Boolean = false
     var onByDefault: Boolean = false
     var canIntegrateWithWebView: Boolean = false
+    var onForExistingUsers: Boolean = false
 
     override fun self(): Toggle = TestToggle(topLevelFeatureEnabled)
     override fun canInjectCredentials(): Toggle = TestToggle(canInjectCredentials)
@@ -36,6 +37,7 @@ class AutofillTestFeature : AutofillFeature {
     override fun canGeneratePasswords(): Toggle = TestToggle(canGeneratePassword)
     override fun canAccessCredentialManagement(): Toggle = TestToggle(canAccessCredentialManagement)
     override fun onByDefault(): Toggle = TestToggle(onByDefault)
+    override fun onForExistingUsers(): Toggle = TestToggle(onForExistingUsers)
 }
 
 class AutofillReportBreakageTestFeature : AutofillSiteBreakageReportingFeature {
