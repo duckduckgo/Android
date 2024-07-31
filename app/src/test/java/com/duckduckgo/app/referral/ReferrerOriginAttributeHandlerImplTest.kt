9300a3e4d5f374920b09c73d1e1bc67746ea73a0
Craig Russell <CDRussell@users.noreply.github.com>
2024-07-31 17:26:47 +0100

    Include returning user param in referrer install pixel

--- app/src/test/java/com/duckduckgo/app/referral/ReferrerOriginAttributeHandlerImplTest.kt
+++ app/src/test/java/com/duckduckgo/app/referral/ReferrerOriginAttributeHandlerImplTest.kt
@@ -1,23 +1,16 @@
 package com.duckduckgo.app.referral
 
-import com.duckduckgo.app.pixels.AppPixelName.REFERRAL_INSTALL_UTM_CAMPAIGN
 import com.duckduckgo.app.referral.ReferrerOriginAttributeHandlerImpl.Companion.DEFAULT_ATTRIBUTION_FOR_PLAY_STORE_INSTALLS
 import com.duckduckgo.app.referral.ReferrerOriginAttributeHandlerImpl.Companion.ORIGIN_ATTRIBUTE_KEY
-import com.duckduckgo.app.referral.ReferrerOriginAttributeHandlerImpl.Companion.PIXEL_PARAM_LOCALE
-import com.duckduckgo.app.referral.ReferrerOriginAttributeHandlerImpl.Companion.PIXEL_PARAM_ORIGIN
 import com.duckduckgo.app.statistics.pixels.Pixel
-import com.duckduckgo.app.statistics.pixels.Pixel.PixelType.UNIQUE
 import com.duckduckgo.appbuildconfig.api.AppBuildConfig
 import com.duckduckgo.common.test.CoroutineTestRule
 import com.duckduckgo.verifiedinstallation.installsource.VerificationCheckPlayStoreInstall
 import java.util.*
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.kotlin.any
 import org.mockito.kotlin.argumentCaptor
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -40,11 +33,7 @@ class ReferrerOriginAttributeHandlerImplTest {
     }
 
     private val testee = ReferrerOriginAttributeHandlerImpl(
-        pixel = pixel,
-        appBuildConfig = appBuildConfig,
         appReferrerDataStore = appReferrerDataStore,
-        appCoroutineScope = coroutineTestRule.testScope,
-        dispatchers = coroutineTestRule.testDispatcherProvider,
         playStoreInstallChecker = playStoreInstallChecker,
     )
 
@@ -98,28 +87,7 @@ class ReferrerOriginAttributeHandlerImplTest {
     }
 
     private fun verifyOriginAttributeProcessed(campaign: String?) {
-        verifyPixelFired()
-        verifyPixelParamsAttached(campaign)
         verifyOriginAttributePersisted(campaign)
-    }
-
-    private fun verifyPixelFired() {
-        verify(pixel).fire(
-            eq(REFERRAL_INSTALL_UTM_CAMPAIGN),
-            parameters = captor.capture(),
-            encodedParameters = any(),
-            type = eq(UNIQUE),
-        )
-    }
-
-    private fun verifyPixelParamsAttached(campaign: String?) {
-        val params = captor.firstValue
-
-        val expectedOriginParamAdded = campaign != null
-        assertEquals(expectedOriginParamAdded, params.containsKey(PIXEL_PARAM_ORIGIN))
-
-        assertEquals("en-US", params[PIXEL_PARAM_LOCALE])
-        assertEquals(campaign, params[PIXEL_PARAM_ORIGIN])
     }
 
     private fun verifyOriginAttributePersisted(campaign: String?) {
