f2c35095bf0f2841175958715d4ffc9adbb2899b
Craig Russell <CDRussell@users.noreply.github.com>
2024-08-05 16:08:02 +0100

    Add ATB initialized callback to ATB plugin, and rename for clarity (#4850)
    Task/Issue URL: https://app.asana.com/0/0/1207965428441026/f 
    
    ### Description
    - Renames `RefreshRetentionAtbPlugin` ➡️ `AtbLifecyclePlugin`
    - Adds atb initialized callback
    - Makes default implementation of the callbacks no-ops
    
    No other changes in this PR.
    
    ### Steps to test this PR
    - [ ] QA-optional

--- subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/pixels/SubscriptionRefreshRetentionAtbPlugin.kt
+++ subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/pixels/SubscriptionRefreshRetentionAtbPlugin.kt
@@ -17,7 +17,7 @@
 package com.duckduckgo.subscriptions.impl.pixels
 
 import com.duckduckgo.app.di.AppCoroutineScope
-import com.duckduckgo.app.statistics.api.RefreshRetentionAtbPlugin
+import com.duckduckgo.app.statistics.api.AtbLifecyclePlugin
 import com.duckduckgo.di.scopes.AppScope
 import com.duckduckgo.subscriptions.impl.SubscriptionsManager
 import com.duckduckgo.subscriptions.impl.repository.isActive
@@ -31,7 +31,7 @@ class SubscriptionRefreshRetentionAtbPlugin @Inject constructor(
     @AppCoroutineScope private val coroutineScope: CoroutineScope,
     private val subscriptionsManager: SubscriptionsManager,
     private val pixelSender: SubscriptionPixelSender,
-) : RefreshRetentionAtbPlugin {
+) : AtbLifecyclePlugin {
 
     override fun onSearchRetentionAtbRefreshed() = Unit // no-op
 
