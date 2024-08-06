e0c4a876cc37707a4029ba06deb010ac7a2459ee
Aitor Viana <aitorvs@gmail.com>
2024-08-06 11:26:43 +0100

    Support experiments in Active plugins (#4860)
    Task/Issue URL: https://app.asana.com/0/488551667048375/1207977093713099/f
    
    ### Description
    Add support for experiments in active plugins.
    Add support to force active plugins enabled for internal builds.
    
    ### Steps to test this PR
    
    Ensure that DuDuckGo folder has been removed from Downloads folder to avoid returning user.
    
    _Control Group - mq - Play Flavour_
    - [ ] Change Privacy Config URL to `https://jsonblob.com/api/jsonBlob/1243573265112096768`
    - [ ] Fresh install
    - [ ] Filter log to validate that `mq` variant has been assigned
    - [ ] Complete Onboarding and open New Tab
    - [ ] Verify legacy NTP is visible
    - [ ] Try a few times, ensure only legacy version is available
    
    _Variant Group - mr - Play Flavour_
    - [ ] Change Privacy Config URL to `https://jsonblob.com/api/jsonBlob/1270091613312245760`
    - [ ] Fresh install
    - [ ] Filter log to validate that `mr` variant has been assigned
    - [ ] Complete Onboarding and open New Tab
    - [ ] Verify new NTP is visible
    - [ ] Try a few times, ensure only new version is available
    
    _Control Group - mq - Internal Flavour_
    - [x] Change Privacy Config URL to `https://jsonblob.com/api/jsonBlob/1243573265112096768 `
    - [x] Fresh install
    - [x] Filter log to validate that `mr` variant has been assigned
    - [x] Complete Onboarding and open New Tab
    - [x] Verify new NTP is visible
    - [x] Try a few times, ensure only new version is available

--- anvil/anvil-compiler/src/main/java/com/duckduckgo/anvil/compiler/ContributesActivePluginPointCodeGenerator.kt
+++ anvil/anvil-compiler/src/main/java/com/duckduckgo/anvil/compiler/ContributesActivePluginPointCodeGenerator.kt
@@ -23,6 +23,8 @@ import com.duckduckgo.anvil.annotations.ContributesRemoteFeature
 import com.duckduckgo.anvil.annotations.PriorityKey
 import com.duckduckgo.feature.toggles.api.RemoteFeatureStoreNamed
 import com.duckduckgo.feature.toggles.api.Toggle
+import com.duckduckgo.feature.toggles.api.Toggle.Experiment
+import com.duckduckgo.feature.toggles.api.Toggle.InternalAlwaysEnabled
 import com.google.auto.service.AutoService
 import com.squareup.anvil.annotations.ContributesBinding
 import com.squareup.anvil.annotations.ContributesMultibinding
@@ -314,6 +316,12 @@ class ContributesActivePluginPointCodeGenerator : CodeGenerator {
         val pluginRemoteFeatureClassName = "${vmClass.shortName}_ActivePlugin_RemoteFeature"
         val pluginRemoteFeatureStoreClassName = "${vmClass.shortName}_ActivePlugin_RemoteFeature_MultiProcessStore"
         val pluginPriority = vmClass.annotations.firstOrNull { it.fqName == ContributesActivePlugin::class.fqName }?.priorityOrNull()
+        val pluginSupportExperiments = vmClass.annotations.firstOrNull {
+            it.fqName == ContributesActivePlugin::class.fqName
+        }?.isExperimentOrNull() ?: false
+        val pluginInternalAlwaysEnabled = vmClass.annotations.firstOrNull {
+            it.fqName == ContributesActivePlugin::class.fqName
+        }?.internalAlwaysEnabledOrNull() ?: false
 
         // Check if there's another plugin class, in the same plugin point, that has the same class simplename
         // we can't allow that because the backing remote feature would be the same
@@ -403,14 +411,24 @@ class ContributesActivePluginPointCodeGenerator : CodeGenerator {
                             .build(),
                     )
                     addFunction(
-                        FunSpec.builder(featureName)
-                            .addModifiers(ABSTRACT)
-                            .addAnnotation(
+                        FunSpec.builder(featureName).apply {
+                            addModifiers(ABSTRACT)
+                            addAnnotation(
                                 AnnotationSpec.builder(Toggle.DefaultValue::class)
                                     .addMember("defaultValue = %L", featureDefaultValue)
                                     .build(),
                             )
-                            .returns(Toggle::class)
+                            // If the active plugin defines [supportExperiments = true] we mark it as Experiment
+                            if (pluginSupportExperiments) {
+                                addAnnotation(AnnotationSpec.builder(Experiment::class).build())
+                            }
+                            // If the active plugin defines [internalAlwaysEnabled = true] we mark it as InternalAlwaysEnabled
+                            if (pluginInternalAlwaysEnabled) {
+                                addAnnotation(AnnotationSpec.builder(InternalAlwaysEnabled::class).build())
+                            }
+
+                            returns(Toggle::class)
+                        }
                             .build(),
                     )
                 }.build(),
@@ -578,6 +596,12 @@ class ContributesActivePluginPointCodeGenerator : CodeGenerator {
 
     @OptIn(ExperimentalAnvilApi::class)
     private fun AnnotationReference.priorityOrNull(): Int? = argumentAt("priority", 3)?.value()
+
+    @OptIn(ExperimentalAnvilApi::class)
+    private fun AnnotationReference.isExperimentOrNull(): Boolean? = argumentAt("supportExperiments", 4)?.value()
+
+    @OptIn(ExperimentalAnvilApi::class)
+    private fun AnnotationReference.internalAlwaysEnabledOrNull(): Boolean? = argumentAt("internalAlwaysEnabled", 5)?.value()
 
     private fun ClassReference.Psi.pluginClassName(
         fqName: FqName,
