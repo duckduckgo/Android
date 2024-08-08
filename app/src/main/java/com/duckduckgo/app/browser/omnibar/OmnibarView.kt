ce2ff5249f8946a1d039084e62b1d02067f0e246
David González <malmstein@gmail.com>
2024-08-08 11:53:25 +0200

    adding omnibar view and skeleton viewmodel

--- app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarView.kt
+++ app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarView.kt
@@ -18,18 +18,84 @@ package com.duckduckgo.app.browser.omnibar
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.duckduckgo.anvil.annotations.InjectWith
-import com.duckduckgo.app.browser.databinding.IncludeOmnibarToolbarBinding
+import com.duckduckgo.app.browser.databinding.ViewOmnibarBinding
+import com.duckduckgo.app.browser.omnibar.Omnibar.Action
+import com.duckduckgo.app.browser.omnibar.Omnibar.Content
 import com.duckduckgo.common.ui.viewbinding.viewBinding
+import com.duckduckgo.common.utils.ViewViewModelFactory
 import com.duckduckgo.di.scopes.ViewScope
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.SCROLL_FLAG_SNAP
+import javax.inject.Inject
+
+interface Omnibar {
+
+    // setActionListener?
+    fun onAction(actionHandler: (Action) -> Unit)
+
+    // setContentListener?
+    fun onContent(contentHandler: (Content) -> Unit)
+    fun enableScrolling()
+    fun disableScrolling()
+
+    sealed class Action {
+        data class onUrlRequested(val url: String) : Action()
+        data class onMenuItemPressed(val menu: MenuItem) : Action()
+    }
+
+    sealed class MenuItem {
+        object Refresh : MenuItem()
+    }
+
+    sealed class Content {
+        data class Suggestions(val list: List<String>) : Content()
+        object FocusedView : Content()
+    }
+}
 
 @InjectWith(ViewScope::class)
 class OmnibarView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
-) : FrameLayout(context, attrs, defStyle) {
+) : FrameLayout(context, attrs, defStyle), Omnibar {
 
-    private val binding: IncludeOmnibarToolbarBinding by viewBinding()
+    @Inject
+    lateinit var viewModelFactory: ViewViewModelFactory
+
+    private val binding: ViewOmnibarBinding by viewBinding()
+
+    private val viewModel: OmnibarViewModel by lazy {
+        ViewModelProvider(findViewTreeViewModelStoreOwner()!!, viewModelFactory)[OmnibarViewModel::class.java]
+    }
+
+    override fun onAction(actionHandler: (Action) -> Unit) {
+    }
+
+    override fun onContent(contentHandler: (Content) -> Unit) {
+    }
+
+    override fun enableScrolling() {
+        updateScrollFlag(SCROLL_FLAG_SCROLL or SCROLL_FLAG_SNAP or SCROLL_FLAG_ENTER_ALWAYS, binding.toolbarContainer)
+    }
+
+    override fun disableScrolling() {
+        updateScrollFlag(0, binding.toolbarContainer)
+    }
+
+    private fun updateScrollFlag(
+        flags: Int,
+        toolbarContainer: View,
+    ) {
+        val params = toolbarContainer.layoutParams as AppBarLayout.LayoutParams
+        params.scrollFlags = flags
+        toolbarContainer.layoutParams = params
+    }
 }
