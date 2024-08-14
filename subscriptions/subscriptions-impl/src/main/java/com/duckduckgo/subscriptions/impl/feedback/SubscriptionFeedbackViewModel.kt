bemusementpark/DuckDuckGoAndroid

Add tests for unified feedback flow

subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/feedback/SubscriptionFeedbackViewModel.kt
@@ -74,7 +74,9 @@ class SubscriptionFeedbackViewModel @Inject constructor(
 
             viewState.emit(
                 ViewState(
-                    feedbackMetadata = viewState.value.feedbackMetadata,
+                    feedbackMetadata = viewState.value.feedbackMetadata.copy(
+                        source = DDG_SETTINGS,
+                    ),
                     currentFragmentState = newFragmentState,
                     previousFragmentState = previousFragmentState,
                     isForward = true,
@@ -201,7 +203,9 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                 PARAMS_KEY_DESC to (metadata.description ?: ""),
                 PARAMS_KEY_APP_NAME to (metadata.appName ?: ""),
                 PARAMS_KEY_APP_PACKAGE to (metadata.appPackageName ?: ""),
-                PARAMS_KEY_CUSTOM_METADATA to feedbackCustomMetadataProvider.getCustomMetadata(metadata.category),
+                PARAMS_KEY_CUSTOM_METADATA to feedbackCustomMetadataProvider.getCustomMetadata(
+                    metadata.category,
+                ),
             ),
         )
     }
@@ -315,7 +319,7 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                             reportType = null,
                         ),
                         currentFragmentState = newState,
-                        previousFragmentState = null,
+                        previousFragmentState = if (currentFeedbackMetadata.source == DDG_SETTINGS) FeedbackGeneral else null,
                         isForward = false,
                     )
 
@@ -329,17 +333,17 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                     )
 
                     is FeedbackSubCategory -> {
+                        val supportsCategory = when (currentFeedbackMetadata.source) {
+                            SUBSCRIPTION_SETTINGS, VPN_MANAGEMENT, VPN_EXCLUDED_APPS -> true
+                            else -> false
+                        }
                         val previousState =
-                            if (currentFeedbackMetadata.reportType == REPORT_PROBLEM &&
-                                (
-                                    currentFeedbackMetadata.source == SUBSCRIPTION_SETTINGS ||
-                                        currentFeedbackMetadata.source == VPN_MANAGEMENT ||
-                                        currentFeedbackMetadata.source == VPN_EXCLUDED_APPS
-                                    )
-                            ) {
+                            if (currentFeedbackMetadata.reportType == REPORT_PROBLEM && supportsCategory) {
                                 FeedbackAction
                             } else {
-                                FeedbackCategory(currentFeedbackMetadata.reportType?.asTitle() ?: -1)
+                                FeedbackCategory(
+                                    currentFeedbackMetadata.reportType?.asTitle() ?: -1,
+                                )
                             }
                         ViewState(
                             feedbackMetadata = currentFeedbackMetadata.copy(
@@ -459,7 +463,6 @@ class SubscriptionFeedbackViewModel @Inject constructor(
 
     sealed class Command {
         data object FeedbackCompleted : Command()
-        data object HideKeyboard : Command()
     }
 
     internal data class ViewState(

duckduckgo/Android

subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/feedback/SubscriptionFeedbackViewModel.kt
@@ -76,7 +76,9 @@ class SubscriptionFeedbackViewModel @Inject constructor(
 
             viewState.emit(
                 ViewState(
-                    feedbackMetadata = viewState.value.feedbackMetadata,
+                    feedbackMetadata = viewState.value.feedbackMetadata.copy(
+                        source = DDG_SETTINGS,
+                    ),
                     currentFragmentState = newFragmentState,
                     previousFragmentState = previousFragmentState,
                     isForward = true,
@@ -114,8 +116,7 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                     }
                 }
 
-                GENERAL_FEEDBACK -> FeedbackSubmit(reportType.asTitle())
-                REQUEST_FEATURE -> FeedbackSubmit(reportType.asTitle())
+                GENERAL_FEEDBACK, REQUEST_FEATURE -> FeedbackSubmit(reportType.asTitle())
             }
 
             viewState.emit(
@@ -203,7 +204,9 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                 PARAMS_KEY_DESC to (metadata.description ?: ""),
                 PARAMS_KEY_APP_NAME to (metadata.appName ?: ""),
                 PARAMS_KEY_APP_PACKAGE to (metadata.appPackageName ?: ""),
-                PARAMS_KEY_CUSTOM_METADATA to feedbackCustomMetadataProvider.getCustomMetadata(metadata.category),
+                PARAMS_KEY_CUSTOM_METADATA to feedbackCustomMetadataProvider.getCustomMetadata(
+                    metadata.category,
+                ),
             ),
         )
     }
@@ -320,7 +323,7 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                             reportType = null,
                         ),
                         currentFragmentState = newState,
-                        previousFragmentState = null,
+                        previousFragmentState = if (currentFeedbackMetadata.source == DDG_SETTINGS) FeedbackGeneral else null,
                         isForward = false,
                     )
 
@@ -334,17 +337,17 @@ class SubscriptionFeedbackViewModel @Inject constructor(
                     )
 
                     is FeedbackSubCategory -> {
+                        val autoAssignedCategory = when (currentFeedbackMetadata.source) {
+                            SUBSCRIPTION_SETTINGS, VPN_MANAGEMENT, VPN_EXCLUDED_APPS -> true
+                            else -> false
+                        }
                         val previousState =
-                            if (currentFeedbackMetadata.reportType == REPORT_PROBLEM &&
-                                (
-                                    currentFeedbackMetadata.source == SUBSCRIPTION_SETTINGS ||
-                                        currentFeedbackMetadata.source == VPN_MANAGEMENT ||
-                                        currentFeedbackMetadata.source == VPN_EXCLUDED_APPS
-                                    )
-                            ) {
+                            if (currentFeedbackMetadata.reportType == REPORT_PROBLEM && autoAssignedCategory) {
                                 FeedbackAction
                             } else {
-                                FeedbackCategory(currentFeedbackMetadata.reportType?.asTitle() ?: -1)
+                                FeedbackCategory(
+                                    currentFeedbackMetadata.reportType?.asTitle() ?: -1,
+                                )
                             }
                         ViewState(
                             feedbackMetadata = currentFeedbackMetadata.copy(
@@ -464,8 +467,6 @@ class SubscriptionFeedbackViewModel @Inject constructor(
 
     sealed class Command {
         data object FeedbackCompleted : Command()
-        data object HideKeyboard : Command()
-
         data class ShowHelpPages(val url: String) : Command()
     }
 