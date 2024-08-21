628d998901be42c357e58442b0e446b24c6189b3
Ana Capatina <anikiki@users.noreply.github.com>
2024-08-21 11:59:14 +0100

    Add the DB and store exemptions  (#4899)
    Task/Issue URL:
    https://app.asana.com/0/1200581511062568/1207935486792480/f
    
    ### Description
    Added `adclick_exemptions` db and stored tab exemptions.
    
    ### Steps to test this PR
    See the **Tests** section in the description of
    https://app.asana.com/0/1200581511062568/1207935486792480/f.
    
    ### NO UI changes

--- ad-click/ad-click-impl/src/main/java/com/duckduckgo/adclick/impl/di/AdClickModule.kt
+++ ad-click/ad-click-impl/src/main/java/com/duckduckgo/adclick/impl/di/AdClickModule.kt
@@ -18,10 +18,14 @@ package com.duckduckgo.adclick.impl.di
 
 import android.content.Context
 import androidx.room.Room
+import com.duckduckgo.adclick.impl.AdClickData
+import com.duckduckgo.adclick.impl.DuckDuckGoAdClickData
+import com.duckduckgo.adclick.impl.remoteconfig.AdClickAttributionFeature
 import com.duckduckgo.adclick.impl.remoteconfig.AdClickAttributionRepository
 import com.duckduckgo.adclick.impl.remoteconfig.RealAdClickAttributionRepository
 import com.duckduckgo.adclick.impl.store.AdClickDatabase
 import com.duckduckgo.adclick.impl.store.AdClickDatabase.Companion.ALL_MIGRATIONS
+import com.duckduckgo.adclick.impl.store.exemptions.AdClickExemptionsDatabase
 import com.duckduckgo.app.di.AppCoroutineScope
 import com.duckduckgo.app.di.IsMainProcess
 import com.duckduckgo.common.utils.DispatcherProvider
@@ -45,8 +49,8 @@ class AdClickModule {
             .build()
     }
 
-    @SingleInstanceIn(AppScope::class)
     @Provides
+    @SingleInstanceIn(AppScope::class)
     fun provideAdClickAttributionRepository(
         database: AdClickDatabase,
         @AppCoroutineScope appCoroutineScope: CoroutineScope,
@@ -54,5 +58,25 @@ class AdClickModule {
         @IsMainProcess isMainProcess: Boolean,
     ): AdClickAttributionRepository {
         return RealAdClickAttributionRepository(database, appCoroutineScope, dispatcherProvider, isMainProcess)
+    }
+
+    @Provides
+    @SingleInstanceIn(AppScope::class)
+    fun provideAdClickExemptionsDatabase(context: Context): AdClickExemptionsDatabase {
+        return Room.databaseBuilder(context, AdClickExemptionsDatabase::class.java, "adclick_exemptions.db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @SingleInstanceIn(AppScope::class)
+    fun provideAdClickData(
+        database: AdClickExemptionsDatabase,
+        @AppCoroutineScope appCoroutineScope: CoroutineScope,
+        dispatcherProvider: DispatcherProvider,
+        adClickAttributionFeature: AdClickAttributionFeature,
+        @IsMainProcess isMainProcess: Boolean,
+    ): AdClickData {
+        return DuckDuckGoAdClickData(database, appCoroutineScope, dispatcherProvider, adClickAttributionFeature, isMainProcess)
     }
 }
