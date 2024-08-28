6b1e9713d590ee35efa2305959bbdc30f56894b7
Mike Scamell <mscamell@duckduckgo.com>
2024-08-28 15:54:52 +0100

    create ShowOnAppLaunchViewModel

---
+++ app/src/main/java/com/duckduckgo/app/generalsettings/showonapplaunch/ShowOnAppLaunchViewModel.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2024 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.generalsettings.showonapplaunch
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.duckduckgo.anvil.annotations.ContributesViewModel
+import com.duckduckgo.app.generalsettings.showonapplaunch.ShowOnAppLaunchViewModel.ShowOnAppLaunchOption.LastOpenedTab
+import com.duckduckgo.common.utils.DispatcherProvider
+import com.duckduckgo.di.scopes.ActivityScope
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
+
+@ContributesViewModel(ActivityScope::class)
+class ShowOnAppLaunchViewModel @Inject constructor(
+    dispatcherProvider: DispatcherProvider,
+) : ViewModel() {
+
+    sealed class ShowOnAppLaunchOption {
+
+        data object LastOpenedTab : ShowOnAppLaunchOption()
+        data object NewTabPage : ShowOnAppLaunchOption()
+        data class SpecificPage(val url: String) : ShowOnAppLaunchOption()
+    }
+
+    data class ViewState(
+        val selectedOption: ShowOnAppLaunchOption,
+    )
+
+    private val _viewState = MutableStateFlow<ViewState?>(null)
+    val viewState = _viewState.asStateFlow().filterNotNull()
+
+    init {
+        viewModelScope.launch(dispatcherProvider.io()) {
+            // TODO get selected option from prefs
+
+            _viewState.value = ViewState(
+                selectedOption = LastOpenedTab,
+            )
+        }
+    }
+}
