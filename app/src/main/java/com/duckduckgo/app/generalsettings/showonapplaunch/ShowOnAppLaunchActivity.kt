07d76de85a3d4e684e0b248e11f60f97cf0befbb
Mike Scamell <mscamell@duckduckgo.com>
2024-08-28 15:54:05 +0100

    setup ui elements
    I added in the onOptionsItemSelected for the up button as I noticed that while we fade into the Activity, we do not fade when pressing back buy slide. This stops this does the fade animation.

--- app/src/main/java/com/duckduckgo/app/generalsettings/showonapplaunch/ShowOnAppLaunchActivity.kt
+++ app/src/main/java/com/duckduckgo/app/generalsettings/showonapplaunch/ShowOnAppLaunchActivity.kt
@@ -16,11 +16,52 @@
 
 package com.duckduckgo.app.generalsettings.showonapplaunch
 
+import android.os.Bundle
+import android.view.MenuItem
 import com.duckduckgo.anvil.annotations.ContributeToActivityStarter
 import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.app.browser.databinding.ActivityShowOnAppLaunchSettingBinding
 import com.duckduckgo.common.ui.DuckDuckGoActivity
+import com.duckduckgo.common.ui.viewbinding.viewBinding
 import com.duckduckgo.di.scopes.ActivityScope
 
 @InjectWith(ActivityScope::class)
 @ContributeToActivityStarter(ShowOnAppLaunchScreenNoParams::class)
-class ShowOnAppLaunchActivity : DuckDuckGoActivity()
+class ShowOnAppLaunchActivity : DuckDuckGoActivity() {
+
+    private val binding: ActivityShowOnAppLaunchSettingBinding by viewBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        setupToolbar(binding.includeToolbar.toolbar)
+
+        configureUiEventHandlers()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun configureUiEventHandlers() {
+        binding.lastOpenedTabCheckListItem.setOnClickListener {
+            // TODO: Implement this
+        }
+
+        binding.newTabCheckListItem.setOnClickListener {
+            // TODO: Implement this
+        }
+
+        binding.specificPageCheckListItem.setOnClickListener {
+            // TODO: Implement this
+        }
+    }
+}
