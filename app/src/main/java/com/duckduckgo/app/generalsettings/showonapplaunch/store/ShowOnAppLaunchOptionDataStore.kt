0bfb6b968773876e4a908e367fcc6090faeeb493
Mike Scamell <mscamell@duckduckgo.com>
2024-08-29 10:45:47 +0100

    create ShowOnAppLaunchOptionDataStore
    setShowOnAppLaunchOption() takes a ShowOnAppLaunchOption and stores it's id, and in the case of SpecificPage it will also store the url set by the user. If there is no url  we default to "duckduckgo.com" I'm waiting for confirmation this is what we want but it seems a sane default
    
    optionFlow() maps an int to the corresponding ShowOnAppLaunchOption and in the case of SpecificPage also fetches the URL. We always expect a url as we have a default, which is why we assert not null

---
+++ app/src/main/java/com/duckduckgo/app/generalsettings/showonapplaunch/store/ShowOnAppLaunchOptionDataStore.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2024 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.generalsettings.showonapplaunch.store
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.duckduckgo.app.generalsettings.showonapplaunch.model.ShowOnAppLaunchOption
+import com.duckduckgo.app.generalsettings.showonapplaunch.model.ShowOnAppLaunchOption.LastOpenedTab
+import com.duckduckgo.app.generalsettings.showonapplaunch.model.ShowOnAppLaunchOption.NewTabPage
+import com.duckduckgo.app.generalsettings.showonapplaunch.model.ShowOnAppLaunchOption.SpecificPage
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesBinding
+import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+interface ShowOnAppLaunchOptionDataStore {
+    val optionFlow: Flow<ShowOnAppLaunchOption>
+
+    suspend fun setShowOnAppLaunchOption(showOnAppLaunchOption: ShowOnAppLaunchOption)
+}
+
+@ContributesBinding(AppScope::class)
+class ShowOnAppLaunchOptionPrefsDataStore @Inject constructor(
+    @ShowOnAppLaunch private val store: DataStore<Preferences>,
+) : ShowOnAppLaunchOptionDataStore {
+
+    override val optionFlow: Flow<ShowOnAppLaunchOption> = store.data.map { preferences ->
+        preferences[intPreferencesKey(KEY_SHOW_ON_APP_LAUNCH_OPTION)]?.let { optionId ->
+            when (val option = ShowOnAppLaunchOption.mapToOption(optionId)) {
+                LastOpenedTab,
+                NewTabPage,
+                -> option
+                is SpecificPage -> {
+                    val url = preferences[stringPreferencesKey(KEY_SHOW_ON_APP_LAUNCH_SPECIFIC_PAGE_URL)]!!
+                    SpecificPage(url)
+                }
+            }
+        } ?: LastOpenedTab
+    }
+
+    override suspend fun setShowOnAppLaunchOption(showOnAppLaunchOption: ShowOnAppLaunchOption) {
+        store.edit { preferences ->
+            preferences[intPreferencesKey(KEY_SHOW_ON_APP_LAUNCH_OPTION)] = showOnAppLaunchOption.id
+
+            if (showOnAppLaunchOption is SpecificPage) {
+                preferences[stringPreferencesKey(KEY_SHOW_ON_APP_LAUNCH_SPECIFIC_PAGE_URL)] = showOnAppLaunchOption.url.ifBlank {
+                    DEFAULT_SPECIFIC_PAGE_URL
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_SPECIFIC_PAGE_URL = "duckduckgo.com"
+        private const val KEY_SHOW_ON_APP_LAUNCH_OPTION = "SHOW_ON_APP_LAUNCH_OPTION"
+        private const val KEY_SHOW_ON_APP_LAUNCH_SPECIFIC_PAGE_URL = "SHOW_ON_APP_LAUNCH_SPECIFIC_PAGE_URL"
+    }
+}
