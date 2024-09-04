be94e1130dd8c1690b324b61cd3586aa6d473ddb
Aitor Viana <aitorvs@gmail.com>
2024-09-04 13:43:36 +0100

    fix test

--- statistics/statistics-impl/src/main/java/com/duckduckgo/app/statistics/pixels/StatisticsPixelName.kt
+++ statistics/statistics-impl/src/main/java/com/duckduckgo/app/statistics/pixels/StatisticsPixelName.kt
@@ -16,9 +16,12 @@
 
 package com.duckduckgo.app.statistics.pixels
 
+import androidx.annotation.VisibleForTesting
+import androidx.annotation.VisibleForTesting.Companion.PACKAGE_PRIVATE
 import com.duckduckgo.app.statistics.pixels.Pixel.PixelName
 
-internal enum class StatisticsPixelName(override val pixelName: String) : PixelName {
+@VisibleForTesting(otherwise = PACKAGE_PRIVATE)
+enum class StatisticsPixelName(override val pixelName: String) : PixelName {
     BROWSER_DAILY_ACTIVE_FEATURE_STATE("m_browser_feature_daily_active_user_d"),
     RETENTION_SEGMENTS("m_retention_segments"),
 }
