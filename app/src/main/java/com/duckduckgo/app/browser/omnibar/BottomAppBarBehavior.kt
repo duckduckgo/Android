90c638c9a2b57cf4da09bd457d15f44de04200d0
Ondrej Ruttkay <0nko@users.noreply.github.com>
2024-09-09 17:50:21 +0200

    Omnibar position: Snackbar (#4954)
    Task/Issue URL:
    https://app.asana.com/0/1207418217763355/1208166986247270/f
    
    ### Description
    
    ### Steps to test this PR
    
    - [ ] Change the omnibar position to the bottom
    - [ ] Trigger a condition to show a snackbar (see the diff below)
    - [ ] Notice the snackbar is displayed above the omnibar
    
    This patch will triggers a snackbar after tapping the fire button:
    ```kotlin
    Index: app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
    IDEA additional info:
    Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP
    <+>UTF-8
    ===================================================================
    diff --git a/app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt b/app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
    --- a/app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt	(revision Staged)
    +++ b/app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt	(date 1725012737445)
    @@ -3299,6 +3299,7 @@
         }
     
         fun onFireMenuSelected() {
    +        showErrorWithAction()
             val cta = currentCtaViewState().cta
             if (cta is OnboardingDaxDialogCta.DaxFireButtonCta) {
                 onUserDismissedCta(cta)
    ``` 
    
    ### UI changes
    | Before  | After |
    | ------ | ----- |
    
    ![image](https://github.com/user-attachments/assets/2f7c3158-48b9-4ffa-bb73-bd86fcba47f7)|![image](https://github.com/user-attachments/assets/76400958-f7d6-4ca7-b91b-9a9308807e74)|

--- app/src/main/java/com/duckduckgo/app/browser/omnibar/BottomAppBarBehavior.kt
+++ app/src/main/java/com/duckduckgo/app/browser/omnibar/BottomAppBarBehavior.kt
@@ -16,32 +16,113 @@
 
 package com.duckduckgo.app.browser.omnibar
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.google.android.material.behavior.HideBottomViewOnScrollBehavior
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewCompat.NestedScrollType
+import com.duckduckgo.app.browser.R
+import com.google.android.material.snackbar.Snackbar
+import kotlin.math.max
+import kotlin.math.min
 
 /*
  * This custom behavior for the bottom omnibar is necessary because the default `HideBottomViewOnScrollBehavior` does not work.
  * The reason is that the `DuckDuckGoWebView` is passing only unconsumed movement, which `HideBottomViewOnScrollBehavior` ignores.
  */
-class BottomAppBarBehavior<V : View>(context: Context, attrs: AttributeSet?) : HideBottomViewOnScrollBehavior<V>(context, attrs) {
-    override fun onNestedScroll(
+class BottomAppBarBehavior<V : View>(context: Context, attrs: AttributeSet) : CoordinatorLayout.Behavior<V>(context, attrs) {
+    @NestedScrollType
+    private var lastStartedType: Int = 0
+    private var offsetAnimator: ValueAnimator? = null
+
+    @SuppressLint("RestrictedApi")
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: V, dependency: View): Boolean {
+        if (dependency is Snackbar.SnackbarLayout) {
+            updateSnackbar(child, dependency)
+        }
+        return super.layoutDependsOn(parent, child, dependency)
+    }
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: V,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int,
+    ): Boolean {
+        if (axes == ViewCompat.SCROLL_AXIS_VERTICAL) {
+            lastStartedType = type
+            offsetAnimator?.cancel()
+            return true
+        } else {
+            return false
+        }
+    }
+
+    override fun onNestedPreScroll(
         coordinatorLayout: CoordinatorLayout,
         child: V,
         target: View,
-        dxConsumed: Int,
-        dyConsumed: Int,
-        dxUnconsumed: Int,
-        dyUnconsumed: Int,
-        type: Int,
+        dx: Int,
+        dy: Int,
         consumed: IntArray,
+        type: Int,
     ) {
-        if (dyConsumed > 0 || dyUnconsumed > 0) {
-            slideDown(child)
-        } else if (dyConsumed < 0 || dyUnconsumed < 0) {
-            slideUp(child)
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+
+        if (target.id != R.id.autoCompleteSuggestionsList && target.id != com.duckduckgo.newtabpage.impl.R.id.newTabContentScroll) {
+            child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+        }
+    }
+
+    override fun onStopNestedScroll(coordinatorLayout: CoordinatorLayout, child: V, target: View, type: Int) {
+        if (lastStartedType == ViewCompat.TYPE_TOUCH || type == ViewCompat.TYPE_NON_TOUCH) {
+            val dY = child.translationY
+            val threshold = child.height * 0.5f
+            if (dY >= threshold) {
+                // slide down
+                animateToolbarVisibility(child, isVisible = false)
+            } else {
+                // slide up
+                animateToolbarVisibility(child, isVisible = true)
+            }
+        }
+    }
+
+    private fun animateToolbarVisibility(child: View, isVisible: Boolean) {
+        if (offsetAnimator == null) {
+            offsetAnimator = ValueAnimator().apply {
+                interpolator = DecelerateInterpolator()
+                duration = 150L
+            }
+
+            offsetAnimator?.addUpdateListener {
+                child.translationY = it.animatedValue as Float
+            }
+        } else {
+            offsetAnimator?.cancel()
+        }
+
+        val targetTranslation = if (isVisible) 0f else child.height.toFloat()
+        offsetAnimator?.setFloatValues(child.translationY, targetTranslation)
+        offsetAnimator?.start()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun updateSnackbar(child: View, snackbarLayout: Snackbar.SnackbarLayout) {
+        if (snackbarLayout.layoutParams is CoordinatorLayout.LayoutParams) {
+            val params = snackbarLayout.layoutParams as CoordinatorLayout.LayoutParams
+
+            params.anchorId = child.id
+            params.anchorGravity = Gravity.TOP
+            params.gravity = Gravity.TOP
+            snackbarLayout.layoutParams = params
         }
     }
 }
