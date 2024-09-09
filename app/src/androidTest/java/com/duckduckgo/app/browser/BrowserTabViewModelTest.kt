839d7ef7b3e19be3b16db9ab238663fdb1848d0f
Ondrej Ruttkay <0nko@users.noreply.github.com>
2024-09-09 17:31:34 +0200

    Omnibar position: Onboarding messages (#4942)
    Task/Issue URL: https://app.asana.com/0/0/1208157864474375/f
    
    ### Description
    
    ### Steps to test this PR
    
    - [ ] Clear the app storage and trigger onboarding
    - [ ] Change the omnibar position to the bottom
    - [ ] Go to “google.com"
    - [ ] Notice that during onboarding steps, whenever there is the “hand”
    icon, it points to the bottom
    
    ### UI changes
    | Before  | After |
    | ------ | ----- |
    
    ![image](https://github.com/user-attachments/assets/f4bad558-02d6-48cb-bea9-ab028d88b8b5)|![image](https://github.com/user-attachments/assets/49c4e50d-037a-45cb-922d-ac36b655edb2)|
    
    ---------
    
    Co-authored-by: David González <malmstein@gmail.com>

--- app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
@@ -5459,7 +5459,7 @@ class BrowserTabViewModelTest {
 
     @Test
     fun whenTrackersBlockedCtaShownThenPrivacyShieldIsHighlighted() = runTest {
-        val cta = OnboardingDaxDialogCta.DaxTrackersBlockedCta(mockOnboardingStore, mockAppInstallStore, emptyList())
+        val cta = OnboardingDaxDialogCta.DaxTrackersBlockedCta(mockOnboardingStore, mockAppInstallStore, emptyList(), mockSettingsDataStore)
         testee.ctaViewState.value = ctaViewState().copy(cta = cta)
 
         testee.onOnboardingDaxTypingAnimationFinished()
@@ -5469,7 +5469,7 @@ class BrowserTabViewModelTest {
 
     @Test
     fun givenPrivacyShieldHighlightedWhenShieldIconSelectedThenStopPulse() = runTest {
-        val cta = OnboardingDaxDialogCta.DaxTrackersBlockedCta(mockOnboardingStore, mockAppInstallStore, emptyList())
+        val cta = OnboardingDaxDialogCta.DaxTrackersBlockedCta(mockOnboardingStore, mockAppInstallStore, emptyList(), mockSettingsDataStore)
         testee.ctaViewState.value = ctaViewState().copy(cta = cta)
 
         testee.onPrivacyShieldSelected()
@@ -5488,7 +5488,7 @@ class BrowserTabViewModelTest {
 
     @Test
     fun whenUserDismissDaxTrackersBlockedDialogThenFinishPrivacyShieldPulse() {
-        val cta = OnboardingDaxDialogCta.DaxTrackersBlockedCta(mockOnboardingStore, mockAppInstallStore, emptyList())
+        val cta = OnboardingDaxDialogCta.DaxTrackersBlockedCta(mockOnboardingStore, mockAppInstallStore, emptyList(), mockSettingsDataStore)
         setCta(cta)
 
         testee.onUserDismissedCta(cta)
