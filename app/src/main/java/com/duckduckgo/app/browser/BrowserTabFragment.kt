1596a08b3955870949be03b38a28fd744de76c6e
Ondrej Ruttkay <0nko@users.noreply.github.com>
2024-09-09 17:02:49 +0200

    Omnibar position: Dummy toolbar (#4941)
    Task/Issue URL: https://app.asana.com/0/0/1208157864474373/f
    
    ### Description
    
    ### Steps to test this PR
    
    - [ ] Start the omnibar at the top
    - [ ] Reload the page and notice the dummy toolbar is shown at the top
    - [ ] Change the omnibar to the bottom
    - [ ] Reload the page and notice the dummy toolbar is shown at the
    bottom

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -152,6 +152,8 @@ import com.duckduckgo.app.browser.omnibar.OmnibarScrolling
 import com.duckduckgo.app.browser.omnibar.animations.BrowserTrackersAnimatorHelper
 import com.duckduckgo.app.browser.omnibar.animations.PrivacyShieldAnimationHelper
 import com.duckduckgo.app.browser.omnibar.animations.TrackersAnimatorListener
+import com.duckduckgo.app.browser.omnibar.model.OmnibarPosition.BOTTOM
+import com.duckduckgo.app.browser.omnibar.model.OmnibarPosition.TOP
 import com.duckduckgo.app.browser.print.PrintDocumentAdapterFactory
 import com.duckduckgo.app.browser.print.PrintInjector
 import com.duckduckgo.app.browser.print.SinglePrintSafeguardFeature
@@ -2394,7 +2396,7 @@ class BrowserTabFragment :
             R.layout.include_duckduckgo_browser_webview,
             binding.webViewContainer,
             true,
-        ).findViewById(R.id.browserWebView) as DuckDuckGoWebView
+        ).findViewById<DuckDuckGoWebView>(R.id.browserWebView)!!
 
         webView?.let {
             it.isSafeWebViewEnabled = safeWebViewFeature.self().isEnabled()
