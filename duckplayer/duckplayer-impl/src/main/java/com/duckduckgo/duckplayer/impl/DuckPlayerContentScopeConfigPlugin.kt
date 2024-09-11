f624527012d51b412583202c3e7e3d97c691670e
Cris Barreiro <cbarreiro@duckduckgo.com>
2024-09-11 16:28:13 +0200

    Fix duckPlayer RC set to internal not working in FE

--- duckplayer/duckplayer-impl/src/main/java/com/duckduckgo/duckplayer/impl/DuckPlayerContentScopeConfigPlugin.kt
+++ duckplayer/duckplayer-impl/src/main/java/com/duckduckgo/duckplayer/impl/DuckPlayerContentScopeConfigPlugin.kt
@@ -16,20 +16,40 @@
 
 package com.duckduckgo.duckplayer.impl
 
+import com.duckduckgo.appbuildconfig.api.AppBuildConfig
+import com.duckduckgo.appbuildconfig.api.isInternalBuild
 import com.duckduckgo.contentscopescripts.api.ContentScopeConfigPlugin
 import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.duckplayer.api.DuckPlayer
+import com.duckduckgo.duckplayer.api.DuckPlayer.DuckPlayerState.ENABLED
 import com.duckduckgo.duckplayer.api.DuckPlayerFeatureName
 import com.squareup.anvil.annotations.ContributesMultibinding
 import javax.inject.Inject
+import kotlinx.coroutines.runBlocking
+import org.json.JSONObject
 
 @ContributesMultibinding(AppScope::class)
 class DuckPlayerContentScopeConfigPlugin @Inject constructor(
     private val duckPlayerFeatureRepository: DuckPlayerFeatureRepository,
+    private val appBuildConfig: AppBuildConfig,
+    private val duckPlayer: DuckPlayer,
 ) : ContentScopeConfigPlugin {
 
     override fun config(): String {
         val featureName = DuckPlayerFeatureName.DuckPlayer.value
-        val config = duckPlayerFeatureRepository.getDuckPlayerRemoteConfigJson()
+
+        val config = duckPlayerFeatureRepository.getDuckPlayerRemoteConfigJson().let { jsonString ->
+            if (appBuildConfig.isInternalBuild() && runBlocking { duckPlayer.getDuckPlayerState() == ENABLED }) {
+                runCatching {
+                    JSONObject(jsonString).takeIf { it.getString("state") == "internal" }?.apply {
+                        put("state", "enabled")
+                    }?.toString() ?: jsonString
+                }.getOrDefault(jsonString)
+            } else {
+                jsonString
+            }
+        }
+
         return "\"$featureName\":$config"
     }
 
