56916b27c4a61c2e5245fd30aff023a0c3e77488
Marcos <marcosh@duckduckgo.com>
2024-09-18 15:14:34 +0100

    Fix formatting from external contribution (#5030)
    Task/Issue URL:
    https://app.asana.com/0/1125189844152671/1208318107615954/f
    
    ### Description
    Fixes formatting issues
    
    ### Steps to test this PR
    CI passes

--- app/src/main/java/com/duckduckgo/app/onboarding/ui/page/WelcomePage.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/ui/page/WelcomePage.kt
@@ -22,7 +22,6 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
