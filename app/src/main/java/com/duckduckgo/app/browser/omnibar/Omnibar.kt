c1c2d189185314c9a4a3e3ce7264580215068f07
Ondrej Ruttkay <ruttkay@gmail.com>
2024-09-20 09:27:35 +0200

    Update the logic to show top and bottom omnibar

--- app/src/main/java/com/duckduckgo/app/browser/omnibar/Omnibar.kt
+++ app/src/main/java/com/duckduckgo/app/browser/omnibar/Omnibar.kt
@@ -23,6 +23,8 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.updateLayoutParams
 import com.duckduckgo.app.browser.databinding.FragmentBrowserTabBinding
 import com.duckduckgo.app.browser.omnibar.model.OmnibarPosition
+import com.duckduckgo.common.ui.view.gone
+import com.duckduckgo.common.ui.view.show
 import com.duckduckgo.mobile.android.R as CommonR
 
 @SuppressLint("ClickableViewAccessibility")
@@ -40,10 +42,13 @@ class Omnibar(
 
     val appBarLayout: LegacyOmnibarView by lazy {
         if (omnibarPosition == OmnibarPosition.TOP || !omnibarPositionFeature.self().isEnabled()) {
-            binding.rootView.removeView(binding.legacyOmnibarBottom)
+            binding.legacyOmnibar.show()
+            binding.legacyOmnibarBottom.gone()
+
             binding.legacyOmnibar
         } else {
-            binding.rootView.removeView(binding.legacyOmnibar)
+            binding.legacyOmnibar.gone()
+            binding.legacyOmnibarBottom.show()
 
             // remove the default top abb bar behavior
             removeAppBarBehavior(binding.autoCompleteSuggestionsList)
