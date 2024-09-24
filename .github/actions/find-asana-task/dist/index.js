7300e156401a16035fd89ebf39ed2cddcf6c28f3
David González <malmstein@gmail.com>
2024-09-24 11:33:09 +0200

    added missing action files

---
+++ .github/actions/find-asana-task/dist/index.js
@@ -0,0 +1,35 @@
+// src/index.js
+const core = require('@actions/core');
+const axios = require('axios');
+
+async function run() {
+  try {
+    const taskName = core.getInput('task_name');
+    const asanaToken = core.getInput('asana_token');
+    const projectId = core.getInput('project_id');
+
+    const response = await axios.get(`https://app.asana.com/api/1.0/tasks/search`, {
+      headers: {
+        Authorization: `Bearer ${asanaToken}`,
+      },
+      params: {
+        project: projectId,
+        text: taskName,
+      },
+    });
+
+    const tasks = response.data.data;
+    const task = tasks.find(t => t.name.toLowerCase() === taskName.toLowerCase());
+
+    if (!task) {
+      core.setFailed(`Task '${taskName}' not found in project ID '${projectId}'.`);
+    } else {
+      core.info(`Task ID for '${taskName}': ${task.id}`);
+      core.setOutput('task_id', task.id);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
