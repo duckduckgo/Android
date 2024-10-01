52446fcacdf9fe462960120f820663041fc93c44
Mike Scamell <mikescamell@users.noreply.github.com>
2024-10-01 09:58:36 +0200

    Dax Dialog Onboarding Experiment Updates (#5074)
    Task/Issue URL: https://app.asana.com/0/1207908166761516/1208267346828963/f 
    
    ### Description
    
    Adds a new DaxBubbleCardViewExperiment for upcoming experiment with onboarding.
    
    These are not visible will be used later in another task. To see these dialogs they will need to be enabled in code.
    
    ### Steps to test this PR
    
    _Launch onboarding_
    - [ ] Ensure dialog shape and arrows have not changed 
    
    ### UI changes
    
    There should be none. But here are examples of what the dialogs should look like when shown:
    
    ![Screenshot_20240926_121050](https://github.com/user-attachments/assets/480f2f4e-9e8d-44c7-a48c-48017f1afc9b)
    
    ![Screenshot_20240926_122205](https://github.com/user-attachments/assets/219a0875-9874-471e-ac01-bc83a8a4cc2f)
    
    ![Screenshot_20240926_155207](https://github.com/user-attachments/assets/17f96275-896a-4850-9ffe-96bb9716a8d9)

---
+++ common/common-ui/src/main/java/com/duckduckgo/common/ui/view/shape/OffsetEndTreatment.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.duckduckgo.common.ui.view.shape
+
+import com.google.android.material.shape.EdgeTreatment
+import com.google.android.material.shape.ShapePath
+
+class OffsetEndTreatment(private val other: EdgeTreatment, private val offsetPx: Int) :
+    EdgeTreatment() {
+
+    override fun getEdgePath(
+        length: Float,
+        center: Float,
+        interpolation: Float,
+        shapePath: ShapePath,
+    ) {
+        other.getEdgePath(length, (center * 2) - offsetPx, interpolation, shapePath)
+    }
+}
