3131c4b4b97bdbeefcb388042911a9ea05d48796
Cris Barreiro <cbarreiro@duckduckgo.com>
2024-10-08 14:56:12 +0200

    Open Duck Player videos in new tab

--- app/src/main/java/com/duckduckgo/app/browser/BrowserWebViewClient.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserWebViewClient.kt
@@ -72,6 +72,7 @@ import com.duckduckgo.common.utils.plugins.PluginPoint
 import com.duckduckgo.cookies.api.CookieManagerProvider
 import com.duckduckgo.duckplayer.api.DuckPlayer
 import com.duckduckgo.duckplayer.api.DuckPlayer.DuckPlayerState.ENABLED
+import com.duckduckgo.duckplayer.impl.DUCK_PLAYER_OPEN_IN_YOUTUBE_PATH
 import com.duckduckgo.experiments.api.loadingbarexperiment.LoadingBarExperimentManager
 import com.duckduckgo.history.api.NavigationHistory
 import com.duckduckgo.privacy.config.api.AmpLinks
@@ -80,6 +81,7 @@ import com.duckduckgo.user.agent.api.ClientBrandHintProvider
 import java.net.URI
 import javax.inject.Inject
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.forEach
 import timber.log.Timber
 
 private const val ABOUT_BLANK = "about:blank"
@@ -118,6 +120,16 @@ class BrowserWebViewClient @Inject constructor(
     var clientProvider: ClientBrandHintProvider? = null
     private var lastPageStarted: String? = null
     private var start: Long? = null
+
+    private var shouldOpenDuckPlayerInNewTab: Boolean = false
+
+    init {
+        appCoroutineScope.launch {
+            duckPlayer.observeShouldOpenInNewTab().collect {
+                shouldOpenDuckPlayerInNewTab = it
+            }
+        }
+    }
 
     /**
      * This is the method of url overriding available from API 24 onwards
@@ -185,7 +197,11 @@ class BrowserWebViewClient @Inject constructor(
                         webView.loadUrl(url.toString())
                         return true
                     } else {
-                        shouldOverrideWebRequest(url, webView, isForMainFrame)
+                        if (shouldOpenDuckPlayerInNewTab) {
+                            shouldOverrideWebRequest(url, webView, isForMainFrame, openInNewTab = true)
+                        } else {
+                            shouldOverrideWebRequest(url, webView, isForMainFrame, openInNewTab = false)
+                        }
                     }
                 }
                 is SpecialUrlDetector.UrlType.NonHttpAppLink -> {
@@ -266,10 +282,16 @@ class BrowserWebViewClient @Inject constructor(
                 }
                 is SpecialUrlDetector.UrlType.DuckScheme -> {
                     webViewClientListener?.let { listener ->
-                        loadUrl(listener, webView, url.toString())
+                        if (
+                            url.pathSegments?.firstOrNull()?.equals(DUCK_PLAYER_OPEN_IN_YOUTUBE_PATH, ignoreCase = true) == true ||
+                            !shouldOpenDuckPlayerInNewTab
+                        ) {
+                            loadUrl(listener, webView, url.toString())
+                        } else {
+                            listener.openLinkInNewTab(url)
+                        }
                         true
-                    }
-                    false
+                    } ?: false
                 }
             }
         } catch (e: Throwable) {
@@ -284,6 +306,7 @@ class BrowserWebViewClient @Inject constructor(
         url: Uri,
         webView: WebView,
         isForMainFrame: Boolean,
+        openInNewTab: Boolean = false,
     ): Boolean {
         if (requestRewriter.shouldRewriteRequest(url)) {
             webViewClientListener?.let { listener ->
@@ -298,7 +321,14 @@ class BrowserWebViewClient @Inject constructor(
                 clientProvider?.let { provider ->
                     if (provider.shouldChangeBranding(url.toString())) {
                         provider.setOn(webView.settings, url.toString())
-                        loadUrl(listener, webView, url.toString())
+                        if (openInNewTab) {
+                            listener.openLinkInNewTab(url)
+                        } else {
+                            loadUrl(listener, webView, url.toString())
+                        }
+                        return true
+                    } else if (openInNewTab) {
+                        webViewClientListener?.openLinkInNewTab(url)
                         return true
                     } else {
                         return false
@@ -306,6 +336,9 @@ class BrowserWebViewClient @Inject constructor(
                 }
                 return false
             }
+        } else if (openInNewTab) {
+            webViewClientListener?.openLinkInNewTab(url)
+            return true
         }
         return false
     }
