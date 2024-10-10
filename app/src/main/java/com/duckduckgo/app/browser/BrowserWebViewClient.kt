42e93466e04e29eedad5aa3e207c74d02ef7b8e5
Cris Barreiro <cbarreiro@duckduckgo.com>
2024-10-10 16:18:59 +0200

    Add feature flag for opening in new tab

--- app/src/main/java/com/duckduckgo/app/browser/BrowserWebViewClient.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserWebViewClient.kt
@@ -73,6 +73,7 @@ import com.duckduckgo.common.utils.plugins.PluginPoint
 import com.duckduckgo.cookies.api.CookieManagerProvider
 import com.duckduckgo.duckplayer.api.DuckPlayer
 import com.duckduckgo.duckplayer.api.DuckPlayer.DuckPlayerState.ENABLED
+import com.duckduckgo.duckplayer.api.DuckPlayer.OpenDuckPlayerInNewTab.On
 import com.duckduckgo.duckplayer.impl.DUCK_PLAYER_OPEN_IN_YOUTUBE_PATH
 import com.duckduckgo.experiments.api.loadingbarexperiment.LoadingBarExperimentManager
 import com.duckduckgo.history.api.NavigationHistory
@@ -128,7 +129,7 @@ class BrowserWebViewClient @Inject constructor(
     init {
         appCoroutineScope.launch {
             duckPlayer.observeShouldOpenInNewTab().collect {
-                shouldOpenDuckPlayerInNewTab = it
+                shouldOpenDuckPlayerInNewTab = it is On
             }
         }
     }
