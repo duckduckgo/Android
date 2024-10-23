91e31288fd25488bdee22c81136f375a7688ad0f
Marcos <marcosh@duckduckgo.com>
2024-10-23 16:22:43 +0100

    Adds new public APIs to Toggles and FeatureTogglesInventory (#5179)
    Task/Issue URL:
    https://app.asana.com/0/1125189844152671/1208600117694850/f
    
    ### Description
    Adds new public APIs to Toggles and FeatureTogglesInventory
    
    ### Steps to test this PR
    
    - [x] Tests pass
    - [x] Check code

--- feature-toggles/feature-toggles-api/src/main/java/com/duckduckgo/feature/toggles/api/FeatureTogglesInventory.kt
+++ feature-toggles/feature-toggles-api/src/main/java/com/duckduckgo/feature/toggles/api/FeatureTogglesInventory.kt
@@ -21,4 +21,14 @@ interface FeatureTogglesInventory {
      * @return returns a list of ALL feature flags (aka [Toggle]s) currently used in the app
      */
     suspend fun getAll(): List<Toggle>
+
+    /**
+     * @return returns the list of all sub-features for a given top level feature
+     */
+    suspend fun getAllTogglesForParent(name: String): List<Toggle> = emptyList()
+
+    /**
+     * @return returns ALL toggles that have an assigned cohort AND "state": "enabled"
+     */
+    suspend fun getAllActiveExperimentToggles(): List<Toggle> = emptyList()
 }
