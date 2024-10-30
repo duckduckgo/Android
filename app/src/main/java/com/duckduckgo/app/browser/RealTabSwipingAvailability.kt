31521ba071db3bae02d831218be37f1b0f83ba94
Ondrej Ruttkay <ruttkay@gmail.com>
2024-10-30 14:03:30 +0100

    Add a class for checking if tab swiping is enabled

---
+++ app/src/main/java/com/duckduckgo/app/browser/RealTabSwipingAvailability.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.browser
+
+import com.duckduckgo.app.cta.db.DismissedCtaDao
+import com.duckduckgo.app.cta.model.CtaId
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesBinding
+import javax.inject.Inject
+
+@ContributesBinding(AppScope::class)
+class RealTabSwipingAvailability @Inject constructor(
+    swipingTabsFeature: SwipingTabsFeature,
+    private val dismissedCtaDao: DismissedCtaDao,
+) : TabSwipingAvailability {
+    private val isOnboardingComplete: Boolean
+        get() = dismissedCtaDao.exists(CtaId.DAX_END)
+
+    override val isSwipingTabsEnabled: Boolean = swipingTabsFeature.self().isEnabled() && isOnboardingComplete
+}
