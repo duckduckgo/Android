41464dca809c0663b2997530ed35e57bd09e6a37
Craig Russell <CDRussell@users.noreply.github.com>
2024-11-12 11:25:09 +0000

    Separate disabled state from non-editable state for DaxTextInput

--- common/common-ui/src/main/java/com/duckduckgo/common/ui/view/text/DaxTextInput.kt
+++ common/common-ui/src/main/java/com/duckduckgo/common/ui/view/text/DaxTextInput.kt
@@ -201,10 +201,15 @@ class DaxTextInput @JvmOverloads constructor(
         get() = binding.internalEditText.isEnabled
         set(value) {
             binding.internalEditText.isEnabled = value
-            binding.internalInputLayout.isEnabled = value
-            binding.root.alpha = if (value) ENABLED_OPACITY else DISABLED_OPACITY
             handleIsEditableChangeForEndIcon(value)
         }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        binding.internalInputLayout.isEnabled = enabled
+        binding.internalPasswordIcon.isEnabled = enabled
+        binding.root.alpha = if (enabled) ENABLED_OPACITY else DISABLED_OPACITY
+    }
 
     override var error: String?
         get() = binding.internalInputLayout.error.toString()
@@ -219,8 +224,6 @@ class DaxTextInput @JvmOverloads constructor(
     }
 
     private fun handleIsEditableChangeForEndIcon(isEditable: Boolean) {
-        binding.internalPasswordIcon.isEnabled = isEditable
-
         if (binding.internalInputLayout.endIconMode != END_ICON_NONE) {
             binding.internalInputLayout.isEndIconVisible = !isEditable
             if (isEditable && isPassword) {
