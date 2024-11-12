e2dfddac2e00082169bf7621106801f65613ed2e
Cris Barreiro <cbarreiro@duckduckgo.com>
2024-11-12 09:08:39 +0100

    Disable old protections popup

--- privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupFeature.kt
+++ privacy-protections-popup/privacy-protections-popup-impl/src/main/java/com/duckduckgo/privacyprotectionspopup/impl/PrivacyProtectionsPopupFeature.kt
@@ -20,7 +20,6 @@ import com.duckduckgo.anvil.annotations.ContributesRemoteFeature
 import com.duckduckgo.di.scopes.AppScope
 import com.duckduckgo.feature.toggles.api.Toggle
 import com.duckduckgo.feature.toggles.api.Toggle.DefaultValue
-import com.duckduckgo.feature.toggles.api.Toggle.InternalAlwaysEnabled
 
 @ContributesRemoteFeature(
     scope = AppScope::class,
@@ -28,7 +27,6 @@ import com.duckduckgo.feature.toggles.api.Toggle.InternalAlwaysEnabled
 )
 interface PrivacyProtectionsPopupFeature {
     @DefaultValue(false)
-    @InternalAlwaysEnabled
     fun self(): Toggle
 }
 
