eef94c7766eeeeb31325ca6b89269b27a4346c26
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2024-11-13 13:45:51 +0100

    migrate ppro upsell messages to new API for checking if user is signed in

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/view/message/SubscriptionsExt.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/view/message/SubscriptionsExt.kt
@@ -19,5 +19,5 @@ package com.duckduckgo.mobile.android.vpn.ui.tracker_activity.view.message
 import com.duckduckgo.subscriptions.api.Subscriptions
 
 suspend fun Subscriptions.isUpsellEligible(): Boolean {
-    return getAccessToken() == null && isEligible()
+    return !isSignedIn() && isEligible()
 }
