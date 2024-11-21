c5af1d0db8b548cec77540004b20db4e86dfd260
0nko <oruttkay@duckduckgo.com>
2024-11-21 16:37:05 +0100

    Disable viewpager swiping over the webview

--- app/src/main/java/com/duckduckgo/app/browser/DuckDuckGoWebView.kt
+++ app/src/main/java/com/duckduckgo/app/browser/DuckDuckGoWebView.kt
@@ -227,7 +227,9 @@ class DuckDuckGoWebView : WebView, NestedScrollingChild3 {
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(ev: MotionEvent): Boolean {
-        var returnValue = false
+        requestDisallowInterceptTouchEvent(true)
+
+        val returnValue: Boolean
 
         val event = MotionEvent.obtain(ev)
         val action = event.actionMasked
