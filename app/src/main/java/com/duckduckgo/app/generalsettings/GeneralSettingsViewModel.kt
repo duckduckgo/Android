3a7713c06c1fc30cf65230b958aa9cfc0ce98c63
Mike Scamell <mscamell@duckduckgo.com>
2024-11-22 11:47:24 +0000

    wait for ViewState before observing ShowOnAppLaunchOption
    This was a race condition, the ViewState could be null still in some rare cases. With this solution, we null check the ViewState before attempting an update

--- app/src/main/java/com/duckduckgo/app/generalsettings/GeneralSettingsViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/generalsettings/GeneralSettingsViewModel.kt
@@ -154,7 +154,7 @@ class GeneralSettingsViewModel @Inject constructor(
     private fun observeShowOnAppLaunchOption() {
         showOnAppLaunchOptionDataStore.optionFlow
             .onEach { showOnAppLaunchOption ->
-                _viewState.update { it!!.copy(showOnAppLaunchSelectedOption = showOnAppLaunchOption) }
+                _viewState.update { it?.copy(showOnAppLaunchSelectedOption = showOnAppLaunchOption) }
             }.launchIn(viewModelScope)
     }
 
