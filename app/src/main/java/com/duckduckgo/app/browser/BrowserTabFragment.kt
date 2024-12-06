27fcbb2b407bfa794935fb6445e1d659a51c3c6e
joshliebe <joshliebe@gmail.com>
2024-12-06 09:36:41 +0000

    Set compile_sdk to 35

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -2017,7 +2017,7 @@ class BrowserTabFragment :
 
     private fun launchDialogForIntent(
         context: Context,
-        pm: PackageManager?,
+        pm: PackageManager,
         intent: Intent,
         activities: List<ResolveInfo>,
         useFirstActivityFound: Boolean,
