203d7c14b986defbf2854c801e63081fd76f3d5e
Mike Scamell <mscamell@duckduckgo.com>
2024-12-12 10:52:17 +0100

    formatting

--- app/src/main/java/com/duckduckgo/app/webtrackingprotection/WebTrackingProtectionActivity.kt
+++ app/src/main/java/com/duckduckgo/app/webtrackingprotection/WebTrackingProtectionActivity.kt
@@ -24,7 +24,6 @@ import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.URLSpan
 import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
