cc21a12f8f3d4bce0e930e1f2887680e861f70a7
Mike Scamell <mikescamell@users.noreply.github.com>
2024-12-13 16:16:26 +0100

    Update Settings: Update Ppro Purchasing and Setting items (#5385)
    Task/Issue URL:
    https://app.asana.com/0/1207908166761516/1208966262488270/f
    
    ### Description
    
    Initial changes to start moving Ppro settings to new Settings world.
    This PR focuses specifically on the Purchasing state as well as the
    Subscription Setting item state, as these are part of the same plugin.
    
    
    [Designs](https://www.figma.com/design/CjH849hL53lhsPlf6Ufeo4/%E2%9A%99%EF%B8%8F-Browser-Settings-Documentation-(All-Platforms)?node-id=7605-431390&node-type=instance&t=ZKIMXkdZqzh5gEjU-11)
    
    ### Steps to test this PR
    
    Prerequisite: Enable `newSettings` feature toggle
    
    Prerequisite: Apply the patch in the [Asana
    task](https://app.asana.com/0/1207908166761516/1208966262488270/f)
    
    Also remember VPN, PIR, and ITR are **not** changed in this PR
    
    ### _Not purchased_
    - [x] Set ln 99 of `ProSettingViewModel` to
    `_viewState.emit(viewState.value.copy(status = EXPIRED, region =
    SubscriptionRegion.US))`
    - [x] Open Settings
    - [x] Check against
    [designs](https://www.figma.com/design/CjH849hL53lhsPlf6Ufeo4/%E2%9A%99%EF%B8%8F-Browser-Settings-Documentation-(All-Platforms)?node-id=7605-431390&node-type=instance&t=ZKIMXkdZqzh5gEjU-11)
    - [x] Click items
    - [x] Ensure correct screens are open
    
    | Before  | After |
    | ------ | ----- |
    
    ![not_purchased_original](https://github.com/user-attachments/assets/bfc77f48-77f2-49f6-96fd-c650b082fa15)|![Screenshot_20241212_192120](https://github.com/user-attachments/assets/316945a7-b5ae-4777-aa37-05822f19ec0b)|
    
    ### _Purchased_
    - [x] Set ln 99 of `ProSettingViewModel` to
    `_viewState.emit(viewState.value.copy(status = AUTO_RENEWABLE, region =
    SubscriptionRegion.US))`
    - [x] Open Settings
    - [x] Check against
    [designs](https://www.figma.com/design/CjH849hL53lhsPlf6Ufeo4/%E2%9A%99%EF%B8%8F-Browser-Settings-Documentation-(All-Platforms)?node-id=7605-431390&node-type=instance&t=ZKIMXkdZqzh5gEjU-11)
    - [x] Click Subscription Settings
    - [x] Ensure Subscription Settings is opened
    
    | Before  | After |
    | ------ | ----- |
    
    ![purchased_original](https://github.com/user-attachments/assets/d0c244c0-5535-4adc-944a-a324a8f1eda7)|![subscribed](https://github.com/user-attachments/assets/459f5e63-f161-4809-b25c-d01422ebfcfa)|
    
    ### _Activating (Waiting)_
    - [x] Set ln 99 of `ProSettingViewModel` to
    `_viewState.emit(viewState.value.copy(status = WAITING, region =
    SubscriptionRegion.US))`
    - [x] Open Settings
    - [x] Check against
    [designs](https://www.figma.com/design/CjH849hL53lhsPlf6Ufeo4/%E2%9A%99%EF%B8%8F-Browser-Settings-Documentation-(All-Platforms)?node-id=7605-431390&node-type=instance&t=ZKIMXkdZqzh5gEjU-11)
    - [x] Click Subscription Settings
    - [x] Ensure Subscription Settings is opened
    
    | Before  | After |
    | ------ | ----- |
    
    ![original_waiting](https://github.com/user-attachments/assets/05934c81-c3da-4b40-bfd7-c453f07f919e)|![activating](https://github.com/user-attachments/assets/40ec262f-b764-44f0-9d80-eaabb5ce6f73)|
    
    ### _Expired_
    - [x] Set ln 99 of `ProSettingViewModel` to
    `_viewState.emit(viewState.value.copy(status = EXPIRED, region =
    SubscriptionRegion.US))`
    - [x] Open Settings
    - [x] Check against
    [designs](https://www.figma.com/design/CjH849hL53lhsPlf6Ufeo4/%E2%9A%99%EF%B8%8F-Browser-Settings-Documentation-(All-Platforms)?node-id=7605-431390&node-type=instance&t=ZKIMXkdZqzh5gEjU-11)
    - [x] Click Subscription Settings
    - [x] Ensure Subscription Settings is opened
    
    | Before  | After |
    | ------ | ----- |
    
    ![original_expired](https://github.com/user-attachments/assets/3d26f2b7-382c-4d2a-b5ee-3c16b28e413f)|![expired](https://github.com/user-attachments/assets/3972412e-6daf-4a10-99b1-48f85d104914)|

--- subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/settings/views/LegacyProSettingView.kt
+++ subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/settings/views/LegacyProSettingView.kt
@@ -20,17 +20,18 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.findViewTreeLifecycleOwner
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.common.ui.view.gone
+import com.duckduckgo.common.ui.view.listitem.CheckListItem.CheckItemStatus.ALERT
+import com.duckduckgo.common.ui.view.listitem.CheckListItem.CheckItemStatus.DISABLED
+import com.duckduckgo.common.ui.view.show
 import com.duckduckgo.common.ui.viewbinding.viewBinding
 import com.duckduckgo.common.utils.ConflatedJob
 import com.duckduckgo.common.utils.ViewViewModelFactory
 import com.duckduckgo.di.scopes.ViewScope
-import com.duckduckgo.mobile.android.R as CommonR
 import com.duckduckgo.navigation.api.GlobalActivityStarter
 import com.duckduckgo.subscriptions.api.SubscriptionStatus.AUTO_RENEWABLE
 import com.duckduckgo.subscriptions.api.SubscriptionStatus.EXPIRED
@@ -40,14 +41,14 @@ import com.duckduckgo.subscriptions.api.SubscriptionStatus.NOT_AUTO_RENEWABLE
 import com.duckduckgo.subscriptions.api.SubscriptionStatus.WAITING
 import com.duckduckgo.subscriptions.impl.R
 import com.duckduckgo.subscriptions.impl.SubscriptionsConstants
-import com.duckduckgo.subscriptions.impl.databinding.ViewSettingsBinding
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.Command
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.Command.OpenBuyScreen
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.Command.OpenRestoreScreen
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.Command.OpenSettings
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.ViewState
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.ViewState.SubscriptionRegion.ROW
-import com.duckduckgo.subscriptions.impl.settings.views.ProSettingViewModel.ViewState.SubscriptionRegion.US
+import com.duckduckgo.subscriptions.impl.databinding.LegacyViewSettingsBinding
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.Command
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.Command.OpenBuyScreen
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.Command.OpenRestoreScreen
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.Command.OpenSettings
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.ViewState
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.ViewState.SubscriptionRegion.ROW
+import com.duckduckgo.subscriptions.impl.settings.views.LegacyProSettingViewModel.ViewState.SubscriptionRegion.US
 import com.duckduckgo.subscriptions.impl.ui.RestoreSubscriptionActivity.Companion.RestoreSubscriptionScreenWithParams
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsActivity.Companion.SubscriptionsSettingsScreenWithEmptyParams
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionsWebViewActivityWithParams
@@ -61,7 +62,7 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 @InjectWith(ViewScope::class)
-class ProSettingView @JvmOverloads constructor(
+class LegacyProSettingView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
@@ -75,10 +76,10 @@ class ProSettingView @JvmOverloads constructor(
 
     private var coroutineScope: CoroutineScope? = null
 
-    private val binding: ViewSettingsBinding by viewBinding()
+    private val binding: LegacyViewSettingsBinding by viewBinding()
 
-    private val viewModel: ProSettingViewModel by lazy {
-        ViewModelProvider(findViewTreeViewModelStoreOwner()!!, viewModelFactory)[ProSettingViewModel::class.java]
+    private val viewModel: LegacyProSettingViewModel by lazy {
+        ViewModelProvider(findViewTreeViewModelStoreOwner()!!, viewModelFactory)[LegacyProSettingViewModel::class.java]
     }
 
     private var job: ConflatedJob = ConflatedJob()
@@ -134,53 +135,42 @@ class ProSettingView @JvmOverloads constructor(
     private fun renderView(viewState: ViewState) {
         when (viewState.status) {
             AUTO_RENEWABLE, NOT_AUTO_RENEWABLE, GRACE_PERIOD -> {
-                with(binding) {
-                    subscriptionBuyContainer.isGone = true
-                    subscriptionRestoreContainer.isGone = true
-                    subscriptionSetting.isGone = true
-
-                    subscribedSubscriptionSetting.isVisible = true
-                    subscriptionSettingContainer.isVisible = true
-                }
+                binding.subscriptionBuyContainer.gone()
+                binding.subscriptionRestoreContainer.gone()
+                binding.subscriptionWaitingContainer.gone()
+                binding.subscriptionSettingContainer.show()
             }
             WAITING -> {
-                with(binding) {
-                    subscriptionBuyContainer.isGone = true
-                    subscriptionRestoreContainer.isGone = true
-                    subscribedSubscriptionSetting.isGone = true
-
-                    subscriptionSettingContainer.isVisible = true
-                    subscriptionSetting.setSecondaryText(context.getString(R.string.subscriptionSettingActivating))
-                }
+                binding.subscriptionBuyContainer.gone()
+                binding.subscriptionWaitingContainer.show()
+                binding.subscriptionSettingContainer.gone()
+                binding.subscriptionRestoreContainer.show()
             }
             EXPIRED, INACTIVE -> {
-                with(binding) {
-                    subscriptionBuyContainer.isGone = true
-                    subscriptionRestoreContainer.isGone = true
-                    subscribedSubscriptionSetting.isGone = true
-
-                    subscriptionSettingContainer.isVisible = true
-                    subscriptionSetting.setSecondaryText(context.getString(R.string.subscriptionSettingExpired))
-                    subscriptionSetting.setTrailingIconResource(CommonR.drawable.ic_exclamation_red_16)
-                }
+                binding.subscriptionBuy.setPrimaryText(context.getString(R.string.subscriptionSettingExpired))
+                binding.subscriptionBuy.setSecondaryText(context.getString(R.string.subscriptionSettingExpiredSubtitle))
+                binding.subscriptionBuy.setItemStatus(ALERT)
+                binding.subscriptionGet.setText(R.string.subscriptionSettingExpiredViewPlans)
+                binding.subscriptionBuyContainer.show()
+                binding.subscriptionSettingContainer.show()
+                binding.subscriptionWaitingContainer.gone()
+                binding.subscriptionRestoreContainer.gone()
             }
             else -> {
-                with(binding) {
-                    subscriptionBuy.setPrimaryText(context.getString(R.string.subscriptionSettingSubscribe))
-                    subscriptionBuy.setSecondaryText(
-                        when (viewState.region) {
-                            ROW -> context.getString(R.string.subscriptionSettingSubscribeSubtitleRow)
-                            US -> context.getString(R.string.subscriptionSettingSubscribeSubtitle)
-                            else -> ""
-                        },
-                    )
-                    subscriptionGet.setText(R.string.subscriptionSettingGet)
-
-                    subscriptionBuyContainer.isVisible = true
-                    subscriptionRestoreContainer.isVisible = true
-
-                    subscriptionSettingContainer.isGone = true
-                }
+                binding.subscriptionBuy.setPrimaryText(context.getString(R.string.subscriptionSettingSubscribe))
+                binding.subscriptionBuy.setSecondaryText(
+                    when (viewState.region) {
+                        ROW -> context.getString(R.string.subscriptionSettingSubscribeSubtitleRow)
+                        US -> context.getString(R.string.subscriptionSettingSubscribeSubtitle)
+                        else -> ""
+                    },
+                )
+                binding.subscriptionBuy.setItemStatus(DISABLED)
+                binding.subscriptionGet.setText(R.string.subscriptionSettingGet)
+                binding.subscriptionBuyContainer.show()
+                binding.subscriptionSettingContainer.gone()
+                binding.subscriptionWaitingContainer.gone()
+                binding.subscriptionRestoreContainer.show()
             }
         }
     }
