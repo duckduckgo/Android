47a9f7f6b287433be0e3ae98b5c955d3768f73a4
Noelia Alcala <nalcalag@gmail.com>
2025-01-15 10:22:56 +0100

    Remove temporary pixel when not showing trackers network dialog (#5471)
    Task/Issue URL:
    https://app.asana.com/0/1205278999335242/1208163185498327/f
    
    ### Description
    Remove temporary pixel when skipping major network onboarding dialog
    when Duck Player dialog is shown
    
    ### Steps to test this PR
    
    _Pixel removed_
    - [x] Fresh install
    - [x] Go to any youtube URL
    - [x] Check pixel `m_onboarding_skip_major_network_unique` is not sent
    and it works as expected
    
    ### No UI changes

--- app/src/main/java/com/duckduckgo/app/pixels/AppPixelName.kt
+++ app/src/main/java/com/duckduckgo/app/pixels/AppPixelName.kt
@@ -49,7 +49,6 @@ enum class AppPixelName(override val pixelName: String) : Pixel.PixelName {
     ONBOARDING_DAX_ALL_CTA_HIDDEN("m_odc_h"),
     ONBOARDING_DAX_CTA_OK_BUTTON("m_odc_ok"),
     ONBOARDING_DAX_CTA_CANCEL_BUTTON("m_onboarding_dax_cta_cancel"),
-    ONBOARDING_SKIP_MAJOR_NETWORK_UNIQUE("m_onboarding_skip_major_network_unique"),
     ONBOARDING_VISIT_SITE_CTA_SHOWN("onboarding_visit_site_cta_shown"),
 
     BROWSER_MENU_ALLOWLIST_ADD("mb_wla"),
