bd57fd6b0cae4a7a6f1c95285647705a15aefca0
joshliebe <joshliebe@gmail.com>
2025-01-22 10:39:05 +0000

    Updated isEnabled comment

--- duckchat/duckchat-api/src/main/java/com/duckduckgo/duckchat/api/DuckChat.kt
+++ duckchat/duckchat-api/src/main/java/com/duckduckgo/duckchat/api/DuckChat.kt
@@ -24,6 +24,7 @@ import android.net.Uri
 interface DuckChat {
     /**
      * Checks whether DuckChat is enabled based on remote config flag.
+     * Uses a cached value - does not perform disk I/O.
      *
      * @return true if DuckChat is enabled, false otherwise.
      */
