d6991bb38b8a7886eca94d8ef1acbe45d31493e9
laghee <20972610+laghee@users.noreply.github.com>
2025-01-23 18:31:56 +0100

    Fix browser loading viewstate

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -3139,6 +3139,7 @@ class BrowserTabViewModel @Inject constructor(
 
     override fun onReceivedMaliciousSiteWarning(url: Uri) {
         // TODO (cbarreiro): Fire pixel
+        loadingViewState.postValue(currentLoadingViewState().copy(isLoading = false, url = url.toString()))
         browserViewState.postValue(
             currentBrowserViewState().copy(
                 browserShowing = false,
