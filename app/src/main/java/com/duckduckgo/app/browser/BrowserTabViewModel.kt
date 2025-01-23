c718c41c4980e79309d9f205375275c19c41d862
laghee <20972610+laghee@users.noreply.github.com>
2025-01-23 13:19:02 +0100

    Fix threading issue with browser viewstate

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -3146,11 +3146,12 @@ class BrowserTabViewModel @Inject constructor(
 
     override fun onReceivedMaliciousSiteWarning(url: Uri) {
         // TODO (cbarreiro): Fire pixel
-        browserViewState.value =
+        browserViewState.postValue(
             currentBrowserViewState().copy(
                 browserShowing = false,
                 showPrivacyShield = HighlightableButton.Visible(enabled = false),
             )
+        )
         command.postValue(ShowWarningMaliciousSite(url))
     }
 
