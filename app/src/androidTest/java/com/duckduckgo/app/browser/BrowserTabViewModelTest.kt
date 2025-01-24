9c6fe6173b84aa6d6d3fca2126555021c0ba9aaa
Ana Capatina <anikiki@gmail.com>
2025-01-24 17:41:35 +0000

    Fixed failing tests.

--- app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
@@ -71,6 +71,7 @@ import com.duckduckgo.app.browser.WebViewErrorResponse.LOADING
 import com.duckduckgo.app.browser.WebViewErrorResponse.OMITTED
 import com.duckduckgo.app.browser.addtohome.AddToHomeCapabilityDetector
 import com.duckduckgo.app.browser.applinks.AppLinksHandler
+import com.duckduckgo.app.browser.apppersonality.AppPersonalityFeature
 import com.duckduckgo.app.browser.camera.CameraHardwareChecker
 import com.duckduckgo.app.browser.certificates.BypassedSSLCertificatesRepository
 import com.duckduckgo.app.browser.certificates.remoteconfig.SSLCertificatesFeature
@@ -502,6 +503,7 @@ class BrowserTabViewModelTest {
     private val mockBrokenSitePrompt: BrokenSitePrompt = mock()
     private val mockTabStatsBucketing: TabStatsBucketing = mock()
     private val mockDuckChatJSHelper: DuckChatJSHelper = mock()
+    private val fakeAppPersonalityFeature = FakeFeatureToggleFactory.create(AppPersonalityFeature::class.java)
 
     @Before
     fun before() = runTest {
@@ -672,6 +674,7 @@ class BrowserTabViewModelTest {
             brokenSitePrompt = mockBrokenSitePrompt,
             tabStatsBucketing = mockTabStatsBucketing,
             maliciousSiteBlockerWebViewIntegration = mock(),
+            appPersonalityFeature = fakeAppPersonalityFeature,
         )
 
         testee.loadData("abc", null, false, false)
