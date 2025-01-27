8f5f80eb6ac25e39c6ab4c3be51dba2489f9264b
0nko <oruttkay@duckduckgo.com>
2025-01-27 22:09:26 +0100

    Optimize imports

--- app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarLayout.kt
+++ app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarLayout.kt
@@ -87,10 +87,8 @@ import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import timber.log.Timber
