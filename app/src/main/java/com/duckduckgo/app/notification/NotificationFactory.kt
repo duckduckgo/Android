913c5ea5864ea56117bd4ed5f5ea24aac568cb5e
joshliebe <joshliebe@gmail.com>
2025-02-14 13:08:08 +0000

    Fix formatting

--- app/src/main/java/com/duckduckgo/app/notification/NotificationFactory.kt
+++ app/src/main/java/com/duckduckgo/app/notification/NotificationFactory.kt
@@ -21,7 +21,6 @@ import android.app.PendingIntent
 import android.content.Context
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
 import com.duckduckgo.app.notification.model.NotificationSpec
 import javax.inject.Inject
 
