f8350ab5b381d83b9094dc53e0e8bbde0fbffd03
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-02-17 18:47:37 +0100

    Do not allow privacy shield highlight on error page

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -3274,7 +3274,7 @@ class BrowserTabViewModel @Inject constructor(
             browserViewState.postValue(
                 currentBrowserViewState().copy(
                     browserShowing = false,
-                    showPrivacyShield = HighlightableButton.Visible(enabled = false),
+                    showPrivacyShield = HighlightableButton.Gone,
                     maliciousSiteDetected = true,
                     maliciousSiteStatus = maliciousSiteStatus,
                 ),
