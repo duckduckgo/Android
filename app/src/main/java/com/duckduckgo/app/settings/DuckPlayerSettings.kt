7c559515aeeba0b7f7d10b3d75f9ae7f858feecc
Łukasz Paczos <lpaczos@duckduckgo.com>
2025-02-21 15:37:39 +0100

    duck player setting

--- app/src/main/java/com/duckduckgo/app/settings/DuckPlayerSettings.kt
+++ app/src/main/java/com/duckduckgo/app/settings/DuckPlayerSettings.kt
@@ -26,10 +26,11 @@ import com.duckduckgo.anvil.annotations.ContributesViewModel
 import com.duckduckgo.anvil.annotations.InjectWith
 import com.duckduckgo.anvil.annotations.PriorityKey
 import com.duckduckgo.app.browser.R
-import com.duckduckgo.app.browser.databinding.ViewSettingsItemDuckChatBinding
-import com.duckduckgo.app.settings.DuckChatSettingViewModel.Command
-import com.duckduckgo.app.settings.DuckChatSettingViewModel.Command.LaunchDuckChatScreen
-import com.duckduckgo.app.settings.DuckChatSettingViewModel.ViewState
+import com.duckduckgo.app.browser.databinding.ContentSettingsDuckPlayerBinding
+import com.duckduckgo.app.settings.DuckPlayerSettingViewModel.Command
+import com.duckduckgo.app.settings.DuckPlayerSettingViewModel.Command.LaunchDuckPlayerScreen
+import com.duckduckgo.app.settings.DuckPlayerSettingViewModel.ViewState
+import com.duckduckgo.app.statistics.pixels.Pixel
 import com.duckduckgo.common.ui.settings.RootSettingsNode
 import com.duckduckgo.common.ui.settings.SettingNodeView
 import com.duckduckgo.common.ui.settings.SettingViewModel
@@ -38,49 +39,49 @@ import com.duckduckgo.common.ui.viewbinding.viewBinding
 import com.duckduckgo.common.utils.ViewViewModelFactory
 import com.duckduckgo.di.scopes.ActivityScope
 import com.duckduckgo.di.scopes.ViewScope
-import com.duckduckgo.duckchat.api.DuckChatSettingsNoParams
+import com.duckduckgo.duckplayer.api.DuckPlayerSettingsNoParams
+import com.duckduckgo.duckplayer.impl.DuckPlayerPixelName.DUCK_PLAYER_SETTINGS_PRESSED
 import com.duckduckgo.navigation.api.GlobalActivityStarter
 import com.squareup.anvil.annotations.ContributesMultibinding
 import dagger.android.support.AndroidSupportInjection
 import java.util.UUID
 import javax.inject.Inject
 
-@ContributesMultibinding(scope = ActivityScope::class)
-@PriorityKey(308)
-class DuckChatSettingNode @Inject constructor() : RootSettingsNode {
+@ContributesMultibinding(ActivityScope::class)
+@PriorityKey(309)
+class DuckPlayerSetting @Inject constructor() : RootSettingsNode {
     override val categoryNameResId = R.string.settingsHeadingMainSettings
     override val children: List<SettingsNode> = emptyList()
-
     override val id: UUID = UUID.randomUUID()
 
     override fun getView(context: Context): View {
-        return DuckChatSettingNodeView(context, searchableId = id)
+        return DuckPlayerSettingNodeView(context, searchableId = id)
     }
 
     override fun generateKeywords(): Set<String> {
         return setOf(
-            "duck", "chat", "ai", "llm", "assistant", "intelligence", "artificial", "duck chat", "duck ai"
+            "duck", "player", "duck player", "youtube", "videos", "privacy",
         )
     }
 }
 
 @SuppressLint("ViewConstructor")
 @InjectWith(ViewScope::class)
-class DuckChatSettingNodeView(
+class DuckPlayerSettingNodeView(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0,
     searchableId: UUID,
-) : SettingNodeView<Command, ViewState, DuckChatSettingViewModel>(context, attrs, defStyle, searchableId) {
+) : SettingNodeView<Command, ViewState, DuckPlayerSettingViewModel>(context, attrs, defStyle, searchableId) {
 
     @Inject
     lateinit var viewModelFactory: ViewViewModelFactory
 
-    override fun provideViewModel(): DuckChatSettingViewModel {
-        return ViewModelProvider(findViewTreeViewModelStoreOwner()!!, viewModelFactory)[DuckChatSettingViewModel::class.java]
+    override fun provideViewModel(): DuckPlayerSettingViewModel {
+        return ViewModelProvider(findViewTreeViewModelStoreOwner()!!, viewModelFactory)[DuckPlayerSettingViewModel::class.java]
     }
 
-    private val binding: ViewSettingsItemDuckChatBinding by viewBinding()
+    private val binding: ContentSettingsDuckPlayerBinding by viewBinding()
 
     @Inject
     lateinit var globalActivityStarter: GlobalActivityStarter
@@ -89,48 +90,52 @@ class DuckChatSettingNodeView(
         AndroidSupportInjection.inject(this)
         super.onAttachedToWindow()
 
-        binding.duckChatSetting.setOnClickListener {
-            viewModel.onDuckChatSettingClicked()
+        binding.duckPlayerSetting.setOnClickListener {
+            viewModel.onDuckPlayerSettingClicked()
         }
     }
 
     override fun renderView(viewState: ViewState) {
-        with(binding.duckChatSetting) {
-            visibility = if (viewState.showDuckChatSetting) {
-                View.VISIBLE
+        with(binding.duckPlayerSetting) {
+            visibility = if (viewState.showDuckPlayerSetting) {
+                android.view.View.VISIBLE
             } else {
-                View.GONE
+                android.view.View.GONE
             }
         }
     }
 
     override fun processCommands(command: Command) {
         when (command) {
-            LaunchDuckChatScreen -> {
-                globalActivityStarter.start(context, DuckChatSettingsNoParams)
+            LaunchDuckPlayerScreen -> {
+                globalActivityStarter.start(context, DuckPlayerSettingsNoParams)
             }
         }
     }
 }
 
 @ContributesViewModel(ViewScope::class)
-class DuckChatSettingViewModel @Inject constructor() : SettingViewModel<Command, ViewState>(ViewState()) {
+class DuckPlayerSettingViewModel @Inject constructor(
+    private val pixel: Pixel,
+) : SettingViewModel<Command, ViewState>(ViewState()) {
 
     override fun getSearchMissViewState(): ViewState {
         return ViewState(
-            showDuckChatSetting = false,
+            showDuckPlayerSetting = false,
         )
     }
 
-    fun onDuckChatSettingClicked() {
-        _commands.trySend(LaunchDuckChatScreen)
+    fun onDuckPlayerSettingClicked() {
+        _commands.trySend(LaunchDuckPlayerScreen)
+        pixel.fire(DUCK_PLAYER_SETTINGS_PRESSED)
     }
 
     data class ViewState(
-        val showDuckChatSetting: Boolean = true,
+        val showDuckPlayerSetting: Boolean = true,
     )
 
     sealed class Command {
-        data object LaunchDuckChatScreen : Command()
+        data object LaunchDuckPlayerScreen : Command()
     }
 }
+
