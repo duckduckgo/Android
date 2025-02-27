7c39032db35270959848c18138211b9d3d23d40a
Mike Scamell <mscamell@duckduckgo.com>
2025-02-27 09:34:52 +0100

    Do not allow the animated tile to be swapped
    This may change in future but for now we're keeping the animated tile in first position and it cannot be swapped

--- app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherActivity.kt
+++ app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherActivity.kt
@@ -46,6 +46,7 @@ import com.duckduckgo.app.pixels.AppPixelName
 import com.duckduckgo.app.settings.SettingsActivity
 import com.duckduckgo.app.settings.db.SettingsDataStore
 import com.duckduckgo.app.statistics.pixels.Pixel
+import com.duckduckgo.app.tabs.TabSwitcherAnimationFeature
 import com.duckduckgo.app.tabs.model.TabEntity
 import com.duckduckgo.app.tabs.model.TabSwitcherData.LayoutType
 import com.duckduckgo.app.tabs.ui.TabSwitcherViewModel.Command
@@ -116,6 +117,9 @@ class TabSwitcherActivity : DuckDuckGoActivity(), TabSwitcherListener, Coroutine
 
     @Inject
     lateinit var duckChat: DuckChat
+
+    @Inject
+    lateinit var tabSwitcherAnimationFeature: TabSwitcherAnimationFeature
 
     private val viewModel: TabSwitcherViewModel by bindViewModel()
 
@@ -414,11 +418,23 @@ class TabSwitcherActivity : DuckDuckGoActivity(), TabSwitcherListener, Coroutine
     }
 
     override fun onTabMoved(from: Int, to: Int) {
-        val tabCount = viewModel.tabSwitcherItems.value?.size ?: 0
-        val canSwap = from in 0..< tabCount && to in 0..< tabCount
-        if (canSwap) {
-            tabsAdapter.onTabMoved(from, to)
-            viewModel.onTabMoved(from, to)
+        if (tabSwitcherAnimationFeature.self().isEnabled()) {
+            val isTrackerAnimationTileVisible = viewModel.tabSwitcherItems.value?.get(0) is TabSwitcherItem.TrackerAnimationTile
+            val canSwapFromIndex = if (isTrackerAnimationTileVisible) 1 else 0
+            val tabItemCount = viewModel.tabSwitcherItems.value?.count { it is TabSwitcherItem.Tab } ?: 0
+
+            val canSwap = from in canSwapFromIndex..<tabItemCount && to in canSwapFromIndex..<tabItemCount
+            if (canSwap) {
+                tabsAdapter.onTabMoved(from, to)
+                viewModel.onTabMoved(from, to)
+            }
+        } else {
+            val tabCount = viewModel.tabSwitcherItems.value?.size ?: 0
+            val canSwap = from in 0..<tabCount && to in 0..<tabCount
+            if (canSwap) {
+                tabsAdapter.onTabMoved(from, to)
+                viewModel.onTabMoved(from, to)
+            }
         }
     }
 
