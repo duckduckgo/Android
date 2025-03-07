f3210cc305d5787d0f34eea56d74480a538a141c
Ana Capatina <anikiki@gmail.com>
2025-03-07 12:57:59 +0000

    Limit burst to 3 times.

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -71,6 +71,7 @@ import com.duckduckgo.app.browser.SpecialUrlDetector.UrlType.ShouldLaunchPrivacy
 import com.duckduckgo.app.browser.WebViewErrorResponse.LOADING
 import com.duckduckgo.app.browser.WebViewErrorResponse.OMITTED
 import com.duckduckgo.app.browser.addtohome.AddToHomeCapabilityDetector
+import com.duckduckgo.app.browser.animations.store.TrackersBurstAnimationPreferencesStore
 import com.duckduckgo.app.browser.applinks.AppLinksHandler
 import com.duckduckgo.app.browser.camera.CameraHardwareChecker
 import com.duckduckgo.app.browser.certificates.BypassedSSLCertificatesRepository
@@ -472,6 +473,8 @@ class BrowserTabViewModel @Inject constructor(
     private val appPersonalityFeature: AppPersonalityFeature,
     private val userStageStore: UserStageStore,
     private val privacyDashboardExternalPixelParams: PrivacyDashboardExternalPixelParams,
+    private val trackersBurstAnimationPreferencesStore: TrackersBurstAnimationPreferencesStore,
+
 ) : WebViewClientListener,
     EditSavedSiteListener,
     DeleteBookmarkListener,
@@ -3866,7 +3869,8 @@ class BrowserTabViewModel @Inject constructor(
         } else if (appPersonalityFeature.self().isEnabled() &&
             (appPersonalityFeature.variant3().isEnabled() || appPersonalityFeature.variant4().isEnabled())
         ) {
-            if (logos.size > TRACKER_LOGO_ANIMATION_THRESHOLD) {
+            if (logos.size > TRACKER_LOGO_ANIMATION_THRESHOLD && trackersBurstAnimationPreferencesStore.fetchCount() < 3) {
+                trackersBurstAnimationPreferencesStore.incrementCount()
                 val ignoreLogos = appPersonalityFeature.variant4().isEnabled()
                 command.value = Command.StartExperimentTrackersBurstAnimation(logos, ignoreLogos)
                 viewModelScope.launch {
