2e2a8db81d84a805597520beb32f0fa882e37e59
0nko <oruttkay@duckduckgo.com>
2025-03-12 13:29:41 +0100

    Use a sealed class instead of an abstract class

--- app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherItem.kt
+++ app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherItem.kt
@@ -19,9 +19,17 @@ package com.duckduckgo.app.tabs.ui
 import com.duckduckgo.app.tabs.model.TabEntity
 
 sealed class TabSwitcherItem(val id: String) {
-    abstract class Tab(val tabEntity: TabEntity) : TabSwitcherItem(tabEntity.tabId)
-    data class NormalTab(private val entity: TabEntity, val isActive: Boolean) : Tab(entity)
-    data class SelectableTab(private val entity: TabEntity, val isSelected: Boolean) : Tab(entity)
+    sealed class Tab(val tabEntity: TabEntity) : TabSwitcherItem(tabEntity.tabId) {
+        data class NormalTab(
+            private val entity: TabEntity,
+            val isActive: Boolean
+        ) : Tab(entity)
+
+        data class SelectableTab(
+            private val entity: TabEntity,
+            val isSelected: Boolean
+        ) : Tab(entity)
+    }
     data class TrackerAnimationInfoPanel(val trackerCount: Int) : TabSwitcherItem("TrackerAnimationInfoPanel") {
         companion object {
             const val ANIMATED_TILE_NO_REPLACE_ALPHA = 0.4f
