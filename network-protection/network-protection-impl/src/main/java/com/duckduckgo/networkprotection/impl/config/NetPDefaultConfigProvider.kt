207f113b4d0f33e18810dfbcf4f2f7dfff27faf3
Aitor Viana <aitorvs@gmail.com>
2025-03-12 18:09:09 +0000

    Generate VPN routes dynamically

--- network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/config/NetPDefaultConfigProvider.kt
+++ network-protection/network-protection-impl/src/main/java/com/duckduckgo/networkprotection/impl/config/NetPDefaultConfigProvider.kt
@@ -18,6 +18,9 @@ package com.duckduckgo.networkprotection.impl.config
 
 import com.duckduckgo.common.utils.DispatcherProvider
 import com.duckduckgo.di.scopes.VpnScope
+import com.duckduckgo.networkprotection.impl.VpnRemoteFeatures
+import com.duckduckgo.networkprotection.impl.configuration.CONTROLLER_NETP_DUCKDUCKGO_COM
+import com.duckduckgo.networkprotection.impl.configuration.VpnLocalDns
 import com.duckduckgo.networkprotection.impl.exclusion.NetPExclusionListRepository
 import com.duckduckgo.networkprotection.impl.exclusion.systemapps.SystemAppsExclusionRepository
 import com.duckduckgo.networkprotection.impl.settings.NetPSettingsLocalConfig
@@ -27,6 +30,7 @@ import java.net.Inet4Address
 import java.net.InetAddress
 import javax.inject.Inject
 import kotlinx.coroutines.withContext
+import logcat.logcat
 
 interface NetPDefaultConfigProvider {
     fun mtu(): Int = 1280
@@ -53,6 +57,8 @@ class RealNetPDefaultConfigProvider @Inject constructor(
     private val netPSettingsLocalConfig: NetPSettingsLocalConfig,
     private val systemAppsExclusionRepository: SystemAppsExclusionRepository,
     private val netpVpnSettingsDataStore: NetpVpnSettingsDataStore,
+    private val vpnRemoteFeatures: VpnRemoteFeatures,
+    private val vpnLocalDns: VpnLocalDns,
 ) : NetPDefaultConfigProvider {
     override suspend fun exclusionList(): Set<String> {
         return mutableSetOf<String>().apply {
@@ -62,14 +68,45 @@ class RealNetPDefaultConfigProvider @Inject constructor(
     }
 
     override suspend fun routes(): Map<String, Int> = withContext(dispatcherProvider.io()) {
+        val isLocalDnsEnabled = vpnRemoteFeatures.runtimeIpRouteGeneration().isEnabled()
+
         return@withContext if (netPSettingsLocalConfig.vpnExcludeLocalNetworkRoutes().isEnabled()) {
-            WgVpnRoutes.wgVpnDefaultRoutes.toMutableMap().apply {
+            val routes = if (isLocalDnsEnabled) {
+                // get the controller IPs
+                val controllerIPs = vpnLocalDns.lookup(CONTROLLER_NETP_DUCKDUCKGO_COM)
+                    .filterIsInstance<Inet4Address>()
+                    .mapNotNull { it.hostAddress }
+                    .associateWith { 32 }
+
+                val excludedRanges = WgVpnRoutes.vpnDefaultExcludedRoutes + controllerIPs
+                logcat { "Generating VPN routes dynamically, excluded ranges: $excludedRanges" }
+
+                WgVpnRoutes().generateVpnRoutes(excludedRanges)
+            } else {
+                WgVpnRoutes.wgVpnDefaultRoutes
+            }
+            routes.toMutableMap().apply {
                 fallbackDns().filterIsInstance<Inet4Address>().mapNotNull { it.hostAddress }.forEach { ip ->
                     this[ip] = 32
                 }
             }
         } else {
-            WgVpnRoutes.wgVpnRoutesIncludingLocal.toMutableMap().apply {
+            val routes = if (isLocalDnsEnabled) {
+                // get the controller IPs
+                val controllerIPs = vpnLocalDns.lookup(CONTROLLER_NETP_DUCKDUCKGO_COM)
+                    .filterIsInstance<Inet4Address>()
+                    .mapNotNull { it.hostAddress }
+                    .associateWith { 32 }
+
+                val excludedRanges = WgVpnRoutes.vpnExcludedSpecialRoutes + controllerIPs
+                logcat { "Generating VPN routes dynamically, excluded ranges: $excludedRanges" }
+
+                WgVpnRoutes().generateVpnRoutes(excludedRanges)
+            } else {
+                WgVpnRoutes.wgVpnRoutesIncludingLocal
+            }
+
+            routes.toMutableMap().apply {
                 fallbackDns().filterIsInstance<Inet4Address>().mapNotNull { it.hostAddress }.forEach { ip ->
                     this[ip] = 32
                 }
