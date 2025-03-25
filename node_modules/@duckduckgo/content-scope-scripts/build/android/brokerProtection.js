c7ea85bf5acf22e8903e2b02005829a3369d5e7b
Dax Mobile <44842493+daxmobile@users.noreply.github.com>
2025-03-25 10:40:24 +0000

    Update content scope scripts to version 8.5.0 (#5819)
    - Automated content scope scripts dependency update
    
    This PR updates the content scope scripts dependency to the latest
    available version and copies the necessary files.
    If tests have failed, see
    https://app.asana.com/0/1202561462274611/1203986899650836/f for further
    information on what to do next.
    
    - [x] All tests must pass
    
    Co-authored-by: daxmobile <daxmobile@users.noreply.github.com>

--- node_modules/@duckduckgo/content-scope-scripts/build/android/brokerProtection.js
+++ node_modules/@duckduckgo/content-scope-scripts/build/android/brokerProtection.js
@@ -1787,7 +1787,7 @@
   function isGloballyDisabled(args) {
     return args.site.allowlisted || args.site.isBroken;
   }
-  var platformSpecificFeatures = ["windowsPermissionUsage", "messageBridge"];
+  var platformSpecificFeatures = ["windowsPermissionUsage", "messageBridge", "favicon"];
   function isPlatformSpecificFeature(featureName) {
     return platformSpecificFeatures.includes(featureName);
   }
@@ -1833,12 +1833,13 @@
       "brokerProtection",
       "performanceMetrics",
       "breakageReporting",
-      "autofillPasswordImport"
+      "autofillPasswordImport",
+      "favicon"
     ]
   );
   var platformSupport = {
     apple: ["webCompat", ...baseFeatures],
-    "apple-isolated": ["duckPlayer", "brokerProtection", "performanceMetrics", "clickToLoad", "messageBridge"],
+    "apple-isolated": ["duckPlayer", "brokerProtection", "performanceMetrics", "clickToLoad", "messageBridge", "favicon"],
     android: [...baseFeatures, "webCompat", "breakageReporting", "duckPlayer", "messageBridge"],
     "android-broker-protection": ["brokerProtection"],
     "android-autofill-password-import": ["autofillPasswordImport"],
