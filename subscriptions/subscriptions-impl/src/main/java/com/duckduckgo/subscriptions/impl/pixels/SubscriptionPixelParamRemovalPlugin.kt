639ef0f8bb02d3070930cbb0dba9678beddbb00b
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2025-04-02 13:13:16 +0200

    make SubscriptionPixel remove all params by default

--- subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/pixels/SubscriptionPixelParamRemovalPlugin.kt
+++ subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/pixels/SubscriptionPixelParamRemovalPlugin.kt
@@ -20,15 +20,15 @@ import com.duckduckgo.common.utils.plugins.pixel.PixelParamRemovalPlugin
 import com.duckduckgo.common.utils.plugins.pixel.PixelParamRemovalPlugin.PixelParameter
 import com.duckduckgo.di.scopes.AppScope
 import com.squareup.anvil.annotations.ContributesMultibinding
+import java.util.EnumSet
 import javax.inject.Inject
 
 @ContributesMultibinding(AppScope::class)
 class SubscriptionPixelParamRemovalPlugin @Inject constructor() : PixelParamRemovalPlugin {
     override fun names(): List<Pair<String, Set<PixelParameter>>> {
         return SubscriptionPixel.entries
-            .filter { pixel -> pixel.removedParameters.isNotEmpty() }
-            .map { pixel -> pixel.baseName to pixel.removedParameters }
-            .toList()
+            .map { pixel -> pixel.baseName to EnumSet.complementOf(EnumSet.copyOf(pixel.includedParameters)) }
+            .filter { (_, removedParameters) -> removedParameters.isNotEmpty() }
             .plus("m_ppro_feedback" to PixelParameter.removeAtb())
     }
 }
