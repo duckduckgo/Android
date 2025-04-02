bc6214d1c736e3b3c8aab18cdaa7d79d78273294
Noelia Alcala <nalcalag@gmail.com>
2025-04-02 17:38:47 +0100

    Update ppro upsell origins

--- app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/view/message/PproUpsellRevokedMessagePlugin.kt
+++ app-tracking-protection/vpn-impl/src/main/java/com/duckduckgo/mobile/android/vpn/ui/tracker_activity/view/message/PproUpsellRevokedMessagePlugin.kt
@@ -73,6 +73,6 @@ class PproUpsellRevokedMessagePlugin @Inject constructor(
     companion object {
         internal const val PRIORITY_PPRO_REVOKED = PRIORITY_REVOKED - 1
         private const val PPRO_UPSELL_ANNOTATION = "ppro_upsell_link"
-        private const val PPRO_UPSELL_URL = "https://duckduckgo.com/pro?origin=funnel_pro_android_apptp_revoked"
+        private const val PPRO_UPSELL_URL = "https://duckduckgo.com/pro?origin=funnel_apptrackingprotection_android__revoked"
     }
 }
