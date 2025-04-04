9f86158198095eaac0e90a86a28001591394da6a
Aitor Viana <aitorvs@gmail.com>
2025-04-04 14:15:23 +0100

    remove DrmBlockFeatureExceptionStore

---
+++ site-permissions/site-permissions-impl/src/test/java/com/duckduckgo/site/permissions/impl/drmblock/RealDrmBlockRepositoryTest.kt
@@ -0,0 +1,53 @@
+package com.duckduckgo.site.permissions.impl.drmblock
+
+import com.duckduckgo.common.test.CoroutineTestRule
+import com.duckduckgo.feature.toggles.api.FakeFeatureToggleFactory
+import com.duckduckgo.feature.toggles.api.FeatureExceptions.FeatureException
+import com.duckduckgo.feature.toggles.api.Toggle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class RealDrmBlockRepositoryTest {
+    @get:Rule
+    var coroutineRule = CoroutineTestRule()
+
+    private val drmBlockFeature = FakeFeatureToggleFactory.create(DrmBlockFeature::class.java)
+
+    @Before
+    fun setup() {
+        drmBlockFeature.self().setRawStoredState(Toggle.State(exceptions = exceptions))
+    }
+
+    @Test
+    fun whenRepositoryIsCreatedThenExceptionsLoadIntoMemory() = runTest {
+        val repository = RealDrmBlockRepository(
+            drmBlockFeature,
+            coroutineRule.testScope,
+            coroutineRule.testDispatcherProvider,
+            true,
+        )
+        assertEquals(exceptions, repository.exceptions)
+    }
+
+    @Test
+    fun whenRemoteConfigUpdateThenExceptionsUpdated() = runTest {
+        val repository = RealDrmBlockRepository(
+            drmBlockFeature,
+            coroutineRule.testScope,
+            coroutineRule.testDispatcherProvider,
+            true,
+        )
+
+        assertEquals(exceptions, repository.exceptions)
+        drmBlockFeature.self().setRawStoredState(Toggle.State(exceptions = emptyList()))
+        repository.onPrivacyConfigDownloaded()
+        assertEquals(emptyList<FeatureException>(), repository.exceptions)
+    }
+
+    companion object {
+        val exceptions = listOf(FeatureException("example.com", "reason"))
+    }
+}
