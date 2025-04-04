0a37a6d2c2178e4844e7403a2d7e74ae283f8cc8
Aitor Viana <aitorvs@gmail.com>
2025-04-04 14:15:23 +0100

    remove DrmBlockFeatureExceptionStore

--- site-permissions/site-permissions-store/src/main/java/com/duckduckgo/site/permissions/store/SitePermissionsDatabase.kt
+++ site-permissions/site-permissions-store/src/main/java/com/duckduckgo/site/permissions/store/SitePermissionsDatabase.kt
@@ -20,8 +20,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import com.duckduckgo.site.permissions.store.drmblock.DrmBlockDao
-import com.duckduckgo.site.permissions.store.drmblock.DrmBlockExceptionEntity
 import com.duckduckgo.site.permissions.store.sitepermissions.SitePermissionsDao
 import com.duckduckgo.site.permissions.store.sitepermissions.SitePermissionsEntity
 import com.duckduckgo.site.permissions.store.sitepermissionsallowed.SitePermissionAllowedEntity
@@ -29,18 +27,16 @@ import com.duckduckgo.site.permissions.store.sitepermissionsallowed.SitePermissi
 
 @Database(
     exportSchema = true,
-    version = 4,
+    version = 5,
     entities = [
         SitePermissionsEntity::class,
         SitePermissionAllowedEntity::class,
-        DrmBlockExceptionEntity::class,
     ],
 )
 
 abstract class SitePermissionsDatabase : RoomDatabase() {
     abstract fun sitePermissionsDao(): SitePermissionsDao
     abstract fun sitePermissionsAllowedDao(): SitePermissionsAllowedDao
-    abstract fun drmBlockDao(): DrmBlockDao
 }
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
@@ -55,4 +51,10 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
     }
 }
 
-val ALL_MIGRATIONS = arrayOf(MIGRATION_1_2, MIGRATION_3_4)
+val MIGRATION_4_5 = object : Migration(4, 5) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DROP TABLE IF EXISTS `drm_block_exceptions`")
+    }
+}
+
+val ALL_MIGRATIONS = arrayOf(MIGRATION_1_2, MIGRATION_3_4, MIGRATION_4_5)
