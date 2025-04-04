78b40a633ded05ae3ac3fecfb3935b9a81699c11
Aitor Viana <aitorvs@gmail.com>
2025-04-04 13:05:35 +0100

    Remove AutoconsentExceptionsStore

--- autoconsent/autoconsent-impl/src/test/java/com/duckduckgo/autoconsent/impl/remoteconfig/RealAutoconsentExceptionsRepositoryTest.kt
+++ autoconsent/autoconsent-impl/src/test/java/com/duckduckgo/autoconsent/impl/remoteconfig/RealAutoconsentExceptionsRepositoryTest.kt
@@ -16,74 +16,56 @@
 
 package com.duckduckgo.autoconsent.impl.remoteconfig
 
-import com.duckduckgo.autoconsent.impl.store.AutoconsentDao
-import com.duckduckgo.autoconsent.impl.store.AutoconsentDatabase
-import com.duckduckgo.autoconsent.impl.store.AutoconsentExceptionEntity
-import com.duckduckgo.autoconsent.impl.store.toFeatureException
 import com.duckduckgo.common.test.CoroutineTestRule
+import com.duckduckgo.feature.toggles.api.FakeFeatureToggleFactory
+import com.duckduckgo.feature.toggles.api.FeatureExceptions.FeatureException
+import com.duckduckgo.feature.toggles.api.Toggle
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentMatchers.anyList
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.reset
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
 
 class RealAutoconsentExceptionsRepositoryTest {
 
     @get:Rule
     var coroutineRule = CoroutineTestRule()
 
-    private val mockDatabase: AutoconsentDatabase = mock()
-    private val mockDao: AutoconsentDao = mock()
-
-    lateinit var repository: AutoconsentExceptionsRepository
+    private val autoconsentFeature = FakeFeatureToggleFactory.create(AutoconsentFeature::class.java)
 
     @Before
-    fun before() {
-        whenever(mockDatabase.autoconsentDao()).thenReturn(mockDao)
+    fun setup() {
+        autoconsentFeature.self().setRawStoredState(Toggle.State(exceptions = listOf(exception)))
     }
 
     @Test
-    fun whenRepositoryIsCreatedThenExceptionsLoadedIntoMemory() {
-        givenDaoContainsExceptions()
-
-        repository = RealAutoconsentExceptionsRepository(TestScope(), coroutineRule.testDispatcherProvider, mockDatabase, isMainProcess = true)
-
-        assertEquals(exception.toFeatureException(), repository.exceptions.first())
+    fun whenRepositoryIsCreatedThenExceptionsLoadedIntoMemory() = runTest {
+        val repository = RealAutoconsentExceptionsRepository(
+            TestScope(),
+            coroutineRule.testDispatcherProvider,
+            autoconsentFeature,
+            isMainProcess = true,
+        )
+        assertEquals(exception, repository.exceptions.first())
     }
 
     @Test
-    fun whenUpdateAllThenUpdateAllCalled() = runTest {
-        repository = RealAutoconsentExceptionsRepository(TestScope(), coroutineRule.testDispatcherProvider, mockDatabase, isMainProcess = true)
+    fun whenRemoteConfigUpdateThenExceptionsUpdated() = runTest {
+        val repository = RealAutoconsentExceptionsRepository(
+            TestScope(),
+            coroutineRule.testDispatcherProvider,
+            autoconsentFeature,
+            isMainProcess = true,
+        )
 
-        repository.insertAllExceptions(listOf())
-
-        verify(mockDao).updateAllExceptions(anyList())
-    }
-
-    @Test
-    fun whenUpdateAllThenPreviousExceptionsAreCleared() = runTest {
-        givenDaoContainsExceptions()
-        repository = RealAutoconsentExceptionsRepository(TestScope(), coroutineRule.testDispatcherProvider, mockDatabase, isMainProcess = true)
-
-        assertEquals(1, repository.exceptions.size)
-        reset(mockDao)
-
-        repository.insertAllExceptions(listOf())
-
-        assertEquals(0, repository.exceptions.size)
-    }
-
-    private fun givenDaoContainsExceptions() {
-        whenever(mockDao.getExceptions()).thenReturn(listOf(exception))
+        assertEquals(listOf(exception), repository.exceptions)
+        autoconsentFeature.self().setRawStoredState(Toggle.State(exceptions = emptyList()))
+        repository.onPrivacyConfigDownloaded()
+        assertEquals(emptyList<FeatureException>(), repository.exceptions)
     }
 
     companion object {
-        val exception = AutoconsentExceptionEntity("example.com", "reason")
+        val exception = FeatureException("example.com", "reason")
     }
 }
