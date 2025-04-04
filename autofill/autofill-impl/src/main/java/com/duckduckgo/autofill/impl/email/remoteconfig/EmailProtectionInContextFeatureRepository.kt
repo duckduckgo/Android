254e7df5d0712525a32971748bcebba1d5677d7f
Aitor Viana <aitorvs@gmail.com>
2025-04-04 16:11:49 +0100

    remove EmailProtectionInContextRemoteExceptionsPersister

---
+++ autofill/autofill-impl/src/main/java/com/duckduckgo/autofill/impl/email/remoteconfig/EmailProtectionInContextFeatureRepository.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2025 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.autofill.impl.email.remoteconfig
+
+import com.duckduckgo.app.di.AppCoroutineScope
+import com.duckduckgo.app.di.IsMainProcess
+import com.duckduckgo.autofill.impl.email.incontext.EmailProtectionInContextSignupFeature
+import com.duckduckgo.common.utils.DispatcherProvider
+import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.privacy.config.api.PrivacyConfigCallbackPlugin
+import com.squareup.anvil.annotations.ContributesBinding
+import com.squareup.anvil.annotations.ContributesMultibinding
+import dagger.SingleInstanceIn
+import java.util.concurrent.CopyOnWriteArrayList
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+interface EmailProtectionInContextFeatureRepository {
+    val exceptions: CopyOnWriteArrayList<String>
+}
+
+@ContributesBinding(
+    scope = AppScope::class,
+    boundType = EmailProtectionInContextFeatureRepository::class,
+)
+@ContributesMultibinding(
+    scope = AppScope::class,
+    boundType = PrivacyConfigCallbackPlugin::class,
+)
+@SingleInstanceIn(AppScope::class)
+class RealEmailProtectionInContextFeatureRepository @Inject constructor(
+    private val feature: EmailProtectionInContextSignupFeature,
+    @AppCoroutineScope private val coroutineScope: CoroutineScope,
+    private val dispatcherProvider: DispatcherProvider,
+    @IsMainProcess private val isMainProcess: Boolean,
+) : EmailProtectionInContextFeatureRepository, PrivacyConfigCallbackPlugin {
+
+    override val exceptions = CopyOnWriteArrayList<String>()
+
+    init {
+        loadToMemory()
+    }
+
+    override fun onPrivacyConfigDownloaded() {
+        loadToMemory()
+    }
+
+    private fun loadToMemory() {
+        coroutineScope.launch(dispatcherProvider.io()) {
+            if (isMainProcess) {
+                exceptions.clear()
+                exceptions.addAll(feature.self().getExceptions().map { it.domain })
+            }
+        }
+    }
+}
