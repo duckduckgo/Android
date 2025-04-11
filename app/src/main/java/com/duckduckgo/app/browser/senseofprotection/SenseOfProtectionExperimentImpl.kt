a9a336dbd9e9e2379f44bcf9c4af2c0b8d107cab
Mike Scamell <mscamell@duckduckgo.com>
2025-04-11 11:14:29 +0100

    Implement Sense of Protection experiment logic and tests

---
+++ app/src/main/java/com/duckduckgo/app/browser/senseofprotection/SenseOfProtectionExperimentImpl.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2025 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.browser.senseofprotection
+
+import com.duckduckgo.app.browser.senseofprotection.SenseOfProtectionToggles.Cohorts.MODIFIED_CONTROL
+import com.duckduckgo.browser.api.UserBrowserProperties
+import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.feature.toggles.api.Toggle.State.CohortName
+import com.squareup.anvil.annotations.ContributesBinding
+import dagger.SingleInstanceIn
+import javax.inject.Inject
+
+private const val EXISTING_USER_DAY_COUNT_THRESHOLD = 28
+
+interface SenseOfProtectionExperiment {
+
+    fun isEnabled(cohort: CohortName): Boolean
+}
+
+@ContributesBinding(
+    scope = AppScope::class,
+    boundType = SenseOfProtectionExperiment::class,
+)
+@SingleInstanceIn(AppScope::class)
+class SenseOfProtectionExperimentImpl @Inject constructor(
+    private val userBrowserProperties: UserBrowserProperties,
+    private val senseOfProtectionToggles: SenseOfProtectionToggles,
+) : SenseOfProtectionExperiment {
+
+    override fun isEnabled(cohortName: CohortName): Boolean {
+        return if (userBrowserProperties.daysSinceInstalled() > EXISTING_USER_DAY_COUNT_THRESHOLD) {
+            // A user might have already been enrolled in the new user experiment so we need to check they are not part of a cohort before we can
+            // enroll them into the existing user experiment
+            if (isNotEnrolledInNewUserExperiment()) {
+                isExistingUserExperimentEnabled(cohortName)
+            } else {
+                false
+            }
+        } else {
+            isNewUserExperimentEnabled(cohortName)
+        }
+    }
+
+    private fun isNotEnrolledInNewUserExperiment(): Boolean =
+        senseOfProtectionToggles.senseOfProtectionNewUserExperimentApr25().getCohort() == null
+
+    private fun isNewUserExperimentEnabled(cohortName: CohortName): Boolean =
+        senseOfProtectionToggles.senseOfProtectionNewUserExperimentApr25().isEnabled(cohort = cohortName)
+
+    private fun isExistingUserExperimentEnabled(cohortName: CohortName): Boolean =
+        senseOfProtectionToggles.senseOfProtectionExistingUserExperimentApr25().isEnabled(cohort = cohortName)
+}
