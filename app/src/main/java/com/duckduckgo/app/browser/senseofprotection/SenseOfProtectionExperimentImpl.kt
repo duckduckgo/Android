e5cd8b6d15a3ebf988f1fc9f7d9a08c20e277d3d
Mike Scamell <mscamell@duckduckgo.com>
2025-04-16 17:51:39 +0100

    Enrol new users when "Try a Search" CTA is visible
    This will be just before the user potentially sees the new trackers blocked animation.
    
    We can remove the isEnabled function now as it's not needed and repurpose it.

--- app/src/main/java/com/duckduckgo/app/browser/senseofprotection/SenseOfProtectionExperimentImpl.kt
+++ app/src/main/java/com/duckduckgo/app/browser/senseofprotection/SenseOfProtectionExperimentImpl.kt
@@ -19,7 +19,6 @@ package com.duckduckgo.app.browser.senseofprotection
 import com.duckduckgo.app.browser.senseofprotection.SenseOfProtectionToggles.Cohorts.MODIFIED_CONTROL
 import com.duckduckgo.app.browser.senseofprotection.SenseOfProtectionToggles.Cohorts.VARIANT_2
 import com.duckduckgo.app.di.AppCoroutineScope
-import com.duckduckgo.app.lifecycle.MainProcessLifecycleObserver
 import com.duckduckgo.app.statistics.pixels.Pixel
 import com.duckduckgo.browser.api.UserBrowserProperties
 import com.duckduckgo.common.utils.DispatcherProvider
@@ -36,7 +35,7 @@ private const val EXISTING_USER_DAY_COUNT_THRESHOLD = 28
 
 interface SenseOfProtectionExperiment {
 
-    fun isEnabled(cohort: CohortName): Boolean
+    fun enrolUserInNewExperimentIfEligible(): Boolean
     fun getTabManagerPixelParams(): Map<String, String>
     fun firePrivacyDashboardClickedPixelIfInExperiment()
     fun isUserEnrolledInVariant2CohortAndExperimentEnabled(): Boolean
@@ -65,17 +64,11 @@ class SenseOfProtectionExperimentImpl @Inject constructor(
         }
     }
 
-    override fun isEnabled(cohortName: CohortName): Boolean {
-        return if (userBrowserProperties.daysSinceInstalled() > EXISTING_USER_DAY_COUNT_THRESHOLD) {
-            // A user might have already been enrolled in the new user experiment so we need to check they are not part of a cohort before we can
-            // enroll them into the existing user experiment
-            if (isNotEnrolledInNewUserExperiment()) {
-                isExistingUserExperimentEnabled(cohortName)
-            } else {
-                false
-            }
+    override fun enrolUserInNewExperimentIfEligible(): Boolean {
+        return if (userBrowserProperties.daysSinceInstalled() <= EXISTING_USER_DAY_COUNT_THRESHOLD) {
+            isNewUserExperimentEnabled(cohortName = MODIFIED_CONTROL)
         } else {
-            isNewUserExperimentEnabled(cohortName)
+            false
         }
     }
 
