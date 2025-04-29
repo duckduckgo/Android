3e9e495bc4ae8ebc92ae8e2b8de4eecf7f6b90ae
0nko <oruttkay@duckduckgo.com>
2025-04-29 18:56:50 +0200

    Implement copy link text functionality

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -4312,8 +4312,20 @@ class BrowserTabFragment :
         return message.data.getString(URL_BUNDLE_KEY)
     }
 
-    private fun getLongPressTarget(hitTestResult: HitTestResult): LongPressTarget? =
-        when {
+    /**
+     * Use requestFocusNodeHref to get the a tag title for the touched link.
+     */
+    private fun getTargetTitleForLinkSource(): String? {
+        val handler = Handler()
+        val message = handler.obtainMessage()
+
+        webView?.requestFocusNodeHref(message)
+
+        return message.data.getString(TITLE_BUNDLE_KEY)?.trim()
+    }
+
+    private fun getLongPressTarget(hitTestResult: HitTestResult): LongPressTarget? {
+        return when {
             hitTestResult.extra == null -> null
             hitTestResult.type == UNKNOWN_TYPE -> null
             hitTestResult.type == IMAGE_TYPE ->
@@ -4330,11 +4342,11 @@ class BrowserTabFragment :
                     type = hitTestResult.type,
                 )
 
-            else ->
-                LongPressTarget(
-                    url = hitTestResult.extra,
-                    type = hitTestResult.type,
-                )
+            else -> LongPressTarget(
+                url = hitTestResult.extra,
+                type = hitTestResult.type,
+                text = getTargetTitleForLinkSource(),
+            )
         }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
@@ -4911,6 +4923,7 @@ class BrowserTabFragment :
         private const val PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 200
 
         private const val URL_BUNDLE_KEY = "url"
+        private const val TITLE_BUNDLE_KEY = "title"
 
         private const val DOWNLOAD_CONFIRMATION_TAG = "DOWNLOAD_CONFIRMATION_TAG"
         private const val DAX_DIALOG_DIALOG_TAG = "DAX_DIALOG_TAG"
--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -2939,8 +2939,8 @@ class BrowserTabViewModel @Inject constructor(
         target: LongPressTarget,
         menu: ContextMenu,
     ) {
-        logcat(INFO) { "Long pressed on ${target.type}, (url=${target.url}), (image url = ${target.imageUrl})" }
-        longPressHandler.handleLongPress(target.type, target.url, menu)
+        logcat(INFO) { "Long pressed on ${target.type}, (url=${target.url}), (text=${target.url}) (image url = ${target.imageUrl})" }
+        longPressHandler.handleLongPress(target.type, target.url, target.text, menu)
     }
 
     fun userSelectedItemFromLongPressMenu(
@@ -2983,6 +2983,11 @@ class BrowserTabViewModel @Inject constructor(
 
             is RequiredAction.CopyLink -> {
                 command.value = CopyLink(requiredAction.url)
+                true
+            }
+
+            is RequiredAction.CopyLinkText -> {
+                command.value = CopyLink(requiredAction.text)
                 true
             }
 
