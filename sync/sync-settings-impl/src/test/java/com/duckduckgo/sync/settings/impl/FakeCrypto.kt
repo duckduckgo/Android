60293c8054ca30bbe56cd031be7dbe421dada549
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-04-30 12:37:14 +0200

    Fix tests

--- sync/sync-settings-impl/src/test/java/com/duckduckgo/sync/settings/impl/FakeCrypto.kt
+++ sync/sync-settings-impl/src/test/java/com/duckduckgo/sync/settings/impl/FakeCrypto.kt
@@ -19,7 +19,7 @@ package com.duckduckgo.sync.settings.impl
 import com.duckduckgo.sync.api.*
 
 class FakeCrypto : SyncCrypto {
-    override fun encrypt(text: String) = text
+    override suspend fun encrypt(text: String) = text
 
-    override fun decrypt(data: String) = data
+    override suspend fun decrypt(data: String) = data
 }
