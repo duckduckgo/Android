9df3b2c353a6569ef2551aceb73c917f9c17a286
Ana Capatina <anikiki@gmail.com>
2025-05-01 12:43:14 +0100

    Added lint-baseline.xml file to the dax-prompts-impl module.

--- dax-prompts/dax-prompts-impl/src/test/java/com/duckduckgo/daxprompts/impl/ui/DaxPromptDuckPlayerViewModelTest.kt
+++ dax-prompts/dax-prompts-impl/src/test/java/com/duckduckgo/daxprompts/impl/ui/DaxPromptDuckPlayerViewModelTest.kt
@@ -26,13 +26,10 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
-import org.robolectric.RobolectricTestRunner
 
 @ExperimentalCoroutinesApi
-@RunWith(RobolectricTestRunner::class)
 class DaxPromptDuckPlayerViewModelTest {
 
     @get:Rule
