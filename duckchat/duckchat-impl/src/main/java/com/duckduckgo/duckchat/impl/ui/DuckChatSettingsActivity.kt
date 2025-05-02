1f133352d9a579078a19c44681c3048a70a210d1
joshliebe <joshliebe@gmail.com>
2025-05-03 00:26:31 +0100

    Rename screenName

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/DuckChatSettingsActivity.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/DuckChatSettingsActivity.kt
@@ -43,7 +43,7 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 @InjectWith(ActivityScope::class)
-@ContributeToActivityStarter(DuckChatSettingsNoParams::class, screenName = "duck.ai")
+@ContributeToActivityStarter(DuckChatSettingsNoParams::class, screenName = "duck.ai_settings")
 class DuckChatSettingsActivity : DuckDuckGoActivity() {
 
     private val viewModel: DuckChatSettingsViewModel by bindViewModel()
