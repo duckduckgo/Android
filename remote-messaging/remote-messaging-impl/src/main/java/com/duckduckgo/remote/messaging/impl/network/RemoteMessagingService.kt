7200cd02752d0bb89005912e640977fd4d571625
David González <malmstein@gmail.com>
2025-05-09 10:42:16 +0200

    Visual updates: add shadows in New Tab scrolling

--- remote-messaging/remote-messaging-impl/src/main/java/com/duckduckgo/remote/messaging/impl/network/RemoteMessagingService.kt
+++ remote-messaging/remote-messaging-impl/src/main/java/com/duckduckgo/remote/messaging/impl/network/RemoteMessagingService.kt
@@ -23,6 +23,6 @@ import retrofit2.http.GET
 
 @ContributesServiceApi(AppScope::class)
 interface RemoteMessagingService {
-    @GET("https://staticcdn.duckduckgo.com/remotemessaging/config/v1/android-config.json")
+    @GET("https://www.jsonblob.com/api/1369705136011796480")
     suspend fun config(): JsonRemoteMessagingConfig
 }
