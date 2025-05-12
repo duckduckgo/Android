140587df63aae9563df9fdfea7a0f58ba7c840e0
Cristian Monforte <cristian@duckduckgo.com>
2025-05-12 12:47:52 +0200

    Re-enable Privacy Test cases (#6042)
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1202552961248957/task/1210170077557891?focus=true
    
    ### Description
    Tries to fix issues on privacy tests so we can re-enable them again.
    Also updates workflow triggers so they run if CSS path changes.
    
    
    ### Steps to test this PR
    
    Should pass ->
    https://github.com/duckduckgo/Android/actions/runs/14960434017
    
    ### UI changes
    | Before  | After |
    | ------ | ----- |
    !(Upload before screenshot)|(Upload after screenshot)|

--- app/src/androidTest/java/com/duckduckgo/espresso/JsObjectIdlingResource.kt
+++ app/src/androidTest/java/com/duckduckgo/espresso/JsObjectIdlingResource.kt
@@ -18,9 +18,9 @@ package com.duckduckgo.espresso
 
 import android.os.Handler
 import android.os.Looper
+import android.os.SystemClock
 import android.webkit.WebView
 import androidx.test.espresso.IdlingResource
-import androidx.test.espresso.IdlingResource.ResourceCallback
 
 class JsObjectIdlingResource(
     private val webView: WebView,
@@ -34,6 +34,8 @@ class JsObjectIdlingResource(
 
     private val handler = Handler(Looper.getMainLooper())
     private val checkInterval = 100L // milliseconds
+    private val timeoutMillis = 20_000L // 20 seconds
+    private val startTime = SystemClock.elapsedRealtime()
 
     override fun getName(): String = "JsObjectIdlingResource for $objectName"
 
@@ -45,6 +47,13 @@ class JsObjectIdlingResource(
     }
 
     private fun pollForJsObject() {
+        if (SystemClock.elapsedRealtime() - startTime > timeoutMillis) {
+            isIdle = true
+            callback?.onTransitionToIdle()
+            // fail test if the object is not found within the timeout
+            throw AssertionError("JS object '$objectName' did not appear within timeout.")
+        }
+
         webView.evaluateJavascript(
             "(typeof $objectName !== 'undefined')",
         ) { result ->
