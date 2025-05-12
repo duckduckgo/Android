duckduckgo/Android

commit 721fe39889b008f2b5429bff287ce1c546ec4609

Visual updates: pixels (#6039)
Task/Issue URL:
https://app.asana.com/1/137249556945/task/1210172444512883

### Description
This PR adds the missing pixels for the engagement with the new browser
navigation

### Steps to test this PR

_List of pixels to validate_
- [x] New Tab button on browser navigation bar ->
`m_browser_nav_new_tab_pressed`
- [x] Bookmarks button on bottom toolbar ->
`m_browser_nav_bookmarks_pressed`
- [x] Passwords button on bottom toolbar ->
`m_browser_nav_passwords_pressed `
- [x] Duck.ai entry from address bar ->
`aichat_experiment_searchbar_button_open`
- [x] Dismiss keyboard in New Tab Page (back arrow in focused state) ->
`m_addressbar_focus_close_ntp`
- [x] Dismiss keyboard in SERP Page (back arrow in focused state) ->
`m_addressbar_focus_close_serp`
- [x] Dismiss keyboard in Website Page (back arrow in focused state) ->
`m_addressbar_focus_close_website`
- [x] Firebutton -> `m_browser_nav_fire_pressed `
- [x] Tab Manager -> `m_browser_nav_tabs_pressed `
- [x] Tab Manager Long Press -> `m_browser_nav_tabs_long_pressed `
- [x] Menu -> `m_browser_nav_menu_pressed `

---
- To see the specific tasks where the Asana app for GitHub is being
used, see below:
  - https://app.asana.com/0/0/1210172444512883

app/src/main/java/com/duckduckgo/app/browser/navigation/bar/view/BrowserNavigationBarViewModel.kt
@@ -26,11 +26,13 @@ import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarView.V
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarView.ViewMode.NewTab
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyAutofillButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyBookmarksButtonClicked
-import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyFireButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyMenuButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyNewTabButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyTabsButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyTabsButtonLongClicked
+import com.duckduckgo.app.pixels.AppPixelName
+import com.duckduckgo.app.statistics.pixels.Pixel
+import com.duckduckgo.app.statistics.pixels.Pixel.PixelParameter.FIRE_BUTTON_STATE
 import com.duckduckgo.app.tabs.model.TabRepository
 import com.duckduckgo.common.ui.experiments.visual.store.VisualDesignExperimentDataStore
 import com.duckduckgo.common.utils.DispatcherProvider
@@ -52,6 +54,7 @@ import kotlinx.coroutines.flow.update
 class BrowserNavigationBarViewModel @Inject constructor(
     private val visualDesignExperimentDataStore: VisualDesignExperimentDataStore,
     private val tabRepository: TabRepository,
+    private val pixel: Pixel,
     private val dispatcherProvider: DispatcherProvider,
 ) : ViewModel(), DefaultLifecycleObserver {
     private val _commands = Channel<Command>(capacity = Channel.CONFLATED)
@@ -73,30 +76,39 @@ class BrowserNavigationBarViewModel @Inject constructor(
     }.flowOn(dispatcherProvider.io()).stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), ViewState())
 
     fun onFireButtonClicked() {
-        _commands.trySend(NotifyFireButtonClicked)
+        pixel.fire(
+            AppPixelName.BROWSER_NAV_FIRE_PRESSED.pixelName,
+            mapOf(FIRE_BUTTON_STATE to _viewState.value.fireButtonHighlighted.toString()),
+        )
     }
 
     fun onTabsButtonClicked() {
+        pixel.fire(AppPixelName.BROWSER_NAV_TABS_PRESSED.pixelName)
         _commands.trySend(NotifyTabsButtonClicked)
     }
 
     fun onTabsButtonLongClicked() {
+        pixel.fire(AppPixelName.BROWSER_NAV_TABS_LONG_PRESSED.pixelName)
         _commands.trySend(NotifyTabsButtonLongClicked)
     }
 
     fun onMenuButtonClicked() {
+        pixel.fire(AppPixelName.BROWSER_NAV_MENU_PRESSED.pixelName)
         _commands.trySend(NotifyMenuButtonClicked)
     }
 
     fun onNewTabButtonClicked() {
+        pixel.fire(AppPixelName.BROWSER_NAV_NEW_TAB_PRESSED.pixelName)
         _commands.trySend(NotifyNewTabButtonClicked)
     }
 
     fun onAutofillButtonClicked() {
+        pixel.fire(AppPixelName.BROWSER_NAV_PASSWORDS_PRESSED.pixelName)
         _commands.trySend(NotifyAutofillButtonClicked)
     }
 
     fun onBookmarksButtonClicked() {
+        pixel.fire(AppPixelName.BROWSER_NAV_BOOKMARKS_PRESSED.pixelName)
         _commands.trySend(NotifyBookmarksButtonClicked)
     }
 

app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
@@ -5987,10 +5987,9 @@ class BrowserTabViewModelTest {
     }
 
     @Test
-    fun whenOnDuckChatOmnibarButtonClickedThenOpenDuckChatAndSendPixel() {
+    fun whenOnDuckChatOmnibarButtonClickedThenOpenDuckChat() {
         testee.onDuckChatOmnibarButtonClicked("example")
         verify(mockDuckChat).openDuckChatWithAutoPrompt("example")
-        verify(mockPixel).fire(DuckChatPixelName.DUCK_CHAT_SEARCHBAR_BUTTON_OPEN)
     }
 
     @Test

app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -4064,7 +4064,6 @@ class BrowserTabViewModel @Inject constructor(
 
     fun onDuckChatOmnibarButtonClicked(query: String?) {
         openDuckChat(query = query)
-        pixel.fire(DuckChatPixelName.DUCK_CHAT_SEARCHBAR_BUTTON_OPEN)
     }
 
     private fun openDuckChat(pixelName: Pixel.PixelName? = null, query: String? = null) {

app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarLayout.kt
@@ -461,6 +461,7 @@ open class OmnibarLayout @JvmOverloads constructor(
             omnibarItemPressedListener?.onBrowserMenuPressed()
         }
         aiChatMenu?.setOnClickListener {
+            viewModel.onDuckChatButtonPressed()
             omnibarItemPressedListener?.onDuckChatButtonPressed()
         }
         shieldIcon.setOnClickListener {

app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarLayoutViewModel.kt
@@ -57,6 +57,7 @@ import com.duckduckgo.common.ui.experiments.visual.store.VisualDesignExperimentD
 import com.duckduckgo.common.utils.DispatcherProvider
 import com.duckduckgo.di.scopes.FragmentScope
 import com.duckduckgo.duckchat.api.DuckChat
+import com.duckduckgo.duckchat.impl.pixel.DuckChatPixelName
 import com.duckduckgo.duckplayer.api.DuckPlayer
 import com.duckduckgo.privacy.dashboard.impl.pixels.PrivacyDashboardPixels
 import com.duckduckgo.voice.api.VoiceSearchAvailability
@@ -86,7 +87,7 @@ class OmnibarLayoutViewModel @Inject constructor(
     private val userBrowserProperties: UserBrowserProperties,
     private val dispatcherProvider: DispatcherProvider,
     private val defaultBrowserPromptsExperiment: DefaultBrowserPromptsExperiment,
-    visualDesignExperimentDataStore: VisualDesignExperimentDataStore,
+    private val visualDesignExperimentDataStore: VisualDesignExperimentDataStore,
     private val senseOfProtectionExperiment: SenseOfProtectionExperiment,
     private val duckChat: DuckChat,
 ) : ViewModel() {
@@ -405,11 +406,12 @@ class OmnibarLayoutViewModel @Inject constructor(
                 )
             }
         }
-
-        pixel.fire(
-            AppPixelName.MENU_ACTION_FIRE_PRESSED.pixelName,
-            mapOf(FIRE_BUTTON_STATE to pulseAnimationPlaying.toString()),
-        )
+        if (!_viewState.value.isVisualDesignExperimentEnabled) {
+            pixel.fire(
+                AppPixelName.MENU_ACTION_FIRE_PRESSED.pixelName,
+                mapOf(FIRE_BUTTON_STATE to pulseAnimationPlaying.toString()),
+            )
+        }
     }
 
     fun onPrivacyShieldButtonPressed() {
@@ -580,6 +582,15 @@ class OmnibarLayoutViewModel @Inject constructor(
         }
     }
 
+    fun onBackButtonPressed() {
+        Timber.d("Omnibar: onBackButtonPressed")
+        firePixelBasedOnCurrentUrl(
+            AppPixelName.ADDRESS_BAR_NEW_TAB_PAGE_CLOSED,
+            AppPixelName.ADDRESS_BAR_SERP_CLOSED,
+            AppPixelName.ADDRESS_BAR_WEBSITE_CLOSED,
+        )
+    }
+
     fun onEnterKeyPressed() {
         Timber.d("Omnibar: onEnterKeyPressed")
         firePixelBasedOnCurrentUrl(
@@ -697,6 +708,15 @@ class OmnibarLayoutViewModel @Inject constructor(
                     viewMode = customTabMode.copy(title = decoration.title),
                 )
             }
+        }
+    }
+
+    fun onDuckChatButtonPressed() {
+        val experimentEnabled = viewState.value.isVisualDesignExperimentEnabled
+        if (experimentEnabled) {
+            pixel.fire(DuckChatPixelName.DUCK_CHAT_EXPERIMENT_SEARCHBAR_BUTTON_OPEN)
+        } else {
+            pixel.fire(DuckChatPixelName.DUCK_CHAT_SEARCHBAR_BUTTON_OPEN)
         }
     }
 }

app/src/main/java/com/duckduckgo/app/browser/omnibar/experiments/FadeOmnibarLayout.kt
@@ -333,6 +333,7 @@ class FadeOmnibarLayout @JvmOverloads constructor(
     fun setFadeOmnibarItemPressedListener(itemPressedListener: FadeOmnibarItemPressedListener) {
         fadeOmnibarItemPressedListener = itemPressedListener
         backIcon.setOnClickListener {
+            viewModel.onBackButtonPressed()
             fadeOmnibarItemPressedListener?.onBackButtonPressed()
         }
     }

app/src/main/java/com/duckduckgo/app/pixels/AppPixelName.kt
@@ -334,6 +334,9 @@ enum class AppPixelName(override val pixelName: String) : Pixel.PixelName {
     ADDRESS_BAR_NEW_TAB_PAGE_CANCELLED("m_addressbar_focus_cancel_ntp"),
     ADDRESS_BAR_WEBSITE_CANCELLED("m_addressbar_focus_cancel_website"),
     ADDRESS_BAR_SERP_CANCELLED("m_addressbar_focus_cancel_serp"),
+    ADDRESS_BAR_NEW_TAB_PAGE_CLOSED("m_addressbar_focus_close_ntp"),
+    ADDRESS_BAR_WEBSITE_CLOSED("m_addressbar_focus_close_website"),
+    ADDRESS_BAR_SERP_CLOSED("m_addressbar_focus_close_serp"),
 
     KEYBOARD_GO_NEW_TAB_CLICKED("m_keyboard_go_click_ntp"),
     KEYBOARD_GO_WEBSITE_CLICKED("m_keyboard_go_click_website"),
@@ -435,4 +438,13 @@ enum class AppPixelName(override val pixelName: String) : Pixel.PixelName {
     TAB_MANAGER_INFO_PANEL_IMPRESSIONS("m_tab_manager_info_panel_impressions"),
     TAB_MANAGER_INFO_PANEL_DISMISSED("m_tab_manager_info_panel_dismissed"),
     TAB_MANAGER_INFO_PANEL_TAPPED("m_tab_manager_info_panel_tapped"),
+
+    BROWSER_NAV_NEW_TAB_PRESSED("m_browser_nav_new_tab_pressed"),
+    BROWSER_NAV_BOOKMARKS_PRESSED("m_browser_nav_bookmarks_pressed"),
+    BROWSER_NAV_PASSWORDS_PRESSED("m_browser_nav_passwords_pressed"),
+    BROWSER_NAV_AI_PRESSED("m_browser_nav_duck_ai_pressed"),
+    BROWSER_NAV_FIRE_PRESSED("m_browser_nav_fire_pressed"),
+    BROWSER_NAV_TABS_PRESSED("m_browser_nav_tabs_pressed"),
+    BROWSER_NAV_TABS_LONG_PRESSED("m_browser_nav_tabs_long_pressed"),
+    BROWSER_NAV_MENU_PRESSED("m_browser_nav_menu_pressed"),
 }

app/src/test/java/com/duckduckgo/app/browser/omnibar/OmnibarLayoutViewModelTest.kt
@@ -32,6 +32,7 @@ import com.duckduckgo.common.test.CoroutineTestRule
 import com.duckduckgo.common.ui.experiments.visual.store.VisualDesignExperimentDataStore
 import com.duckduckgo.common.ui.experiments.visual.store.VisualDesignExperimentDataStore.FeatureState
 import com.duckduckgo.duckchat.api.DuckChat
+import com.duckduckgo.duckchat.impl.pixel.DuckChatPixelName
 import com.duckduckgo.duckplayer.api.DuckPlayer
 import com.duckduckgo.privacy.dashboard.impl.pixels.PrivacyDashboardPixels
 import com.duckduckgo.voice.api.VoiceSearchAvailability
@@ -66,7 +67,8 @@ class OmnibarLayoutViewModelTest {
     private val userBrowserProperties: UserBrowserProperties = mock()
 
     private val mockVisualDesignExperimentDataStore: VisualDesignExperimentDataStore = mock()
-    private val defaultVisualExperimentNavBarStateFlow = MutableStateFlow(FeatureState(isAvailable = false, isEnabled = false))
+    private val disabledVisualExperimentNavBarStateFlow = MutableStateFlow(FeatureState(isAvailable = false, isEnabled = false))
+    private val enabledVisualExperimentNavBarStateFlow = MutableStateFlow(FeatureState(isAvailable = true, isEnabled = true))
 
     private val defaultBrowserPromptsExperimentHighlightOverflowMenuFlow = MutableStateFlow(false)
     private val defaultBrowserPromptsExperiment: DefaultBrowserPromptsExperiment = mock()
@@ -89,7 +91,7 @@ class OmnibarLayoutViewModelTest {
         whenever(tabRepository.flowTabs).thenReturn(flowOf(emptyList()))
         whenever(voiceSearchAvailability.shouldShowVoiceSearch(any(), any(), any(), any())).thenReturn(true)
         whenever(duckPlayer.isDuckPlayerUri(DUCK_PLAYER_URL)).thenReturn(true)
-        whenever(mockVisualDesignExperimentDataStore.experimentState).thenReturn(defaultVisualExperimentNavBarStateFlow)
+        whenever(mockVisualDesignExperimentDataStore.experimentState).thenReturn(disabledVisualExperimentNavBarStateFlow)
         whenever(duckChat.showInAddressBar).thenReturn(duckChatShowInAddressBarFlow)
 
         initializeViewModel()
@@ -1053,7 +1055,7 @@ class OmnibarLayoutViewModelTest {
 
     @Test
     fun whenNavigationBarExperimentEnabledThenShowChatMenuTrue() = runTest {
-        defaultVisualExperimentNavBarStateFlow.value = FeatureState(isAvailable = true, isEnabled = true)
+        disabledVisualExperimentNavBarStateFlow.value = FeatureState(isAvailable = true, isEnabled = true)
 
         testee.viewState.test {
             val viewState = awaitItem()
@@ -1064,7 +1066,7 @@ class OmnibarLayoutViewModelTest {
     @Test
     fun whenNavigationBarExperimentEnabledAndChatEntryPointDisabledThenShowChatMenuFalse() = runTest {
         duckChatShowInAddressBarFlow.value = false
-        defaultVisualExperimentNavBarStateFlow.value = FeatureState(isAvailable = true, isEnabled = true)
+        disabledVisualExperimentNavBarStateFlow.value = FeatureState(isAvailable = true, isEnabled = true)
 
         testee.viewState.test {
             val viewState = awaitItem()
@@ -1115,6 +1117,26 @@ class OmnibarLayoutViewModelTest {
             val viewState = awaitItem()
             assertTrue(viewState.showChatMenu)
         }
+    }
+
+    @Test
+    fun whenDuckChatButtonPressedAndExperimentEnabledThenPixelSent() = runTest {
+        whenever(mockVisualDesignExperimentDataStore.experimentState).thenReturn(enabledVisualExperimentNavBarStateFlow)
+        initializeViewModel()
+
+        testee.viewState.test {
+            val viewState = awaitItem()
+            assertTrue(viewState.isVisualDesignExperimentEnabled)
+            testee.onDuckChatButtonPressed()
+            verify(pixel).fire(DuckChatPixelName.DUCK_CHAT_EXPERIMENT_SEARCHBAR_BUTTON_OPEN)
+        }
+    }
+
+    @Test
+    fun whenDuckChatButtonPressedAndExperimentDisabledThenPixelSent() = runTest {
+        testee.onDuckChatButtonPressed()
+
+        verify(pixel).fire(DuckChatPixelName.DUCK_CHAT_SEARCHBAR_BUTTON_OPEN)
     }
 
     private fun givenSiteLoaded(loadedUrl: String) {

commit 4b4c6cf96e011ac5da7bcc8cfef10d00a8b55f78

fix fire button not responding to clicks (#6046)
Task/Issue URL:
https://app.asana.com/1/137249556945/project/715106103902962/task/1210224367817693?focus=true

### Description
Fixes a regression from https://github.com/duckduckgo/Android/pull/6039
where we disconnected the fire button handler from the
`BrowserNavigationBarViewModel`.

While I was there, I added unit test for all click handlers in the
bottom nav bar.

### Steps to test this PR

- [ ] Enable UI experiment.
- [ ] Verify that fire button clicks are handled.

app/src/main/java/com/duckduckgo/app/browser/navigation/bar/view/BrowserNavigationBarViewModel.kt
@@ -26,6 +26,7 @@ import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarView.V
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarView.ViewMode.NewTab
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyAutofillButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyBookmarksButtonClicked
+import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyFireButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyMenuButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyNewTabButtonClicked
 import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command.NotifyTabsButtonClicked
@@ -80,6 +81,7 @@ class BrowserNavigationBarViewModel @Inject constructor(
             AppPixelName.BROWSER_NAV_FIRE_PRESSED.pixelName,
             mapOf(FIRE_BUTTON_STATE to _viewState.value.fireButtonHighlighted.toString()),
         )
+        _commands.trySend(NotifyFireButtonClicked)
     }
 
     fun onTabsButtonClicked() {

app/src/test/java/com/duckduckgo/app/browser/navigation/bar/view/BrowserNavigationBarViewModelTest.kt
@@ -1,0 +1,137 @@
+/*
+ * Copyright (c) 2025 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.browser.navigation.bar.view
+
+import app.cash.turbine.test
+import com.duckduckgo.app.browser.navigation.bar.view.BrowserNavigationBarViewModel.Command
+import com.duckduckgo.app.statistics.pixels.Pixel
+import com.duckduckgo.app.tabs.model.TabRepository
+import com.duckduckgo.common.test.CoroutineTestRule
+import com.duckduckgo.common.ui.experiments.visual.store.VisualDesignExperimentDataStore
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class BrowserNavigationBarViewModelTest {
+
+    @get:Rule
+    val coroutineTestRule: CoroutineTestRule = CoroutineTestRule()
+
+    @Mock
+    private lateinit var visualDesignExperimentDataStoreMock: VisualDesignExperimentDataStore
+
+    @Mock
+    private lateinit var tabRepositoryMock: TabRepository
+
+    @Mock
+    private lateinit var pixelMock: Pixel
+
+    private lateinit var testee: BrowserNavigationBarViewModel
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+
+        testee = BrowserNavigationBarViewModel(
+            visualDesignExperimentDataStore = visualDesignExperimentDataStoreMock,
+            tabRepository = tabRepositoryMock,
+            pixel = pixelMock,
+            dispatcherProvider = coroutineTestRule.testDispatcherProvider,
+        )
+    }
+
+    @Test
+    fun `when Fire button clicked, then send view command`() = runTest {
+        testee.onFireButtonClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyFireButtonClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when Tabs button clicked, then send view command`() = runTest {
+        testee.onTabsButtonClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyTabsButtonClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when Tabs button long clicked, then send view command`() = runTest {
+        testee.onTabsButtonLongClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyTabsButtonLongClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when Menu button clicked, then send view command`() = runTest {
+        testee.onMenuButtonClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyMenuButtonClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when NewTab button clicked, then send view command`() = runTest {
+        testee.onNewTabButtonClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyNewTabButtonClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when Autofill button clicked, then send view command`() = runTest {
+        testee.onAutofillButtonClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyAutofillButtonClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `when Bookmarks button clicked, then send view command`() = runTest {
+        testee.onBookmarksButtonClicked()
+
+        testee.commands.test {
+            val command = awaitItem()
+            Assert.assertEquals(Command.NotifyBookmarksButtonClicked, command)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+}