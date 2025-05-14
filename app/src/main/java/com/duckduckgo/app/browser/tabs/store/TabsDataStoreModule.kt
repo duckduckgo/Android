a2d54ac685c2703bb238181edc74e43c8ef39b76
0nko <oruttkay@duckduckgo.com>
2025-05-14 11:06:25 +0200

    Add new tab state data store

---
+++ app/src/main/java/com/duckduckgo/app/browser/tabs/store/TabsDataStoreModule.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2024 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.browser.tabs.store
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.duckduckgo.di.scopes.AppScope
+import com.squareup.anvil.annotations.ContributesTo
+import dagger.Module
+import dagger.Provides
+import javax.inject.Qualifier
+
+@ContributesTo(AppScope::class)
+@Module
+object TabsDataStoreModule {
+
+    private val Context.tabsDataStore: DataStore<Preferences> by preferencesDataStore(
+        name = "browserTabs",
+    )
+
+    @Provides
+    @Tabs
+    fun provideBookmarksDataStore(context: Context): DataStore<Preferences> = context.tabsDataStore
+}
+
+@Qualifier
+internal annotation class Tabs
