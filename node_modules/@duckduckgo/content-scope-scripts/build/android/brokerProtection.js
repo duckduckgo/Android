b9fa0cd1666dfff1e52311dda7ccec9d4b6d3e01
Dax Mobile <44842493+daxmobile@users.noreply.github.com>
2025-05-15 10:33:01 +0200

    Update content scope scripts to version 8.22.0 (#6070)
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1210257699578837/f
    
     ----- 
    - Automated content scope scripts dependency update
    
    This PR updates the content scope scripts dependency to the latest
    available version and copies the necessary files.
    If tests have failed, see
    https://app.asana.com/0/1202561462274611/1203986899650836/f for further
    information on what to do next.
    
    - [ ] All tests must pass
    
    Co-authored-by: daxmobile <daxmobile@users.noreply.github.com>

--- node_modules/@duckduckgo/content-scope-scripts/build/android/brokerProtection.js
+++ node_modules/@duckduckgo/content-scope-scripts/build/android/brokerProtection.js
@@ -4310,7 +4310,18 @@
       __privateAdd(this, _bundledConfig);
       /** @type {string} */
       __publicField(this, "name");
-      /** @type {{ debug?: boolean, desktopModeEnabled?: boolean, forcedZoomEnabled?: boolean, featureSettings?: Record<string, unknown>, assets?: import('./content-feature.js').AssetConfig | undefined, site: import('./content-feature.js').Site, messagingConfig?: import('@duckduckgo/messaging').MessagingConfig } | null} */
+      /**
+       * @type {{
+       *   debug?: boolean,
+       *   desktopModeEnabled?: boolean,
+       *   forcedZoomEnabled?: boolean,
+       *   featureSettings?: Record<string, unknown>,
+       *   assets?: import('./content-feature.js').AssetConfig | undefined,
+       *   site: import('./content-feature.js').Site,
+       *   messagingConfig?: import('@duckduckgo/messaging').MessagingConfig,
+       *   currentCohorts?: [{feature: string, cohort: string, subfeature: string}],
+       * } | null}
+       */
       __privateAdd(this, _args);
       this.name = name;
       const { bundledConfig, site, platform } = args;
@@ -4373,6 +4384,9 @@
      * @typedef {object} ConditionBlock
      * @property {string[] | string} [domain]
      * @property {object} [urlPattern]
+     * @property {object} [experiment]
+     * @property {string} [experiment.experimentName]
+     * @property {string} [experiment.cohort]
      */
     /**
      * Takes multiple conditional blocks and returns true if any apply.
@@ -4394,7 +4408,8 @@
     _matchConditionalBlock(conditionBlock) {
       const conditionChecks = {
         domain: this._matchDomainConditional,
-        urlPattern: this._matchUrlPatternConditional
+        urlPattern: this._matchUrlPatternConditional,
+        experiment: this._matchExperimentConditional
       };
       for (const key in conditionBlock) {
         if (!conditionChecks[key]) {
@@ -4404,6 +4419,31 @@
         }
       }
       return true;
+    }
+    /**
+     * Takes a condition block and returns true if the current experiment matches the experimentName and cohort.
+     * Expects:
+     * ```json
+     * {
+     *   "experiment": {
+     *      "experimentName": "experimentName",
+     *      "cohort": "cohort-name"
+     *    }
+     * }
+     * ```
+     * Where featureName "ContentScopeExperiments" has a subfeature "experimentName" and cohort "cohort-name"
+     * @param {ConditionBlock} conditionBlock
+     * @returns {boolean}
+     */
+    _matchExperimentConditional(conditionBlock) {
+      if (!conditionBlock.experiment) return false;
+      const experiment = conditionBlock.experiment;
+      if (!experiment.experimentName || !experiment.cohort) return false;
+      const currentCohorts = this.args?.currentCohorts;
+      if (!currentCohorts) return false;
+      return currentCohorts.some((cohort) => {
+        return cohort.feature === "ContentScopeExperiments" && cohort.subfeature === experiment.experimentName && cohort.cohort === experiment.cohort;
+      });
     }
     /**
      * Takes a condtion block and returns true if the current url matches the urlPattern.
