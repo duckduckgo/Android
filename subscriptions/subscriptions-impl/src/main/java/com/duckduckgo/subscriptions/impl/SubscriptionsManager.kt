20c8d41a4816702e3f8c6961ad7d414bb132bdd6
Noelia Alcala <nalcalag@gmail.com>
2025-05-21 00:15:18 +0100

    Remove logic for sending Free trials experiment details to BE metrics

--- subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/SubscriptionsManager.kt
+++ subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/SubscriptionsManager.kt
@@ -428,12 +428,9 @@ class RealSubscriptionsManager @Inject constructor(
         packageName: String,
         purchaseToken: String,
     ): Boolean {
-        var experimentName: String? = null
-        var cohort: String? = null
-        experimentAssigned?.let { // FE experiment details
-            cohort = it.cohort
-            experimentName = it.name
-        }
+        // FE experiment details
+        val experimentName: String? = experimentAssigned?.name
+        val cohort: String? = experimentAssigned?.cohort
         return try {
             val confirmationResponse = subscriptionsService.confirm(
                 ConfirmationBody(
