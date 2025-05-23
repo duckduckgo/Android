f4bbf0a1c9470458e197007843f4c08889209adc
Ondrej Ruttkay <0nko@users.noreply.github.com>
2025-05-23 18:14:22 +0200

    Duck chat: Search Assist link feature (#6132)
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1207418217763355/task/1210188925025667?focus=true
    
    ### Description
    
    This PR adds the Search Assist settings link to Duck.AI settings.
    
    ### Steps to test this PR
    
    QA-optional, this is just a feature branch and it's already been tested.
    
    ---------
    
    Co-authored-by: Dax The Translator <daxmobile@duckduckgo.com>

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/DuckChatSettingsActivity.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/DuckChatSettingsActivity.kt
@@ -26,6 +26,7 @@ import androidx.lifecycle.lifecycleScope
 import com.duckduckgo.anvil.annotations.ContributeToActivityStarter
 import com.duckduckgo.anvil.annotations.InjectWith
 import com.duckduckgo.app.statistics.pixels.Pixel
+import com.duckduckgo.app.tabs.BrowserNav
 import com.duckduckgo.browser.api.ui.BrowserScreens.WebViewActivityWithParams
 import com.duckduckgo.common.ui.DuckDuckGoActivity
 import com.duckduckgo.common.ui.spans.DuckDuckGoClickableSpan
@@ -66,6 +67,9 @@ class DuckChatSettingsActivity : DuckDuckGoActivity() {
 
     @Inject
     lateinit var globalActivityStarter: GlobalActivityStarter
+
+    @Inject
+    lateinit var browserNav: BrowserNav
 
     @Inject
     lateinit var pixel: Pixel
@@ -118,18 +122,24 @@ class DuckChatSettingsActivity : DuckDuckGoActivity() {
             isVisible = viewState.shouldShowAddressBarToggle
             quietlySetIsChecked(viewState.showInAddressBar, addressBarToggleListener)
         }
+        binding.showDuckChatSearchSettingsLink.setOnClickListener {
+            viewModel.duckChatSearchAISettingsClicked()
+        }
     }
 
     private fun processCommand(command: DuckChatSettingsViewModel.Command) {
         when (command) {
-            is DuckChatSettingsViewModel.Command.OpenLearnMore -> {
+            is DuckChatSettingsViewModel.Command.OpenLink -> {
                 globalActivityStarter.start(
                     this,
                     WebViewActivityWithParams(
-                        url = command.learnMoreLink,
+                        url = command.link,
                         screenTitle = getString(R.string.duck_chat_title),
                     ),
                 )
+            }
+            is DuckChatSettingsViewModel.Command.OpenLinkInNewTab -> {
+                startActivity(browserNav.openInNewTab(this@DuckChatSettingsActivity, command.link))
             }
         }
     }
