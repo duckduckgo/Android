4e6ab03e7422668cd43dfed2cf39231b9f7fc81e
Mike Scamell <mscamell@duckduckgo.com>
2025-06-05 20:56:17 +0200

    Add DuckDuckGo theme components: colors, shapes, and typography

---
+++ common/common-ui/src/main/java/com/duckduckgo/common/ui/compose/theme/Shape.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.common.ui.compose.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Shape
+
+@Immutable
+data class DuckDuckGoShapes(
+    val small: Shape,
+    val medium: Shape,
+    val large: Shape,
+)
+
+val LocalDuckDuckGoShapes = staticCompositionLocalOf<DuckDuckGoShapes> {
+    error("No DuckDuckGoShapes provided")
+}
