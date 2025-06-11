a816b3fe37152df5ac28ae2259a4ac3bbbaf1fbc
Mike Scamell <mscamell@duckduckgo.com>
2025-06-11 13:04:01 +0100

    formatting

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -2908,7 +2908,7 @@ class BrowserTabFragment :
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun configureWebView() {
-        if(!onboardingDesignExperimentToggles.buckOnboarding().isEnabled()) {
+        if (!onboardingDesignExperimentToggles.buckOnboarding().isEnabled()) {
             if (swipingTabsFeature.isEnabled) {
                 binding.daxDialogOnboardingCtaContent.layoutTransition.setAnimateParentHierarchy(false)
             }
@@ -3083,7 +3083,7 @@ class BrowserTabFragment :
         if (onboardingDesignExperimentToggles.buckOnboarding().isEnabled()) {
             buckDialogIntroBubble.root.gone()
             newBrowserTab.newTabLayout.setBackgroundColor(
-                requireContext().getColorFromAttr(CommonR.attr.daxColorSurface)
+                requireContext().getColorFromAttr(CommonR.attr.daxColorSurface),
             )
         } else {
             newBrowserTab.browserBackground.setImageResource(0)
