05c12334b82142df71437ee99b7817180b4200ac
David González <malmstein@gmail.com>
2025-06-12 14:41:16 +0200

    chenge asana client

--- .github/actions/pr-asana-sync/dist/index.js
+++ .github/actions/pr-asana-sync/dist/index.js
@@ -67531,6 +67531,29 @@ exports.getDueOn = getDueOn;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -67541,7 +67564,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const asana_1 = __nccwpck_require__(2286);
+const asana = __importStar(__nccwpck_require__(2286));
 const core_1 = __nccwpck_require__(7484);
 const github_1 = __nccwpck_require__(3228);
 const markdown_1 = __nccwpck_require__(3758);
@@ -67551,7 +67574,7 @@ const CUSTOM_FIELD_NAMES = {
     status: 'Github Status'
 };
 const USER_MAP = JSON.parse((0, core_1.getInput)('USER_MAP', { required: false }) || '{}');
-const client = asana_1.Client.create({
+const client = asana.Client.create({
     defaultHeaders: {
         'asana-enable': 'new_user_task_lists,new_project_templates,new_goal_memberships'
     }
