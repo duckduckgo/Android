9764c93a16c69657bd171abd6e437528597dac1e
Mike Scamell <mscamell@duckduckgo.com>
2025-06-23 10:36:47 +0100

    Add BBCardView and BBTopEdgeTreatment for custom card UI with speech bubble effect

---
+++ common/common-ui/src/main/java/com/duckduckgo/common/ui/view/shape/BBCardView.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.common.ui.view.shape
+
+import android.app.UiModeManager
+import android.content.Context
+import android.util.AttributeSet
+import com.duckduckgo.common.ui.view.toPx
+import com.duckduckgo.mobile.android.R
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.shape.ShapeAppearanceModel
+
+class BBCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = com.google.android.material.R.attr.cardViewStyle,
+) : MaterialCardView(context, attrs, defStyleAttr) {
+
+    init {
+        elevation = 0f
+
+        setCardBackgroundColor(context.getColor(R.color.bbSurfaceColor))
+
+        val edgeTreatment = BBTopEdgeTreatment()
+        val offsetEdgeTreatment = OffsetStartTreatment(
+            other = edgeTreatment,
+            offsetPx = 56.toPx(),
+        )
+
+        shapeAppearanceModel = ShapeAppearanceModel.builder()
+            .setAllCornerSizes(56.toPx() * 1f)
+            .setTopEdge(offsetEdgeTreatment)
+            .build()
+    }
+}
