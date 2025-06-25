d0bc406ec2c1172e82ba5db4fffcdff6e6b9e5d9
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-06-25 11:36:17 +0200

    Remove debug logs

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -379,7 +379,6 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import logcat.LogPriority.ERROR
 import logcat.LogPriority.INFO
@@ -2430,11 +2429,6 @@ class BrowserTabViewModel @Inject constructor(
     }
 
     fun onBrokenSiteSelected() {
-        logcat("Cris") {
-            runBlocking {
-                "Report broken site: ${site?.activeContentScopeExperiments?.map { "${it.featureName().name}, ${it.getCohort()}" }}"
-            }
-        }
         command.value = BrokenSiteFeedback(BrokenSiteData.fromSite(site, reportFlow = MENU))
     }
 
@@ -3597,7 +3591,6 @@ class BrowserTabViewModel @Inject constructor(
         isActiveCustomTab: Boolean = false,
         getWebViewUrl: () -> String?,
     ) {
-        logcat("Cris") { "received CSS callback: $method" }
         when (method) {
             "webShare" -> if (id != null && data != null) {
                 webShare(featureName, method, id, data)
