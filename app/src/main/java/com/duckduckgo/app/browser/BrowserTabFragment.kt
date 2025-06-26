bb6d0c60998ca182d6666bbb0730898a45c0f200
Mike Scamell <mscamell@duckduckgo.com>
2025-06-26 17:47:21 +0100

    Delay Buck end animation visibility and adjust layout constraints for improved onboarding experience
    Update animation to latest revision

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -4411,9 +4411,12 @@ class BrowserTabFragment :
 
             if (onboardingDesignExperimentToggles.buckOnboarding().isEnabled()) {
                 if (configuration is DaxBubbleCta.DaxEndCta) {
-                    with(newBrowserTab.buckEndAnimation) {
-                        isVisible = true
-                        playAnimation()
+                    lifecycleScope.launch {
+                        with(newBrowserTab.buckEndAnimation) {
+                            delay(500)
+                            isVisible = true
+                            playAnimation()
+                        }
                     }
                 }
             }
