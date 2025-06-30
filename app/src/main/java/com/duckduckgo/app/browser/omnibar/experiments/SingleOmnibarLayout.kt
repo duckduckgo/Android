fa2d3b4c48b6ebf63bc566d906d6f9201cbf7fd3
0nko <oruttkay@duckduckgo.com>
2025-06-30 18:01:02 +0200

    Reduce the size of the bottom toolbar shadow

--- app/src/main/java/com/duckduckgo/app/browser/omnibar/experiments/SingleOmnibarLayout.kt
+++ app/src/main/java/com/duckduckgo/app/browser/omnibar/experiments/SingleOmnibarLayout.kt
@@ -39,6 +39,7 @@ import com.duckduckgo.app.browser.omnibar.model.OmnibarPosition
 import com.duckduckgo.common.ui.view.gone
 import com.duckduckgo.common.ui.view.hide
 import com.duckduckgo.common.ui.view.show
+import com.duckduckgo.common.ui.view.toPx
 import com.duckduckgo.di.scopes.FragmentScope
 import com.duckduckgo.mobile.android.R as CommonR
 import com.duckduckgo.navigation.api.GlobalActivityStarter
@@ -101,6 +102,8 @@ class SingleOmnibarLayout @JvmOverloads constructor(
             toolbarContainer.updatePadding(
                 top = toolbarContainerPaddingTopWhenAtBottom,
             )
+
+            omnibarCard.cardElevation = 1f.toPx(context)
         }
     }
 
