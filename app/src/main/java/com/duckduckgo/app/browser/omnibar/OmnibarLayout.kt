262e3f751178e2eedfcfb331ed84941809373070
Mike Scamell <mscamell@duckduckgo.com>
2025-07-04 10:20:16 +0100

    Add BB onboarding pulse animation and related resources

--- app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarLayout.kt
+++ app/src/main/java/com/duckduckgo/app/browser/omnibar/OmnibarLayout.kt
@@ -198,7 +198,7 @@ open class OmnibarLayout @JvmOverloads constructor(
     }
 
     private val pulseAnimation: PulseAnimation by lazy {
-        PulseAnimation(lifecycleOwner)
+        PulseAnimation(lifecycleOwner, onboardingDesignExperimentToggles)
     }
 
     private var omnibarTextListener: Omnibar.TextListener? = null
@@ -808,7 +808,6 @@ open class OmnibarLayout @JvmOverloads constructor(
                     targetView = targetView,
                     isSenseOfProtectionExperimentAndShieldView = isPrivacyShieldAnimation &&
                         runBlocking { senseOfProtectionExperiment.shouldShowNewPrivacyShield() },
-                    isBuckOnboardingExperiment = onboardingDesignExperimentToggles.buckOnboarding().isEnabled(),
                 )
             }
         } else {
