8fda1c16f0a5b5d3ae2529350fe4316ecb3c6435
daxmobile <daxmobile@users.noreply.github.com>
2025-07-08 14:38:30 +0000

    Update content scope scripts to version 10.8.0

--- node_modules/@duckduckgo/content-scope-scripts/build/android/contentScope.js
+++ node_modules/@duckduckgo/content-scope-scripts/build/android/contentScope.js
@@ -7302,9 +7302,6 @@
       let id = null;
       const vParam = url.searchParams.get("v");
       const tParam = url.searchParams.get("t");
-      if (url.searchParams.has("list") && !url.searchParams.has("index")) {
-        return null;
-      }
       let time = null;
       if (vParam && _VideoParams.validVideoId.test(vParam)) {
         id = vParam;
