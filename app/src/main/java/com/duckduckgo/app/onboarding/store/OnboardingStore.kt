4eed29ec82ac7bec9a9c68d9cf316264bd47b5eb
nalcalag <nalcalag@gmail.com>
2025-07-09 20:16:22 +0200

    Removed temporary pixel for visit site CTA count

--- app/src/main/java/com/duckduckgo/app/onboarding/store/OnboardingStore.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/store/OnboardingStore.kt
@@ -20,11 +20,9 @@ import com.duckduckgo.app.cta.ui.DaxBubbleCta.DaxDialogIntroOption
 
 interface OnboardingStore {
     var onboardingDialogJourney: String?
-    var visitSiteCtaDisplayCount: Int
 
     @Deprecated(message = "Parameter used for a temporary pixel")
     fun getSearchOptions(): List<DaxDialogIntroOption>
     fun getSitesOptions(): List<DaxDialogIntroOption>
     fun getExperimentSearchOptions(): List<DaxDialogIntroOption>
-    fun clearVisitSiteCtaDisplayCount()
 }
