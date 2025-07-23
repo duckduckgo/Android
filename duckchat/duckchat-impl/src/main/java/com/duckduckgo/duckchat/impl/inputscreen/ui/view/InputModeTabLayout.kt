52ee9b065b8abb30aab2a45ec94eb7e43eafa776
Łukasz Paczos <lpaczos@duckduckgo.com>
2025-07-23 14:45:29 +0200

    compute Input Screen tab indicator height to always be vertically centered in the tab switcher (#6435)
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1208671518894266/task/1210767017822401
    
    ### Description
    Previously, the dimensions picked for the tab switcher and its tab
    indicators often resulted in the switcher having a height of an odd
    number of pixels while the indicator had height of even number of
    pixels. This resulted in the pill not being perfectly vertically aligned
    within the switcher. It depends on the pixel density of the device, but
    happened often on the devices we tested on.
    
    This PR adds a bit of logic to subtract a pixel when needed, to allow
    for that perfect vertical alignment.
    
    ### Steps to test this PR
    
    - [ ] Try a commit before this change and verify that the space below
    the tab indicator is bigger than the space above it.
    - [ ] Try this change and verify that the space is now equal.

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/inputscreen/ui/view/InputModeTabLayout.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/inputscreen/ui/view/InputModeTabLayout.kt
@@ -50,6 +50,7 @@ class InputModeTabLayout @JvmOverloads constructor(
             buildShadowedTabIndicator(
                 context = context,
                 heightPx = resources.getDimensionPixelSize(R.dimen.inputModeTabIndicatorHeight),
+                containerHeightPx = resources.getDimensionPixelSize(R.dimen.inputModeSwitchHeight),
                 horizontalInsetPx = resources.getDimensionPixelSize(R.dimen.inputModeTabIndicatorHorizontalInset),
                 elevationPx = resources.getDimensionPixelSize(R.dimen.inputModeTabIndicatorElevation),
             ),
@@ -75,6 +76,7 @@ class InputModeTabLayout @JvmOverloads constructor(
     private fun buildShadowedTabIndicator(
         context: Context,
         heightPx: Int,
+        containerHeightPx: Int,
         horizontalInsetPx: Int,
         elevationPx: Int,
     ): InsetDrawable {
@@ -83,8 +85,23 @@ class InputModeTabLayout @JvmOverloads constructor(
             .setAllCorners(CornerFamily.ROUNDED, cornerRadius)
             .build()
 
+        // Computing a pill height that will be perfectly centered in the container vertically, with equal top and bottom insets,
+        // and matching horizontal insets, if possible.
+        val shadowPillHeightCandidates = listOf(heightPx, heightPx - 1, heightPx + 1).filter { candidate ->
+            // only pick values that will result in even vertical insets
+            (containerHeightPx - candidate) % 2 == 0
+        }
+        val shadowPillHeight = shadowPillHeightCandidates
+            .firstOrNull { candidate ->
+                // prefer values that will result in equal horizontal and vertical insets
+                val verticalInsetsPx = (containerHeightPx - candidate) / 2
+                verticalInsetsPx == horizontalInsetPx
+            }
+            ?: shadowPillHeightCandidates.firstOrNull()
+            ?: heightPx
+
         val shadowedPill = object : MaterialShapeDrawable(pill) {
-            override fun getIntrinsicHeight(): Int = heightPx
+            override fun getIntrinsicHeight(): Int = shadowPillHeight
         }.apply {
             shadowCompatibilityMode = MaterialShapeDrawable.SHADOW_COMPAT_MODE_ALWAYS
             initializeElevationOverlay(context)
