duckduckgo/Android

commit 648e3496132ad92ef8d3758a65b8c4e10563a2d8

remove nested material cards in tab switcher adapter (#6521)
Task/Issue URL:
https://app.asana.com/1/137249556945/project/1208671518894266/task/1210935007420667?focus=true

### Description
Removes nested `MaterialCardView` that was used only to apply rounded
corners to the image. Now, we're using glide transformation instead.

### Steps to test this PR

Verify that tab switcher previews look exactly the same before and after
the change.

app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherAdapter.kt
@@ -36,6 +36,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.Transformation
 import com.bumptech.glide.load.engine.Resource
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.duckduckgo.app.browser.R
 import com.duckduckgo.app.browser.databinding.ItemTabGridBinding
@@ -74,6 +75,7 @@ import java.io.File
 import java.security.MessageDigest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import logcat.LogPriority.ERROR
 import logcat.LogPriority.VERBOSE
 import logcat.logcat
 
@@ -398,10 +400,18 @@ class TabSwitcherAdapter(
                     return@launch
                 }
 
-                glide.load(cachedWebViewPreview)
-                    .transition(DrawableTransitionOptions.withCrossFade())
-                    .optionalTransform(fitAndClipBottom())
-                    .into(tabPreview)
+                try {
+                    glide.load(cachedWebViewPreview)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .transform(
+                            fitAndClipBottom(),
+                            RoundedCorners(tabPreview.context.resources.getDimensionPixelSize(CommonR.dimen.smallShapeCornerRadius)),
+                        )
+                        .into(tabPreview)
+                } catch (e: Exception) {
+                    logcat(ERROR) { "Error loading tab preview for ${tab.tabId}: ${e.message}" }
+                    glide.load(AndroidR.drawable.ic_dax_icon_72).into(tabPreview)
+                }
             }
         } else {
             glide.clear(tabPreview)

app/src/main/res/layout/item_tab_grid_new.xml
@@ -94,25 +94,16 @@
             app:typography="h5"
             tools:text="Slashdot" />
 
-        <com.google.android.material.card.MaterialCardView
+        <ImageView
+            android:id="@+id/tabPreview"
             android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            app:cardCornerRadius="@dimen/keyline_2"
-            app:cardElevation="@dimen/keyline_empty"
+            android:layout_height="@dimen/gridItemPreviewHeightNew"
             android:layout_marginTop="6dp"
+            android:importantForAccessibility="no"
             app:layout_constraintBottom_toBottomOf="parent"
             app:layout_constraintEnd_toEndOf="parent"
             app:layout_constraintStart_toStartOf="parent"
-            app:layout_constraintTop_toBottomOf="@id/close">
-
-            <ImageView
-                android:id="@+id/tabPreview"
-                android:layout_width="match_parent"
-                android:layout_height="@dimen/gridItemPreviewHeightNew"
-                android:importantForAccessibility="no"
-                android:scaleType="center" />
-
-        </com.google.android.material.card.MaterialCardView>
+            app:layout_constraintTop_toBottomOf="@id/close" />
 
     </androidx.constraintlayout.widget.ConstraintLayout>
 

commit 59d5f98ef746a54b996784bda3bc07e66031bb3d

ensure that tab switcher grid items always scale images and uses square page previews in the old design (#6529)
Task/Issue URL:
https://app.asana.com/1/137249556945/project/1208671518894266/task/1210976600737969?focus=true

### Description
Fixes a regression from https://github.com/duckduckgo/Android/pull/6521
which unintentionally removed scaling from placeholder images in the tab
switcher grid items.

### Steps to test this PR
- [x] Open the tab switcher while on new tab page and verify that the
Dax icon is the right scale.

### UI changes
| Before  | After |
| ------ | ----- |
<img width="960" height="2142" alt="Screenshot_20250805_163405"
src="https://github.com/user-attachments/assets/abd14c30-5c85-452c-8647-71531c7685aa"
/>|<img width="960" height="2142" alt="Screenshot_20250805_163432"
src="https://github.com/user-attachments/assets/88115fb5-a0f1-4c21-a6e6-32b52319d75c"
/>|

app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherAdapter.kt
@@ -403,10 +403,18 @@ class TabSwitcherAdapter(
                 try {
                     glide.load(cachedWebViewPreview)
                         .transition(DrawableTransitionOptions.withCrossFade())
-                        .transform(
-                            fitAndClipBottom(),
-                            RoundedCorners(tabPreview.context.resources.getDimensionPixelSize(CommonR.dimen.smallShapeCornerRadius)),
-                        )
+                        .let {
+                            if (isVisualExperimentEnabled) {
+                                it.transform(
+                                    fitAndClipBottom(),
+                                    RoundedCorners(tabPreview.context.resources.getDimensionPixelSize(CommonR.dimen.smallShapeCornerRadius)),
+                                )
+                            } else {
+                                it.transform(
+                                    fitAndClipBottom(),
+                                )
+                            }
+                        }
                         .into(tabPreview)
                 } catch (e: Exception) {
                     logcat(ERROR) { "Error loading tab preview for ${tab.tabId}: ${e.message}" }

app/src/main/res/layout/item_tab_grid_new.xml
@@ -100,6 +100,7 @@
             android:layout_height="@dimen/gridItemPreviewHeightNew"
             android:layout_marginTop="6dp"
             android:importantForAccessibility="no"
+            android:scaleType="center"
             app:layout_constraintBottom_toBottomOf="parent"
             app:layout_constraintEnd_toEndOf="parent"
             app:layout_constraintStart_toStartOf="parent"