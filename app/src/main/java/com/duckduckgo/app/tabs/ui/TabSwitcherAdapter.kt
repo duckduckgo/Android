13e0958ad5bf962b29565e42d5c41386bbbaafeb
0nko <oruttkay@duckduckgo.com>
2025-08-14 10:48:41 +0200

    Optimize imports

--- app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherAdapter.kt
+++ app/src/main/java/com/duckduckgo/app/tabs/ui/TabSwitcherAdapter.kt
@@ -72,15 +72,15 @@ import com.duckduckgo.common.ui.view.show
 import com.duckduckgo.common.ui.view.toPx
 import com.duckduckgo.common.utils.DispatcherProvider
 import com.duckduckgo.common.utils.swap
+import com.duckduckgo.mobile.android.R as AndroidR
+import com.duckduckgo.mobile.android.R as CommonR
+import java.io.File
+import java.security.MessageDigest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import logcat.LogPriority.ERROR
 import logcat.LogPriority.VERBOSE
 import logcat.logcat
-import java.io.File
-import java.security.MessageDigest
-import com.duckduckgo.mobile.android.R as AndroidR
-import com.duckduckgo.mobile.android.R as CommonR
 
 class TabSwitcherAdapter(
     private val isVisualExperimentEnabled: Boolean,
