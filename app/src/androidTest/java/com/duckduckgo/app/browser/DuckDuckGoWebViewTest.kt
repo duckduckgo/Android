e7d634838e00523b58a7e74ec8dd6e89b412ea8c
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-08-22 12:25:34 +0200

    Send internal status to CSS

--- app/src/androidTest/java/com/duckduckgo/app/browser/DuckDuckGoWebViewTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/browser/DuckDuckGoWebViewTest.kt
@@ -19,20 +19,12 @@ package com.duckduckgo.app.browser
 import androidx.test.annotation.UiThreadTest
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.duckduckgo.app.browser.api.WebViewCapabilityChecker
-import com.duckduckgo.app.browser.api.WebViewCapabilityChecker.WebViewCapability.DocumentStartJavaScript
-import com.duckduckgo.browser.api.webviewcompat.WebViewCompatWrapper
 import com.duckduckgo.common.test.CoroutineTestRule
-import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
-import org.mockito.kotlin.never
-import org.mockito.kotlin.whenever
 
 @RunWith(AndroidJUnit4::class)
 class DuckDuckGoWebViewTest {
@@ -41,8 +33,6 @@ class DuckDuckGoWebViewTest {
     val coroutineRule = CoroutineTestRule()
 
     private lateinit var testee: DuckDuckGoWebView
-    private val mockWebViewCapabilityChecker: WebViewCapabilityChecker = mock()
-    private val mockWebViewCompatWrapper: WebViewCompatWrapper = mock()
 
     @Before
     @UiThreadTest
@@ -56,23 +46,5 @@ class DuckDuckGoWebViewTest {
     @UiThreadTest
     fun whenWebViewInitialisedThenSafeBrowsingDisabled() {
         assertFalse(testee.settings.safeBrowsingEnabled)
-    }
-
-    @Test
-    fun whenSafeAddDocumentStartJavaScriptWithFeatureEnabledThenAddScript() = runTest {
-        whenever(mockWebViewCapabilityChecker.isSupported(DocumentStartJavaScript)).thenReturn(true)
-
-        testee.safeAddDocumentStartJavaScript("script", setOf("*"))
-
-        verify(mockWebViewCompatWrapper).addDocumentStartJavaScript(testee, "script", setOf("*"))
-    }
-
-    @Test
-    fun whenSafeAddDocumentStartJavaScriptWithFeatureDisabledThenDoNotAddScript() = runTest {
-        whenever(mockWebViewCapabilityChecker.isSupported(DocumentStartJavaScript)).thenReturn(false)
-
-        testee.safeAddDocumentStartJavaScript("script", setOf("*"))
-
-        verify(mockWebViewCompatWrapper, never()).addDocumentStartJavaScript(testee, "script", setOf("*"))
     }
 }
