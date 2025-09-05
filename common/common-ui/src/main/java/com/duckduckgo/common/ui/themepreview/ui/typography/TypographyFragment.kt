d964ad8b827c6a40942ca610f4c61fbc80944a21
Mike Scamell <mscamell@duckduckgo.com>
2025-09-05 16:55:30 +0100

    Add ComposeView components for typography demonstration in TypographyFragment

--- common/common-ui/src/main/java/com/duckduckgo/common/ui/themepreview/ui/typography/TypographyFragment.kt
+++ common/common-ui/src/main/java/com/duckduckgo/common/ui/themepreview/ui/typography/TypographyFragment.kt
@@ -21,7 +21,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import com.duckduckgo.common.ui.compose.component.core.text.DaxTextPrimary
+import com.duckduckgo.common.ui.compose.theme.DuckDuckGoTheme
 import com.duckduckgo.common.ui.view.text.DaxTextView
 import com.duckduckgo.common.ui.view.text.DaxTextView.Typography
 import com.duckduckgo.mobile.android.R
@@ -44,5 +49,137 @@ class TypographyFragment : Fragment() {
     ) {
         val daxTextView = view.findViewById<DaxTextView>(R.id.typographyTitle)
         daxTextView.setTypography(Typography.Body1)
+        
+        setupComposeViews(view)
+    }
+    
+    private fun setupComposeViews(view: View) {
+        // Title
+        setupComposeView(view, R.id.compose_title_label) { 
+            DaxTextPrimary(text = "Text Appearance Title", style = DuckDuckGoTheme.typography.title)
+        }
+        setupComposeView(view, R.id.compose_title_lorem) { 
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", style = DuckDuckGoTheme.typography.title)
+        }
+        
+        // H1
+        setupComposeView(view, R.id.compose_h1_label) {
+            DaxTextPrimary(text = "Text Appearance H1", style = DuckDuckGoTheme.typography.h1)
+        }
+        setupComposeView(view, R.id.compose_h1_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.h1)
+        }
+        
+        // H2
+        setupComposeView(view, R.id.compose_h2_label) {
+            DaxTextPrimary(text = "Text Appearance H2", style = DuckDuckGoTheme.typography.h2)
+        }
+        setupComposeView(view, R.id.compose_h2_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.h2)
+        }
+        
+        // H3
+        setupComposeView(view, R.id.compose_h3_label) {
+            DaxTextPrimary(text = "Text Appearance H3", style = DuckDuckGoTheme.typography.h3)
+        }
+        setupComposeView(view, R.id.compose_h3_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.h3)
+        }
+        
+        // H4
+        setupComposeView(view, R.id.compose_h4_label) {
+            DaxTextPrimary(text = "Text Appearance H4", style = DuckDuckGoTheme.typography.h4)
+        }
+        setupComposeView(view, R.id.compose_h4_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.h4)
+        }
+        
+        // H5
+        setupComposeView(view, R.id.compose_h5_label) {
+            DaxTextPrimary(text = "Text Appearance H5", style = DuckDuckGoTheme.typography.h5)
+        }
+        setupComposeView(view, R.id.compose_h5_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.h5)
+        }
+        
+        // Body1
+        setupComposeView(view, R.id.compose_body1_label) {
+            DaxTextPrimary(text = "Text Appearance Body1", style = DuckDuckGoTheme.typography.body1)
+        }
+        setupComposeView(view, R.id.compose_body1_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.body1)
+        }
+        
+        // Body1 Bold (using body1 style for now as bold variant doesn't exist)
+        setupComposeView(view, R.id.compose_body1_bold_label) {
+            DaxTextPrimary(text = "Text Appearance Body1 Bold", style = DuckDuckGoTheme.typography.body1)
+        }
+        setupComposeView(view, R.id.compose_body1_bold_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.body1)
+        }
+        
+        // Body1 Mono (using body1 style for now as mono variant doesn't exist)
+        setupComposeView(view, R.id.compose_body1_mono_label) {
+            DaxTextPrimary(text = "Text Appearance Body1 Mono", style = DuckDuckGoTheme.typography.body1)
+        }
+        setupComposeView(view, R.id.compose_body1_mono_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.body1)
+        }
+        
+        // Body2
+        setupComposeView(view, R.id.compose_body2_label) {
+            DaxTextPrimary(text = "Text Appearance Body2", style = DuckDuckGoTheme.typography.body2)
+        }
+        setupComposeView(view, R.id.compose_body2_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.body2)
+        }
+        
+        // Body2 Bold (using body2 style for now as bold variant doesn't exist)
+        setupComposeView(view, R.id.compose_body2_bold_label) {
+            DaxTextPrimary(text = "Text Appearance Body2 Bold", style = DuckDuckGoTheme.typography.body2)
+        }
+        setupComposeView(view, R.id.compose_body2_bold_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.body2)
+        }
+        
+        // Button
+        setupComposeView(view, R.id.compose_button_label) {
+            DaxTextPrimary(text = "Text Appearance Button", style = DuckDuckGoTheme.typography.button)
+        }
+        setupComposeView(view, R.id.compose_button_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.button)
+        }
+        
+        // Caption
+        setupComposeView(view, R.id.compose_caption_label) {
+            DaxTextPrimary(text = "Text Appearance Caption", style = DuckDuckGoTheme.typography.caption)
+        }
+        setupComposeView(view, R.id.compose_caption_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.caption)
+        }
+        
+        // Caption All Caps (using caption style for now as all caps variant doesn't exist)
+        setupComposeView(view, R.id.compose_caption_allcaps_label) {
+            DaxTextPrimary(text = "Text Appearance Caption All Caps", style = DuckDuckGoTheme.typography.caption)
+        }
+        setupComposeView(view, R.id.compose_caption_allcaps_lorem) {
+            DaxTextPrimary(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", style = DuckDuckGoTheme.typography.caption)
+        }
+        
+        // Manual Typography
+        setupComposeView(view, R.id.compose_manual_typography) {
+            DaxTextPrimary(text = "Text Appearance Body 1 set manually", style = DuckDuckGoTheme.typography.body1)
+        }
+    }
+    
+    private fun setupComposeView(view: View, id: Int, content: @Composable () -> Unit) {
+        view.findViewById<ComposeView>(id)?.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                DuckDuckGoTheme {
+                    content()
+                }
+            }
+        }
     }
 }
