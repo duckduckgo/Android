b708a9e83bf942709a775da3d92215d304063074
David González <malmstein@gmail.com>
2025-09-11 15:01:45 +0200

    Added new feature flag for chat history

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/feature/DuckChatFeature.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/feature/DuckChatFeature.kt
@@ -66,4 +66,10 @@ interface DuckChatFeature {
      */
     @Toggle.DefaultValue(DefaultFeatureValue.TRUE)
     fun showInputScreenAutomaticallyOnNewTab(): Toggle
+
+    /**
+     * @return `true` when the Setting for allowing Duck.ai chats to be deleted with the Fire Button is enabled
+     */
+    @Toggle.DefaultValue(DefaultFeatureValue.TRUE)
+    fun clearHistory(): Toggle
 }
