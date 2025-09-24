14aaf2ef681b3e1f3d81ff3463862ea2e22593fc
0nko <oruttkay@duckduckgo.com>
2025-09-24 10:25:42 +0200

    Update the private search settings link

--- app/src/main/java/com/duckduckgo/app/privatesearch/PrivateSearchActivity.kt
+++ app/src/main/java/com/duckduckgo/app/privatesearch/PrivateSearchActivity.kt
@@ -116,6 +116,6 @@ class PrivateSearchActivity : DuckDuckGoActivity() {
     }
 
     companion object {
-        private const val DUCKDUCKGO_SETTINGS_WEB_LINK = "https://duckduckgo.com/settings"
+        private const val DUCKDUCKGO_SETTINGS_WEB_LINK = "https://duckduckgo.com/settings?return=privateSearch"
     }
 }
