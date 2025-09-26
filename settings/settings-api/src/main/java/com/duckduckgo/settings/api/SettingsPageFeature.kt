3444f673f48631ea07b6e9964e14a57ae93565b3
0nko <oruttkay@duckduckgo.com>
2025-09-26 15:31:57 +0200

    Fix spotless issues

--- settings/settings-api/src/main/java/com/duckduckgo/settings/api/SettingsPageFeature.kt
+++ settings/settings-api/src/main/java/com/duckduckgo/settings/api/SettingsPageFeature.kt
@@ -20,7 +20,6 @@ import com.duckduckgo.feature.toggles.api.Toggle
 import com.duckduckgo.feature.toggles.api.Toggle.DefaultFeatureValue
 
 interface SettingsPageFeature {
-
     @Toggle.DefaultValue(DefaultFeatureValue.FALSE)
     @Toggle.InternalAlwaysEnabled
     fun self(): Toggle
