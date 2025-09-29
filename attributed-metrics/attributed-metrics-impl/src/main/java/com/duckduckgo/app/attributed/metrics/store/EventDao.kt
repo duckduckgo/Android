5c9eac1dae75c0c54bfd523d46d7814d8fd4f967
Cristian Monforte <cristian@duckduckgo.com>
2025-09-29 16:55:36 +0200

    adds internal event repository and database

---
+++ attributed-metrics/attributed-metrics-impl/src/main/java/com/duckduckgo/app/attributed/metrics/store/EventDao.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2024 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.attributed.metrics.store
+
+import androidx.room.*
+
+@Dao
+interface EventDao {
+    @Transaction
+    @Query("SELECT * FROM event_metrics WHERE eventName = :eventName AND day >= :startDay ORDER BY day DESC")
+    suspend fun getEventsByNameAndTimeframe(
+        eventName: String,
+        startDay: String,
+    ): List<EventEntity>
+
+    @Query("SELECT COUNT(DISTINCT day) FROM event_metrics WHERE eventName = :eventName AND day >= :startDay")
+    suspend fun getDaysWithEvents(
+        eventName: String,
+        startDay: String,
+    ): Int
+
+    @Query("SELECT SUM(count) FROM event_metrics WHERE eventName = :eventName AND day >= :startDay")
+    suspend fun getTotalEvents(
+        eventName: String,
+        startDay: String,
+    ): Int
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEvent(event: EventEntity)
+
+    @Query(
+        """
+        UPDATE event_metrics
+        SET count = count + 1
+        WHERE eventName = :eventName AND day = :day
+    """,
+    )
+    suspend fun incrementEventCount(
+        eventName: String,
+        day: String,
+    )
+
+    @Query("SELECT count FROM event_metrics WHERE eventName = :eventName AND day = :day")
+    suspend fun getEventCount(
+        eventName: String,
+        day: String,
+    ): Int?
+
+    @Query("DELETE FROM event_metrics WHERE day < :day")
+    suspend fun deleteEventsOlderThan(day: String)
+}
