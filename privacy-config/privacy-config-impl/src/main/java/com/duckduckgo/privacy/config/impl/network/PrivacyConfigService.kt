ca6807c474e08d17b6776dc4bcc636911b662156
David González <malmstein@gmail.com>
2025-10-03 15:26:02 +0200

    rollback remote config

--- privacy-config/privacy-config-impl/src/main/java/com/duckduckgo/privacy/config/impl/network/PrivacyConfigService.kt
+++ privacy-config/privacy-config-impl/src/main/java/com/duckduckgo/privacy/config/impl/network/PrivacyConfigService.kt
@@ -18,13 +18,13 @@ package com.duckduckgo.privacy.config.impl.network
 
 import com.duckduckgo.anvil.annotations.ContributesServiceApi
 import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.privacy.config.api.PRIVACY_REMOTE_CONFIG_URL
 import com.duckduckgo.privacy.config.impl.models.JsonPrivacyConfig
 import retrofit2.Response
 import retrofit2.http.GET
 
 @ContributesServiceApi(AppScope::class)
 interface PrivacyConfigService {
-    // @GET(PRIVACY_REMOTE_CONFIG_URL)
-    @GET("http://www.jsonblob.com/api/1420032018883993600")
+    @GET(PRIVACY_REMOTE_CONFIG_URL)
     suspend fun privacyConfig(): Response<JsonPrivacyConfig>
 }
