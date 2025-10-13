0b151e4587baa815b402e5ecff4cca1b83e4e7f7
nalcalag <nalcalag@gmail.com>
2025-10-13 13:12:45 +0100

    About page migrated to new subscription rebranding

--- app/src/main/java/com/duckduckgo/app/about/AboutDuckDuckGoActivity.kt
+++ app/src/main/java/com/duckduckgo/app/about/AboutDuckDuckGoActivity.kt
@@ -40,7 +40,6 @@ import com.duckduckgo.app.about.AboutDuckDuckGoViewModel.Command.LaunchWebViewWi
 import com.duckduckgo.app.about.AboutDuckDuckGoViewModel.Command.LaunchWebViewWithPPROUrl
 import com.duckduckgo.app.about.AboutDuckDuckGoViewModel.Command.LaunchWebViewWithPrivacyPolicyUrl
 import com.duckduckgo.app.about.AboutDuckDuckGoViewModel.Command.LaunchWebViewWithVPNUrl
-import com.duckduckgo.app.about.AboutDuckDuckGoViewModel.ViewState
 import com.duckduckgo.app.browser.BrowserActivity
 import com.duckduckgo.app.browser.R
 import com.duckduckgo.app.browser.databinding.ActivityAboutDuckDuckGoBinding
@@ -87,6 +86,7 @@ class AboutDuckDuckGoActivity : DuckDuckGoActivity() {
 
         configureUiEventHandlers()
         observeViewModel()
+        configureClickableLinks()
     }
 
     override fun onResume() {
@@ -94,20 +94,15 @@ class AboutDuckDuckGoActivity : DuckDuckGoActivity() {
         viewModel.resetEasterEggCounter()
     }
 
-    private fun configureClickableLinks(viewState: ViewState) {
+    private fun configureClickableLinks() {
         with(binding.includeContent.aboutText) {
-            text = addClickableLinks(viewState)
+            text = addClickableLinks()
             movementMethod = LinkMovementMethod.getInstance()
         }
     }
 
-    private fun addClickableLinks(viewState: ViewState): SpannableString {
-        val fullText = if (viewState.rebrandingEnabled) {
-            getText(R.string.aboutDescriptionBrandUpdate2025Rebranding) as SpannedString
-        } else {
-            getText(R.string.aboutDescriptionBrandUpdate2025) as SpannedString
-        }
-
+    private fun addClickableLinks(): SpannableString {
+        val fullText = getText(R.string.aboutDescriptionBrandUpdate2025) as SpannedString
         val spannableString = SpannableString(fullText)
         val annotations = fullText.getSpans(0, fullText.length, Annotation::class.java)
 
@@ -194,7 +189,6 @@ class AboutDuckDuckGoActivity : DuckDuckGoActivity() {
             .onEach { viewState ->
                 viewState.let {
                     binding.includeContent.aboutVersion.setSecondaryText(it.version)
-                    configureClickableLinks(viewState)
                 }
             }.launchIn(lifecycleScope)
 
