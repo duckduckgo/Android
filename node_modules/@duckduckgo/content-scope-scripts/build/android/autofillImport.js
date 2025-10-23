72ee7b2b05da2e5b107b5dbb8bcf0c604b5c0c86
Dax Mobile <44842493+daxmobile@users.noreply.github.com>
2025-10-23 03:49:57 -0400

    Update content scope scripts to version 11.41.0 (#6986)
    Task/Issue URL:
    https://app.asana.com/0/488551667048375/1211725549968021/f
    
     ----- 
    - Automated content scope scripts dependency update
    
    This PR updates the content scope scripts dependency to the latest
    available version and copies the necessary files.
    
    Tests will only run if something has changed in the
    `node_modules/@duckduckgo/content-scope-scripts` folder.
    
    If only the package version has changed, there is no need to run the
    tests.
    
    If tests have failed, see
    https://app.asana.com/0/1202561462274611/1203986899650836/f for further
    information on what to do next.
    
    _`content-scope-scripts` folder update_
    - [ ] All tests must pass
    - [ ] Privacy tests must pass
    
    _Only `content-scope-scripts` package update_
    - [ ] All tests must pass
    - [ ] Privacy tests do not need to run
    
    Co-authored-by: daxmobile <daxmobile@users.noreply.github.com>

--- node_modules/@duckduckgo/content-scope-scripts/build/android/autofillImport.js
+++ node_modules/@duckduckgo/content-scope-scripts/build/android/autofillImport.js
@@ -4710,6 +4710,7 @@
        *   platform: import('./utils.js').Platform,
        *   desktopModeEnabled?: boolean,
        *   forcedZoomEnabled?: boolean,
+       *   isDdgWebView?: boolean,
        *   featureSettings?: Record<string, unknown>,
        *   assets?: import('./content-feature.js').AssetConfig | undefined,
        *   site: import('./content-feature.js').Site,
