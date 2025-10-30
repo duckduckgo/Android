a260c9ef9dfeb7044e6618d3acdd9d7db3d8d9a5
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-10-30 10:27:00 +0100

    Add base script for de-risking new WebViewCompat APIs

---
+++ app/src/main/res/raw/webviewcompat_test_script.js
@@ -0,0 +1,29 @@
+let supportedMessages = ["ContextMenuOpened", "PageStarted"];
+
+const delay = $DELAY$;
+const postInitialPing = $POST_INITIAL_PING$;
+const replyToNativeMessages = $REPLY_TO_NATIVE_MESSAGES$;
+
+  const pingMessage = 'Ping:' + window.location.href + ' ' + delay + 'ms'
+
+
+if (postInitialPing) {
+    setTimeout(() => {
+        webViewCompatTestObj.postMessage(pingMessage)
+    }, delay)
+}
+
+
+webViewCompatTestObj.onmessage = function(event) {
+    console.log("webViewCompatTestObj received", event.data)
+    if (replyToNativeMessages && supportedMessages.includes(event.data)) {
+        webViewCompatTestObj.postMessage(event.data + " from webViewCompatTestObj")
+    }
+}
+
+window.onmessage = function(event) {
+    console.log("window received", event.data)
+    if (replyToNativeMessages && supportedMessages.includes(event.data)) {
+        webViewCompatTestObj.postMessage(event.data + " from window")
+    }
+}
