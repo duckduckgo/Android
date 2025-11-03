d41f77f93e66e172676974e2fbcfd2a92b54a547
Ondrej Ruttkay <0nko@users.noreply.github.com>
2025-11-03 18:53:12 +0100

    Update app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
    Co-authored-by: Łukasz Paczos <lpaczos@duckduckgo.com>

--- app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
+++ app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
@@ -192,7 +192,6 @@ interface AndroidBrowserConfigFeature {
      * sub-feature flag enabled
      * If the remote feature is not present defaults to `false`
      */
-    @Toggle.DefaultValue(DefaultFeatureValue.FALSE)
-    @Toggle.InternalAlwaysEnabled
+    @Toggle.DefaultValue(DefaultFeatureValue.INTERNAL)
     fun splitOmnibar(): Toggle
 }
