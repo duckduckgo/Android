c06a4fcd41e511cbd7f8d69547600c8acfab61e1
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-11-04 12:10:10 +0100

    Add base script for de-risking new WebViewCompat APIs (#7034)
    Task/Issue URL: https://app.asana.com/1/137249556945/task/1211755269770776?focus=true
    
    ### Description
    Create base script to be used as a test bed. Script contains placeholder so it can be configured as needed from native
    
    ### Steps to test this PR
    n/a
    
    ### UI changes
    n/a

---
+++ app/src/main/res/raw/webviewcompat_test_script.js
@@ -0,0 +1,33 @@
+const supportedMessages = ["ContextMenuOpened", "PageStarted"];
+
+const delay = $DELAY$;
+const postInitialPing = $POST_INITIAL_PING$;
+const replyToNativeMessages = $REPLY_TO_NATIVE_MESSAGES$;
+
+const webViewCompatPingMessage = 'Ping:' + window.location.href + ' ' + delay + 'ms'
+
+
+if (postInitialPing) {
+    if (delay > 0) {
+        setTimeout(() => {
+            webViewCompatTestObj.postMessage(webViewCompatPingMessage)
+        }, delay)
+    } else {
+        webViewCompatTestObj.postMessage(webViewCompatPingMessage)
+    }
+}
+
+
+webViewCompatTestObj.onmessage = function(event) {
+    console.log("webViewCompatTestObj received", event.data)
+    if (replyToNativeMessages && supportedMessages.includes(event.data)) {
+        webViewCompatTestObj.postMessage(event.data + " from webViewCompatTestObj")
+    }
+}
+
+window.onmessage = function(event) {
+    console.log("window received", event.data)
+    if (replyToNativeMessages && supportedMessages.includes(event.data)) {
+        webViewCompatTestObj.postMessage(event.data + " from window")
+    }
+}
