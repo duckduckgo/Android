aa55164fb27fbd5aba8242de79d5c1935851a5c9
David González <malmstein@gmail.com>
2025-11-05 23:38:07 +0100

    added first omnibar iteration:

---
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/DuckChatOmnibarLayout.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2025 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.duckchat.impl.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.browser.ui.omnibar.OmnibarType
+import com.duckduckgo.browser.ui.tabs.NewTabSwitcherButton
+import com.duckduckgo.di.scopes.FragmentScope
+import com.duckduckgo.duckchat.impl.R
+import com.google.android.material.appbar.AppBarLayout
+import dagger.android.support.AndroidSupportInjection
+
+@InjectWith(FragmentScope::class)
+class DuckChatOmnibarLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0,
+) : AppBarLayout(context, attrs, defStyle) {
+
+    internal val tabsMenu: NewTabSwitcherButton by lazy { findViewById(R.id.inputFieldTabsMenu) }
+    internal val fireIconMenu: FrameLayout by lazy { findViewById(R.id.inputFieldFireButton) }
+    internal val browserMenu: FrameLayout by lazy { findViewById(R.id.inputFieldBrowserMenu) }
+
+    private var omnibarItemPressedListener: ItemPressedListener? = null
+
+    interface ItemPressedListener {
+        fun onTabsButtonPressed()
+
+        fun onFireButtonPressed()
+
+        fun onBrowserMenuPressed()
+    }
+
+    init {
+        inflate(context, R.layout.view_duck_chat_omnibar, this)
+
+        AndroidSupportInjection.inject(this)
+
+        val attr = context.theme.obtainStyledAttributes(attrs, R.styleable.DuckChatOmnibarLayout, defStyle, 0)
+        val omnibarType = OmnibarType.entries[attr.getInt(R.styleable.DuckChatOmnibarLayout_duckOmnibarPosition, 0)]
+        val isTopPosition = omnibarType == OmnibarType.SINGLE_TOP || omnibarType == OmnibarType.SPLIT
+    }
+
+    fun setOmnibarItemPressedListener(itemPressedListener: ItemPressedListener) {
+        omnibarItemPressedListener = itemPressedListener
+        tabsMenu.setOnClickListener {
+            omnibarItemPressedListener?.onTabsButtonPressed()
+        }
+
+        fireIconMenu.setOnClickListener {
+            omnibarItemPressedListener?.onFireButtonPressed()
+        }
+
+        browserMenu.setOnClickListener {
+            omnibarItemPressedListener?.onBrowserMenuPressed()
+        }
+    }
+
+    fun setTabsCount(tabs: Int) {
+        tabsMenu.count = tabs
+    }
+}
