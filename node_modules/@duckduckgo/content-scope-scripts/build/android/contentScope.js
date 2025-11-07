b02fbd36050ac61644c74c7c547c4b76133718a3
daxmobile <daxmobile@users.noreply.github.com>
2025-11-07 02:08:31 +0000

    Update content scope scripts to version 12.0.0

--- node_modules/@duckduckgo/content-scope-scripts/build/android/contentScope.js
+++ node_modules/@duckduckgo/content-scope-scripts/build/android/contentScope.js
@@ -1322,7 +1322,7 @@
   function isGloballyDisabled(args) {
     return args.site.allowlisted || args.site.isBroken;
   }
-  var platformSpecificFeatures = ["navigatorInterface", "duckAiListener", "windowsPermissionUsage", "messageBridge", "favicon"];
+  var platformSpecificFeatures = ["navigatorInterface", "windowsPermissionUsage", "messageBridge", "favicon"];
   function isPlatformSpecificFeature(featureName) {
     return platformSpecificFeatures.includes(featureName);
   }
@@ -1363,7 +1363,6 @@
       "messageBridge",
       "duckPlayer",
       "duckPlayerNative",
-      "duckAiListener",
       "duckAiDataClearing",
       "harmfulApis",
       "webCompat",
@@ -1378,7 +1377,7 @@
     ]
   );
   var platformSupport = {
-    apple: ["webCompat", "duckPlayerNative", ...baseFeatures, "duckAiListener", "duckAiDataClearing", "pageContext"],
+    apple: ["webCompat", "duckPlayerNative", ...baseFeatures, "duckAiDataClearing", "pageContext"],
     "apple-isolated": [
       "duckPlayer",
       "duckPlayerNative",
@@ -1414,7 +1413,6 @@
       "messageBridge",
       "webCompat",
       "pageContext",
-      "duckAiListener",
       "duckAiDataClearing"
     ],
     firefox: ["cookie", ...baseFeatures, "clickToLoad"],
