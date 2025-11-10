ad2d52608ac216d66ec5aeb816d7265e847d2810
Cris Barreiro <cbarreiro@duckduckgo.com>
2025-11-10 16:35:10 +0100

    Fix tests

--- app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
+++ app/src/androidTest/java/com/duckduckgo/app/browser/BrowserTabViewModelTest.kt
@@ -8041,7 +8041,7 @@ class BrowserTabViewModelTest {
     class FakeCustomHeadersProvider(
         var headers: Map<String, String>,
     ) : CustomHeadersProvider {
-        override fun getCustomHeaders(url: String): Map<String, String> = headers
+        override suspend fun getCustomHeaders(url: String): Map<String, String> = headers
     }
 
     class FakeContentScopeScriptsSubscriptionEventPlugin(
