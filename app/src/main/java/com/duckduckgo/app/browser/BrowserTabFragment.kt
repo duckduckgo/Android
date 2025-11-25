934fa492eaec43145ac1b847255a679068465c2c
0nko <oruttkay@duckduckgo.com>
2025-11-25 15:02:35 +0100

    Optimize omnibar initialization by dynamically adding the one that's needed

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -2003,9 +2003,8 @@ class BrowserTabFragment :
         }
     }
 
-    fun getBottomNavigationBar(): BrowserNavigationBarView {
-        return binding.navigationBar
-    }
+    val navigationBar: BrowserNavigationBarView
+        get() = binding.navigationBar
 
     private fun processCommand(it: Command?) {
         if (it is NavigationCommand) {
