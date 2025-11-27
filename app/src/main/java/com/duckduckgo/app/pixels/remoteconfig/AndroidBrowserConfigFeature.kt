5841702291fbc422e16ef73d0ec682782622f70b
Gerard Paligot <gerard.paligot@gmail.com>
2025-11-27 10:14:51 +0100

    fix: shortUrlDefault kdoc (format and content)

--- app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
+++ app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
@@ -222,11 +222,11 @@ interface AndroidBrowserConfigFeature {
     @Toggle.InternalAlwaysEnabled
     fun newCustomTab(): Toggle
 
-    /*
+    /**
      * Controls default URL display for new users only.
      * @return `true` when the remote config has the global "shorterUrlDefault" androidBrowserConfig
      * sub-feature flag enabled
-     * If the remote feature is not present defaults to `false`
+     * If the remote feature is not present defaults to `true`
      */
     @Toggle.DefaultValue(DefaultFeatureValue.TRUE)
     fun shorterUrlDefault(): Toggle
