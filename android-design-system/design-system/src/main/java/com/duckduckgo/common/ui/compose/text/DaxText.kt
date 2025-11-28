0469850478f6d7d69e6edc5f76f9bdaff079ef35
Domen Lanisnik <dlanisnik@duckduckgo.com>
2025-11-28 08:05:29 +0100

    Finalize component

--- android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/compose/text/DaxText.kt
+++ android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/compose/text/DaxText.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewLightDark
