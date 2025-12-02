4686be8d22e1b091d3e8839932106dec71c10a63
Ana Capatina <anikiki@gmail.com>
2025-12-02 17:27:50 +0000

    Add remoteMessageModalSurface toggle for remote config feature.

--- app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
+++ app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
@@ -229,4 +229,12 @@ interface AndroidBrowserConfigFeature {
      */
     @Toggle.DefaultValue(DefaultFeatureValue.INTERNAL)
     fun showInputScreenOnboarding(): Toggle
+
+    /**
+     * @return `true` when the remote config has the global "remoteMessageModalSurface" androidBrowserConfig
+     * sub-feature flag enabled
+     * If the remote feature is not present defaults to `false`
+     */
+    @Toggle.DefaultValue(DefaultFeatureValue.FALSE)
+    fun remoteMessageModalSurface(): Toggle
 }
