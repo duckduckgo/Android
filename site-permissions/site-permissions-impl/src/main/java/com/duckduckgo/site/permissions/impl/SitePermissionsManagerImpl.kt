92e6c9052a9d8ef966896099c5f091fea17f8fe0
joshliebe <joshliebe@gmail.com>
2025-12-09 22:56:21 +0000

    Update mic site permissions check

--- site-permissions/site-permissions-impl/src/main/java/com/duckduckgo/site/permissions/impl/SitePermissionsManagerImpl.kt
+++ site-permissions/site-permissions-impl/src/main/java/com/duckduckgo/site/permissions/impl/SitePermissionsManagerImpl.kt
@@ -16,9 +16,12 @@
 
 package com.duckduckgo.site.permissions.impl
 
+import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.webkit.PermissionRequest
+import androidx.core.content.ContextCompat
 import androidx.core.location.LocationManagerCompat
 import com.duckduckgo.common.utils.DispatcherProvider
 import com.duckduckgo.di.scopes.AppScope
@@ -26,6 +29,7 @@ import com.duckduckgo.site.permissions.api.SitePermissionsManager
 import com.duckduckgo.site.permissions.api.SitePermissionsManager.LocationPermissionRequest
 import com.duckduckgo.site.permissions.api.SitePermissionsManager.SitePermissionQueryResponse
 import com.duckduckgo.site.permissions.api.SitePermissionsManager.SitePermissions
+import com.duckduckgo.site.permissions.impl.feature.MicrophoneSitePermissionsFeature
 import com.squareup.anvil.annotations.ContributesBinding
 import kotlinx.coroutines.withContext
 import logcat.logcat
@@ -38,6 +42,8 @@ class SitePermissionsManagerImpl @Inject constructor(
     private val locationManager: LocationManager,
     private val sitePermissionsRepository: SitePermissionsRepository,
     private val dispatcherProvider: DispatcherProvider,
+    private val context: Context,
+    private val microphoneSitePermissionsFeature: MicrophoneSitePermissionsFeature,
 ) : SitePermissionsManager {
 
     private suspend fun getSitePermissionsGranted(
@@ -63,7 +69,19 @@ class SitePermissionsManagerImpl @Inject constructor(
 
         logcat { "Permissions: sitePermissionsAllowedToAsk in $url ${sitePermissionsAllowedToAsk.asList()}" }
 
-        val sitePermissionsGranted = getSitePermissionsGranted(url, tabId, sitePermissionsAllowedToAsk)
+        val sitePermissionsGranted = if (microphoneSitePermissionsFeature.self().isEnabled()) {
+            getSitePermissionsGranted(url, tabId, sitePermissionsAllowedToAsk).filter { permission ->
+                if (permission == PermissionRequest.RESOURCE_AUDIO_CAPTURE) {
+                    ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(context, Manifest.permission.MODIFY_AUDIO_SETTINGS) == PackageManager.PERMISSION_GRANTED
+                } else {
+                    true
+                }
+            }.toTypedArray()
+        } else {
+            getSitePermissionsGranted(url, tabId, sitePermissionsAllowedToAsk)
+        }
+
         if (sitePermissionsGranted.isNotEmpty()) {
             withContext(dispatcherProvider.main()) {
                 logcat { "Permissions: site permission granted" }
