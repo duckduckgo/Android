879d184f19b11e01d99d26de1c1daea016dc4a29
Gerard Paligot <gerard.paligot@gmail.com>
2025-12-15 12:38:59 +0100

    Allow explicit theme selection when system default matches current mode

--- android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/store/ThemingSharedPreferences.kt
+++ android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/store/ThemingSharedPreferences.kt
@@ -69,10 +69,9 @@ class ThemingSharedPreferences @Inject constructor(
             isInNightMode: Boolean,
         ) =
             when (value) {
+                THEME_SYSTEM_DEFAULT -> DuckDuckGoTheme.SYSTEM_DEFAULT
                 THEME_LIGHT -> DuckDuckGoTheme.LIGHT
-
                 THEME_DARK -> DuckDuckGoTheme.DARK
-
                 else -> if (isInNightMode) {
                     DuckDuckGoTheme.DARK
                 } else {
