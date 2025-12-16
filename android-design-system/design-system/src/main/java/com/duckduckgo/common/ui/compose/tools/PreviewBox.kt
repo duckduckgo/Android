9294106e963a54918c23be8a0c01c086b1833e65
Gérard Paligot <gerard.paligot@gmail.com>
2025-12-16 11:38:39 +0100

    Add Compose ColorPalette (#7346)
    Task/Issue URL:
    https://app.asana.com/1/137249556945/project/1212015278241917/task/1211660386444403?focus=true
    
    ### Description
    
    We integrated color palette and semantic colors in the compose theme and
    display them in the ADS color tab screen.
    
    Very open for feedback about the new color data class!
    
    ### Steps to test this PR
    
    _Checking ADS colors_
    - [x] Open the app and go to "Android Design System Preview" screen from
    Settings
    - [x] Open color palette tab
    - [x] Check the view and compose colors are the same
    - [x] Switch the theme in dark mode and check view and compose colors
    again
    
    ### UI changes
    | Light  | Dark |
    | ------ | ----- |
    <img width="238" height="1008" alt="image"
    src="https://github.com/user-attachments/assets/ff3b9d7b-80f2-428d-9009-c490785e3bf8"
    /> | <img width="238" height="1008" alt="image"
    src="https://github.com/user-attachments/assets/b4b03a48-4b25-4cc1-bab0-c8e855297f98"
    />
    
    Co-authored-by: Mike Scamell <mscamell@duckduckgo.com>

--- android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/compose/tools/PreviewBox.kt
+++ android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/compose/tools/PreviewBox.kt
@@ -18,22 +18,37 @@ package com.duckduckgo.common.ui.compose.tools
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.duckduckgo.common.ui.compose.theme.DuckDuckGoTheme
 
 @Composable
-internal fun PreviewBox(
-    backgroundColor: Color = Color.White,
-    content: @Composable () -> Unit,
+fun PreviewBoxInverted(
+    modifier: Modifier = Modifier,
+    color: @Composable () -> Color = { DuckDuckGoTheme.colors.backgrounds.backgroundInverted },
+    content: @Composable BoxScope.() -> Unit,
+) = PreviewBox(
+    modifier = modifier,
+    color = color,
+    content = content,
+)
+
+@Composable
+fun PreviewBox(
+    modifier: Modifier = Modifier,
+    color: @Composable () -> Color = { DuckDuckGoTheme.colors.backgrounds.background },
+    content: @Composable BoxScope.() -> Unit,
 ) {
-    Box(
-        modifier = Modifier
-            .background(backgroundColor)
-            .padding(16.dp),
-    ) {
-        content()
+    DuckDuckGoTheme {
+        Box(
+            modifier = modifier
+                .background(color())
+                .padding(all = 16.dp),
+            content = content,
+        )
     }
 }
