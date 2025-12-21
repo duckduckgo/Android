ad55df9b0e24d5b74f61ce320a1ef3cd81d26b6a
Dax Mobile <44842493+daxmobile@users.noreply.github.com>
2025-12-21 04:35:24 -0500

    Update content scope scripts to version 12.17.0 (#7416)
    - Automated content scope scripts dependency update
    
    This PR updates the content scope scripts dependency to the latest
    available version and copies the necessary files.
    
    Tests will only run if something has changed in the
    `node_modules/@duckduckgo/content-scope-scripts` folder.
    
    If only the package version has changed, there is no need to run the
    tests.
    
    If tests have failed, see
    https://app.asana.com/0/1202561462274611/1203986899650836/f for further
    information on what to do next.
    
    _`content-scope-scripts` folder update_
    - [ ] All tests must pass
    - [ ] Privacy tests must pass
    
    _Only `content-scope-scripts` package update_
    - [ ] All tests must pass
    - [ ] Privacy tests do not need to run
    
    Co-authored-by: daxmobile <daxmobile@users.noreply.github.com>

--- node_modules/@duckduckgo/content-scope-scripts/build/android/contentScope.js
+++ node_modules/@duckduckgo/content-scope-scripts/build/android/contentScope.js
@@ -1290,7 +1290,12 @@
     });
     output.featureSettings = parseFeatureSettings(data, enabledFeatures);
     output.bundledConfig = data;
+    output.messagingContextName = output.messagingContextName || "contentScopeScripts";
     return output;
+  }
+  function getLoadArgs(processedConfig) {
+    const { platform, site, bundledConfig, messagingConfig, messageSecret: messageSecret2, messagingContextName, currentCohorts } = processedConfig;
+    return { platform, site, bundledConfig, messagingConfig, messageSecret: messageSecret2, messagingContextName, currentCohorts };
   }
   function computeEnabledFeatures(data, topLevelHostname, platformVersion, platformSpecificFeatures2 = []) {
     const remoteFeatureNames = Object.keys(data.features);
@@ -1379,7 +1384,7 @@
     ]
   );
   var platformSupport = {
-    apple: ["webCompat", "duckPlayerNative", ...baseFeatures, "webInterferenceDetection", "duckAiDataClearing", "pageContext"],
+    apple: ["webCompat", "duckPlayerNative", ...baseFeatures, "webInterferenceDetection", "pageContext"],
     "apple-isolated": [
       "duckPlayer",
       "duckPlayerNative",
@@ -1390,6 +1395,7 @@
       "messageBridge",
       "favicon"
     ],
+    "apple-ai-clear": ["duckAiDataClearing"],
     android: [...baseFeatures, "webCompat", "webInterferenceDetection", "breakageReporting", "duckPlayer", "messageBridge"],
     "android-broker-protection": ["brokerProtection"],
     "android-autofill-import": ["autofillImport"],
@@ -4562,7 +4568,8 @@
        *   assets?: import('./content-feature.js').AssetConfig | undefined,
        *   site: import('./content-feature.js').Site,
        *   messagingConfig?: import('@duckduckgo/messaging').MessagingConfig,
-       *   currentCohorts?: [{feature: string, cohort: string, subfeature: string}],
+       *   messagingContextName: string,
+       *   currentCohorts?: Array<{feature: string, cohort: string, subfeature: string}>,
        * } | null}
        */
       __privateAdd(this, _args);
@@ -5057,9 +5064,9 @@
      * @return {MessagingContext}
      */
     _createMessagingContext() {
-      const contextName = this.injectName === "apple-isolated" ? "contentScopeScriptsIsolated" : "contentScopeScripts";
+      if (!this.args) throw new Error("messaging requires args to be set");
       return new MessagingContext({
-        context: contextName,
+        context: this.args.messagingContextName,
         env: this.isDebug ? "development" : "production",
         featureName: this.name
       });
@@ -10387,13 +10394,7 @@
       target: globalThis,
       debug: processedConfig.debug
     });
-    load({
-      platform: processedConfig.platform,
-      site: processedConfig.site,
-      bundledConfig: processedConfig.bundledConfig,
-      messagingConfig: processedConfig.messagingConfig,
-      messageSecret: processedConfig.messageSecret
-    });
+    load(getLoadArgs(processedConfig));
     init(processedConfig);
   }
   initCode();
