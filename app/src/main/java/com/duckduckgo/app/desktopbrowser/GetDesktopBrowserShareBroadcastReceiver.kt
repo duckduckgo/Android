57905aaab21414a812741c3bb572556f52c5cc4c
catalinradoiu <radoiu_catalin1996@yahoo.com>
2026-02-17 21:06:31 +0200

    Create GetDesktopBrowserShareEventHandler that will be used in the new GetDesktopBrowserShareBroadcastReceiver, to notify that the link was shared

---
+++ app/src/main/java/com/duckduckgo/app/desktopbrowser/GetDesktopBrowserShareBroadcastReceiver.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2026 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.desktopbrowser
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.duckduckgo.anvil.annotations.InjectWith
+import com.duckduckgo.app.di.AppCoroutineScope
+import com.duckduckgo.app.settings.db.SettingsDataStore
+import com.duckduckgo.common.utils.DispatcherProvider
+import com.duckduckgo.di.scopes.ReceiverScope
+import dagger.android.AndroidInjection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@InjectWith(ReceiverScope::class)
+class GetDesktopBrowserShareBroadcastReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var settingsDataStore: SettingsDataStore
+
+    @Inject
+    lateinit var sharedEvent: GetDesktopBrowserShareEventHandler
+
+    @Inject
+    lateinit var dispatcherProvider: DispatcherProvider
+
+    @Inject
+    @AppCoroutineScope
+    lateinit var coroutineScope: CoroutineScope
+
+    override fun onReceive(
+        context: Context,
+        intent: Intent,
+    ) {
+        AndroidInjection.inject(this, context)
+
+        val pendingResult = goAsync()
+        coroutineScope.launch(dispatcherProvider.io()) {
+            settingsDataStore.getDesktopBrowserSettingDismissed = true
+            sharedEvent.onLinkShared()
+            pendingResult.finish()
+        }
+    }
+}
