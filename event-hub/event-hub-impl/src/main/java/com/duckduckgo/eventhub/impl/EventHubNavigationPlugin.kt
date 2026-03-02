0fc419e9bf3a9f601d2a064f1733cc41788e6adb
Ben Moon <software@guiltydolphin.com>
2026-03-02 18:25:09 +0000

    Fix EventHubPixeManager naming

--- event-hub/event-hub-impl/src/main/java/com/duckduckgo/eventhub/impl/EventHubNavigationPlugin.kt
+++ event-hub/event-hub-impl/src/main/java/com/duckduckgo/eventhub/impl/EventHubNavigationPlugin.kt
@@ -26,7 +26,7 @@ import javax.inject.Inject
 
 @ContributesMultibinding(AppScope::class)
 class EventHubNavigationPlugin @Inject constructor(
-    private val pixelManager: RealEventHubPixelManager,
+    private val pixelManager: EventHubPixelManager,
 ) : JsInjectorPlugin {
 
     override fun onPageStarted(
