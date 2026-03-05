5722c0996c76c391e0d81837f1506486e257f03e
David González <malmstein@gmail.com>
2026-03-05 15:48:31 +0100

    we don't show a permission message for duck.ai or SERP

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -1501,7 +1501,10 @@ class BrowserTabFragment :
         }
     }
 
-    private fun renderBrowserMenu(viewState: BrowserViewState, omnibarViewMode: Omnibar.ViewMode = omnibar.viewMode) {
+    private fun renderBrowserMenu(
+        viewState: BrowserViewState,
+        omnibarViewMode: Omnibar.ViewMode = omnibar.viewMode
+    ) {
         val browseMenuState = browserMenuViewStateFactory.create(
             omnibarViewMode = omnibarViewMode,
             viewState = viewState,
@@ -2767,10 +2770,12 @@ class BrowserTabFragment :
                 omnibar.setViewMode(Browser(it.url))
                 nativeInputManager.hideNativeInput(binding.rootView, omnibar)
             }
+
             is Command.ShowDuckAIContextualMode -> showDuckChatContextualSheet(it.tabId)
             is Command.StartAddressBarTrackersAnimation -> {
                 omnibar.startTrackersAnimation(it.trackerEntities)
             }
+
             is Command.PageContextReceived -> {
                 sharedContextualViewModel.onPageContextReceived(it.tabId, it.pageContext)
             }
@@ -3279,6 +3284,7 @@ class BrowserTabFragment :
                                             showToast(R.string.unableToOpenLink)
                                         }
                                     }
+
                                     LaunchInExternalAppOptions.CLOSE_TAB -> {
                                         launch {
                                             viewModel.closeCurrentTab()
