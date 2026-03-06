e4cce1a8783903c371bbe4254fec98fa5403fbba
Mike Scamell <mscamell@duckduckgo.com>
2026-03-06 22:13:18 +0000

    spotless

--- app/src/main/java/com/duckduckgo/app/onboarding/ui/page/BrandDesignUpdateWelcomePage.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/ui/page/BrandDesignUpdateWelcomePage.kt
@@ -22,7 +22,6 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.animation.PathInterpolator
