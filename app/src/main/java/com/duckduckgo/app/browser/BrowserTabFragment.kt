7225943171c23c47f51e072eb7428bd2d6e5dcb2
nalcalag <nalcalag@gmail.com>
2026-03-10 11:34:36 +0000

    clean up

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabFragment.kt
@@ -5134,12 +5134,6 @@ class BrowserTabFragment :
             renderIfChanged(viewState, lastSeenCtaViewState) {
                 lastSeenCtaViewState = viewState
                 when {
-                    viewState.cta is DaxBubbleCta -> {
-                        hideNewTab()
-                        showCta(viewState.cta)
-                        this@BrowserTabFragment.hideKeyboard()
-                    }
-
                     viewState.cta != null -> {
                         hideNewTab()
                         showCta(viewState.cta)
