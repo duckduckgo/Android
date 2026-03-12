09755943c79820d7224c03a8de6e9661b860884f
Mike Scamell <mscamell@duckduckgo.com>
2026-03-12 12:31:00 +0000

    edge-to-edge: Migrate CrashANRsInternalSettingsActivity

--- anrs/anrs-internal/src/main/java/com/duckduckgo/app/anr/internal/feature/CrashANRsInternalSettingsActivity.kt
+++ anrs/anrs-internal/src/main/java/com/duckduckgo/app/anr/internal/feature/CrashANRsInternalSettingsActivity.kt
@@ -17,6 +17,10 @@
 package com.duckduckgo.app.anr.internal.feature
 
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewGroupCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.duckduckgo.anvil.annotations.ContributeToActivityStarter
 import com.duckduckgo.anvil.annotations.InjectWith
 import com.duckduckgo.app.anr.internal.databinding.ActivityCrashAnrInternalSettingsBinding
@@ -25,6 +29,7 @@ import com.duckduckgo.common.ui.DuckDuckGoActivity
 import com.duckduckgo.common.ui.viewbinding.viewBinding
 import com.duckduckgo.common.utils.plugins.PluginPoint
 import com.duckduckgo.di.scopes.ActivityScope
+import com.duckduckgo.edgetoedge.api.EdgeToEdge
 import com.duckduckgo.navigation.api.GlobalActivityStarter
 import javax.inject.Inject
 
@@ -35,14 +40,32 @@ class CrashANRsInternalSettingsActivity : DuckDuckGoActivity() {
     @Inject
     lateinit var settings: PluginPoint<CrashANRsSettingPlugin>
 
+    @Inject
+    lateinit var edgeToEdge: EdgeToEdge
+
     private val binding: ActivityCrashAnrInternalSettingsBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        edgeToEdge.enableIfToggled(this)
         setContentView(binding.root)
         setupToolbar(binding.toolbar)
+        setupEdgeToEdge()
 
         setupUiElementState()
+    }
+
+    private fun setupEdgeToEdge() {
+        if (!edgeToEdge.isEnabled()) return
+        ViewGroupCompat.installCompatInsetsDispatch(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
+            val insets = windowInsets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(),
+            )
+            binding.appBar.updatePadding(top = insets.top)
+            binding.scrollView.updatePadding(bottom = insets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     private fun setupUiElementState() {
