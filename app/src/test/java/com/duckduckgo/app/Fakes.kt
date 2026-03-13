a17cb965b8f3049619a589502ad156c32a5f0b49
Gerard Paligot <gerard.paligot@gmail.com>
2026-03-13 18:06:36 +0100

    Display blue dot in the Download menu item

--- app/src/test/java/com/duckduckgo/app/Fakes.kt
+++ app/src/test/java/com/duckduckgo/app/Fakes.kt
@@ -232,6 +232,12 @@ class FakeSettingsDataStore :
             store["showTrackersCountInAddressBar"] = value
         }
 
+    override var hasNewDownload: Boolean
+        get() = store["hasNewDownload"] as Boolean? ?: false
+        set(value) {
+            store["hasNewDownload"] = value
+        }
+
     override var singleTabFireDialogShownCount: Int
         get() = store["singleTabFireDialogShownCount"] as Int? ?: 0
         set(value) {
