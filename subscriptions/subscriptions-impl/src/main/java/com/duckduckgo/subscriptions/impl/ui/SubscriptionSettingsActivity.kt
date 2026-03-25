bbc1c82aeff1ad009084c6a4c7f4ae6c2b0ff830
Cristian Monforte <cristian@duckduckgo.com>
2026-03-25 18:16:10 +0100

    Remove switch plans options in subscription settings (replaced by view all plans)

--- subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/ui/SubscriptionSettingsActivity.kt
+++ subscriptions/subscriptions-impl/src/main/java/com/duckduckgo/subscriptions/impl/ui/SubscriptionSettingsActivity.kt
@@ -54,17 +54,14 @@ import com.duckduckgo.subscriptions.impl.SubscriptionsConstants.FAQS_URL
 import com.duckduckgo.subscriptions.impl.databinding.ActivitySubscriptionSettingsBinding
 import com.duckduckgo.subscriptions.impl.internal.SubscriptionsUrlProvider
 import com.duckduckgo.subscriptions.impl.pixels.SubscriptionPixelSender
-import com.duckduckgo.subscriptions.impl.switch_plan.SwitchPlanBottomSheetDialogFactory
 import com.duckduckgo.subscriptions.impl.ui.ChangePlanActivity.Companion.ChangePlanScreenWithEmptyParams
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.Command
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.Command.FinishSignOut
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.Command.GoToActivationScreen
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.Command.GoToEditEmailScreen
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.Command.GoToPortal
-import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.Command.ShowSwitchPlanDialog
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.SubscriptionDuration.Monthly
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.SubscriptionDuration.Yearly
-import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.SwitchPlanType
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionSettingsViewModel.ViewState
 import com.duckduckgo.subscriptions.impl.ui.SubscriptionsWebViewActivityWithParams.ToolbarConfig.CustomTitle
 import kotlinx.coroutines.flow.filterIsInstance
@@ -84,9 +81,6 @@ class SubscriptionSettingsActivity : DuckDuckGoActivity() {
 
     @Inject
     lateinit var subscriptionsUrlProvider: SubscriptionsUrlProvider
-
-    @Inject
-    lateinit var switchPlanDialogFactory: SwitchPlanBottomSheetDialogFactory
 
     private val viewModel: SubscriptionSettingsViewModel by bindViewModel()
     private val binding: ActivitySubscriptionSettingsBinding by viewBinding()
@@ -181,7 +175,6 @@ class SubscriptionSettingsActivity : DuckDuckGoActivity() {
         if (viewState.status in listOf(INACTIVE, EXPIRED)) {
             binding.viewPlans.isVisible = true
             binding.changePlan.isVisible = false
-            binding.switchPlan.isVisible = false
             binding.subscriptionActiveStatusContainer.isVisible = false
             binding.subscriptionExpiredStatusContainer.isVisible = true
             binding.subscriptionExpiredStatusText.text = getString(string.subscriptionsExpiredData, viewState.date)
@@ -214,21 +207,6 @@ class SubscriptionSettingsActivity : DuckDuckGoActivity() {
                         binding.tierName.gone()
                     }
                 }
-            }
-
-            // Show switch plan option if available (Android only, Plus tier only)
-            if (viewState.switchPlanAvailable && viewState.platform.lowercase() == "google" && viewState.subscriptionTier == PLUS) {
-                binding.switchPlan.show()
-                val switchText = when (viewState.duration) {
-                    Monthly -> getString(string.subscriptionSettingSwitchUpgradeDynamic, viewState.savingsPercentage.toString())
-                    Yearly -> getString(string.subscriptionSettingSwitchDowngrade)
-                }
-                binding.switchPlan.setPrimaryText(switchText)
-                binding.switchPlan.setClickListener {
-                    viewModel.onSwitchPlanClicked(viewState.duration)
-                }
-            } else {
-                binding.switchPlan.gone()
             }
 
             // Free Trial active
@@ -351,21 +329,7 @@ class SubscriptionSettingsActivity : DuckDuckGoActivity() {
                     ),
                 )
             }
-
-            is ShowSwitchPlanDialog -> showSwitchPlanDialog(command.switchType)
         }
-    }
-
-    private fun showSwitchPlanDialog(switchType: SwitchPlanType) {
-        val dialog = switchPlanDialogFactory.create(
-            context = this,
-            lifecycleOwner = this,
-            switchType = switchType,
-            onSwitchSuccess = {
-                viewModel.onSwitchPlanSuccess()
-            },
-        )
-        dialog.show()
     }
 
     private fun goToFaqs() {
