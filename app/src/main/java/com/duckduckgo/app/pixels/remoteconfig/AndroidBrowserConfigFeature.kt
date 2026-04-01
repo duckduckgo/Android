89401b9ad94edc91937ab4fbfd97a1c258fbefba
David González <malmstein@gmail.com>
2026-04-01 21:46:26 +0200

    enable this for all users

--- app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
+++ app/src/main/java/com/duckduckgo/app/pixels/remoteconfig/AndroidBrowserConfigFeature.kt
@@ -132,7 +132,7 @@ interface AndroidBrowserConfigFeature {
      * sub-feature flag enabled
      * If the remote feature is not present defaults to `false`
      */
-    @Toggle.DefaultValue(DefaultFeatureValue.FALSE)
+    @Toggle.DefaultValue(DefaultFeatureValue.TRUE)
     fun showNTPAfterIdleReturn(): Toggle
 
     @Toggle.DefaultValue(DefaultFeatureValue.TRUE)
