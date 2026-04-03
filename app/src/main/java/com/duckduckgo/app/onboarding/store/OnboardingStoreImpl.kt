83ac6f8955622f5875bf9181abb3b14c8be71fcf
Lukasz Macionczyk <lukasz.macionczyk@gmail.com>
2026-04-03 15:13:43 +0200

    Update prompt suggestion

--- app/src/main/java/com/duckduckgo/app/onboarding/store/OnboardingStoreImpl.kt
+++ app/src/main/java/com/duckduckgo/app/onboarding/store/OnboardingStoreImpl.kt
@@ -83,9 +83,9 @@ class OnboardingStoreImpl @Inject constructor(
                 link = context.getString(R.string.preOnboardingInputModeDemoChatSuggestion2),
             ),
             DaxDialogIntroOption(
-                optionText = context.getString(R.string.onboardingSearchDaxDialogOption4),
-                iconRes = drawable.ic_wand_16,
-                link = context.getString(R.string.onboardingSearchDaxDialogOption4),
+                optionText = context.getString(R.string.preOnboardingInputModeDemoChatSuggestion3),
+                iconRes = drawable.ic_ai_chat_16,
+                link = context.getString(R.string.preOnboardingInputModeDemoChatSuggestion3),
             ),
         )
     }
