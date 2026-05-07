a4b572ca36d76b528fb95d7a1aea48e947224034
Cris Barreiro <cbarreiro@duckduckgo.com>
2026-05-07 17:31:40 +0200

    Add support for RC

---
+++ ad-blocking/ad-blocking-impl/src/main/java/com/duckduckgo/adblocking/impl/AdBlockingExtensionFeature.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2026 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.adblocking.impl
+
+import com.duckduckgo.anvil.annotations.ContributesRemoteFeature
+import com.duckduckgo.app.browser.Domain
+import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.feature.toggles.api.Toggle
+import com.duckduckgo.feature.toggles.api.Toggle.DefaultFeatureValue
+
+@ContributesRemoteFeature(
+    scope = AppScope::class,
+    featureName = "adBlockingExtension",
+)
+interface AdBlockingExtensionFeature {
+    @Toggle.DefaultValue(DefaultFeatureValue.FALSE)
+    fun self(): Toggle
+
+    @Toggle.DefaultValue(DefaultFeatureValue.FALSE)
+    fun enabledByDefault(): Toggle
+}
+
+data class AdBlockingScriptletData(
+    val version: String? = null,
+    val scriptlets: Map<String, ScriptletEntry>? = null,
+    val domains: List<Domain>? = null,
+)
+
+data class ScriptletEntry(
+    val url: String,
+    val signature: String,
+)
