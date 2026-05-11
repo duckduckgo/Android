82257163fc2ef04a1713f7e50e33dda2a121b2eb
Cris Barreiro <cbarreiro@duckduckgo.com>
2026-05-11 16:49:52 +0200

    Add store classes

---
+++ ad-blocking/ad-blocking-impl/src/main/java/com/duckduckgo/adblocking/impl/store/AdBlockingExtensionDatabase.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.adblocking.impl.store
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+
+@Database(
+    exportSchema = true,
+    version = 1,
+    entities = [ScriptletEntity::class],
+)
+abstract class AdBlockingExtensionDatabase : RoomDatabase() {
+    abstract fun adBlockingExtensionDao(): AdBlockingExtensionDao
+}
+
+val AD_BLOCKING_EXTENSION_MIGRATIONS = emptyArray<Migration>()
