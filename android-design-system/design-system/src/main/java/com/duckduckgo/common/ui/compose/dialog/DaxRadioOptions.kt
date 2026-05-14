e75bdce7bd25ce6907b13246ae7770a6022978f9
0nko <oruttkay@duckduckgo.com>
2026-05-14 10:00:04 +0200

    Optimize imports

--- android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/compose/dialog/DaxRadioOptions.kt
+++ android-design-system/design-system/src/main/java/com/duckduckgo/common/ui/compose/dialog/DaxRadioOptions.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
