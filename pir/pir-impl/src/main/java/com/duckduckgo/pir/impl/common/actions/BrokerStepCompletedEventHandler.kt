490a00a7114867d0c87a97f8415aa41e7a0f674f
Domen Lanisnik <dlanisnik@duckduckgo.com>
2026-05-19 11:15:30 +0200

    Clear emailExtractedData on step completion

--- pir/pir-impl/src/main/java/com/duckduckgo/pir/impl/common/actions/BrokerStepCompletedEventHandler.kt
+++ pir/pir-impl/src/main/java/com/duckduckgo/pir/impl/common/actions/BrokerStepCompletedEventHandler.kt
@@ -90,6 +90,7 @@ class BrokerStepCompletedEventHandler @Inject constructor(
                 currentBrokerStepIndex = state.currentBrokerStepIndex + 1,
                 actionRetryCount = 0,
                 generatedEmailData = null,
+                emailExtractedData = emptyMap(),
                 stageStatus = PirStageStatus(
                     currentStage = PirStage.VALIDATE,
                     stageStartMs = currentTimeProvider.currentTimeMillis(),
