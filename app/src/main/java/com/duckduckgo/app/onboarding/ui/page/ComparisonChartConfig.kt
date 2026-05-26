1cc511b5c9b358e413583f2edc9f26a66e86e2d7
Łukasz Paczos <lpaczos@duckduckgo.com>
2026-05-26 14:58:13 +0200

    Onboarding: introduce ComparisonChartConfig data class

---
+++ app/src/main/java/com/duckduckgo/app/onboarding/ui/page/ComparisonChartConfig.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.app.onboarding.ui.page
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.duckduckgo.app.browser.R
+import com.duckduckgo.mobile.android.R as CommonR
+
+data class ComparisonChartConfig(
+    @StringRes val titleRes: Int,
+    @StringRes val primaryCtaTextRes: Int,
+    @DrawableRes val headerLeftIconRes: Int,
+    @StringRes val headerLeftLabelRes: Int?,
+    val rows: List<Row>,
+) {
+    data class Row(
+        @DrawableRes val iconRes: Int,
+        @StringRes val textRes: Int,
+    )
+
+    companion object {
+        val Default = ComparisonChartConfig(
+            titleRes = R.string.preOnboardingDaxDialog2Title,
+            primaryCtaTextRes = R.string.preOnboardingDaxDialog2Button,
+            headerLeftIconRes = CommonR.drawable.ic_chrome_comparison_chart,
+            headerLeftLabelRes = null,
+            rows = listOf(
+                Row(CommonR.drawable.ic_vpn_color_24_rebrand, R.string.preOnboardingComparisonChartItem1),
+                Row(CommonR.drawable.ic_duck_ai_color_24_rebrand, R.string.preOnboardingComparisonChartDuckAi),
+                Row(CommonR.drawable.ic_shield_color_24_rebrand, R.string.preOnboardingComparisonChartItem2),
+                Row(CommonR.drawable.ic_cookies_color_24_rebrand, R.string.preOnboardingComparisonChartItem3),
+                Row(CommonR.drawable.ic_profile_blocker_color_24_rebrand, R.string.preOnboardingComparisonChartItem4),
+            ),
+        )
+    }
+}
