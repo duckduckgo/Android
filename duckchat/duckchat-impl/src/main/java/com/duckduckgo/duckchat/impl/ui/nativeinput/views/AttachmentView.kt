1c49769fdccf83c098a1625c7bef526887784ad4
joshliebe <joshliebe@gmail.com>
2026-09-08 22:20:15 +0100

    Scroll to end when adding a new selection

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/nativeinput/views/AttachmentView.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/nativeinput/views/AttachmentView.kt
@@ -86,6 +86,7 @@ class AttachmentView(
     private var fileAttachmentsContainer: FileAttachmentsContainerView? = null
     private var pageContextContainer: PageContextAttachmentView? = null
     private var textSelectionsContainer: TextSelectionAttachmentsContainerView? = null
+    private var attachmentsScroll: HorizontalScrollView? = null
     private var limitErrorView: TextView? = null
 
     init {
@@ -162,6 +163,7 @@ class AttachmentView(
             isHorizontalScrollBarEnabled = false
         }
         parent.addView(scroll)
+        attachmentsScroll = scroll
 
         val row = LinearLayout(context).apply {
             orientation = LinearLayout.HORIZONTAL
@@ -249,7 +251,9 @@ class AttachmentView(
     private fun syncTextSelections(state: AttachmentViewModel.AttachmentState) {
         val view = textSelectionsContainer ?: return
         if (view.current() != state.textSelections) {
+            val isNewAttachment = state.textSelections.size > view.current().size
             view.render(state.textSelections)
+            if (isNewAttachment) attachmentsScroll?.post { attachmentsScroll?.fullScroll(FOCUS_RIGHT) }
         }
     }
 
