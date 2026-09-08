f695ba4709c722578a7a2d10a570f540619b4ba7
joshliebe <joshliebe@gmail.com>
2026-09-08 11:43:22 +0100

    Disable submission for selections without a prompt

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/nativeinput/views/NativeInputModeWidget.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/ui/nativeinput/views/NativeInputModeWidget.kt
@@ -2099,13 +2099,13 @@ class NativeInputModeWidget @JvmOverloads constructor(
     }
 
     override fun attachmentChanged(
-        hasAttachments: Boolean,
+        allowsBlankSubmit: Boolean,
         limitExceeded: Boolean,
         supportsUpload: Boolean,
     ) {
         val hadLimitError = attachmentLimitExceeded
         attachmentLimitExceeded = limitExceeded
-        this.hasAttachments = hasAttachments
+        this.hasAttachments = allowsBlankSubmit
         if (hadLimitError != attachmentLimitExceeded && !isStreaming) {
             floatingSubmitContainer?.visibility = if (attachmentLimitExceeded) GONE else VISIBLE
         }
