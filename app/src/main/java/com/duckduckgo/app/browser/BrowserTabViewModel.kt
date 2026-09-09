577a8eeb181684660d74b5c567ebffd0319e5b54
Karl Dimla <klmbdimla@gmail.com>
2026-09-09 10:42:56 +0200

    Add feature flag for nativeDuckAiSidebar

--- app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
+++ app/src/main/java/com/duckduckgo/app/browser/BrowserTabViewModel.kt
@@ -5836,6 +5836,21 @@ class BrowserTabViewModel @Inject constructor(
         }
     }
 
+    fun onDuckChatSidebarButtonPressed() {
+        if (duckAiFeatureState.nativeDuckAiSidebar.value) {
+            openDuckChatHistory()
+        } else {
+            openDuckChatSidebar()
+        }
+    }
+
+    fun openDuckChatSidebar() {
+        viewModelScope.launch {
+            val subscriptionEvent = duckChatJSHelper.onNativeAction(NativeAction.SIDEBAR)
+            _subscriptionEventDataChannel.send(subscriptionEvent)
+        }
+    }
+
     fun openDuckChatHistory() {
         if (currentBrowserViewState().showDuckChatHistoryOption) {
             command.value = Command.LaunchDuckChatHistory
