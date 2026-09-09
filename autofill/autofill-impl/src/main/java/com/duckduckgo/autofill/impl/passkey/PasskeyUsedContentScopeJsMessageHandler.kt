780fbcf1aa7ec8669b3ee168c1d833c75bf31301
Craig Russell <1336281+CDRussell@users.noreply.github.com>
2026-09-09 11:50:44 +0100

    Enable passkey support for everyone

---
+++ autofill/autofill-impl/src/main/java/com/duckduckgo/autofill/impl/passkey/PasskeyUsedContentScopeJsMessageHandler.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2026 DuckDuckGo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.duckduckgo.autofill.impl.passkey
+
+import com.duckduckgo.autofill.impl.passkey.PasskeyUsageResult.CreateFailed
+import com.duckduckgo.autofill.impl.passkey.PasskeyUsageResult.Created
+import com.duckduckgo.autofill.impl.passkey.PasskeyUsageResult.UseFailed
+import com.duckduckgo.autofill.impl.passkey.PasskeyUsageResult.Used
+import com.duckduckgo.contentscopescripts.api.ContentScopeJsMessageHandlersPlugin
+import com.duckduckgo.di.scopes.AppScope
+import com.duckduckgo.js.messaging.api.JsMessage
+import com.duckduckgo.js.messaging.api.JsMessageCallback
+import com.duckduckgo.js.messaging.api.JsMessageHandler
+import com.duckduckgo.js.messaging.api.JsMessaging
+import com.squareup.anvil.annotations.ContributesMultibinding
+import logcat.LogPriority.INFO
+import logcat.LogPriority.WARN
+import logcat.logcat
+import javax.inject.Inject
+
+@ContributesMultibinding(AppScope::class)
+class PasskeyUsedContentScopeJsMessageHandler @Inject constructor(
+    private val parser: PasskeyUsedMessageParser,
+    private val pixelSender: PasskeyUsagePixelSender,
+) : ContentScopeJsMessageHandlersPlugin {
+
+    override fun getJsMessageHandler(): JsMessageHandler = object : JsMessageHandler {
+        override fun process(
+            jsMessage: JsMessage,
+            jsMessaging: JsMessaging,
+            jsMessageCallback: JsMessageCallback?,
+        ) {
+            processResult(parser.parse(jsMessage))
+        }
+
+        override val allowedDomains: List<String> = emptyList()
+        override val featureName: String = PasskeyUsedMessageParser.FEATURE_NAME
+        override val methods: List<String> = PasskeyUsedMessageParser.METHODS
+    }
+
+    private fun processResult(result: PasskeyUsageResult?) {
+        when (result) {
+            is Used -> {
+                logcat(INFO) { "Autofill-passkey: passkey used" }
+                pixelSender.firePasskeyUsed()
+            }
+            is UseFailed -> {
+                logcat(INFO) { "Autofill-passkey: failure when using passkey: ${result.error.name}" }
+                pixelSender.fireUseFailed(result.error)
+            }
+            is Created -> {
+                logcat(INFO) { "Autofill-passkey: new passkey created" }
+                pixelSender.firePasskeyCreated()
+            }
+            is CreateFailed -> {
+                logcat(INFO) { "Autofill-passkey: failure when creating a passkey: ${result.error.name}" }
+                pixelSender.fireCreateFailed(result.error)
+            }
+            null -> {
+                logcat(WARN) { "Autofill-passkey: ignoring unrecognised passkey notification" }
+                pixelSender.fireUnknownEvent()
+            }
+        }
+    }
+}
